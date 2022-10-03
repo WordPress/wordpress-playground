f4dbc74c54430ed848400da319598d971d97616a
Adam Zieliński <adam@adamziel.com>
2022-10-03 11:09:02 +1100

    Further reduced state: No imports. Still crashing!

--- dist-web/web-worker.js
+++ dist-web/web-worker.js
@@ -1,30 +1,4 @@
 (() => {
-  var __create = Object.create;
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __getProtoOf = Object.getPrototypeOf;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-  }) : x)(function(x) {
-    if (typeof require !== "undefined")
-      return require.apply(this, arguments);
-    throw new Error('Dynamic require of "' + x + '" is not supported');
-  });
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-    mod
-  ));
-
   // src/shared/php-wrapper.mjs
   var STR = "string";
   var NUM = "number";
@@ -79,41 +53,16 @@
     refresh = this.clear;
   };
 
-  // src/shared/wordpress.mjs
-  if (typeof XMLHttpRequest === "undefined") {
-    import("xmlhttprequest").then(({ XMLHttpRequest: XMLHttpRequest2 }) => {
-      global.XMLHttpRequest = XMLHttpRequest2;
-    });
-    global.atob = function(data) {
-      return Buffer.from(data).toString("base64");
-    };
-  }
-
   // src/web/web-worker.js
   if ("function" === typeof importScripts) {
     console.log("[WebWorker] Spawned");
     document = {};
     importScripts("/webworker-php.js");
-    let isReady = false;
     async function init() {
       const php = new PHPWrapper();
-      await php.init(PHP, {
-        async onPreInit(FS, phpModule) {
-          globalThis.PHPModule = phpModule;
-          importScripts("/wp.js");
-          FS.mkdirTree("/usr/local/etc");
-          FS.createLazyFile("/usr/local/etc", "php.ini", "/etc/php.ini", true, false);
-          importScripts("/wp-lazy-files.js");
-          getLazyFiles().forEach(({ path, filename, fullPath }) => {
-            FS.mkdirTree("/usr/local/etc");
-            FS.createLazyFile(path, filename, fullPath, true, false);
-          });
-        }
-      });
+      await php.init(PHP, {});
+      console.log("loaded");
     }
-    const browser = init();
-    browser.then(async () => {
-      console.log("test");
-    });
+    init();
   }
 })();
--- src/web/web-worker.js
+++ src/web/web-worker.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-inner-declarations */
 
 import PHPWrapper from '../shared/php-wrapper.mjs';
-import WordPress from '../shared/wordpress.mjs';
-import WPBrowser from '../shared/wp-browser.mjs';
 
 if ( 'function' === typeof importScripts ) {
 	console.log( '[WebWorker] Spawned' );
@@ -12,21 +10,9 @@ if ( 'function' === typeof importScripts ) {
 
 	importScripts( '/webworker-php.js' );
 
+	async function init() {
 		const php = new PHPWrapper( );
-		await php.init( PHP, {
-			async onPreInit( FS, phpModule ) {
-				globalThis.PHPModule = phpModule;
-				importScripts( '/wp.js' );
-				FS.mkdirTree( '/usr/local/etc' );
-				FS.createLazyFile( '/usr/local/etc', 'php.ini', '/etc/php.ini', true, false );
-
-				importScripts( '/wp-lazy-files.js' );
-				getLazyFiles().forEach( ( { path, filename, fullPath } ) => {
-					FS.mkdirTree( '/usr/local/etc' );
-					FS.createLazyFile( path, filename, fullPath, true, false );
-				} );
-			},
-		} );
+		await php.init( PHP, {} );
 		console.log( 'loaded' );
 	}
 
