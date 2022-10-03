508e7db2446b75ffb7cdabffca913518e9990117
Adam Zieliński <adam@adamziel.com>
2022-10-03 11:20:30 +1100

    Crashes: without document = {}

--- dist-web/web-worker.js
+++ dist-web/web-worker.js
@@ -1,7 +1,6 @@
 (() => {
   // src/web/web-worker.js
   console.log("[WebWorker] Spawned");
-  document = {};
   importScripts("/webworker-php.js");
   new PHP({}).then(() => {
     console.log("[WebWorker] PHP initialized");
--- src/web/web-worker.js
+++ src/web/web-worker.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-inner-declarations */
 
 console.log( '[WebWorker] Spawned' );
-
-// Polyfill for the emscripten loader
-document = {};
-
 importScripts( '/webworker-php.js' );
 
 new PHP( {} )
