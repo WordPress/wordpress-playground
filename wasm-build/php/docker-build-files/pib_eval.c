7c50534903a600f096b51aa438f7789e061c0925
Adam Zieliński <adam@adamziel.com>
2022-10-04 14:05:45 +1100

    Crash: One of zend_string_init(name, 10, 0); zend_string_equals_literal_ci(class_name, "self"); zend_fetch_class(class_name, 0); zend_get_called_scope(EG(current_execute_data));

--- wasm-build/php/docker-build-files/pib_eval.c
+++ wasm-build/php/docker-build-files/pib_eval.c
@@ -1,13 +1,15 @@
 #include <emscripten.h>
 #include <stdlib.h>
 
-#include "zend_ast.h"
-#include "zend_API.h"
-#include "zend_operators.h"
-#include "zend_language_parser.h"
-#include "zend_smart_str.h"
-#include "zend_exceptions.h"
+#include "zend.h"
 #include "zend_constants.h"
+#include "zend_exceptions.h"
+#include "zend_execute.h"
+#include "zend_variables.h"
+#include "zend_operators.h"
+#include "zend_globals.h"
+#include "zend_API.h"
+
 
 int main() { return 0; }
 
@@ -15,19 +17,15 @@ int main() { return 0; }
 
 int EMSCRIPTEN_KEEPALIVE pib_init()
 {
-    int t = 1;
-    zval *zv;
-    zend_throw_error(NULL, NULL);
-    zend_ast_get_zval(NULL);
-    zend_ast_get_constant_name(NULL);
-    uint32_t t2 = 1;
-    zend_get_constant_ex(NULL,NULL,t2);
-    zend_is_true(zv);
-    zend_ast_get_list(NULL);
-    zend_error_noreturn(t,NULL);
-    zend_fetch_dimension_const(NULL,NULL,NULL,t);
+    zend_string *cname;
+	const char *name = ZSTR_VAL(cname);
+	size_t name_len = ZSTR_LEN(cname);
+    zend_string *class_name = zend_string_init(name, 10, 0);
+    zend_string_equals_literal_ci(class_name, "self");
+    zend_fetch_class(class_name, 0);
+    zend_get_called_scope(EG(current_execute_data));
 
-    // This works:
+
     return 1;
 }
 
