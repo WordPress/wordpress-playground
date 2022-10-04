224e2438088db64cab2a8ae24db6e3e839ab2944
Adam Zieliński <adam@adamziel.com>
2022-10-04 12:58:45 +1100

    No crash: Removed big if

--- src/web/web-worker.js
+++ src/web/web-worker.js
@@ -2,8 +2,8 @@ console.log( '[WebWorker] Spawned' );
 
 const noop = function()	{};
 const wasmTable = new WebAssembly.Table( {
-	initial: 866,
-	maximum: 866,
+	initial: 163,
+	maximum: 163,
 	element: 'anyfunc',
 } );
 const WASM_PAGE_SIZE = 65536;
