40fec969e9eb1574ce260c3172e52d76780f9f65
Adam Zieliński <adam@adamziel.com>
2022-10-04 14:41:36 +1100

    Crash: Almost the minimal repro with GC_SET_REFCOUND

--- wasm-build/php/docker-build-files/pib_eval.c
+++ wasm-build/php/docker-build-files/pib_eval.c
@@ -17,26 +17,10 @@
 
 void EMSCRIPTEN_KEEPALIVE pib_init(zend_object *object)
 {
-
-	ZEND_ASSERT(GC_REFCOUNT(object) == 0);
-
-	/* GC might have released this object already. */
-	if (UNEXPECTED(GC_TYPE(object) == IS_NULL)) {
-		return;
-	}
-
-	/*	Make sure we hold a reference count during the destructor call
-		otherwise, when the destructor ends the storage might be freed
-		when the refcount reaches 0 a second time
-	 */
 	if (!(OBJ_FLAGS(object) & IS_OBJ_DESTRUCTOR_CALLED)) {
-		GC_ADD_FLAGS(object, IS_OBJ_DESTRUCTOR_CALLED);
-
 		if (object->handlers->dtor_obj != zend_objects_destroy_object
 				|| object->ce->destructor) {
 			GC_SET_REFCOUNT(object, 1);
-			object->handlers->dtor_obj(object);
-			GC_DELREF(object);
 		}
 	}
 }
