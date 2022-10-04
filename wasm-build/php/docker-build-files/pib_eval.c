32b2d1f94adc88477bd717118e044a31838e9363
Adam Zieliński <adam@adamziel.com>
2022-10-04 13:42:37 +1100

    Crash: Half of zend_handle_undef_args2

--- wasm-build/php/docker-build-files/pib_eval.c
+++ wasm-build/php/docker-build-files/pib_eval.c
@@ -4586,50 +4586,6 @@ ZEND_API zend_result ZEND_FASTCALL zend_handle_undef_args2(zend_execute_data *ca
 		}
 
 		return SUCCESS;
-	} else {
-		if (fbc->common.fn_flags & ZEND_ACC_USER_ARG_INFO) {
-			/* Magic function, let it deal with it. */
-			return SUCCESS;
-		}
-
-		uint32_t num_args = ZEND_CALL_NUM_ARGS(call);
-		for (uint32_t i = 0; i < num_args; i++) {
-			zval *arg = ZEND_CALL_VAR_NUM(call, i);
-			if (!Z_ISUNDEF_P(arg)) {
-				continue;
-			}
-
-			zend_internal_arg_info *arg_info = &fbc->internal_function.arg_info[i];
-			if (i < fbc->common.required_num_args) {
-				zend_execute_data *old = start_fake_frame(call, NULL);
-				zend_argument_error(zend_ce_argument_count_error, i + 1, "not passed");
-				end_fake_frame(call, old);
-				return FAILURE;
-			}
-
-			zval default_value;
-			if (zend_get_default_from_internal_arg_info(&default_value, arg_info) == FAILURE) {
-				zend_execute_data *old = start_fake_frame(call, NULL);
-				zend_argument_error(zend_ce_argument_count_error, i + 1,
-					"must be passed explicitly, because the default value is not known");
-				end_fake_frame(call, old);
-				return FAILURE;
-			}
-
-			if (Z_TYPE(default_value) == IS_CONSTANT_AST) {
-				zend_execute_data *old = start_fake_frame(call, NULL);
-				zend_result ret = zval_update_constant_ex(&default_value, fbc->common.scope);
-				end_fake_frame(call, old);
-				if (ret == FAILURE) {
-					return FAILURE;
-				}
-			}
-
-			ZVAL_COPY_VALUE(arg, &default_value);
-			if (ZEND_ARG_SEND_MODE(arg_info) & ZEND_SEND_BY_REF) {
-				ZVAL_NEW_REF(arg, arg);
-			}
-		}
 	}
 
 	return SUCCESS;
