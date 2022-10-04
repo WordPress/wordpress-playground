4c8bc14715255777c7cc9a64b42240b932300002
Adam Zieliński <adam@adamziel.com>
2022-10-04 11:55:36 +1100

    Crash: Minimal zend_call_function repro

--- src/web/web-worker.js
+++ src/web/web-worker.js
@@ -2,8 +2,8 @@ console.log( '[WebWorker] Spawned' );
 
 const noop = function()	{};
 const wasmTable = new WebAssembly.Table( {
-	initial: 994,
-	maximum: 994,
+	initial: 930,
+	maximum: 930,
 	element: 'anyfunc',
 } );
 const WASM_PAGE_SIZE = 65536;
