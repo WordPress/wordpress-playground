3e2d2b49b54079f8e4d9a4a7b186cd594ba8064e
Adam Zieliński <adam@adamziel.com>
2022-10-05 19:12:00 +1100

    Tighten the max_updates check

--- wasm-build/php/docker-output/fix/wat_utils.py
+++ wasm-build/php/docker-output/fix/wat_utils.py
@@ -285,7 +285,7 @@ with open(read_from, 'r') as fp:
 
 removed_functions = []
 num_updates = 0
-max_updates = 7
+max_updates = 1
 while True:
     graph = call_graph(wat)
     fns = []
@@ -310,13 +310,13 @@ while True:
             removed_functions.append(fn)
             print("✅")
             num_updates += 1
-            if num_updates > max_updates:
+            if num_updates >= max_updates:
                 break
         else:
             print("❌")
             with open(read_from, 'w') as fp:
                 fp.write(wat)
-    if num_updates > max_updates:
+    if num_updates >= max_updates:
         break
 
 as_str = " ".join(removed_functions)
