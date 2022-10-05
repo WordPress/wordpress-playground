53bc9d974aa5126a062f3ca3b1d3afb608ccc2dc
Adam Zieliński <adam@adamziel.com>
2022-10-05 18:12:51 +1100

    Crash: removed       (func $abortStackOverflow (type 0)

--- wasm-build/php/docker-output/fix/wat_utils.py
+++ wasm-build/php/docker-output/fix/wat_utils.py
@@ -1,12 +1,15 @@
+from functools import reduce
 from multiprocessing.sharedctypes import Value
 from pprint import pprint
 from collections import defaultdict
+from tkinter import E
 
 
 def get_function_meta(wat, name):
     """ Returns the wat function signature
 
-    >>> get_function_meta('''(func $_test (type 6) (result i32)
+    >>> get_function_meta('''(import "test" "_test" (func $_test (type 0)))
+    ... (func $_test (type 6) (result i32)
     ... (local i32 i32 i32)
     ... global.get 16
     ... local.set 2
@@ -15,7 +18,7 @@ def get_function_meta(wat, name):
     ... local.set 0
     ... local.get 0
     ... return)''', '$_test')
-    {'params': [], 'return': ['i32'], 'starts_at': 0, 'ends_at': 137}
+    {'params': [], 'return': ['i32'], 'starts_at': 47, 'ends_at': 184}
     """
     idx = 0
     while True:
@@ -23,8 +26,9 @@ def get_function_meta(wat, name):
         try:
             idx = wat.index(func_declaration, idx)
         except ValueError:
-            raise ValueError(f"Function declaration not found: {name}")
-        if wat[idx + len(func_declaration) + 1] == ")":
+            return {"params": [], "return": [], "starts_at": 0, "ends_at": 0}
+        if wat[idx + len(func_declaration) + 1] == ")" or wat[idx - 2] == '"':
+            idx += 1
             continue
         else:
             break
@@ -55,22 +59,29 @@ def get_function_meta(wat, name):
         _return = [func_body.split("(result ")[1].split(")")[0]]
     except IndexError:
         _return = []
+    try:
+        _type = func_body.split("(type ")[1].split(")")[0]
+    except IndexError:
+        _type = "-1"
 
-    return {"params": _params, "return": _return, "starts_at": starts_at, "ends_at": ends_at}
+    if _type == "0" and not len(_params):
+        _params = ["i32"]
+    print(_params, _type)
+    return {"params": _params, "return": _return, "type": _type, "starts_at": starts_at, "ends_at": ends_at}
 
 
 def remove_function_calls(wat, fn):
     """ Removes calls of a specific function
 
     >>> remove_function_calls('''
+    ... (import "remove_calls" "_remove_calls" (func $_remove_calls (type 0)))
     ... (func $remove_calls (type 0) 
     ... return)
     ... (func $main (type 6) (result i32)
     ... (local i32 i32 i32)
     ... call $remove_calls
-    ... call $remove_calls
-    ... return)''', '$remove_calls')
-    '\\n(func $remove_calls (type 0) \\nreturn)\\n(func $main (type 6) (result i32)\\n(local i32 i32 i32)\\nreturn)'
+    ... call $remove_calls)''', '$remove_calls')
+    '\\n(import "remove_calls" "_remove_calls" (func $_remove_calls (type 0)))\\n(func $remove_calls (type 0) \\nreturn)\\n(func $main (type 6) (result i32)\\n(local i32 i32 i32)\\n)'
 
     >>> remove_function_calls('''
     ... (func $remove_calls (type 0) (param i32) (result i32) 
@@ -90,15 +101,27 @@ def remove_function_calls(wat, fn):
     new_contents = []
     skip_one = False
     # Assume the instructions are separated by newlines
+    call_str = f'call {fn}'
     for line in wat.split("\n"):
         if skip_one:
             skip_one = False
             continue
-        if line.strip() != f'call {fn}':
+        stripped = line.strip()
+        is_call = stripped.startswith(call_str) and (
+            len(stripped) == len(call_str) or stripped[len(
+                call_str)] == " " or stripped[len(call_str)] == ")"
+        )
+        if not is_call:
             new_contents.append(line)
             continue
+
         for _ in range(len(meta['params'])):
             new_contents.pop()
+
+        line_without_call = line.replace(call_str, "")
+        if line_without_call.strip() != "":
+            new_contents.append(line_without_call)
+
         if len(meta['return']):
             skip_one = True
     return "\n".join(new_contents)
@@ -108,13 +131,14 @@ def remove_function_declaration(wat, fn):
     """ Removes a declaration of a specific function
 
     >>> remove_function_declaration('''
+    ... (import "remove_me" "_remove_me" (func $_remove_me (type 0)))
     ... (func $keep_me_1 (type 0) (param i32) (result i32) 
     ... return)
     ... (func $remove_me (type 0) (param i32) (result i32) 
     ... return)
     ... (func $keep_me_2 (type 0) (param i32) (result i32) 
     ... return)''', '$remove_me')
-    '\\n(func $keep_me_1 (type 0) (param i32) (result i32) \\nreturn)\\n(func $keep_me_2 (type 0) (param i32) (result i32) \\nreturn)'
+    '\\n(import "remove_me" "_remove_me" (func $_remove_me (type 0)))\\n(func $keep_me_1 (type 0) (param i32) (result i32) \\nreturn)\\n(func $keep_me_2 (type 0) (param i32) (result i32) \\nreturn)'
     """
     meta = get_function_meta(wat, fn)
     wat = wat[:meta['starts_at']] + wat[meta['ends_at']:]
@@ -130,9 +154,10 @@ def remove_function_export(wat, fn):
     ... return)
     ... (export "_keep_me" (func $_keep_me))
     ... (export "_remove_me" (func $_remove_me))
+    ... (import "remove_me" "_remove_me" (func $_remove_me (type 0)))
     ... (elem (;0;) (global.get 1) func $keep_me_1 $_remove_me $_remove_me $keep_me_2)
     ... ''', '$_remove_me')
-    '\\n(func $remove_me (type 0) (param i32) (result i32) \\nreturn)\\n(export "_keep_me" (func $_keep_me))\\n\\n(elem (;0;) (global.get 1) func $keep_me_1 $keep_me_2)\\n'
+    '\\n(func $remove_me (type 0) (param i32) (result i32) \\nreturn)\\n(export "_keep_me" (func $_keep_me))\\n\\n(import "remove_me" "_remove_me" (func $_remove_me (type 0)))\\n(elem (;0;) (global.get 1) func $keep_me_1 $keep_me_2)\\n'
     """
     export_substr = f'(export "{fn[1:]}" (func {fn}))'
     if export_substr in wat:
@@ -165,13 +190,16 @@ def remove_function_export(wat, fn):
 def call_graph(wat):
     """ Computes a call graph of a WAT file.
 
-    >>> call_graph('''
+    >>> g = call_graph('''
     ... (func $a (type 0) (param i32) (result i32) 
     ...    call $b
     ...    return)
     ... (func $b (type 0) (param i32) (result i32) 
     ...    return)
     ... ''')
+    >>> g['key_calls_values'] = dict(g['key_calls_values'])
+    >>> g['keys_called_by_values'] = dict(g['keys_called_by_values'])
+    >>> g
     {'key_calls_values': {'$a': {'$b'}, '$b': set()}, 'keys_called_by_values': {'$a': set(), '$b': {'$a'}}}
     """
     key_calls_values = defaultdict(lambda: set())
@@ -188,13 +216,16 @@ def call_graph(wat):
             key_calls_values[fn_name].add(called_fn)
             keys_called_by_values[called_fn].add(fn_name)
     return {
-        "key_calls_values": dict(key_calls_values),
-        "keys_called_by_values": dict(keys_called_by_values)
+        "key_calls_values": key_calls_values,
+        "keys_called_by_values": keys_called_by_values
     }
 
 
 def remove_function(wat, fn):
-    return remove_function_declaration(remove_function_calls(remove_function_export(wat, fn), fn), fn)
+    wat = remove_function_export(wat, fn)
+    wat = remove_function_calls(wat, fn)
+    wat = remove_function_declaration(wat, fn)
+    return wat
 
 
 def find_unused_functions(wat, keep=None):
@@ -228,7 +259,46 @@ def find_unused_functions(wat, keep=None):
     return removed_fns
 
 
-# if __name__ == "__main__":
+def remove_unused_functions(wat):
+    fns = find_unused_functions(wat, ['$_pib_init2'])
+    print("Removing the following unused functions:")
+    pprint(fns)
+    return reduce(remove_function, fns, wat)
+
+
+def remove_functions_with_callers_but_no_callees(wat):
+    graph = call_graph(wat)
+    fns = []
+    for fn, called_by in graph["keys_called_by_values"].items():
+        if len(called_by) > 0 and len(graph["key_calls_values"][fn]) == 0:
+            fns.append(fn)
+
+    fns = fns[:1]
+    print("Removing the following function that do not make any calls:")
+    pprint(fns)
+    return reduce(remove_function, fns, wat)
+
+
+if __name__ == "__main__":
+    print(remove_function_declaration(remove_function_calls('''
+      (func $_fmt_x (type 29) (param i64 i32 i32) (result i32)
+    (local i32 i32 i32 i32 i32 i32 i32 i32 i32 i32 i32 i32 i32 i32 i64 i64)
+    global.get 16
+    local.set 16
+    local.get 0
+    i64.const 0
+    i64.eq)
+                local.get 380
+                local.get 85
+                local.get 102
+                call $_fmt_x
+                local.set 140
+                local.get 108
+                i64.load
+                local.set 381
+                local.get 381
+                i64.const 0
+    ''', '$_fmt_x'), '$_fmt_x'))
 #     import doctest
 #     doctest.testmod()
 
@@ -238,12 +308,7 @@ write_to = 'fix/updated2.wat'
 with open(read_from, 'r') as fp:
     wat = "".join(fp.readlines())
 
-unused_functions = find_unused_functions(wat, ['$_pib_init2'])
-print("Removing the following unused functions:")
-pprint(unused_functions)
-updated_wat = wat
-for fn in unused_functions:
-    updated_wat = remove_function(updated_wat, fn)
+updated_wat = remove_functions_with_callers_but_no_callees(wat)
 
 if len(updated_wat):
     with open(write_to, 'w') as fp:
