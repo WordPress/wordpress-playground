23e4b982026862d286b5a8f8a46c831f21fd059a
Adam Zielinski <adam@adamziel.com>
2022-10-11 12:19:29 -1000

    Update wasm-build/php/web-publish.sh

--- wasm-build/php/web-publish.sh
+++ wasm-build/php/web-publish.sh
@@ -5,5 +5,5 @@ set -e
 root_dir=../..
 dest_dir=$root_dir/dist-web
 
-cp ./docker-output/webworker-* $dest_dir/
+cp ./docker-output/php* $dest_dir/
 cp $root_dir/src/shared/etc/php.ini $dest_dir/etc/
