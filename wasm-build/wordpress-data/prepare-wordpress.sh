0cb7e6220688b8591dd45f438aefa7a3fc0b3b56
Adam Zieliński <adam@adamziel.com>
2022-10-14 13:39:18 -1000

    Keep the block CSS and JS files plus the wp-emoji-loader file – all read by PHP

--- wasm-build/wordpress-data/prepare-wordpress.sh
+++ wasm-build/wordpress-data/prepare-wordpress.sh
@@ -11,7 +11,7 @@ rm -rf wordpress-static
 # Download specific version of WordPress
 wp_zip_url=${1:-${WP_URL:-https://wordpress.org/wordpress-6.0.2.zip}}
 wget -O wp.zip $wp_zip_url
-unzip wp.zip
+unzip -q wp.zip
 rm wp.zip
 
 # Patch WordPress with sqlite support
@@ -49,9 +49,18 @@ find ./ -type f -name '*.woff' | xargs rm -r 2> /dev/null
 find ./ -type f -name '*.wof2' | xargs rm -r 2> /dev/null
 find ./ -type f -name '*.jpeg' | xargs rm -r 2> /dev/null
 find ./ -type f -name '*.jpg' | xargs rm -r 2> /dev/null
-# Keep the theme style.css files, as they're read by PHP
-find ./ -type f -name '*.css' -not -path '*/wp-content/themes/*/style.css' | xargs rm 2> /dev/null
-find ./ -type f -name '*.js' | xargs rm 2> /dev/null
+
+# Keep only the CSS files that are read by PHP
+find ./ -type f -name '*.css' \
+    -not -path '*/wp-includes/blocks/*/*.min.css' \
+    -not -path '*/wp-content/themes/*/style.css' | xargs rm 2> /dev/null
+find ./ -type f -name '*-rtl.min.css' | xargs rm 2> /dev/null
+
+# Keep only the JS files that are read by PHP
+find ./ -type f -name '*.js' \
+    -not -path '*/wp-includes/blocks/*/*.min.js' \
+    -not -name 'wp-emoji-loader.min.js' \
+    | xargs rm 2> /dev/null
 
 # Remove whitespace from PHP files
 for phpfile in $(find ./ -type f -name '*.php'); do
@@ -70,7 +79,7 @@ php -S 127.0.0.1:8000&
 sleep 6
 http_response=$(curl -o ./debug.txt -s -w "%{http_code}\n" -XPOST http://127.0.0.1:8000/wp-admin/install.php\?step\=2 --data "language=en&prefix=wp_&weblog_title=My WordPress Website&user_name=admin&admin_password=password&admin_password2=password&Submit=Install WordPress&pw_weak=1&admin_email=admin@localhost.com")
 pkill php
-if [ $http_response != "200" ]; then
+if [ "$http_response" != "200" ]; then
     exit 'WordPress installation failed'
     cat debug.txt
 fi
