fb0032efa24f3969972268a0da735c8670a443c6
Adam Zieliński <adam@adamziel.com>
2022-10-20 11:36:24 -1000

    Add PHPServer and PHPBrowser utils

---
+++ packages/php-wasm-web/src/index.js
@@ -0,0 +1,7 @@
+import PHPServer from "./php-server"
+export { PHPServer };
+
+import PHPBrowser from "./php-browser";
+export { PHPBrowser };
+
+export { isURLScoped, getURLScope, setURLScope, removeURLScope } from "./urls";
