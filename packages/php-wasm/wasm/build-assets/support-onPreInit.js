b442f742389ed6a5373e7904abba7df067da995c
Adam Zieliński <adam@adamziel.com>
2022-10-21 18:54:02 -1000

    php-wasm: Remove the custom Module.onPreInit hook

--- packages/php-wasm/wasm/build-assets/support-onPreInit.js
+++
@@ -1,9 +0,0 @@
-Module.preInit = function() {
-	if ( Module.onPreInit ) {
-		Module.onPreInit( {
-			Module: Module,
-			FS: typeof FS !== 'undefined' ? FS : undefined,
-			NODEFS: typeof NODEFS !== 'undefined' ? NODEFS : undefined
-		});
-	}
-};
