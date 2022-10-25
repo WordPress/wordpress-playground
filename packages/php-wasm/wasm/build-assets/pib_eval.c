e4c928102e820f339867cf72d9ed1d6a9133609c
Adam Zieliński <adam@adamziel.com>
2022-10-24 16:47:13 -1000

    Document pib_eval.c

--- packages/php-wasm/wasm/build-assets/pib_eval.c
+++ packages/php-wasm/wasm/build-assets/pib_eval.c
@@ -1,3 +1,10 @@
+/**
+ * Public API for php.wasm.
+ * 
+ * This file abstracts the entire PHP API with the minimal set
+ * of functions required to run PHP code in JavaScript.
+ */
+
 #include "sapi/embed/php_embed.h"
 #include <emscripten.h>
 #include <stdlib.h>
@@ -16,24 +23,18 @@
 #include "sqlite_statement.c"
 #include "pdo_sqlite.c"
 
+// Placeholder for emcc.
 int main() { return 0; }
 
-int EMSCRIPTEN_KEEPALIVE pib_init()
-{
-	putenv("USE_ZEND_ALLOC=0");
-
-	return php_embed_init(0, NULL);
-}
-
-void pib_finally()
-{
-	fflush(stdout);
-	fprintf(stdout, "\n");
-
-	fflush(stderr);
-	fprintf(stderr, "\n");
-}
-
+/*
+ * Function: pib_run
+ * ----------------------------
+ *   Runs a PHP script. Writes the output to stdout and stderr,
+ *
+ *   code: The PHP code to run. Must include the `<?php` opener.
+ *
+ *   returns: the exit code. 0 means success, 1 means the code died, 2 means an error.
+ */
 int EMSCRIPTEN_KEEPALIVE pib_run(char *code)
 {
 	int retVal = 255; // Unknown error.
@@ -60,11 +61,26 @@ int EMSCRIPTEN_KEEPALIVE pib_run(char *code)
 	return retVal;
 }
 
-void EMSCRIPTEN_KEEPALIVE pib_destroy()
+/*
+ * Function: pib_finally
+ * ----------------------------
+ *   Flush any buffered stdout and stderr contents.
+ */
+void pib_finally()
 {
-	return php_embed_shutdown();
+	fflush(stdout);
+	fprintf(stdout, "\n");
+
+	fflush(stderr);
+	fprintf(stderr, "\n");
 }
 
+/*
+ * Function: pib_refresh
+ * ----------------------------
+ *   Destroy the current PHP context (variables, functions, memory etc)
+ *   and start a new one.
+ */
 int EMSCRIPTEN_KEEPALIVE pib_refresh()
 {
 	pib_destroy();
@@ -72,7 +88,40 @@ int EMSCRIPTEN_KEEPALIVE pib_refresh()
 	return pib_init();
 }
 
-// <FILE UPLOADS SUPPORT>
+/*
+ * Function: pib_destroy
+ * ----------------------------
+ *   Destroy the current PHP context.
+ *   This function trashes the entire memory including all loaded variables,
+ *   functions, classes, etc. It's like the final cleanup after running a script.
+ */
+void EMSCRIPTEN_KEEPALIVE pib_destroy()
+{
+	return php_embed_shutdown();
+}
+
+/*
+ * Function: pib_init
+ * ----------------------------
+ *   Creates a new PHP context.
+ *   This function enables running PHP code, allocating variables, etc.
+ *   It must be called before running any script.
+ */
+int EMSCRIPTEN_KEEPALIVE pib_init()
+{
+	putenv("USE_ZEND_ALLOC=0");
+
+	return php_embed_init(0, NULL);
+}
+
+// === FILE UPLOADS SUPPORT ===
+
+/*
+ * Function: free_filename
+ * ----------------------------
+ *   Frees the memory after a zval allocated to store the uploaded
+ *   variable name.
+ */
 static void free_filename(zval *el) {
 	// Uncommenting this code causes a runtime error in the browser:
 	// @TODO evaluate whether keeping it commented leads to a memory leak
@@ -81,6 +130,17 @@ static void free_filename(zval *el) {
 	// zend_string_release_ex(filename, 0);
 }
 
+/*
+ * Function: pib_init_uploaded_files_hash
+ * ----------------------------
+ *   Allocates an internal HashTable to keep track of the legitimate uploads.
+ *   
+ *   Functions like `is_uploaded_file` or `move_uploaded_file` don't work with
+ *   $_FILES entries that are not in an internal hash table. It's a security feature.
+ *   This function allocates that internal hash table.
+ *   
+ *   @see PHP.initUploadedFilesHash in the JavaScript package for more details.
+ */
 void EMSCRIPTEN_KEEPALIVE pib_init_uploaded_files_hash()
 {
 	zend_hash_init(&PG(rfc1867_protected_variables), 8, NULL, NULL, 0);
@@ -91,21 +151,42 @@ void EMSCRIPTEN_KEEPALIVE pib_init_uploaded_files_hash()
 	SG(rfc1867_uploaded_files) = uploaded_files;
 }
 
+/*
+ * Function: pib_register_uploaded_file
+ * ----------------------------
+ *   Registers an uploaded file in the internal hash table.
+ *   
+ *   @see PHP.initUploadedFilesHash in the JavaScript package for more details.
+ */
 void EMSCRIPTEN_KEEPALIVE pib_register_uploaded_file(char *tmp_path_char)
 {
 	zend_string *tmp_path = zend_string_init(tmp_path_char, strlen(tmp_path_char), 1);
 	zend_hash_add_ptr(SG(rfc1867_uploaded_files), tmp_path, tmp_path);
 }
 
+/*
+ * Function: pib_destroy_uploaded_files_hash
+ * ----------------------------
+ *   Destroys the internal hash table to free the memory.
+ *   
+ *   @see PHP.initUploadedFilesHash in the JavaScript package for more details.
+ */
 void EMSCRIPTEN_KEEPALIVE pib_destroy_uploaded_files_hash()
 {
 	destroy_uploaded_files_hash();
 }
-// </FILE UPLOADS SUPPORT>
 
 #ifdef WITH_VRZNO
 #include "../php-src/ext/vrzno/php_vrzno.h"
 
+/*
+ * Function: exec_callback
+ * ----------------------------
+ *   Required by the VRZNO module.
+ *   Why? I'm not sure.
+ *   
+ *   @see https://github.com/seanmorris/vrzno
+ */
 int EMSCRIPTEN_KEEPALIVE exec_callback(zend_function *fptr)
 {
 	int retVal = vrzno_exec_callback(fptr);
@@ -115,6 +196,14 @@ int EMSCRIPTEN_KEEPALIVE exec_callback(zend_function *fptr)
 	return retVal;
 }
 
+/*
+ * Function: del_callback
+ * ----------------------------
+ *   Required by the VRZNO module.
+ *   Why? I'm not sure.
+ *   
+ *   @see https://github.com/seanmorris/vrzno
+ */
 int EMSCRIPTEN_KEEPALIVE del_callback(zend_function *fptr)
 {
 	return vrzno_del_callback(fptr);
