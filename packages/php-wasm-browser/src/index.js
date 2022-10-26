3e1a0f1bc71e483405127cda6864d1cbabc6758c
Adam Zieliński <adam@adamziel.com>
2022-10-25 16:09:52 -1000

    Document getPathQueryFragment

--- packages/php-wasm-browser/src/index.js
+++ packages/php-wasm-browser/src/index.js
@@ -4,7 +4,7 @@ import EmscriptenDownloadMonitor from './emscripten-download-monitor';
 
 export { currentBackend };
 export { initializeWorkerThread } from './worker-thread';
-export { startPHPWorkerThread, getWorkerThreadFrontend } from './worker-thread-api';
+export { initializePHPWorkerThread as startPHPWorkerThread, getWorkerThreadFrontend } from './worker-thread-api';
 export { registerServiceWorker, initializeServiceWorker, seemsLikeAPHPServerPath } from './service-worker';
 export { postMessageExpectReply, awaitReply, responseTo } from './messaging';
 export { cloneResponseMonitorProgress } from './emscripten-download-monitor';
@@ -45,8 +45,24 @@ export async function loadPHPWithProgress(phpLoaderModule, dataDependenciesModul
     );
 }
 
+/**
+ * The default base used to convert a path into the URL object.
+ */
 export const DEFAULT_BASE_URL = 'http://example.com';
 
+/**
+ * Returns a string representing the path, query, and 
+ * fragment of the given URL.
+ * 
+ * @example
+ * ```js
+ * const url = new URL('http://example.com/foo/bar?baz=qux#quux');
+ * getPathQueryFragment(url); // '/foo/bar?baz=qux#quux'
+ * ```
+ * 
+ * @param {URL} url The URL.
+ * @returns {string} The path, query, and fragment.
+ */
 export function getPathQueryFragment(url) {
 	return url.toString().substring(url.origin.length);
 }
