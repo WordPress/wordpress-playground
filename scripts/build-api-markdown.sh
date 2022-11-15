e0fdfe269183c5beab2913ea46f37a2b1ac451b3
Adam Zieliński <adam@adamziel.com>
2022-11-15 19:13:04 +0000

    Simplify the docs building pipeline

--- scripts/build-api-markdown.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-node ./build-scripts/generate-reference-docs.js -i ./build-api/combined -o ./docs/api
-
--- scripts/run-docs-pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run from the repo root
-npm run build:types
-bash ./scripts/build-api-model.sh
-bash ./scripts/build-api-markdown.sh
-node ./scripts/build-readme-md-files.js
