c6a54d4836975d33bbadaeb19036012ccf874d53
Adam Zieliński <adam@adamziel.com>
2023-01-03 20:46:03 +0100

    Patch the Gutenberg plugin after installing it from the plugin directory.
    Apply the same patch as in the WordPress-building Dockerfile to ensure
    no iframe is being loaded using srcdoc instead of src. Consult the
    changeset for more details.

--- src/wordpress-playground/wp-macros.ts
+++ src/wordpress-playground/wp-macros.ts
@@ -1,4 +1,4 @@
-import type { SpawnedWorkerThread } from "src/php-wasm-browser/index";
+import type { SpawnedWorkerThread } from 'src/php-wasm-browser/index';
 
 export async function login(
 	workerThread,
@@ -69,6 +69,63 @@ export async function installPlugin(
 		await workerThread.HTTPRequest({
 			path: activatePluginUrl,
 		});
+	}
+
+	/**
+	 * Pair the site editor's nested iframe to the Service Worker.
+	 *
+	 * Without the patch below, the site editor initiates network requests that
+	 * aren't routed through the service worker. That's a known browser issue:
+	 *
+	 * * https://bugs.chromium.org/p/chromium/issues/detail?id=880768
+	 * * https://bugzilla.mozilla.org/show_bug.cgi?id=1293277
+	 * * https://github.com/w3c/ServiceWorker/issues/765
+	 *
+	 * The problem with iframes using srcDoc and src="about:blank" as they
+	 * fail to inherit the root site's service worker.
+	 *
+	 * Gutenberg loads the site editor using <iframe srcDoc="<!doctype html">
+	 * to force the standards mode and not the quirks mode:
+	 *
+	 * https://github.com/WordPress/gutenberg/pull/38855
+	 *
+	 * This commit patches the site editor to achieve the same result via
+	 * <iframe src="/doctype.html"> and a doctype.html file containing just
+	 * `<!doctype html>`. This allows the iframe to inherit the service worker
+	 * and correctly load all the css, js, fonts, images, and other assets.
+	 *
+	 * Ideally this issue would be fixed directly in Gutenberg and the patch
+	 * below would be removed.
+	 *
+	 * See https://github.com/WordPress/wordpress-playground/issues/42 for more details
+	 */
+	if (
+		(await workerThread.isDir('/wordpress/wp-content/plugins/gutenberg')) &&
+		!(await workerThread.fileExists('/wordpress/.gutenberg-patched'))
+	) {
+		async function patchFile(path, callback) {
+			return await workerThread.writeFile(
+				path,
+				callback(await workerThread.readFile(path))
+			);
+		}
+		await workerThread.writeFile('/wordpress/.gutenberg-patched', '1');
+		await patchFile(
+			`/wordpress/wp-content/plugins/gutenberg/build/block-editor/index.js`,
+			(contents) =>
+				contents.replace(
+					/srcDoc:"[^"]+"/g,
+					'src:"/wp-includes/empty.html"'
+				)
+		);
+		await patchFile(
+			`/wordpress/wp-content/plugins/gutenberg/build/block-editor/index.min.js`,
+			(contents) =>
+				contents.replace(
+					/srcDoc:"[^"]+"/g,
+					'src:"/wp-includes/empty.html"'
+				)
+		);
 	}
 }
 
