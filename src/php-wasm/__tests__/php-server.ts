9ab3a68dae78cf666ec3f4ba374d5bc8d036ddb4
Adam Zieliński <adam@adamziel.com>
2023-01-04 12:39:32 +0100

    Add unit tests for parsing _POST and _FILES arrays
    Related to https://github.com/WordPress/wordpress-playground/issues/93

--- src/php-wasm/__tests__/php-server.ts
+++ src/php-wasm/__tests__/php-server.ts
@@ -24,27 +24,106 @@ describe('PHP Server – boot', () => {
 		server.php.writeFile(
 			'/tests/upload.php',
 			`<?php echo json_encode([
-			'files' => $_FILES,
-			'is_uploaded' => is_uploaded_file($_FILES['file.txt']['tmp_name'])
-		]);`
+				'files' => $_FILES,
+				'is_uploaded' => is_uploaded_file($_FILES['file_txt']['tmp_name'])
+			]);`
 		);
 		const response = await server.request({
 			path: `/upload.php`,
 			method: 'POST',
 			_POST: {},
 			files: {
-				'file.txt': new File(['Hello world'], 'file.txt'),
+				file_txt: new File(['Hello world'], 'file.txt'),
 			},
 		});
 		const bodyText = new TextDecoder().decode(response.body);
 		expect(JSON.parse(bodyText)).toEqual({
 			files: {
-				'file.txt': {
+				file_txt: {
 					name: 'file.txt',
 					type: 'text/plain',
 					tmp_name: expect.any(String),
-					error: 0,
-					size: 1,
+					error: '0',
+					size: '1',
+				},
+			},
+			is_uploaded: true,
+		});
+	});
+});
+
+describe('PHP Server – requests', () => {
+	beforeAll(() => {
+		// Shim the user agent for the server
+		(global as any).navigator = { userAgent: '' };
+	});
+
+	let php, server;
+	beforeEach(async () => {
+		php = await startPHP(phpLoaderModule, 'NODE');
+		php.mkdirTree('/tests');
+		server = new PHPServer(php, {
+			documentRoot: '/tests',
+			absoluteUrl: 'http://localhost/',
+			isStaticFilePath: (path) => path.startsWith('/uploads'),
+		});
+	});
+
+	it('should parse POST arrays in a PHP way', async () => {
+		server.php.writeFile(
+			'/tests/test.php',
+			`<?php echo json_encode($_POST);`
+		);
+		const response = await server.request({
+			path: `/test.php`,
+			method: 'POST',
+			_POST: {
+				'array[key1][subkey1]': 'value1',
+				'array[key1][subkey2]': 'value2',
+				'array[key2][subkey1]': 'value3',
+			},
+		});
+		const bodyText = new TextDecoder().decode(response.body);
+		expect(JSON.parse(bodyText)).toEqual({
+			array: {
+				key1: {
+					subkey1: 'value1',
+					subkey2: 'value2',
+				},
+				key2: {
+					subkey1: 'value3',
+				},
+			},
+		});
+	});
+
+	it('should parse FILES arrays in a PHP way', async () => {
+		server.php.writeFile(
+			'/tests/upload.php',
+			`<?php echo json_encode([
+				'files' => $_FILES,
+				'is_uploaded' => is_uploaded_file($_FILES['file_txt']['first']['tmp_name'])
+			]);`
+		);
+		const response = await server.request({
+			path: `/upload.php`,
+			method: 'POST',
+			_POST: {},
+			files: {
+				'file_txt[first]': new File(['Hello world'], 'file.txt'),
+			},
+		});
+		const bodyText = new TextDecoder().decode(response.body);
+		expect(JSON.parse(bodyText)).toEqual({
+			files: {
+				file_txt: {
+					first: {
+						name: 'file.txt',
+						type: 'text/plain',
+						tmp_name: expect.any(String),
+						error: '0',
+						size: '1',
+					},
 				},
 			},
 			is_uploaded: true,
