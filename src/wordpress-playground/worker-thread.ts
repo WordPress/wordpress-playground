de4e4fd37feee307985ce51516cddcf64e3c3b7f
Adam Zieliński <adam@adamziel.com>
2023-01-25 12:35:35 +0100

    Remove development artifacts

--- src/wordpress-playground/worker-thread.ts
+++ src/wordpress-playground/worker-thread.ts
@@ -63,7 +63,7 @@ function getRequestedPHPModule() {
 		'7.1': 'php-7.1.js',
 		'7.2': 'php-7.2.js',
 		'7.3': 'php-7.3.js',
-		'7.4': 'php-7.4.node.js',
+		'7.4': 'php-7.4.js',
 		'8.0': 'php-8.0.js',
 		'8.1': 'php-8.1.js',
 		'8.2': 'php-8.2.js',
