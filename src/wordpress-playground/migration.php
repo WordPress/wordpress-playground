a9692243cfa56412b82eb3e4e170b6603dfc34a2
Artemio Morales <artemio@altsalt.com>
2023-02-24 14:08:36 -0500

    Fix code formatting and spacing issues
    Co-authored-by: Adam Zielinski <adam@adamziel.com>

--- src/wordpress-playground/migration.php
+++ src/wordpress-playground/migration.php
@@ -12,25 +12,21 @@ function generateZipFile($exportPath, $databasePath, $docRoot) {
             $dir = array_pop($directories);
 
             if ($handle = opendir($dir)) {
-
                 while (false !== ($entry = readdir($handle))) {
-                    
-                    if ($entry == '.' ||
-                        $entry == '..') {
+                    if ($entry == '.' || $entry == '..') {
                         continue;
                     }
 
                     $entry = $dir . $entry;
 
-                    if (is_dir($entry) &&
+                    if (
+                        is_dir($entry) &&
                         strpos($entry, 'wp-content/database') == false &&
-                        strpos($entry, 'wp-includes') == false) {
-
+                        strpos($entry, 'wp-includes') == false
+                    ) {
                             $directory_path = $entry . '/';
                             array_push($directories, $directory_path);
-
-                    } elseif (is_file($entry)) {
-
+                    } else if (is_file($entry)) {
                         $zip->addFile($entry);
                     }
                 }
