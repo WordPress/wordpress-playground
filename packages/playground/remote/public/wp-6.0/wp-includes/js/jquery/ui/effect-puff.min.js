dfae2c1beaac1fa2f8f58bde07fbdb5dbe4d005c
Adam Zieliński <adam@adamziel.com>
2023-03-16 23:59:55 +0100

    Restore WP static files

---
+++ packages/playground/remote/public/wp-6.0/wp-includes/js/jquery/ui/effect-puff.min.js
@@ -0,0 +1,9 @@
+/*!
+ * jQuery UI Effects Puff 1.13.1
+ * http://jqueryui.com
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license.
+ * http://jquery.org/license
+ */
+!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery","./effect","./effect-scale"],e):e(jQuery)}(function(t){"use strict";return t.effects.define("puff","hide",function(e,f){e=t.extend(!0,{},e,{fade:!0,percent:parseInt(e.percent,10)||150});t.effects.effect.scale.call(this,e,f)})});
