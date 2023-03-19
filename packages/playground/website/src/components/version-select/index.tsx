e0dfb264de09c53c2a22ddf0b017103ac37329eb
Adrian Stobbe <adrian.stobbe@sap.com>
2023-03-19 15:31:51 +0100

    add php + wp selector

---
+++ packages/playground/website/src/components/version-select/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type SelectorProps = {
+	name: string;
+	versions: string[];
+};
+
+export default function PHPSelector(props: SelectorProps) {
+	return (
+		<select
+			id={props.name + '-version'}
+			onChange={(event) => {
+				const url = new URL(window.location.toString());
+				url.searchParams.set(props.name, event.target.value);
+				window.location.assign(url);
+			}}
+		>
+			{props.versions.map((value) => (
+				<option
+					selected={
+						new URL(window.location.toString()).searchParams.get(
+							props.name
+						) === value
+					}
+					value={value}
+				>
+					{props.name.toString() + ' ' + value}
+				</option>
+			))}
+		</select>
+	);
+}
