180d20a81853656d5a856a508d7e2b9856ebac0a
sejas <antonio@sejas.es>
2023-04-24 11:49:56 +0100

    wp-now: add sqlite funciton

--- packages/wp-now/src/constants.ts
+++ packages/wp-now/src/constants.ts
@@ -4,5 +4,8 @@ import path from 'path';
 export const WP_NOW_PATH = path.join(os.homedir(), '.wp-now');
 export const WORDPRESS_ZIPS_PATH = path.join(WP_NOW_PATH, 'wordpress_zips');
 export const WORDPRESS_VERSIONS_PATH = path.join(WP_NOW_PATH, 'wordpress_versions');
+export const SQLITE_ZIP_PATH = path.join(WP_NOW_PATH, 'sqlite_zip');
+export const SQLITE_FILENAME = 'sqlite-database-integration-main';
+export const SQLITE_PATH = path.join(WP_NOW_PATH, SQLITE_FILENAME);
 export const WP_DOWNLOAD_URL = 'https://wordpress.org/latest.zip'
 export const SQLITE_URL = 'https://github.com/WordPress/sqlite-database-integration/archive/refs/heads/main.zip'
--- packages/wp-now/src/download.ts
+++ packages/wp-now/src/download.ts
@@ -3,7 +3,7 @@ import fs from 'fs-extra';
 import unzipper from 'unzipper';
 
 import path from 'path';
-import { WORDPRESS_VERSIONS_PATH, WORDPRESS_ZIPS_PATH, WP_DOWNLOAD_URL } from './constants';
+import { SQLITE_FILENAME, SQLITE_PATH, SQLITE_URL, SQLITE_ZIP_PATH, WORDPRESS_VERSIONS_PATH, WORDPRESS_ZIPS_PATH, WP_DOWNLOAD_URL } from './constants';
 
 async function downloadFile(url: string, dest: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -40,5 +40,28 @@ export async function downloadWordPress(fileName = 'latest') {
     }
   } else {
     console.log('WordPress folder already exists. Skipping download.');
+  }
+}
+
+export async function downloadSqlite() {
+  const zipPath = path.join(SQLITE_ZIP_PATH, `${SQLITE_FILENAME}.zip`)
+  const unzipPath = SQLITE_PATH
+  // Check if the Sqlite folder exists, if not, download and unzip
+  if (!fs.existsSync(unzipPath)) {
+    try {
+      fs.ensureDirSync(SQLITE_ZIP_PATH);
+      console.log('Downloading Sqlite...');
+      await downloadFile(SQLITE_URL, zipPath);
+      console.log('Download complete.');
+      // unzip the file
+      console.log('Unzipping Sqlite...');
+      await fs.createReadStream(zipPath).pipe(unzipper.Extract({ path: unzipPath })).promise();
+      console.log('Unzipping complete.');
+
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  } else {
+    console.log('Sqlite folder already exists. Skipping download.');
   }
 }
