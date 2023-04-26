0f3538cfe8ffeb470cf9c8d4931d23dd6a9678ea
Adam Zieliński <adam@adamziel.com>
2023-04-26 10:44:33 +0200

    Adjust setSiteOptions to use the object-based argument type

--- packages/playground/blueprints/src/lib/steps/site-data.ts
+++ packages/playground/blueprints/src/lib/steps/site-data.ts
@@ -9,19 +9,21 @@ export type SetSiteOptionsStep = {
 
 export const setSiteOptions: StepHandler<SetSiteOptionsStep> = async (
 	client,
-	options
+	{ options }
 ) => {
-	const result = await client.run({
-		code: `<?php
+	const code = `<?php
 	include 'wordpress/wp-load.php';
 	$site_options = ${phpVar(options)};
 	foreach($site_options as $name => $value) {
 		update_option($name, $value);
 	}
 	echo "Success";
-	`,
+	`;
+	const result = await client.run({
+		code,
 	});
 	assertSuccess(result);
+	return { code, result };
 };
 
 export interface UpdateUserMetaStep {
@@ -33,17 +35,19 @@ export const updateUserMeta: StepHandler<UpdateUserMetaStep> = async (
 	client,
 	{ meta, userId }
 ) => {
-	const result = await client.run({
-		code: `<?php
+	const code = `<?php
 	include 'wordpress/wp-load.php';
 	$meta = ${phpVar(meta)};
 	foreach($meta as $name => $value) {
 		update_user_meta(${phpVar(userId)}, $name, $value);
 	}
 	echo "Success";
-	`,
+	`;
+	const result = await client.run({
+		code,
 	});
 	assertSuccess(result);
+	return { code, result };
 };
 
 async function assertSuccess(result: PHPResponse) {
