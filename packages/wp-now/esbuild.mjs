47e916e4f4d87e383d8d6616a101c8a8b1dff37d
sejas <antonio@sejas.es>
2023-04-27 16:02:42 +0100

    wp-now: change build phase to esbuild module

---
+++ packages/wp-now/esbuild.mjs
@@ -0,0 +1,10 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+	entryPoints: ['packages/wp-now/src/main.ts'],
+	outdir: 'dist/packages/wp-now',
+	bundle: true,
+	packages: 'external',
+	format: 'esm',
+	platform: 'node',
+});
