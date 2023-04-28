a845a6043395c3fe6ad81a5bb76899dd2db059c5
sejas <antonio@sejas.es>
2023-04-28 12:25:13 +0100

    wp-now: add comments to constants

--- packages/wp-now/src/constants.ts
+++ packages/wp-now/src/constants.ts
@@ -1,12 +1,42 @@
 import os from 'os';
 import path from 'path';
 
+/**
+ * The hidden folder name for storing WP Now related files.
+ */
 export const WP_NOW_HIDDEN_FOLDER = '.wp-now';
+
+/**
+ * The full path to the hidden WP Now folder in the user's home directory.
+ */
 export const WP_NOW_PATH = path.join(os.homedir(), WP_NOW_HIDDEN_FOLDER);
+
+/**
+ * The path where WordPress zip files and unzipped folders will be stored within the WP Now folder.
+ */
 export const WORDPRESS_ZIPS_PATH = path.join(WP_NOW_PATH, 'wordpress_zips');
-export const SQLITE_ZIP_PATH = path.join(WP_NOW_PATH, 'sqlite_zip');
+
+/**
+ * The file name for the SQLite plugin name.
+ */
 export const SQLITE_FILENAME = 'sqlite-database-integration';
-export const SQLITE_PATH = path.join(SQLITE_ZIP_PATH, `${SQLITE_FILENAME}-main`);
+
+/**
+ * The full path to the SQLite database integration folder.
+ */
+export const SQLITE_PATH = path.join(WP_NOW_PATH, `${SQLITE_FILENAME}-main`);
+
+/**
+ * The URL for downloading the latest version of WordPress.
+ */
 export const WP_DOWNLOAD_URL = 'https://wordpress.org/latest.zip'
+
+/**
+ * The URL for downloading the SQLite database integration WordPress Plugin.
+ */
 export const SQLITE_URL = 'https://github.com/WordPress/sqlite-database-integration/archive/refs/heads/main.zip'
+
+/**
+ * The default starting port for running the WP Now server.
+ */
 export const DEFAULT_PORT = 8881;
