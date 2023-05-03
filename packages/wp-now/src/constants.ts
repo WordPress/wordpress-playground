6c532ef2e660d5bd03914be36cc196e69e7d0521
Antonio Sejas <antonio@sejas.es>
2023-05-03 15:07:02 +0100

    wp-now: create first version of the command (#188)
    * wp-now: create dummy commands and new package
    
    * wp-now: re-create wp-now folders using nx
    
    * wp-now: execute php code example
    
    * wp-now: add connect node proxy to php runtime
    
    * wp-now: download latest wp zip in .wp-now dir
    
    * wp-now: download wordpress and proxy requests fixing location
    
    * wp-now: remove unused run method
    
    * wp-now: clean unused imports
    
    * wp-now: css fixed, redirect fixed
    
    * wp-now: setup sqlite
    
    * wp-now: use express and change the response to bytes
    
    * wp-now: rename download lib
    
    * wp-now: add sqlite funciton
    
    * wp-now: move sqlite logic to nodejs and unify paths
    
    * wp-now: split create-server run-cli logic
    
    * wp-now: update yarn lock
    
    * wp-now: import HTTPMethod from abstract
    
    * wp-now: avoid importing abstract
    
    * wp-now: remove async from mountSqlite
    
    * wp-now: clean unnecesary cli
    
    * wp-now: fix imports after merge using NodePHP and universal
    
    * wp-now: change build phase to esbuild module
    
    * wp-now: remove clear console lines
    
    * wp-now: use portFinder for open port source of truth
    
    * wp-now: receive path as paramter
    
    * wp-now: accept path and mode, enable plugin mode
    
    * wp-now: infer mode plugin, theme, index or core
    
    * wp-now: infer mode static directory
    
    * wp-now: rename patchFile to updateFile
    
    * wp-now: add comments to constants
    
    * wp-now: reduce amount of child folders for sqlite
    
    * wp-now: run always wp-now cli mode
    
    Exmaple:
    nx preview wp-now start --path=/Users/macbookpro/gutenberg
    
    * wp-now: use path as a parameter instead of cwd()
    
    * wp-now: fix code style
    
    * wp-now: remove unnecessary log port number
    
    * wp-now: update SQLite plugin name under quotes
    
    * wp-now: remove node env suggestion from run-cli
    
    * wp-now: project.json run commands sequentially
    
    * wp-now: rename wordpress versions path
    
    * wp-now: fix code style
    
    * wp-now: rename requestBodyToMultipartFormData
    
    * wp-now: reuse defineSiteUrl
    
    * wp-now: include a WP-NOW Readme and copy it to dist
    
    * wp-now: improve the readme redaction
    
    * wp-now: improve the readme redaction
    
    * wp-now: add phpVersion parameter
    
    Closes: https://github.com/WordPress/wordpress-playground/issues/232
    
    * wp-now: fix code style
    
    * wp-now: default mode is index
    
    * wp-now: fix code style
    
    * wp-now: use dashes for wordpress-versions folder
    
    * wp-now: update readme
    
    * wp-now: remove wp-content argument to use only home directory
    
    * wp-now: rename phpVersion argument name to php
    
    * wp-now: pass php version value from php argument

---
+++ packages/wp-now/src/constants.ts
@@ -0,0 +1,51 @@
+import os from 'os';
+import path from 'path';
+
+/**
+ * The hidden folder name for storing WP Now related files.
+ */
+export const WP_NOW_HIDDEN_FOLDER = '.wp-now';
+
+/**
+ * The full path to the hidden WP Now folder in the user's home directory.
+ */
+export const WP_NOW_PATH = path.join(os.homedir(), WP_NOW_HIDDEN_FOLDER);
+
+/**
+ * The path where WordPress zip files will be unzipped and stored within the WP Now folder.
+ */
+export const WORDPRESS_VERSIONS_PATH = path.join(
+	WP_NOW_PATH,
+	'wordpress-versions'
+);
+
+/**
+ * The file name for the SQLite plugin name.
+ */
+export const SQLITE_FILENAME = 'sqlite-database-integration';
+
+/**
+ * The full path to the "SQLite database integration" folder.
+ */
+export const SQLITE_PATH = path.join(WP_NOW_PATH, `${SQLITE_FILENAME}-main`);
+
+/**
+ * The URL for downloading the latest version of WordPress.
+ */
+export const WP_DOWNLOAD_URL = 'https://wordpress.org/latest.zip';
+
+/**
+ * The URL for downloading the "SQLite database integration" WordPress Plugin.
+ */
+export const SQLITE_URL =
+	'https://github.com/WordPress/sqlite-database-integration/archive/refs/heads/main.zip';
+
+/**
+ * The default starting port for running the WP Now server.
+ */
+export const DEFAULT_PORT = 8881;
+
+/**
+ * The default PHP version to use when running the WP Now server.
+ */
+export const DEFAULT_PHP_VERSION = '8.0';
---
+++ packages/wp-now/src/main.ts
@@ -0,0 +1,3 @@
+import { runCli } from './run-cli';
+
+runCli();
