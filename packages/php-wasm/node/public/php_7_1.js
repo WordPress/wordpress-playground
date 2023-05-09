4c40c55fafdffc7e8a7e814b9ea5f118dc473047
Adam Zieliński <adam@adamziel.com>
2023-05-10 01:21:00 +0200

    PHP: Resolve "localhost" to "127.0.0.1"

--- packages/php-wasm/node/public/php_7_1.js
+++ packages/php-wasm/node/public/php_7_1.js
@@ -6996,6 +6996,12 @@ if (Module["preInit"]) {
 
 run();
 /**
+ * Emscripten resolves `localhost` to a random IP address. Let's
+ * make it always resolve to 127.0.0.1.
+ */
+DNS.address_map.addrs.localhost = '127.0.0.1';
+
+/**
  * Debugging Asyncify errors is tricky because the stack trace is lost when the
  * error is thrown. This code saves the stack trace in a global variable 
  * so that it can be inspected later.
