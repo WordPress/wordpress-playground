be3a15539d5bed1cd18253705b7ff4e89c869650
sejas <antonio@sejas.es>
2023-05-09 14:27:32 +0100

    wp-now: improve description on isValidWordpressVersion

--- packages/wp-now/src/wp-playground-wordpress/is-valid-wordpress-version.ts
+++ packages/wp-now/src/wp-playground-wordpress/is-valid-wordpress-version.ts
@@ -4,10 +4,10 @@
  * The Regex is based on the releases on https://wordpress.org/download/releases/#betas
  * The version string can be one of the following formats:
  * - "latest"
- * - "x.y" (x and y are integers) i.e. "6.2"
- * - "x.y.z" (x, y and z are integers) i.e. "6.2.1"
- * - "x.y.z-betaN" (N is an integer) i.e. "6.2.1-beta1"
- * - "x.y.z-RCN" (N is an integer) i.e. "6.2-RC1"
+ * - "x.y" (x and y are integers) e.g. "6.2"
+ * - "x.y.z" (x, y and z are integers) e.g. "6.2.1"
+ * - "x.y.z-betaN" (N is an integer) e.g. "6.2.1-beta1"
+ * - "x.y.z-RCN" (N is an integer) e.g. "6.2-RC1"
  *
  * @param version The version string to check.
  * @returns A boolean value indicating whether the version string is a valid WordPress version.
