1f96f7041b8cff3e741408d494a886811e0dfeba
Michal <mmczaplinski@gmail.com>
2023-05-09 10:32:00 -0500

    Update packages/playground/interactive-block-playground/src/index.ts
    Accept changes from Adam
    
    Co-authored-by: Adam Zielinski <adam@adamziel.com>

--- packages/playground/interactive-block-playground/src/index.ts
+++ packages/playground/interactive-block-playground/src/index.ts
@@ -1,51 +1,6 @@
-import {
-	installPlugin,
-	PHPResponse,
-	PlaygroundClient,
-	startPlaygroundWeb,
-} from '@wp-playground/client';
+import { phpVars, startPlaygroundWeb } from '@wp-playground/client';
 
 import { remotePlaygroundOrigin } from './config';
-
-function asDOM(response: PHPResponse) {
-	return new DOMParser().parseFromString(response.text, 'text/html')!;
-}
-
-async function createNewPost(
-	client: PlaygroundClient,
-	title: string,
-	content: string,
-	status = 'publish'
-) {
-	try {
-		const newPostResponse = await client.request({
-			url: '/wp-admin/post-new.php',
-		});
-		const newPostPage = asDOM(newPostResponse);
-		const el = newPostPage.querySelector('#wp-api-request-js-extra');
-		const nonce = el?.textContent?.match(/"nonce":"([a-z0-9]*)"/)![1]!;
-
-		const response = await client.request({
-			method: 'POST',
-			url: '/index.php?rest_route=/wp/v2/posts',
-			headers: {
-				'Content-Type': 'application/json',
-				'X-WP-Nonce': nonce, // Use the nonce provided by WordPress
-			},
-			body: JSON.stringify({ title, content, status }),
-		});
-
-		const data = response.json;
-
-		if (response.httpStatusCode >= 400) {
-			throw new Error(data.message);
-		} else {
-			return data;
-		}
-	} catch (e) {
-		console.error(e);
-	}
-}
 
 // Set the text content of the render.php element
 document.getElementById('render.php')!.textContent = `<?php
@@ -82,6 +37,11 @@ store({
 		'#playground'
 	) as HTMLIFrameElement;
 
+	const js = phpVars({
+		title: 'Test post',
+		content: '<!-- wp:hello/log-block /-->',
+	});
+
 	const client = await startPlaygroundWeb({
 		iframe: playground,
 		remoteUrl: `${remotePlaygroundOrigin}/remote.html`,
@@ -111,45 +71,50 @@ store({
 						url: 'zips/hello.zip',
 					},
 				},
+				{
+					step: 'runPHP',
+					code: `<?php
+					require_once "/wordpress/wp-load.php";
+					$post_id = wp_insert_post([
+						"post_title" => ${js.title},
+						"post_content" => ${js.content},
+						"post_status" => "publish",
+					]);
+					file_put_contents('/post-id.txt', $post_id);
+					`,
+				},
+				{
+					step: 'installPlugin',
+					pluginZipFile: {
+						resource: 'url',
+						url: 'zips/block-interactivity-experiments.zip',
+					},
+				},
 			],
 		},
 	});
 
-	await client.isReady();
-
-	// Install the block-interactivity-experiments plugin
-	const pluginResponse = await fetch(
-		`zips/block-interactivity-experiments.zip`
-	);
-	const blob = await pluginResponse.blob();
-	const pluginFile = new File([blob], 'block-interactivity-experiments.zip');
-	await installPlugin(client, { pluginZipFile: pluginFile });
-
-	const data = await createNewPost(
-		client,
-		'Test post',
-		'<!-- wp:hello/log-block /-->'
-	);
-
-	await client.goTo(`/?p=${data.id}`);
+	const postId = await client.readFileAsText('/post-id.txt');
+	await client.goTo(`/?p=${postId}`);
 
 	document
 		.getElementById('render.php')!
 		.addEventListener('keyup', async (e) => {
 			client.writeFile(
 				'/wordpress/wp-content/plugins/hello/blocks/hello/render.php',
-				e.target.value
+				(e.target as HTMLTextAreaElement).value
 			);
 
-			await client.goTo(`/?p=${data.id}`);
+			await client.goTo(`/?p=${postId}`);
 		});
 
 	document.getElementById('view.js')!.addEventListener('keyup', async (e) => {
 		client.writeFile(
 			'/wordpress/wp-content/plugins/hello/blocks/hello/view.js',
-			e.target.value
+			(e.target as HTMLTextAreaElement).value
 		);
 
-		await client.goTo(`/?p=${data.id}`);
+		await client.goTo(`/?p=${postId}`);
 	});
 })();
+
