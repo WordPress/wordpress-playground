3e20b841fcc1b721d3f8b72183579948bce8f755
Wojtek Naruniec <wojtek@naruniec.me>
2023-05-12 10:30:41 +0200

    Mount db.php file instead of copying it

--- packages/wp-now/src/wp-now.ts
+++ packages/wp-now/src/wp-now.ts
@@ -284,16 +284,10 @@ async function initWordPress(
 
 function mountSqlite(php: NodePHP, vfsDocumentRoot: string) {
 	const sqlitePluginPath = `${vfsDocumentRoot}/wp-content/plugins/${SQLITE_FILENAME}`;
-	if (!php.fileExists(sqlitePluginPath)) {
-		php.mkdirTree(sqlitePluginPath);
-	}
 	if (php.listFiles(sqlitePluginPath).length === 0) {
 		php.mount(SQLITE_PATH, sqlitePluginPath);
+		php.mount(path.join(SQLITE_PATH, 'db.copy'), `${vfsDocumentRoot}/wp-content/db.php`);
 	}
-	cp(php, {
-		fromPath: `${sqlitePluginPath}/db.copy`,
-		toPath: `${vfsDocumentRoot}/wp-content/db.php`,
-	});
 }
 
 function copySqlite(localWordPressPath: string) {
