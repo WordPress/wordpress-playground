fe410c82affc7c368a28e1dece54c5f0929862fa
sejas <antonio@sejas.es>
2023-05-15 14:03:42 +0100

    wp-now: remove for loop on execute php file test

--- packages/wp-now/src/tests/execute-php-file.spec.ts
+++ packages/wp-now/src/tests/execute-php-file.spec.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import path from 'path';
 import { executePHPFile } from '../execute-php-file';
-import { SupportedPHPVersions } from '@php-wasm/universal';
+import { SupportedPHPVersion, SupportedPHPVersions } from '@php-wasm/universal';
 
 const exampleDir = path.join(__dirname, 'execute-php-file');
 
@@ -22,17 +22,30 @@ test('php file execution in index mode', async () => {
 test('php file execution for each PHP Version', async () => {
 	const resultFilePath = path.join(exampleDir, 'php-version-result.txt');
 
-	for (const phpVersion of SupportedPHPVersions) {
-		// reset result file
-		fs.writeFileSync(resultFilePath, '');
-		const result = await executePHPFile(
-			path.join(exampleDir, 'php-version.php'),
-			{ phpVersion }
-		);
-		expect(result.name).toBe('ExitStatus');
-		expect(result.status).toBe(0);
-		const output = fs.readFileSync(resultFilePath, 'utf8');
-		expect(output.substring(0, 16)).toBe(`PHP Version: ${phpVersion}`);
-	}
+	let result = await executePHPFile(
+		path.join(exampleDir, 'php-version.php'),
+		{ phpVersion: '7.4' }
+	);
+	expect(result.name).toBe('ExitStatus');
+	expect(result.status).toBe(0);
+	let output = fs.readFileSync(resultFilePath, 'utf8');
+	expect(output.substring(0, 16)).toBe('PHP Version: 7.4');
+
+	result = await executePHPFile(path.join(exampleDir, 'php-version.php'), {
+		phpVersion: '8.0',
+	});
+	expect(result.name).toBe('ExitStatus');
+	expect(result.status).toBe(0);
+	output = fs.readFileSync(resultFilePath, 'utf8');
+	expect(output.substring(0, 16)).toBe('PHP Version: 8.0');
+
+	result = await executePHPFile(path.join(exampleDir, 'php-version.php'), {
+		phpVersion: '8.2',
+	});
+	expect(result.name).toBe('ExitStatus');
+	expect(result.status).toBe(0);
+	output = fs.readFileSync(resultFilePath, 'utf8');
+	expect(output.substring(0, 16)).toBe('PHP Version: 8.2');
+
 	fs.writeFileSync(resultFilePath, 'PHP Version: X.Y');
 });
