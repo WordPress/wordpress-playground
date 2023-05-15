6686594a069e5355c9cd65516453365098c70daf
sejas <antonio@sejas.es>
2023-05-15 13:09:45 +0100

    wp-now: fix code style

--- packages/wp-now/src/tests/execute-php-file.spec.ts
+++ packages/wp-now/src/tests/execute-php-file.spec.ts
@@ -6,15 +6,17 @@ import { SupportedPHPVersions } from '@php-wasm/universal';
 const exampleDir = path.join(__dirname, 'execute-php-file');
 
 test('php file execution in index mode', async () => {
-  const resultFilePath = path.join(exampleDir, 'hello-world-result.txt')
-  // reset result file
-  fs.writeFileSync(resultFilePath, '');
+	const resultFilePath = path.join(exampleDir, 'hello-world-result.txt');
+	// reset result file
+	fs.writeFileSync(resultFilePath, '');
 
-  const result = await executePHPFile(path.join(exampleDir, 'hello-world.php'));
-  expect(result.name).toBe('ExitStatus');
-  expect(result.status).toBe(0);
-  const output = fs.readFileSync(resultFilePath, 'utf8');
-  expect(output).toBe('Hello World!');
+	const result = await executePHPFile(
+		path.join(exampleDir, 'hello-world.php')
+	);
+	expect(result.name).toBe('ExitStatus');
+	expect(result.status).toBe(0);
+	const output = fs.readFileSync(resultFilePath, 'utf8');
+	expect(output).toBe('Hello World!');
 });
 
 test('php file execution for each PHP Version', async () => {
