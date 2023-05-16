83a794f2e88a555e4be2fa04ce2bba7a84dd3861
sejas <antonio@sejas.es>
2023-05-16 20:10:15 +0100

    wp-now: remove unused slugify

--- packages/wp-now/src/wp-playground-wordpress/slugify.ts
+++
@@ -1,8 +0,0 @@
-export function slugify(text: string): string {
-	return text
-		.toLowerCase()
-		.trim() // trim leading and trailing spaces
-		.replace(/\s+/g, '-') // replace spaces with -
-		.replace(/[^\w-]+/g, '') // remove all non-word chars
-		.replace(/--+/g, '-'); // replace multiple - with single -
-}
