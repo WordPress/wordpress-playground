380d9e5340f7ef0dd981884f853427ef7c9d6d08
sejas <antonio@sejas.es>
2023-05-17 17:14:00 +0100

    wp-now: increase readFileHead to 8KB

--- packages/wp-now/src/wp-playground-wordpress/get-plugin-file.ts
+++ packages/wp-now/src/wp-playground-wordpress/get-plugin-file.ts
@@ -4,10 +4,11 @@ import path, { basename } from 'path';
 /**
  *
  * @param filePath The path to the file to read.
- * @param length The number of bytes to read from the file. By default 4KB.
+ * @param length The number of bytes to read from the file. By default 8KB.
  * @returns The first `length` bytes of the file as string.
+ * @see https://developer.wordpress.org/reference/functions/get_file_data/
  */
-function readFileHead(filePath: string, length = 4096) {
+function readFileHead(filePath: string, length = 8192) {
 	const buffer = Buffer.alloc(length);
 	const fd = fs.openSync(filePath, 'r');
 	fs.readSync(fd, buffer, 0, buffer.length, 0);
