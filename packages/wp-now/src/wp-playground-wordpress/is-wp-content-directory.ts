a38fa9ab098d2e5edb7357ceef8556b7faffa36d
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-05-17 15:18:38 -0700

    wp-now: Allow `plugins`, `themes` or `wp-content` for 'wp-content' mode (#377)
    Fixes https://github.com/WordPress/wordpress-playground/issues/376
    
    ## What?
    
    Allows the existence of `plugins`, `themes`, or `mu-plugins` to trigger
    'wp-content' mode. Previously, 'wp-content' mode required both `themes`
    and `plugins`.
    
    ## Why?
    
    Some 'wp-content' projects may only have a `themes` or `plugins`
    directory at the beginning.
    
    ## Testing Instructions
    
    1. Tests should pass.
    2. Run `nx preview wp-now start --path=/path/to/wp-content-project` and
    verify the server starts as expected.
    3. Run `nx preview wp-now start --path=/path/to/plugin-project` and
    verify the server starts as expected.
    4. Run `nx preview wp-now start --path=/path/to/wordpress-project` and
    verify the server starts as expected.

--- packages/wp-now/src/wp-playground-wordpress/is-wp-content-directory.ts
+++ packages/wp-now/src/wp-playground-wordpress/is-wp-content-directory.ts
@@ -8,10 +8,11 @@ import path from 'path';
  * @returns A boolean value indicating whether the project is a WordPress wp-content directory.
  */
 export function isWpContentDirectory(projectPath: string): Boolean {
+	const muPluginsExists = fs.existsSync(path.join(projectPath, 'mu-plugins'));
 	const pluginsExists = fs.existsSync(path.join(projectPath, 'plugins'));
 	const themesExists = fs.existsSync(path.join(projectPath, 'themes'));
-	if (!pluginsExists || !themesExists) {
-		return false;
+	if (muPluginsExists || pluginsExists || themesExists) {
+		return true;
 	}
-	return true;
+	return false;
 }
