e07ca89dce9bf7e83a7e4073e03efc11c632f49d
sejas <antonio@sejas.es>
2023-05-17 11:14:03 +0100

    wp-now: limit plugin search to first 4KB of the file
    Related to: https://github.com/WordPress/wordpress-playground/issues/227

--- packages/wp-now/src/wp-playground-wordpress/get-plugin-file.ts
+++ packages/wp-now/src/wp-playground-wordpress/get-plugin-file.ts
@@ -3,6 +3,21 @@ import path from 'path';
 
 /**
  *
+ * @param filePath The path to the file to read.
+ * @param length The number of bytes to read from the file. By default 4KB.
+ * @returns The first `length` bytes of the file as string.
+ */
+function readFileHead(filePath: string, length = 4096) {
+	const buffer = Buffer.alloc(length);
+	const fd = fs.openSync(filePath, 'r');
+	fs.readSync(fd, buffer, 0, buffer.length, 0);
+	const fileContentBuffer = buffer.toString('utf8');
+	fs.closeSync(fd);
+	return fileContentBuffer.toString();
+}
+
+/**
+ *
  * @param projectPath The path to the plugin.
  * @returns Path to the plugin file relative to the plugins directory.
  */
@@ -10,10 +25,7 @@ export function getPluginFile(projectPath: string) {
 	const files = fs.readdirSync(projectPath);
 	for (const file of files) {
 		if (file.endsWith('.php')) {
-			const fileContent = fs.readFileSync(
-				path.join(projectPath, file),
-				'utf8'
-			);
+			const fileContent = readFileHead(path.join(projectPath, file));
 			const pluginNameRegex = /plugin name:/i;
 			if (pluginNameRegex.test(fileContent)) {
 				return path.join(path.basename(projectPath), file);
