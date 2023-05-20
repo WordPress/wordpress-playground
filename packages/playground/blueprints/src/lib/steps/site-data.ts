35affd8f1deb4f42bd56d84302234ac215b823e0
Adam Zieliński <adam@adamziel.com>
2023-05-21 01:31:50 +0200

    Add docstrings for updateUserMeta and setSiteOptions

--- packages/playground/blueprints/src/lib/steps/site-data.ts
+++ packages/playground/blueprints/src/lib/steps/site-data.ts
@@ -2,11 +2,32 @@ import { PHPResponse } from '@php-wasm/universal';
 import { phpVar } from '@php-wasm/util';
 import { StepHandler } from '.';
 
+/**
+ * @inheritDoc setSiteOptions
+ *
+ * @example
+ *
+ * <code>
+ * {
+ *     "step": "setSiteOptions",
+ *     "options": {
+ *         "blogname": "My Blog",
+ *         "blogdescription": "A great blog"
+ *     }
+ * }
+ * </code>
+ */
 export type SetSiteOptionsStep = {
+	/** The name of the step. Must be "setSiteOptions". */
 	step: 'setSiteOptions';
+	/** The options to set on the site. */
 	options: Record<string, unknown>;
 };
 
+/**
+ * Sets site options. This is equivalent to calling `update_option` for each
+ * option in the `options` object.
+ */
 export const setSiteOptions: StepHandler<SetSiteOptionsStep> = async (
 	client,
 	{ options }
@@ -26,11 +47,32 @@ export const setSiteOptions: StepHandler<SetSiteOptionsStep> = async (
 	return { code, result };
 };
 
+/**
+ * @inheritDoc updateUserMeta
+ *
+ * @example
+ *
+ * <code>
+ * {
+ *     "step": "updateUserMeta",
+ *     "meta": {
+ * 	       "first_name": "John",
+ * 	       "last_name": "Doe"
+ *     },
+ *     "userId": 1
+ * }
+ * </code>
+ */
 export interface UpdateUserMetaStep {
 	step: 'updateUserMeta';
 	meta: Record<string, unknown>;
 	userId: number;
 }
+
+/**
+ * Updates user meta. This is equivalent to calling `update_user_meta` for each
+ * meta value in the `meta` object.
+ */
 export const updateUserMeta: StepHandler<UpdateUserMetaStep> = async (
 	client,
 	{ meta, userId }
