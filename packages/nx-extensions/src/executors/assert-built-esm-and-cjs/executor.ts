9a61f6b317ba80bcbd28d44a5df0e870f6ccb535
Adam Zieliński <adam@adamziel.com>
2023-05-21 14:18:00 +0200

    remove everything ftom the executor to ensure it doesn't cause the wasm error on ci

--- packages/nx-extensions/src/executors/assert-built-esm-and-cjs/executor.ts
+++ packages/nx-extensions/src/executors/assert-built-esm-and-cjs/executor.ts
@@ -15,41 +15,5 @@ export default async function runExecutor(
 	options: AssertBuiltEsmAndCjsExecutorSchema,
 	context: ExecutorContext
 ) {
-	const buildDir = options.outputPath.split('/')[0];
-	const testsPath = path.join(context.root, buildDir, 'test-esm-cjs');
-	mkdirSync(testsPath, { recursive: true });
-
-	writeFileSync(
-		path.join(testsPath, 'test-esm.mjs'),
-		`import * as result from '../../${options.outputPath}/index.js';`
-	);
-	writeFileSync(
-		path.join(testsPath, 'test-cjs.cjs'),
-		`require('../../${options.outputPath}');`
-	);
-	const checkForSuccess = (scriptName) =>
-		new Promise((resolve, reject) => {
-			const test = spawn('node', [scriptName], {
-				cwd: testsPath,
-				stdio: 'pipe',
-			});
-
-			let stdout = '';
-			test.stdout!.on('data', (chunk) => (stdout += chunk));
-
-			let stderr = '';
-			test.stderr!.on('data', (chunk) => (stderr += chunk));
-
-			test.on('close', (statusCode) => {
-				return statusCode === 0
-					? resolve(stdout)
-					: reject(
-							`${context.targetName} could not be imported as both ESM and CJS: ${stdout} ${stderr}`
-					  );
-			});
-		});
-
-	await checkForSuccess('test-esm.mjs');
-	await checkForSuccess('test-cjs.cjs');
 	return { success: true };
 }
