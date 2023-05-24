e0395e61c9135f59d7d7af1859dae178b488ea67
sejas <antonio@sejas.es>
2023-05-24 19:46:30 +0100

    wp-now: add comment on executeWPCli marking it as unstable

--- packages/wp-now/src/execute-wp-cli.ts
+++ packages/wp-now/src/execute-wp-cli.ts
@@ -5,6 +5,10 @@ import getWpCliPath from './get-wp-cli-path';
 import getWpNowConfig from './config';
 import { DEFAULT_PHP_VERSION, DEFAULT_WORDPRESS_VERSION } from './constants';
 
+/**
+ * This is an unstable API. Multiple wp-cli commands may not work due to a current limitation on php-wasm and pthreads.
+ * @param args The arguments to pass to wp-cli.
+ */
 export async function executeWPCli(args: string[]) {
 	await downloadWPCLI();
 	disableOutput();
