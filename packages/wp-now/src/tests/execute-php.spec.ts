405a2a026c58ad989acc2a7622a5c4c6815affca
sejas <antonio@sejas.es>
2023-05-26 14:30:07 +0100

    wp-now: validate php arguments passed through yargs

--- packages/wp-now/src/tests/execute-php.spec.ts
+++ packages/wp-now/src/tests/execute-php.spec.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import { executePHP } from '../execute-php';
 import getWpNowConfig from '../config';
+import { runCli } from '../run-cli';
 
 const exampleDir = path.join(__dirname, 'execute-php');
 
@@ -47,4 +48,65 @@ test('php file execution for each PHP Version', async () => {
 	expect(output.substring(0, 16)).toBe('PHP Version: 8.2');
 
 	fs.writeFileSync(resultFilePath, 'PHP Version: X.Y');
+});
+
+describe('validate php arguments passed through yargs', () => {
+	let output = '';
+	let consoleLogMock;
+	let processExitMock;
+	const argv = process.argv;
+	beforeEach(() => {
+		consoleLogMock = vi
+			.spyOn(console, 'log')
+			.mockImplementation((newLine: string) => {
+				output += `${newLine}\n`;
+			});
+		processExitMock = vi
+			.spyOn(process, 'exit')
+			.mockImplementation(() => null);
+	});
+
+	afterEach(() => {
+		output = '';
+		process.argv = argv;
+		consoleLogMock.mockRestore();
+		processExitMock.mockRestore();
+	});
+
+	test('php should receive the correct yargs arguments', async () => {
+		process.argv = ['node', 'wp-now', 'php', '--', '--version'];
+		await runCli();
+		expect(output).toMatch(/PHP 8\.0(.*)\(cli\)/i);
+		expect(processExitMock).toHaveBeenCalledWith(0);
+	});
+
+	test('wp-now should change the php version', async () => {
+		process.argv = [
+			'node',
+			'wp-now',
+			'php',
+			'--php=7.4',
+			'--',
+			'--version',
+		];
+		await runCli();
+		expect(output).toMatch(/PHP 7\.4(.*)\(cli\)/i);
+		expect(processExitMock).toHaveBeenCalledWith(0);
+	});
+
+	test('php should execute a file', async () => {
+		const filePath = path.join(exampleDir, 'print-php-version.php');
+		process.argv = ['node', 'wp-now', 'php', filePath];
+		await runCli();
+		expect(output).toMatch(/8\.0/i);
+		expect(processExitMock).toHaveBeenCalledWith(0);
+	});
+
+	test('php should execute a file and change php version', async () => {
+		const filePath = path.join(exampleDir, 'print-php-version.php');
+		process.argv = ['node', 'wp-now', 'php', '--php=7.4', '--', filePath];
+		await runCli();
+		expect(output).toMatch(/7\.4/i);
+		expect(processExitMock).toHaveBeenCalledWith(0);
+	});
 });
