d7ea0c5fdc27ffcbaeb37f3573ea000adc8a15b0
Adam Zielinski <adam@adamziel.com>
2023-05-31 10:07:35 +0200

    Remove interactive-code-block from the main repository so it can live in a dedicated space. See #473. (#475)
    ## What?
    
    Removes the interactive-code-block project from the main WordPress
    playground repository so it can live in a dedicated space.
    
    As outlined in #473, WordPress Playground is re-focusing on [the
    vision](https://github.com/WordPress/wordpress-playground/issues/472)
    and separating the framework from the applications.
    
    Once `interactive-code-block` is committed into a separate repo, a link
    will be added to this PR.

--- packages/interactive-code-block/src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export function setFileName(url: URL, newFilename: any) {
-	const currentScriptPath = url.pathname.split('/').slice(0, -1).join('/');
-	url.pathname = `${currentScriptPath}/${newFilename}`;
-	return url;
-}
