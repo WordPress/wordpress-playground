552d804e5828db613d5a235558484c0adc4574a8
Archit Gajjar <agajjar@Archits-MBP.attlocal.net>
2023-06-10 18:23:28 -0700

    playgroud: make onboarding experience better

--- packages/playground/website/src/components/browser-chrome/index.tsx
+++ packages/playground/website/src/components/browser-chrome/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Modal from '../modal';
 import css from './style.module.css';
 import AddressBar from '../address-bar';
 import classNames from 'classnames';
+import util from "../../util/util.js";
 
 interface BrowserChromeProps {
 	children?: React.ReactNode;
@@ -10,6 +12,16 @@ interface BrowserChromeProps {
 	showAddressBar?: boolean;
 	onUrlChange?: (url: string) => void;
 }
+
+// Check if the user is onboarding for the first time.
+const isOnboarding = util.isOnboardedFirstTime();
+
+// Welcome message to be displayed in the modal.
+const WELCOME_MSG = `Welcome to our brand new WordPress Plyground website! We are thrilled to have you here. 
+Whether you're a first-time visitor or a returning customer, we would like to remind you once:`
+
+// Reminder message to be displayed in the modal.
+const REMINDER = `All your modifications made here are private, and stored temporarily in the session and will be lost upon refreshing the page.`;
 
 export default function BrowserChrome({
 	children,
@@ -21,6 +33,7 @@ export default function BrowserChrome({
 	const addressBarClass = classNames(css.addressBarSlot, {
 		[css.isHidden]: !showAddressBar,
 	});
+	
 	return (
 		<div className={css.wrapper}>
 			<div className={css.window}>
@@ -30,7 +43,17 @@ export default function BrowserChrome({
 					<div className={addressBarClass}>
 						<AddressBar url={url} onUpdate={onUrlChange} />
 					</div>
-
+					{!isOnboarding && (
+						<Modal>
+							<div>
+								  {WELCOME_MSG}
+								<br></br>
+								<p>
+									<strong>{REMINDER}</strong>
+								</p>
+							</div>
+						</Modal>
+					)}
 					<div className={css.toolbarButtons}>
 						{toolbarButtons?.map(
 							(button: React.ReactElement, idx) =>
@@ -41,11 +64,11 @@ export default function BrowserChrome({
 					</div>
 				</div>
 				<div className={css.content}>{children}</div>
-				<div className={css.experimentalNotice}>
-					This is a cool fun experimental WordPress running in your
-					browser :) All your changes are private and gone after a
-					page refresh.
-				</div>
+					<div className={css.experimentalNotice}>
+						This is a cool fun experimental WordPress running in your
+							browser :) All your changes are private and gone after a
+							page refresh.
+					</div>
 			</div>
 		</div>
 	);
---
+++ packages/playground/website/src/util/util.ts
@@ -0,0 +1,26 @@
+/**
+ * @description: Utility functions to support the website.
+*/
+
+/**
+ * @function isOnboardedFirstTime
+ * @description: Checks if the user is onboarded for the first time. 
+ * @returns {boolean} true if the user is onboarded for the first time, false otherwise.
+ * @example isOnboardedFirstTime() // false
+ */
+const isOnboardedFirstTime = (): boolean => {
+
+	// Get the isOnboarded flag value from browser's local storage.
+	const isOnboarded = window.localStorage.getItem('isOnboarded');
+
+	// if the flag is set to 1, then the user is onboarded.
+	if (isOnboarded && parseInt(isOnboarded) === 1) {
+		return true;
+	} else {
+		// Set the flag to 1, so that the user is not onboarded again.
+		window.localStorage.setItem('isOnboarded', "1")
+		return false;
+	}
+};
+
+export default { isOnboardedFirstTime };
