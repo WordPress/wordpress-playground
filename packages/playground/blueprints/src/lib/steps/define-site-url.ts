278819eb312a75dfa102fbea9c1a7e5ff69af62e
Adam Zieliński <adam@adamziel.com>
2023-06-20 18:14:12 +0200

    More complete Blueprints step documentation

--- packages/playground/blueprints/src/lib/steps/define-site-url.ts
+++ packages/playground/blueprints/src/lib/steps/define-site-url.ts
@@ -1,13 +1,26 @@
 import { StepHandler } from '.';
 import { defineWpConfigConsts } from './define-wp-config-consts';
 
+/**
+ * @inheritDoc defineSiteUrl
+ * @hasRunnableExample
+ * @example
+ *
+ * <code>
+ * {
+ * 		"step": "defineSiteUrl",
+ * 		"siteUrl": "https://playground.wordpress.net"
+ * }
+ * </code>
+ */
 export interface DefineSiteUrlStep {
 	step: 'defineSiteUrl';
+	/** The URL */
 	siteUrl: string;
 }
 
 /**
- * Sets site URL of the WordPress installation.
+ * Sets WP_HOME and WP_SITEURL constants for the WordPress installation.
  *
  * @param playground The playground client.
  * @param siteUrl
