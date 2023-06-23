WordPress/wordpress-playground

commit c71e7a9b4065e9ce391644a9e8f821c3c4f7871f

playground.wordpress.net: Support mounting local files (#548)
Since https://github.com/WordPress/wordpress-playground/pull/547 adds
support for OPFS API, I just realized the same API can be used to
read/write local files. This PR adds a local directory picker and a
"Sync local files" button to bring the changes back into Playground's MEMFS.

packages/playground/website/src/components/playground-configuration-group/form.tsx
@@ -1,0 +1,305 @@
+import css from './style.module.css';
+import forms from '../../forms.module.css';
+import { useEffect, useState } from 'react';
+import {
+	SupportedPHPVersion,
+	SupportedPHPVersionsList,
+} from '@php-wasm/universal';
+import { StorageType } from '../../types';
+import { OPFSButton } from './opfs-button';
+import Button from '../button';
+
+// This is duplicated in @wp-playground/remote
+// @TODO: move to a shared package like @wp-playground/wordpress
+export const SupportedWordPressVersions = ['6.2', '6.1', '6.0', '5.9'] as const;
+export const SupportedWordPressVersionsList =
+	SupportedWordPressVersions as any as string[];
+export type SupportedWordPressVersion =
+	(typeof SupportedWordPressVersions)[number];
+
+export interface PlaygroundConfiguration {
+	wp: SupportedWordPressVersion;
+	php: SupportedPHPVersion;
+	storage: StorageType;
+	resetSite?: boolean;
+}
+
+export interface PlaygroundConfigurationFormProps {
+	currentlyRunningWordPressVersion: string | undefined;
+	initialData: PlaygroundConfiguration;
+	onSubmit: (config: PlaygroundConfiguration) => void;
+	onSelectLocalDirectory: 'not-available' | 'origin-mismatch' | (() => any);
+	isMountingLocalDirectory: boolean;
+	mountProgress?: {
+		files: number;
+		total: number;
+	};
+}
+
+export function PlaygroundConfigurationForm({
+	currentlyRunningWordPressVersion,
+	isMountingLocalDirectory = false,
+	mountProgress,
+	initialData,
+	onSubmit,
+	onSelectLocalDirectory,
+}: PlaygroundConfigurationFormProps) {
+	const [php, setPhp] = useState(initialData.php);
+	const [storage, setStorage] = useState<StorageType>(initialData.storage);
+	const [wp, setWp] = useState(initialData.wp);
+	const handleStorageChange = async (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setStorage(event.target.value as any as StorageType);
+	};
+
+	const [resetSite, setResetSite] = useState(initialData.resetSite);
+
+	useEffect(() => {
+		if (
+			currentlyRunningWordPressVersion &&
+			wp !== currentlyRunningWordPressVersion
+		) {
+			setResetSite(true);
+		}
+	}, [wp, resetSite, currentlyRunningWordPressVersion]);
+
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		onSubmit({ php, storage, wp, resetSite });
+	}
+
+	async function handleSelectLocalDirectory(
+		e: React.MouseEvent<HTMLButtonElement>
+	) {
+		e.preventDefault();
+		if ('function' === typeof onSelectLocalDirectory) {
+			onSelectLocalDirectory();
+		}
+	}
+
+	const liveDirectoryAvailable = ![
+		'not-available',
+		'origin-mismatch',
+	].includes(onSelectLocalDirectory as any);
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2 tabIndex={0} style={{ textAlign: 'center', marginTop: 0 }}>
+				Customize Playground
+			</h2>
+			<div className={forms.formGroup}>
+				<label htmlFor="wp-version" className={forms.groupLabel}>
+					Storage type
+				</label>
+				<ul className={css.radioList}>
+					<li>
+						<input
+							type="radio"
+							name="storage"
+							value="temporary"
+							id="storage-temporary"
+							className={forms.radioInput}
+							onChange={handleStorageChange}
+							checked={storage === 'temporary'}
+						/>
+						<label
+							htmlFor="storage-temporary"
+							className={forms.radioLabel}
+						>
+							Temporary: reset on page refresh
+						</label>
+					</li>
+					<li>
+						<input
+							type="radio"
+							name="storage"
+							value="opfs-browser"
+							id="storage-opfs-browser"
+							className={forms.radioInput}
+							onChange={handleStorageChange}
+							checked={storage === 'opfs-browser'}
+						/>
+						<label
+							htmlFor="storage-opfs-browser"
+							className={forms.radioLabel}
+						>
+							Persistent: stored in this browser
+						</label>
+					</li>
+					{storage === 'opfs-browser' ? (
+						<li
+							style={{ marginLeft: 40 }}
+							className={resetSite ? css.danger : ''}
+						>
+							<input
+								type="checkbox"
+								name="reset"
+								value="1"
+								disabled={
+									!!currentlyRunningWordPressVersion &&
+									wp !== currentlyRunningWordPressVersion
+								}
+								id="storage-opfs-browser-reset"
+								className={forms.radioInput}
+								onChange={(
+									event: React.ChangeEvent<HTMLInputElement>
+								) => {
+									setResetSite(event.target.checked);
+								}}
+								checked={resetSite}
+							/>
+							<label
+								htmlFor="storage-opfs-browser-reset"
+								className={forms.radioLabel}
+							>
+								Delete stored data and start fresh
+							</label>
+						</li>
+					) : null}
+					<li>
+						<input
+							type="radio"
+							name="storage"
+							value="opfs-host"
+							id="opfs-host"
+							className={
+								liveDirectoryAvailable
+									? forms.radioInput
+									: `${forms.radioInput} ${forms.notAvailable}`
+							}
+							onChange={handleStorageChange}
+							checked={storage === 'opfs-host'}
+							disabled={!liveDirectoryAvailable}
+						/>
+						<label htmlFor="opfs-host" className={forms.radioLabel}>
+							Live directory from your computer (beta)
+							{'not-available' === onSelectLocalDirectory && (
+								<span>
+									<br />
+									Not supported in this browser.
+								</span>
+							)}
+							{'origin-mismatch' === onSelectLocalDirectory && (
+								<span>
+									<br />
+									Not supported on this site.
+								</span>
+							)}
+						</label>
+					</li>
+					{storage === 'opfs-host' ? (
+						<li>
+							<div>
+								<p>
+									Sync Playground with a local directory. Your
+									files stay private and are <b>not</b>{' '}
+									uploaded anywhere.
+								</p>
+								<p>Select either a:</p>
+								<ul>
+									<li>
+										<b>Empty directory</b> – to save this
+										Playground and start syncing
+									</li>
+									<li>
+										<b>Existing Playground directory</b> –
+										to load it here and start syncing
+									</li>
+								</ul>
+								<p>
+									Files changed in Playground <b>will</b> be
+									synchronized to on your computer.
+								</p>
+								<p>
+									Files changed on your computer{' '}
+									<b>will not</b> be synchronized to
+									Playground. You'll need to click the "Sync
+									local files" button.
+								</p>
+								<div className={forms.submitRow}>
+									<OPFSButton
+										isMounting={isMountingLocalDirectory}
+										mountProgress={mountProgress}
+										onClick={handleSelectLocalDirectory}
+										size="large"
+										variant="primary"
+									>
+										Select a local directory
+									</OPFSButton>
+								</div>
+							</div>
+						</li>
+					) : null}
+				</ul>
+			</div>
+
+			{storage !== 'opfs-host' ? (
+				<>
+					<div
+						className={`${forms.formGroup} ${forms.formGroupLinear}`}
+					>
+						<label
+							htmlFor="php-version"
+							className={forms.groupLabel}
+						>
+							PHP Version
+						</label>
+						<select
+							id="php-version"
+							value={php}
+							className={forms.largeSelect}
+							onChange={(
+								event: React.ChangeEvent<HTMLSelectElement>
+							) => {
+								setPhp(
+									event.target.value as SupportedPHPVersion
+								);
+							}}
+						>
+							{SupportedPHPVersionsList.map((version) => (
+								<option key={version} value={version}>
+									PHP {version}&nbsp;
+								</option>
+							))}
+						</select>
+					</div>
+					<div
+						className={`${forms.formGroup} ${forms.formGroupLinear} ${forms.formGroupLast}`}
+					>
+						<label
+							htmlFor="wp-version"
+							className={forms.groupLabel}
+						>
+							WordPress Version
+						</label>
+						<select
+							id="wp-version"
+							value={wp}
+							className={forms.largeSelect}
+							onChange={(
+								event: React.ChangeEvent<HTMLSelectElement>
+							) => {
+								setWp(
+									event.target
+										.value as SupportedWordPressVersion
+								);
+							}}
+						>
+							{SupportedWordPressVersions.map((version) => (
+								<option key={version} value={version}>
+									WordPress {version}&nbsp;&nbsp;
+								</option>
+							))}
+						</select>
+					</div>
+					<div className={forms.submitRow}>
+						<Button type="submit" variant="primary" size="large">
+							Apply changes
+						</Button>
+					</div>
+				</>
+			) : null}
+		</form>
+	);
+}

packages/playground/website/src/main.tsx
@@ -6,9 +6,13 @@ import './styles.css';
 
 import { makeBlueprint } from './lib/make-blueprint';
 import type { Blueprint } from '@wp-playground/blueprints';
-import { PlaygroundClient } from '@wp-playground/remote';
-import SiteSetupButton from './components/site-setup-button';
-import Button from './components/button';
+import PlaygroundConfigurationGroup from './components/playground-configuration-group';
+import {
+	PlaygroundConfiguration,
+	SupportedWordPressVersions,
+} from './components/playground-configuration-group/form';
+import { SupportedPHPVersions } from '@php-wasm/universal';
+import { StorageType, StorageTypes } from './types';
 
 const query = new URL(document.location.href).searchParams;
 
@@ -46,48 +50,57 @@ try {
 	});
 }
 
-const isSeamless = (query.get('mode') || 'browser') === 'seamless';
-
 // @ts-ignore
 const opfsSupported = typeof navigator?.storage?.getDirectory !== 'undefined';
-const persistent = query.get('persistent') === '1' && opfsSupported;
+let storageRaw = query.get('storage') || '';
+if (StorageTypes.includes(storageRaw as any) && !opfsSupported) {
+	storageRaw = 'temporary';
+} else if (
+	['opfs-browser', 'opfs-host'].includes(storageRaw) &&
+	!opfsSupported
+) {
+	storageRaw = 'temporary';
+}
+const storage = storageRaw as StorageType;
+
+const isSeamless = (query.get('mode') || 'browser') === 'seamless';
+
+const currentConfiguration: PlaygroundConfiguration = {
+	wp: resolveVersion(
+		blueprint.preferredVersions?.wp,
+		SupportedWordPressVersions
+	),
+	php: resolveVersion(blueprint.preferredVersions?.php, SupportedPHPVersions),
+	storage,
+};
+
 const root = createRoot(document.getElementById('root')!);
 root.render(
 	<PlaygroundViewport
-		persistent={persistent}
+		storage={storage}
 		isSeamless={isSeamless}
 		blueprint={blueprint}
 		toolbarButtons={[
-			<SiteSetupButton
-				persistent={persistent}
-				selectedPHP={blueprint.preferredVersions?.php}
-				preferredWP={blueprint.preferredVersions?.wp}
+			<PlaygroundConfigurationGroup
+				key="configuration"
+				initialConfiguration={currentConfiguration}
 			/>,
-			persistent && <OpfsResetButton />,
-			<ImportButton key="export" />,
+			<ImportButton key="import" />,
 			<ExportButton key="export" />,
 		]}
 	/>
 );
 
-function OpfsResetButton({ playground }: { playground?: PlaygroundClient }) {
-	return (
-		<Button
-			onClick={async () => {
-				if (
-					!window.confirm(
-						'This will wipe out all data and start a new site. Do you want to proceed?'
-					)
-				) {
-					return;
-				}
-				if (persistent) {
-					await playground?.resetOpfs();
-				}
-				window.location.reload();
-			}}
-		>
-			Start over
-		</Button>
-	);
+function resolveVersion<T>(
+	version: string | undefined,
+	allVersions: readonly T[]
+): T {
+	if (
+		!version ||
+		!allVersions.includes(version as any) ||
+		version === 'latest'
+	) {
+		return allVersions[0];
+	}
+	return version as T;
 }

packages/playground/remote/src/lib/boot-playground-remote.ts
@@ -26,6 +26,7 @@ export const workerUrl: string = new URL(moduleWorkerUrl, origin) + '';
 // @ts-ignore
 import serviceWorkerPath from '../../service-worker.ts?worker&url';
 import { LatestSupportedWordPressVersion } from './get-wordpress-module';
+import type { SyncProgressCallback } from './opfs/bind-opfs';
 export const serviceWorkerUrl = new URL(serviceWorkerPath, origin);
 
 // Prevent Vite from hot-reloading this file – it would
@@ -63,7 +64,7 @@ export async function bootPlaygroundRemote() {
 		await spawnPHPWorkerThread(workerUrl, {
 			wpVersion,
 			phpVersion,
-			persistent: query.has('persistent') ? 'true' : 'false',
+			storage: query.get('storage') || '',
 		})
 	);
 
@@ -133,6 +134,18 @@ export async function bootPlaygroundRemote() {
 		 */
 		async onMessage(callback: MessageListener) {
 			return await workerApi.onMessage(callback);
+		},
+		/**
+		 * Ditto for this function.
+		 * @see onMessage
+		 * @param callback
+		 * @returns
+		 */
+		async bindOpfs(
+			opfs: FileSystemDirectoryHandle,
+			onProgress?: SyncProgressCallback
+		) {
+			return await workerApi.bindOpfs(opfs, onProgress);
 		},
 	};
 

packages/playground/remote/src/lib/opfs/bind-opfs.ts
@@ -1,0 +1,258 @@
+/**
+ * Uses the FileSystem access API to synchronize MEMFS changes in a compliant
+ * filesystem such as OPFS or local filesystem and restore them on page refresh:
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/File_System_Access_API
+ *
+ * Many synchronous functions are await-ed here because some browsers did not
+ * catch up yet with the latest spec and still return promises.
+ */
+
+/* eslint-disable prefer-rest-params */
+import { __private__dont__use } from '@php-wasm/universal';
+import { Semaphore, joinPaths } from '@php-wasm/util';
+import type { WebPHP } from '@php-wasm/web';
+import { EmscriptenFS } from './types';
+import { journalMemfsToOpfs } from './journal-memfs-to-opfs';
+
+let unbindOpfs: (() => void) | undefined;
+export type SyncProgress = {
+	/** The number of files that have been synced. */
+	files: number;
+	/** The number of all files that need to be synced. */
+	total: number;
+};
+export type SyncProgressCallback = (progress: SyncProgress) => void;
+export type BindOpfsOptions = {
+	php: WebPHP;
+	opfs: FileSystemDirectoryHandle;
+	wordPressAvailableInOPFS?: boolean;
+	onProgress?: SyncProgressCallback;
+};
+export async function bindOpfs({
+	php,
+	opfs,
+	wordPressAvailableInOPFS,
+	onProgress,
+}: BindOpfsOptions) {
+	if (unbindOpfs) {
+		await unbindOpfs();
+	}
+
+	const docroot = php.documentRoot;
+
+	if (wordPressAvailableInOPFS === undefined) {
+		wordPressAvailableInOPFS = await playgroundAvailableInOpfs(opfs);
+	}
+
+	// Setup sync between MEMFS and the local directory
+	if (wordPressAvailableInOPFS) {
+		/**
+		 * Remove DOCROOT in MEMFS to make space for the new one.
+		 * This is an in-memory operation and doesn't affect any
+		 * persisted files.
+		 */
+		try {
+			if (await php.isDir(docroot)) {
+				await php.rmdir(docroot, { recursive: true });
+				await php.mkdirTree(docroot);
+			}
+		} catch (e) {
+			// Ignore any errors
+		}
+
+		await copyOpfsToMemfs(php, opfs, docroot);
+	} else {
+		await copyMemfsToOpfs(php, opfs, docroot, onProgress);
+	}
+
+	unbindOpfs = journalMemfsToOpfs(php, opfs, docroot);
+}
+
+export async function copyOpfsToMemfs(
+	php: WebPHP,
+	opfsRoot: FileSystemDirectoryHandle,
+	memfsRoot: string
+) {
+	const PHPRuntime = php[__private__dont__use];
+	const FS = PHPRuntime.FS;
+	FS.mkdirTree(memfsRoot);
+
+	/**
+	 * Semaphores are used to limit the number of concurrent operations.
+	 * Flooding the browser with 2000 FS operations at the same time
+	 * can get quite slow.
+	 */
+	const semaphore = new Semaphore({
+		concurrency: 40,
+	});
+
+	const ops: Array<Promise<void>> = [];
+	const stack: Array<[FileSystemDirectoryHandle, string]> = [
+		[opfsRoot, memfsRoot],
+	];
+	while (stack.length > 0) {
+		const [opfsParent, memfsParentPath] = stack.pop()!;
+
+		for await (const opfsHandle of opfsParent.values()) {
+			const op = semaphore.run(async () => {
+				const memfsEntryPath = joinPaths(
+					memfsParentPath,
+					opfsHandle.name
+				);
+				if (opfsHandle.kind === 'directory') {
+					try {
+						FS.mkdir(memfsEntryPath);
+					} catch (e) {
+						if ((e as any)?.errno !== 20) {
+							console.error(e);
+							// We ignore the error if the directory already exists,
+							// and throw otherwise.
+							throw e;
+						}
+					}
+					stack.push([opfsHandle, memfsEntryPath]);
+				} else if (opfsHandle.kind === 'file') {
+					const file = await opfsHandle.getFile();
+					const byteArray = new Uint8Array(await file.arrayBuffer());
+					FS.createDataFile(
+						memfsEntryPath,
+						null,
+						byteArray,
+						true,
+						true,
+						true
+					);
+				}
+				ops.splice(ops.indexOf(op), 1);
+			});
+			ops.push(op);
+		}
+		// Let the ongoing operations catch-up to the stack.
+		while (stack.length === 0 && ops.length > 0) {
+			await Promise.any(ops);
+		}
+	}
+}
+
+export async function copyMemfsToOpfs(
+	php: WebPHP,
+	opfsRoot: FileSystemDirectoryHandle,
+	memfsRoot: string,
+	onProgress?: SyncProgressCallback
+) {
+	const PHPRuntime = php[__private__dont__use];
+	const FS = PHPRuntime.FS;
+	// Ensure the memfs directory exists.
+	FS.mkdirTree(memfsRoot);
+
+	// Create all MEMFS directories in OPFS but don't create
+	// files yet. This is quite fast.
+	const filesToCreate: Array<[FileSystemDirectoryHandle, string, string]> =
+		[];
+	async function mirrorMemfsDirectoryinOpfs(
+		memfsParent: string,
+		opfsDir: FileSystemDirectoryHandle
+	) {
+		await Promise.all(
+			FS.readdir(memfsParent)
+				.filter(
+					(entryName: string) =>
+						entryName !== '.' && entryName !== '..'
+				)
+				.map(async (entryName: string) => {
+					const memfsPath = joinPaths(memfsParent, entryName);
+					if (!isMemfsDir(FS, memfsPath)) {
+						filesToCreate.push([opfsDir, memfsPath, entryName]);
+						return;
+					}
+
+					const handle = await opfsDir.getDirectoryHandle(entryName, {
+						create: true,
+					});
+					return await mirrorMemfsDirectoryinOpfs(memfsPath, handle);
+				})
+		);
+	}
+	await mirrorMemfsDirectoryinOpfs(memfsRoot, opfsRoot);
+
+	// Now let's create all the required files in OPFS. This is quite slow
+	// so we report progress.
+	let i = 0;
+	const filesCreated = filesToCreate.map(([opfsDir, memfsPath, entryName]) =>
+		overwriteOpfsFile(opfsDir, entryName, FS, memfsPath).then(() => {
+			onProgress?.({ files: ++i, total: filesToCreate.length });
+		})
+	);
+	await Promise.all(filesCreated);
+}
+
+function isMemfsDir(FS: EmscriptenFS, path: string) {
+	return FS.isDir(FS.lookupPath(path, { follow: true }).node.mode);
+}
+
+export async function overwriteOpfsFile(
+	opfsParent: FileSystemDirectoryHandle,
+	name: string,
+	FS: EmscriptenFS,
+	memfsPath: string
+) {
+	let buffer;
+	try {
+		buffer = FS.readFile(memfsPath, {
+			encoding: 'binary',
+		});
+	} catch (e) {
+		// File was removed, ignore
+		return;
+	}
+
+	const opfsFile = await opfsParent.getFileHandle(name, { create: true });
+	const writer =
+		opfsFile.createWritable !== undefined
+			? // Google Chrome, Firefox, probably more browsers
+			  await opfsFile.createWritable()
+			: // Safari
+			  await opfsFile.createSyncAccessHandle();
+	try {
+		await writer.truncate(0);
+		await writer.write(buffer);
+	} finally {
+		await writer.close();
+	}
+}
+
+export async function opfsFileExists(
+	opfs: FileSystemDirectoryHandle,
+	path: string
+) {
+	try {
+		await opfs.getFileHandle(path);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+export async function playgroundAvailableInOpfs(
+	opfs: FileSystemDirectoryHandle
+) {
+	try {
+		/**
+		 * Assume it's a Playground directory if these files exist:
+		 * - wp-config.php
+		 * - wp-content/database/.ht.sqlite
+		 */
+		await opfs.getFileHandle('wp-config.php', { create: false });
+		const wpContent = await opfs.getDirectoryHandle('wp-content', {
+			create: false,
+		});
+		const database = await wpContent.getDirectoryHandle('database', {
+			create: false,
+		});
+		await database.getFileHandle('.ht.sqlite', { create: false });
+	} catch (e) {
+		return false;
+	}
+	return true;
+}

packages/playground/remote/src/lib/opfs/journal-memfs-to-opfs.ts
@@ -17,7 +17,7 @@ import type {
 } from './types';
 import { __private__dont__use } from '@php-wasm/universal';
 import { joinPaths } from '@php-wasm/util';
-import { copyMemfsToOpfs, overwriteOpfsFile } from './opfs-memfs';
+import { copyMemfsToOpfs, overwriteOpfsFile } from './bind-opfs';
 
 export function journalMemfsToOpfs(
 	php: WebPHP,
@@ -198,6 +198,7 @@ class FilesystemJournal {
 			FS.unlink = originalUnlink;
 			FS.mkdir = originalMkdir;
 			FS.rmdir = originalRmdir;
+			FS.hasJournal = false;
 		};
 		return journal;
 	}

packages/playground/remote/src/lib/opfs/opfs-memfs.ts
@@ -1,179 +1,0 @@
-/**
- * Uses the FileSystem access API to synchronize MEMFS changes in a compliant
- * filesystem such as OPFS or local filesystem and restore them on page refresh:
- *
- * https://developer.mozilla.org/en-US/docs/Web/API/File_System_Access_API
- *
- * Many synchronous functions are await-ed here because some browsers did not
- * catch up yet with the latest spec and still return promises.
- */
-
-/* eslint-disable prefer-rest-params */
-import { __private__dont__use } from '@php-wasm/universal';
-import { Semaphore, joinPaths } from '@php-wasm/util';
-import type { WebPHP } from '@php-wasm/web';
-import { EmscriptenFS } from './types';
-
-export async function copyOpfsToMemfs(
-	php: WebPHP,
-	opfsRoot: FileSystemDirectoryHandle,
-	memfsRoot: string
-) {
-	const PHPRuntime = php[__private__dont__use];
-	const FS = PHPRuntime.FS;
-	FS.mkdirTree(memfsRoot);
-
-	/**
-	 * Semaphores are used to limit the number of concurrent operations.
-	 * Flooding the browser with 2000 FS operations at the same time
-	 * can get quite slow.
-	 */
-	const semaphore = new Semaphore({
-		concurrency: 40,
-	});
-
-	const ops: Array<Promise<void>> = [];
-	const stack: Array<[FileSystemDirectoryHandle, string]> = [
-		[opfsRoot, memfsRoot],
-	];
-	while (stack.length > 0) {
-		const [opfsParent, memfsParentPath] = stack.pop()!;
-
-		for await (const opfsHandle of opfsParent.values()) {
-			const op = semaphore.run(async () => {
-				const memfsEntryPath = joinPaths(
-					memfsParentPath,
-					opfsHandle.name
-				);
-				if (opfsHandle.kind === 'directory') {
-					try {
-						FS.mkdir(memfsEntryPath);
-					} catch (e) {
-						if ((e as any)?.errno !== 20) {
-							console.error(e);
-							// We ignore the error if the directory already exists,
-							// and throw otherwise.
-							throw e;
-						}
-					}
-					stack.push([opfsHandle, memfsEntryPath]);
-				} else if (opfsHandle.kind === 'file') {
-					const file = await opfsHandle.getFile();
-					const byteArray = new Uint8Array(await file.arrayBuffer());
-					FS.createDataFile(
-						memfsEntryPath,
-						null,
-						byteArray,
-						true,
-						true,
-						true
-					);
-				}
-				ops.splice(ops.indexOf(op), 1);
-			});
-			ops.push(op);
-		}
-		// Let the ongoing operations catch-up to the stack.
-		while (stack.length === 0 && ops.length > 0) {
-			await Promise.any(ops);
-		}
-	}
-}
-
-export async function copyMemfsToOpfs(
-	php: WebPHP,
-	opfsRoot: FileSystemDirectoryHandle,
-	memfsRoot: string
-) {
-	const PHPRuntime = php[__private__dont__use];
-	const FS = PHPRuntime.FS;
-	// Ensure the memfs directory exists.
-	FS.mkdirTree(memfsRoot);
-
-	/**
-	 * Semaphores are used to limit the number of concurrent operations.
-	 * Flooding the browser with 2000 FS operations at the same time
-	 * can get quite slow.
-	 */
-	const semaphore = new Semaphore({
-		concurrency: 40,
-	});
-	const ops: Array<Promise<any>> = [];
-	const stack: Array<[Promise<FileSystemDirectoryHandle>, string]> = [
-		[Promise.resolve(opfsRoot), memfsRoot],
-	];
-	while (stack.length) {
-		const [opfsDirPromise, memfsParent] = stack.pop()!;
-		const opfsDir = await opfsDirPromise;
-
-		for (const entryName of FS.readdir(memfsParent)) {
-			if (entryName === '.' || entryName === '..') continue;
-
-			const memfsPath = joinPaths(memfsParent, entryName);
-			const lookup = FS.lookupPath(memfsPath, {
-				follow: true,
-			});
-			const memFsNode = lookup.node;
-			const isDir = FS.isDir(memFsNode.mode);
-
-			const op = semaphore.run(async () => {
-				if (isDir) {
-					const handlePromise = opfsDir.getDirectoryHandle(
-						entryName,
-						{
-							create: true,
-						}
-					);
-					stack.push([handlePromise, memfsPath]);
-				} else {
-					await overwriteOpfsFile(opfsDir, entryName, FS, memfsPath);
-				}
-				ops.splice(ops.indexOf(op), 1);
-			});
-			ops.push(op);
-		}
-
-		// Let the ongoing operations catch-up to the stack.
-		while (stack.length === 0 && ops.length > 0) {
-			await Promise.any(ops);
-		}
-	}
-}
-
-export async function overwriteOpfsFile(
-	opfsParent: FileSystemDirectoryHandle,
-	name: string,
-	FS: EmscriptenFS,
-	memfsPath: string
-) {
-	let buffer;
-	try {
-		buffer = FS.readFile(memfsPath, {
-			encoding: 'binary',
-		});
-	} catch (e) {
-		// File was removed, ignore
-		return;
-	}
-
-	const opfsFile = await opfsParent.getFileHandle(name, { create: true });
-	const accessHandle = await opfsFile.createSyncAccessHandle();
-	try {
-		await accessHandle.truncate(0);
-		await accessHandle.write(buffer);
-	} finally {
-		await accessHandle.close();
-	}
-}
-
-export async function opfsFileExists(
-	opfs: FileSystemDirectoryHandle,
-	path: string
-) {
-	try {
-		await opfs.getFileHandle(path);
-		return true;
-	} catch (e) {
-		return false;
-	}
-}

packages/playground/remote/src/lib/opfs/types.ts
@@ -19,5 +19,15 @@ declare global {
 		move(target: FileSystemDirectoryHandle): Promise<void>;
 		move(name: string): Promise<void>;
 		move(target: FileSystemDirectoryHandle, name: string): Promise<void>;
+		createWritable(): Promise<FileSystemWritableFileStream>;
+	}
+	interface FileSystemWritableFileStream {
+		write(
+			buffer: BufferSource,
+			options?: FileSystemReadWriteOptions
+		): Promise<number>;
+		close(): Promise<void>;
+		seek(offset: number): Promise<void>;
+		truncate(newSize: number): Promise<void>;
 	}
 }

commit 652cee232f88287093a1fced5c30f9588ed4271b

Restore WP nightly into the configuration form
It was accidentally removed due to a regression in c71e7a9b

packages/playground/website/src/components/playground-configuration-group/form.tsx
@@ -11,7 +11,14 @@ import Button from '../button';
 
 // This is duplicated in @wp-playground/remote
 // @TODO: move to a shared package like @wp-playground/wordpress
-export const SupportedWordPressVersions = ['6.2', '6.1', '6.0', '5.9'] as const;
+export const SupportedWordPressVersions = [
+	'nightly',
+	'6.2',
+	'6.1',
+	'6.0',
+	'5.9',
+] as const;
+const LatestSupportedWordPressVersion = '6.2';
 export const SupportedWordPressVersionsList =
 	SupportedWordPressVersions as any as string[];
 export type SupportedWordPressVersion =
@@ -46,7 +53,9 @@ export function PlaygroundConfigurationForm({
 }: PlaygroundConfigurationFormProps) {
 	const [php, setPhp] = useState(initialData.php);
 	const [storage, setStorage] = useState<StorageType>(initialData.storage);
-	const [wp, setWp] = useState(initialData.wp);
+	const [wp, setWp] = useState(
+		initialData.wp || LatestSupportedWordPressVersion
+	);
 	const handleStorageChange = async (
 		event: React.ChangeEvent<HTMLInputElement>
 	) => {

packages/playground/website/src/main.tsx
@@ -13,6 +13,7 @@ import {
 } from './components/playground-configuration-group/form';
 import { SupportedPHPVersions } from '@php-wasm/universal';
 import { StorageType, StorageTypes } from './types';
+import { LatestSupportedWordPressVersion } from '@wp-playground/remote';
 
 const query = new URL(document.location.href).searchParams;
 
@@ -52,13 +53,10 @@ try {
 
 // @ts-ignore
 const opfsSupported = typeof navigator?.storage?.getDirectory !== 'undefined';
-let storageRaw = query.get('storage') || '';
+let storageRaw = query.get('storage');
 if (StorageTypes.includes(storageRaw as any) && !opfsSupported) {
 	storageRaw = 'temporary';
-} else if (
-	['opfs-browser', 'opfs-host'].includes(storageRaw) &&
-	!opfsSupported
-) {
+} else if (!StorageTypes.includes(storageRaw as any)) {
 	storageRaw = 'temporary';
 }
 const storage = storageRaw as StorageType;
@@ -68,10 +66,11 @@ const isSeamless = (query.get('mode') || 'browser') === 'seamless';
 const currentConfiguration: PlaygroundConfiguration = {
 	wp: resolveVersion(
 		blueprint.preferredVersions?.wp,
-		SupportedWordPressVersions
+		SupportedWordPressVersions,
+		LatestSupportedWordPressVersion
 	),
 	php: resolveVersion(blueprint.preferredVersions?.php, SupportedPHPVersions),
-	storage,
+	storage: storage || 'temporary',
 };
 
 const root = createRoot(document.getElementById('root')!);
@@ -93,14 +92,15 @@ root.render(
 
 function resolveVersion<T>(
 	version: string | undefined,
-	allVersions: readonly T[]
+	allVersions: readonly T[],
+	defaultVersion: T = allVersions[0]
 ): T {
 	if (
 		!version ||
 		!allVersions.includes(version as any) ||
 		version === 'latest'
 	) {
-		return allVersions[0];
+		return defaultVersion;
 	}
 	return version as T;
 }