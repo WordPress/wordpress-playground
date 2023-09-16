6f3515860f24b4515b9442aab7eeb6ad1f467740
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-09-16 01:38:01 -0400

    Faster build.

--- packages/playground/compile-wordpress/build-assets/esm-prefix.js
+++ packages/playground/compile-wordpress/build-assets/esm-prefix.js
@@ -13,10 +13,11 @@ export const defaultThemeName = WP_THEME_NAME;
 // into an ESM module.
 // This replaces the Emscripten's MODULARIZE=1 which pollutes the
 // global namespace and does not play well with import() mechanics.
-export default function (PHPModule) {   
+export default function (PHPModule) {
+	const asyncifyStubs = {};
     return new Promise(function(resolve, reject) {
         function onLoadingFailed(error) {
             const wrappingError = new Error(`Failed to load data dependency module "${dependencyFilename}"${typeof error === 'string' ? ` (${error})` : ''}`);
             wrappingError.cause = error instanceof Error ? error : null;
-            reject(wrappingError);        
+            reject(wrappingError);
         };
