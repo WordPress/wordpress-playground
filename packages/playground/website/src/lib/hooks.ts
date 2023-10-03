0cc574fdda8fe1512882a69f73eb8687880334a4
Marc Armengou <83702259+MarcArmengou@users.noreply.github.com>
2023-10-03 13:11:57 +0200

    Update hooks.ts

--- packages/playground/website/src/lib/hooks.ts
+++ packages/playground/website/src/lib/hooks.ts
@@ -5,7 +5,7 @@ import { buildVersion } from './config';
 
 interface UsePlaygroundOptions {
 	blueprint?: Blueprint;
-	storage?: 'opfs-host' | 'browser' | 'temporary';
+	storage?: 'opfs-host' | 'opfs-browser' | 'browser' | 'temporary';
 }
 export function usePlayground({ blueprint, storage }: UsePlaygroundOptions) {
 	const iframeRef = useRef<HTMLIFrameElement>(null);
