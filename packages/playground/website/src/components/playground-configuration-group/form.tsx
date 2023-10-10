WordPress/wordpress-playground

commit a359dec90d3a2aae9ad415ea5bfe543a2aa0029d

Rename opfs-host to device and temporary to none in Query API (#661)
Changed the param names as discussed at:
https://github.com/WordPress/wordpress-playground/issues/636

Kept the old names working, for now.

Co-authored-by: Adam Zielinski <adam@adamziel.com>

packages/playground/website/src/components/playground-configuration-group/form.tsx
@@ -107,17 +107,17 @@ export function PlaygroundConfigurationForm({
 						<input
 							type="radio"
 							name="storage"
-							value="temporary"
-							id="storage-temporary"
+							value="none"
+							id="storage-none"
 							className={forms.radioInput}
 							onChange={handleStorageChange}
-							checked={storage === 'temporary'}
+							checked={storage === 'none'}
 						/>
 						<label
-							htmlFor="storage-temporary"
+							htmlFor="storage-none"
 							className={forms.radioLabel}
 						>
-							Temporary: reset on page refresh
+							None: changes will be lost on page refresh.
 						</label>
 					</li>
 					<li>
@@ -134,7 +134,7 @@ export function PlaygroundConfigurationForm({
 							htmlFor="storage-browser"
 							className={forms.radioLabel}
 						>
-							Persistent: stored in this browser
+							Browser: stored in this browser (cookies).
 						</label>
 					</li>
 					{storage === 'opfs-browser' ? (
@@ -171,19 +171,19 @@ export function PlaygroundConfigurationForm({
 						<input
 							type="radio"
 							name="storage"
-							value="opfs-host"
-							id="opfs-host"
+							value="device"
+							id="device"
 							className={
 								liveDirectoryAvailable
 									? forms.radioInput
 									: `${forms.radioInput} ${forms.notAvailable}`
 							}
 							onChange={handleStorageChange}
-							checked={storage === 'opfs-host'}
+							checked={storage === 'device'}
 							disabled={!liveDirectoryAvailable}
 						/>
-						<label htmlFor="opfs-host" className={forms.radioLabel}>
-							Live directory from your computer (beta)
+						<label htmlFor="device" className={forms.radioLabel}>
+							Device: stored locally in your device (beta).
 							{'not-available' === onSelectLocalDirectory && (
 								<span>
 									<br />
@@ -198,7 +198,7 @@ export function PlaygroundConfigurationForm({
 							)}
 						</label>
 					</li>
-					{storage === 'opfs-host' ? (
+					{(storage === 'opfs-host' || storage === 'device') ? (
 						<li>
 							<div>
 								<p>
@@ -243,8 +243,7 @@ export function PlaygroundConfigurationForm({
 					) : null}
 				</ul>
 			</div>
-
-			{storage !== 'opfs-host' ? (
+			{(storage === 'opfs-host' || storage === 'device') ? (
 				<>
 					<div
 						className={`${forms.formGroup} ${forms.formGroupLinear}`}

packages/playground/website/src/components/playground-configuration-group/index.tsx
@@ -61,7 +61,7 @@ export default function PlaygroundConfigurationGroup({
 	}, [!!playground]);
 
 	const [isResumeLastDirOpen, setResumeLastDirOpen] = useState(
-		initialConfiguration.storage === 'opfs-host' && !!lastDirectoryHandle
+		(initialConfiguration.storage === 'opfs-host' || initialConfiguration.storage === 'device') && !!lastDirectoryHandle
 	);
 	const closeResumeLastDirModal = () => setResumeLastDirOpen(false);
 
@@ -140,13 +140,13 @@ export default function PlaygroundConfigurationGroup({
 
 			setCurrentConfiguration({
 				...currentConfiguration,
-				storage: 'opfs-host',
+				storage: 'device',
 			});
 			await playground.goTo('/');
 
-			// Read current querystring and replace storage=browser with storage=opfs-host.
+			// Read current querystring and replace storage=browser with storage=device.
 			const url = new URL(window.location.href);
-			url.searchParams.set('storage', 'opfs-host');
+			url.searchParams.set('storage', 'device');
 			window.history.pushState({}, '', url.toString());
 
 			alert('You are now using WordPress from your local directory.');
@@ -174,13 +174,13 @@ export default function PlaygroundConfigurationGroup({
 			<Button onClick={openModal}>
 				PHP {currentConfiguration.php} {' - '}
 				WP {runningWp || currentConfiguration.wp} {' - '}
-				{currentConfiguration.storage === 'opfs-host'
+				{(currentConfiguration.storage === 'opfs-host' || currentConfiguration.storage === 'device')
 					? `Local (${dirName})`
 					: currentConfiguration.storage === 'opfs-browser' || currentConfiguration.storage === 'browser'
 					? 'Persistent'
 					: '⚠️ Temporary'}
 			</Button>
-			{currentConfiguration.storage === 'opfs-host' ? (
+			{(currentConfiguration.storage === 'opfs-host' || currentConfiguration.storage === 'device') ? (
 				<SyncLocalFilesButton />
 			) : null}
 			{currentConfiguration.storage === 'opfs-browser' || currentConfiguration.storage === 'browser' ? (

packages/playground/website/src/lib/hooks.ts
@@ -5,7 +5,7 @@ import { buildVersion } from './config';
 
 interface UsePlaygroundOptions {
 	blueprint?: Blueprint;
-	storage?: 'opfs-host' | 'opfs-browser' | 'browser' | 'temporary';
+	storage?: 'opfs-host' | 'opfs-browser' | 'browser' | 'device' | 'none' | 'temporary';
 }
 export function usePlayground({ blueprint, storage }: UsePlaygroundOptions) {
 	const iframeRef = useRef<HTMLIFrameElement>(null);

packages/playground/website/src/types.ts
@@ -1,2 +1,2 @@
-export const StorageTypes = ['browser', 'temporary', 'opfs-host', 'opfs-browser'] as const;
+export const StorageTypes = ['browser', 'device', 'temporary', 'none', 'opfs-host', 'opfs-browser'] as const;
 export type StorageType = (typeof StorageTypes)[number];

commit ef6aa7f499f4f1593774cb71f6d16d7715feb352

Adjust invalid conditional in form.tsx to restore the PHP and WP version picker (#675)
Fixes issue #671 introduced in #661

cc @MarcArmengou

packages/playground/website/src/components/playground-configuration-group/form.tsx
@@ -243,7 +243,7 @@ export function PlaygroundConfigurationForm({
 					) : null}
 				</ul>
 			</div>
-			{(storage === 'opfs-host' || storage === 'device') ? (
+			{(storage !== 'device' && storage !== 'opfs-host') ? (
 				<>
 					<div
 						className={`${forms.formGroup} ${forms.formGroupLinear}`}