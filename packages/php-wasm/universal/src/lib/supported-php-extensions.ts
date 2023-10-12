a927b0b32915b5c54014d311e480f9d985ac10e8
Adam Zieliński <adam@adamziel.com>
2023-10-12 12:46:51 +0200

    Do not rely on as const or any kind of inference in packages/php-wasm/universal/src/lib/supported-php-extensions.ts

--- packages/php-wasm/universal/src/lib/supported-php-extensions.ts
+++ packages/php-wasm/universal/src/lib/supported-php-extensions.ts
@@ -2,7 +2,6 @@ export type SupportedPHPExtension = 'mbstring' | 'xml-bundle' | 'gd';
 export const SupportedPHPExtensionsList = ['mbstring', 'xml-bundle', 'gd'];
 
 export const SupportedPHPExtensionBundles = {
-	'kitchen-sink': SupportedPHPExtensionsList,
-} as const;
-export type SupportedPHPExtensionBundle =
-	keyof typeof SupportedPHPExtensionBundles;
+	'kitchen-sink': ['mbstring', 'xml-bundle', 'gd'],
+};
+export type SupportedPHPExtensionBundle = 'kitchen-sink';
