fed72bdd180511ceeeb97d07b67d3fd7d4caba41
Adam Zieliński <adam@adamziel.com>
2023-10-12 13:22:09 +0200

    Explain why is the extensions list repeated

--- packages/php-wasm/universal/src/lib/supported-php-extensions.ts
+++ packages/php-wasm/universal/src/lib/supported-php-extensions.ts
@@ -1,5 +1,12 @@
+// The extensions list is repeated on purpose to avoid CI errors
+// @see https://github.com/WordPress/wordpress-playground/pull/687
 export type SupportedPHPExtension = 'iconv' | 'mbstring' | 'xml-bundle' | 'gd';
-export const SupportedPHPExtensionsList = ['iconv', 'mbstring', 'xml-bundle', 'gd'];
+export const SupportedPHPExtensionsList = [
+	'iconv',
+	'mbstring',
+	'xml-bundle',
+	'gd',
+];
 
 export const SupportedPHPExtensionBundles = {
 	'kitchen-sink': SupportedPHPExtensionsList,
