78ef1b2ab78a379d14676c6aec866721b6b88e52
Adam Zieliński <adam@adamziel.com>
2023-10-16 12:30:07 +0200

    Apply browser-targeted WordPress patches via Blueprints, not in remote.html.
    Enables patching arbitrary WordPress bundles for compatibility with web
    browsers. Without this PR, remote.html applies these patch right after
    the WordPress module is loaded into the /wordpress VFS directory.
    However, in #700 we want to enable using an arbitrary WordPress build
    from any PR in wordpress-develop. We must, thus, expose a way of
    applying the necessary patches.
    
    Testing instructions
    
    1. Start Playground, log out
    2. Go to /wp-login.php
    3. Confirm there's a message explaining what login credentials to use

--- packages/playground/remote/src/lib/worker-thread.ts
+++ packages/playground/remote/src/lib/worker-thread.ts
@@ -13,7 +13,6 @@ import {
 	SupportedPHPVersion,
 	SupportedPHPVersionsList,
 } from '@php-wasm/universal';
-import { applyWebWordPressPatches } from './web-wordpress-patches';
 import {
 	SyncProgressCallback,
 	bindOpfs,
@@ -175,7 +174,6 @@ try {
 		 * already applied.
 		 */
 		await wordPressModule;
-		applyWebWordPressPatches(php);
 		await applyWordPressPatches(php, {
 			wordpressPath: DOCROOT,
 			patchSecrets: true,
