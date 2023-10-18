75a69bb36e1dd0f4ad4b05171fbddb465065a079
Adam Zieliński <adam@adamziel.com>
2023-10-18 13:50:00 +0200

    Don't rebuild static assets

--- packages/playground/remote/src/wordpress/wp-5.9.js
+++ packages/playground/remote/src/wordpress/wp-5.9.js
@@ -6,6 +6,9 @@ export const dependenciesTotalSize = 13146228;
 import dependencyFilename from './wp-5.9.data?url';
 export { dependencyFilename };
 
+// The default theme name – populated by Dockerfile.
+export const defaultThemeName = 'twentytwentyone';
+
 // Prepending this to the built php.js file manually turns it
 // into an ESM module.
 // This replaces the Emscripten's MODULARIZE=1 which pollutes the
