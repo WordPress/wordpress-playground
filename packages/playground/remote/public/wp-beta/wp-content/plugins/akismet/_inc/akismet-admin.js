8da5f20fb2963b7a2956016a5cbd9520b2685240
Adam Zieliński <adam@adamziel.com>
2023-10-18 13:47:53 +0200

    Don't rebuild static assets

--- packages/playground/remote/public/wp-beta/wp-content/plugins/akismet/_inc/akismet-admin.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-	// Prevent aggressive iframe caching in Firefox
-	var statsIframe = document.getElementById( 'stats-iframe' );
-	if ( statsIframe ) {
-		statsIframe.contentWindow.location.href = statsIframe.src;
-	}
-} );
