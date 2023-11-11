9f8d4ed41b6b8c26f6ae072cbc659e61e960d6b6
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-10 22:23:32 -0500

    Optimizing.

--- packages/playground/website/src/lib/preview-service.ts
+++ packages/playground/website/src/lib/preview-service.ts
@@ -2,15 +2,19 @@ import { unzip } from '@wp-playground/blueprints';
 import { activatePlugin } from '@wp-playground/blueprints';
 
 type previewData = {
+	preloader: ArrayBuffer;
+	postloader: ArrayBuffer;
 	zipPackage: ArrayBuffer;
+	plugin: ArrayBuffer;
 	pluginName: string;
-	pluginUrl: string;
 };
 
 export class PreviewService {
+	static preloader: ArrayBuffer | null;
+	static postloader: ArrayBuffer | null;
 	static zipPackage: ArrayBuffer | null;
+	static plugin: ArrayBuffer | null;
 	static pluginName: string | null;
-	static pluginUrl: string | null;
 	static hasPreview: boolean | null;
 
 	static listen() {
@@ -18,7 +22,6 @@ export class PreviewService {
 			if (event.data?.type !== 'collector-zip-package') {
 				return;
 			}
-
 			PreviewService.setPreview(event.data);
 			window.removeEventListener('message', listenForPreview);
 		};
@@ -27,9 +30,11 @@ export class PreviewService {
 	}
 
 	static setPreview(previewData: previewData) {
+		this.preloader = previewData.preloader;
+		this.postloader = previewData.postloader;
 		this.zipPackage = previewData.zipPackage;
 		this.pluginName = previewData.pluginName;
-		this.pluginUrl = previewData.pluginUrl;
+		this.plugin = previewData.plugin;
 		this.hasPreview = true;
 	}
 
@@ -37,16 +42,20 @@ export class PreviewService {
 		if (!this.hasPreview) {
 			return;
 		}
+		this.preloader = null;
+		this.postloader = null;
 		this.zipPackage = null;
 		this.pluginName = null;
-		this.pluginUrl = null;
+		this.plugin = null;
 		this.hasPreview = false;
 	}
 
 	static installPreview(playground: any) {
-		const zipPackage = PreviewService.zipPackage;
-		const pluginName = PreviewService.pluginName;
-		const pluginUrl = PreviewService.pluginUrl;
+		const preloader = this.preloader ? new Uint8Array(this.preloader) : 'Loading Resources...';
+		const postloader = this.postloader ? new Uint8Array(this.postloader) : 'Activating Plugin...';
+		const zipPackage = this.zipPackage;
+		const pluginName = this.pluginName;
+		const plugin = this.plugin;
 
 		PreviewService.clearPreview();
 
@@ -55,8 +64,9 @@ export class PreviewService {
 		}
 
 		return playground
-			.writeFile('/wordpress/collector-init.html', '...')
-			.then(() => playground.goTo(`/collector-init.html`))
+			.writeFile('/wordpress/collector-loading.html', preloader)
+			.then(() => playground.writeFile('/wordpress/collector-activate.html', postloader))
+			.then(() => playground.goTo(`/collector-loading.html`))
 			.then(() =>
 				playground.writeFile(
 					'/tmp/690013d3-b53b-43f2-8371-b293a3bdc4fb',
@@ -75,27 +85,18 @@ export class PreviewService {
 					extractToPath: '/wordpress',
 				})
 			)
+			.then(() => playground.goTo(`/collector-activate.html`))
 			.then(() =>
 				activatePlugin(playground, {
 					pluginName: 'Collector',
 					pluginPath: '/wordpress/wp-content/plugins/Collector',
 				})
 			)
-			.then(() =>
-				playground.goTo(
-					`/wp-content/plugins/Collector/please-wait.html`
-				)
-			)
-			.then(() => new Promise((accept) => setTimeout(accept, 2500)))
 			.then(() => {
-				if (pluginUrl && pluginName) {
-					return fetch(pluginUrl)
-						.then((r) => r.arrayBuffer())
-						.then((arrayBuffer) =>
-							playground.writeFile(
-								'/plugin.zip',
-								new Uint8Array(arrayBuffer)
-							)
+				if (plugin && pluginName) {
+					return playground.writeFile(
+							'/plugin.zip',
+							new Uint8Array(plugin)
 						)
 						.then(() =>
 							unzip(playground, {
