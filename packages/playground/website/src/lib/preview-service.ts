756ad0300ce9fbc3b37734cebabfaae581776e0d
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-11-11 00:12:45 -0500

    Log into real account with fake password.

--- packages/playground/website/src/lib/preview-service.ts
+++ packages/playground/website/src/lib/preview-service.ts
@@ -1,11 +1,14 @@
 import { unzip } from '@wp-playground/blueprints';
 import { activatePlugin } from '@wp-playground/blueprints';
+import { login } from '@wp-playground/blueprints';
 
 type previewData = {
 	preloader: ArrayBuffer;
 	postloader: ArrayBuffer;
 	zipPackage: ArrayBuffer;
 	plugin: ArrayBuffer;
+	username: string;
+	fakepass: string;
 	pluginName: string;
 };
 
@@ -15,9 +18,20 @@ export class PreviewService {
 	static zipPackage: ArrayBuffer | null;
 	static plugin: ArrayBuffer | null;
 	static pluginName: string | null;
+	static username: string | null;
+	static fakepass: string | null;
 	static hasPreview: boolean | null;
 
 	static listen() {
+
+		if(window.parent)
+		{
+			window.parent.postMessage(
+				{type:'preview-service-listening'},
+				new URL(document.referrer).origin,
+			);
+		}
+
 		const listenForPreview = (event: MessageEvent) => {
 			if (event.data?.type !== 'collector-zip-package') {
 				return;
@@ -34,6 +48,8 @@ export class PreviewService {
 		this.postloader = previewData.postloader;
 		this.zipPackage = previewData.zipPackage;
 		this.pluginName = previewData.pluginName;
+		this.username = previewData.username;
+		this.fakepass = previewData.fakepass;
 		this.plugin = previewData.plugin;
 		this.hasPreview = true;
 	}
@@ -46,15 +62,23 @@ export class PreviewService {
 		this.postloader = null;
 		this.zipPackage = null;
 		this.pluginName = null;
+		this.username = null;
+		this.fakepass = null;
 		this.plugin = null;
 		this.hasPreview = false;
 	}
 
 	static installPreview(playground: any) {
-		const preloader = this.preloader ? new Uint8Array(this.preloader) : 'Loading Resources...';
-		const postloader = this.postloader ? new Uint8Array(this.postloader) : 'Activating Plugin...';
+		const preloader = this.preloader
+			? new Uint8Array(this.preloader)
+			: 'Loading Resources...';
+		const postloader = this.postloader
+			? new Uint8Array(this.postloader)
+			: 'Activating Plugin...';
 		const zipPackage = this.zipPackage;
 		const pluginName = this.pluginName;
+		const username = this.username;
+		const fakepass = this.fakepass;
 		const plugin = this.plugin;
 
 		PreviewService.clearPreview();
@@ -65,7 +89,12 @@ export class PreviewService {
 
 		return playground
 			.writeFile('/wordpress/collector-loading.html', preloader)
-			.then(() => playground.writeFile('/wordpress/collector-activate.html', postloader))
+			.then(() =>
+				playground.writeFile(
+					'/wordpress/collector-activate.html',
+					postloader
+				)
+			)
 			.then(() => playground.goTo(`/collector-loading.html`))
 			.then(() =>
 				playground.writeFile(
@@ -94,10 +123,8 @@ export class PreviewService {
 			)
 			.then(() => {
 				if (plugin && pluginName) {
-					return playground.writeFile(
-							'/plugin.zip',
-							new Uint8Array(plugin)
-						)
+					return playground
+						.writeFile('/plugin.zip', new Uint8Array(plugin))
 						.then(() =>
 							unzip(playground, {
 								extractToPath: '/wordpress/wp-content/plugins',
@@ -111,11 +138,17 @@ export class PreviewService {
 									pluginName,
 								pluginName,
 							})
-						)
-						.then(() => playground?.goTo(`/wp-admin/plugins.php`));
+						);
 				} else {
 					return Promise.resolve();
 				}
-			});
+			})
+			.then(() => login(
+				playground, {
+					username: String(username),
+					password: String(fakepass),
+				}
+			))
+			.then(() => playground?.goTo(`/wp-admin/plugins.php`));
 	}
 }
