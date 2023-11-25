84cd69a79dc3046a1ae2b446d017b0425680530a
Adam Zieliński <adam@adamziel.com>
2023-11-25 16:11:35 +0100

    Add minimal Cypress E2E tests

---
+++ packages/playground/website/cypress.config.ts
@@ -0,0 +1,6 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+	e2e: nxE2EPreset(new URL(import.meta.url).pathname, { cypressDir: 'cypress' }),
+});
