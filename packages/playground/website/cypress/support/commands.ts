1e6087204550f8de0a843e45a7dd3306b2c60fd9
Adam Zieliński <adam@adamziel.com>
2023-11-27 00:17:02 +0100

    Try waiting for iframe

--- packages/playground/website/cypress/support/commands.ts
+++ packages/playground/website/cypress/support/commands.ts
@@ -18,8 +18,10 @@ declare namespace Cypress {
 		inRemoteIframe(
 			callback: (wordPressiFrame: Chainable<Element>) => void
 		): void;
+		inIframeBody(selector: string, callback: Function): void;
 		inWordPressIframe(callback: Function): void;
 		setWordPressUrl(url: string): void;
+		waitForWordPress(url?: string): void;
 	}
 }
 
@@ -31,32 +33,56 @@ Cypress.Commands.add('getIframeBody', (iframeSelector: string) => {
 		.then(cy.wrap) as Cypress.Chainable<Element>;
 });
 
+Cypress.Commands.add('inIframeBody', (selector: string, callback: Function) => {
+	cy.getIframeBody(selector).should('exist');
+	cy.getIframeBody(selector).within(callback as any);
+});
+
 Cypress.Commands.add('inRemoteIframe', (callback: Function) => {
-	cy.getIframeBody('iframe#playground-viewport').should('exist');
-	cy.getIframeBody('iframe#playground-viewport').within(() => {
-		callback();
-	});
+	cy.inIframeBody('iframe#playground-viewport', callback);
 });
 
 Cypress.Commands.add('inWordPressIframe', (callback: Function) => {
+	cy.waitForWordPress();
+	cy.inIframeBody('iframe#playground-viewport', () => {
+		// For some reason we need to wait a few ms here, even
+		// though Cypress should be waiting for the iframe to load
+		// eslint-disable-next-line cypress/no-unnecessary-waiting
+		// cy.wait(50);
+		cy.inIframeBody('iframe#wp', callback);
+	});
+});
+
+Cypress.Commands.add('waitForWordPress', (expectedUrl?: string) => {
+	// Wait for WordPress iframe to change src
 	cy.inRemoteIframe(() => {
-		cy.getIframeBody('iframe#wp').should('exist');
-		cy.getIframeBody('iframe#wp').within(() => {
-			cy.document().should('exist');
-			// For some reason we need to wait a few ms here, even
-			// though Cypress should be waiting for the iframe to load
-			// eslint-disable-next-line cypress/no-unnecessary-waiting
-			// cy.wait(50);
-			callback();
-		});
+		// cy.get('iframe#wp').should('have.attr', 'src');
+		// if (expectedUrl !== undefined) {
+		// 	cy.get('iframe#wp')
+		// 		.invoke('attr', 'src')
+		// 		.should((actualUrl) => {
+		// 			const pathname = new URL(actualUrl + '').pathname;
+		// 			expect(pathname).to.match(
+		// 				new RegExp(
+		// 					`(/scope:0\\.\\d+)?${escapeRegExp(expectedUrl)}`
+		// 				)
+		// 			);
+		// 		});
+		// }
+	});
+	// Wait for the WordPress document to load
+	cy.inWordPressIframe(() => {
+		cy.document().should('exist');
 	});
 });
 
 Cypress.Commands.add('setWordPressUrl', (url: string) => {
 	cy.get('input[name=url]').should('be.visible').type(url);
 	cy.get('input[name=url]').type('{enter}');
-	// Wait for WordPress to reload
-	cy.inWordPressIframe(() => {
-		cy.document().should('exist');
-	});
+
+	cy.waitForWordPress(url);
 });
+
+function escapeRegExp(string: string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
