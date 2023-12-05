90c0e44e2ff64789cec80800f0a182da692881ba
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-12-05 09:42:04 -0500

    Reverting.

--- packages/playground/blueprints/src/lib/steps/common.ts
+++ packages/playground/blueprints/src/lib/steps/common.ts
@@ -52,23 +52,6 @@ export async function fileToUint8Array(file: File) {
  * Import the polyfilled File class below to ensure its buffer is available to
  * functions like writeFile (./client-methods.ts) and fileToUint8Array (above).
  */
-
-globalThis.File =
-	globalThis.File ||
-	class extends Blob {
-		lastModified = 0;
-		constructor(
-			bits: any,
-			name: string,
-			options: { type?: string; lastModified?: number }
-		) {
-			super(bits);
-			if (options.lastModified) {
-				this.lastModified = options.lastModified;
-			}
-		}
-	};
-
 class FilePolyfill extends File {
 	buffers: BlobPart[];
 	constructor(buffers: BlobPart[], name: string) {
