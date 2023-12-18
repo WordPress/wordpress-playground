d55c1c6c80817749a52309e71e30d894fe712088
Adam Zielinski <adam@adamziel.com>
2023-12-18 15:47:05 +0100

    Make the .zip export self-contained: Include the default theme, wp-config, and the sqlite-database-integration plugin (#872)
    ## Description
    
    Includes the `wp-config.php` file and the default WordPress theme (like
    `twentytwentythree`) in the zip file exported via the "Download as .zip"
    button.
    
    Without those files, the exported bundle isn't self-contained. It cannot
    be hosted, and any the importer needs to provide the missing files on
    its own. The theme and the plugins are easy to backfill, but the data
    stored in `wp-config.php` is lost.
    
    ## How is the problem addressed?
    
    This PR adds a temporary private `selfContained` option to the
    `importWpContent`. It is `false` by default to ensure those files are
    not exported with GitHub PRs (the export flow relies on the same logic).
    The zip download button sets it to `true`.
    
    This is a temporary workaround as we currently don't have any better
    tools to deal with this problem. Once the streaming/iterators API ships
    in https://github.com/WordPress/wordpress-playground/pull/851, we'll be
    able to get rid of this hack and just filter the stream of files.
    
    ## Testing Instructions
    
    Unfortunately, this PR ships no unit tests as without #895 there isn't
    an easy way to test the `zipWpContent` function. Here's the manual
    testing steps to take:
    
    1. Open Playground
    2. Make a change in the site content
    3. Export Playground into a zip file
    4. Confirm that zip file contains the `wp-content.php` file as well as
    the `twentytwentyfour` theme and the `sqlite-database-integration`
    plugin
    5. Refresh the Playground tab and import that zip
    6. Confirm it worked and the website is functional and has the content
    update from before
    7. Export it to GitHub, check the "include zip file" checkbox
    8. Confirm the GitHub PR has no `twentytwentyfour` theme, or the
    `wp-config.php` file, or the `sqlite-database-integration` plugin.
    9. Do the same for the zip bundled with the GitHub PR
    10. Import that PR and confirm it imports cleanly

--- packages/playground/blueprints/src/lib/steps/zip-wp-content.ts
+++ packages/playground/blueprints/src/lib/steps/zip-wp-content.ts
@@ -3,31 +3,66 @@ import { runPhpWithZipFunctions } from './common';
 import { wpContentFilesExcludedFromExport } from './common';
 import { UniversalPHP } from '@php-wasm/universal';
 
+interface ZipWpContentOptions {
+	/**
+	 * @private
+	 * A temporary workaround to enable including the WordPress default theme
+	 * in the exported zip file.
+	 */
+	selfContained?: boolean;
+}
+
 /**
  * Replace the current wp-content directory with one from the provided zip file.
  *
  * @param playground Playground client.
  * @param wpContentZip Zipped WordPress site.
  */
-export const zipWpContent = async (playground: UniversalPHP) => {
+export const zipWpContent = async (
+	playground: UniversalPHP,
+	{ selfContained = false }: ZipWpContentOptions = {}
+) => {
 	const zipPath = '/tmp/wordpress-playground.zip';
 
 	const documentRoot = await playground.documentRoot;
 	const wpContentPath = joinPaths(documentRoot, 'wp-content');
 
+	let exceptPaths = wpContentFilesExcludedFromExport;
+	/*
+	 * This is a temporary workaround to enable including the WordPress
+	 * default theme and the SQLite plugin in the exported zip file. Let's
+	 * transition from this workaround to iterator-based streams once the
+	 * new API is merged in PR 851.
+	 */
+	if (selfContained) {
+		// This is a bit backwards, so hang on!
+		// We have a list of paths to exclude.
+		// We then *remove* the default theme and the SQLite plugin from that list.
+		// As a result, we *include* the default theme and the SQLite plugin in the final zip.
+		// It is hacky and will be removed soon.
+		exceptPaths = exceptPaths
+			.filter((path) => !path.startsWith('themes/twenty'))
+			.filter((path) => path !== 'plugins/sqlite-database-integration');
+	}
 	const js = phpVars({
 		zipPath,
 		wpContentPath,
 		documentRoot,
-		exceptPaths: wpContentFilesExcludedFromExport.map((path) =>
+		exceptPaths: exceptPaths.map((path) =>
 			joinPaths(documentRoot, 'wp-content', path)
 		),
+		additionalPaths: selfContained
+			? {
+					[joinPaths(documentRoot, 'wp-config.php')]: 'wp-config.php',
+			  }
+			: {},
 	});
 	await runPhpWithZipFunctions(
 		playground,
 		`zipDir(${js.wpContentPath}, ${js.zipPath}, array(
 			'exclude_paths' => ${js.exceptPaths},
-			'zip_root'      => ${js.documentRoot}
+			'zip_root'      => ${js.documentRoot},
+			'additional_paths' => ${js.additionalPaths}
 		));`
 	);
 
