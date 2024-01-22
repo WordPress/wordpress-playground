7ff08c8916d335b64cefdbfb5400ad3afb30bbf1
Adam Zieliński <adam@adamziel.com>
2024-01-22 17:04:49 +0100

    Adjust a comment

--- packages/playground/website/demos/terminal-component.tsx
+++ packages/playground/website/demos/terminal-component.tsx
@@ -12,7 +12,7 @@ interface TerminalComponentProps {
 /**
  * Naive shell command parser.
  * Ensures that commands like `wp option set blogname "My blog name"` are split into
- * `['wp', 'option', 'set', 'blogname', '"My blog name"']` instead of
+ * `['wp', 'option', 'set', 'blogname', 'My blog name']` instead of
  * `['wp', 'option', 'set', 'blogname', 'My', 'blog', 'name']`.
  *
  * @param command
