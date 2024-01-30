d1b5c28fd273d0da751e500e91ab4b8bdb6a96d2
Adam Zieliński <adam@adamziel.com>
2024-01-30 17:27:14 +0100

    Rebuild all kitchen sinnk PHP versions

--- packages/php-wasm/web/public/kitchen-sink/php_7_0.js
+++ packages/php-wasm/web/public/kitchen-sink/php_7_0.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_0_33/php_7_0.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 9833696; 
+export const dependenciesTotalSize = 9833750; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
--- packages/php-wasm/web/public/kitchen-sink/php_8_0.js
+++ packages/php-wasm/web/public/kitchen-sink/php_8_0.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_0_30/php_8_0.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 9985380; 
+export const dependenciesTotalSize = 9985321; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
