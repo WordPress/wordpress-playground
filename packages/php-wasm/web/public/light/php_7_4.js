3e48c6029d7ee992feaf809829b6658977559f09
Adam Zieliński <adam@adamziel.com>
2024-01-30 16:46:09 +0100

    Rebuild more PHP versions

--- packages/php-wasm/web/public/light/php_7_4.js
+++ packages/php-wasm/web/public/light/php_7_4.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_4_33/php_7_4.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5776078; 
+export const dependenciesTotalSize = 5776044; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
