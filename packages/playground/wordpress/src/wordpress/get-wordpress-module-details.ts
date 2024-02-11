98e41861c50734f4e746aaf120c21f84711d7c6c
deployment_bot <deployment_bot@users.noreply.github.com>
2024-02-11 08:14:56 +0000

    Recompile WordPress

--- packages/playground/wordpress/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress/src/wordpress/get-wordpress-module-details.ts
@@ -24,42 +24,42 @@ export function getWordPressModuleDetails(wpVersion: string = "6.4"): { size: nu
 		case 'nightly':
 			/** @ts-ignore */
 			return {
-				size: 5121367,
+				size: 5121391,
 				url: url_nightly,
 			};
 			
 		case 'beta':
 			/** @ts-ignore */
 			return {
-				size: 5014367,
+				size: 5014366,
 				url: url_beta,
 			};
 			
 		case '6.4':
 			/** @ts-ignore */
 			return {
-				size: 5015120,
+				size: 5015128,
 				url: url_6_4,
 			};
 			
 		case '6.3':
 			/** @ts-ignore */
 			return {
-				size: 3760139,
+				size: 3760151,
 				url: url_6_3,
 			};
 			
 		case '6.2':
 			/** @ts-ignore */
 			return {
-				size: 3654397,
+				size: 3654394,
 				url: url_6_2,
 			};
 			
 		case '6.1':
 			/** @ts-ignore */
 			return {
-				size: 3533948,
+				size: 3533951,
 				url: url_6_1,
 			};
 			
