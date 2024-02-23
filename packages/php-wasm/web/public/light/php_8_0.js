f4e6a37a173bf12c7b237fcb4302c63bdd5c4c7d
Bero <berislav.grgicak@gmail.com>
2024-02-23 14:08:28 +0100

    Rebuild PHP

--- packages/php-wasm/web/public/light/php_8_0.js
+++ packages/php-wasm/web/public/light/php_8_0.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_0_30/php_8_0.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5572684; 
+export const dependenciesTotalSize = 5572709; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
