33e07c94820a08a9cdb11aa39be0f7989f674bf8
Adam Zieliński <adam@adamziel.com>
2024-02-27 10:44:57 +0100

    Remove console.log

--- packages/php-wasm/util/src/lib/create-spawn-handler.ts
+++ packages/php-wasm/util/src/lib/create-spawn-handler.ts
@@ -94,7 +94,6 @@ export class ProcessApi extends EventEmitter {
 	exit(code: number) {
 		if (!this.exited) {
 			this.exited = true;
-			console.log('Emit exit!');
 			this.childProcess.emit('exit', code);
 		}
 	}
