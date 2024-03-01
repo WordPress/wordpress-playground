8447d91000f185e4518335f818e2197a10e4712f
berislav grgičak <berislav.grgicak@gmail.com>
2024-03-01 16:12:39 +0100

    Default url rewrites to /index.php (#1072)
    If a requested file path can't be resolved (file or folder don't exist)
    the request file path is rewritten to /index.php.
    
    ## What problem is it solving?
    
    Ensures a response when requesting WordPress permalink URLs like
    `/category/uncategorized/` using `php.request`.
    
    ## How is the problem addressed?
    
    The code assumed that a path which isn't a PHP file is a folder and
    requested the index.php file in that folder.
    In the case of WordPress URLs the path might not be a folder so adding
    index.php would break a valid request.
    
    To address this, index.php is added to the end of the path only if the
    path is a valid directory.
    If not, the path is rewritten to index.php in the root folder.
    
    ## Testing Instructions
    
    - Confirm that tests pass

--- packages/php-wasm/node/src/test/php-request-handler.spec.ts
+++ packages/php-wasm/node/src/test/php-request-handler.spec.ts
@@ -142,7 +142,7 @@ describe.each(SupportedPHPVersions)(
 			 */
 			php.writeFile(
 				'/index.php',
-				`<?php 
+				`<?php
 				echo json_encode($_POST);`
 			);
 			const response = await handler.request({
@@ -163,7 +163,7 @@ describe.each(SupportedPHPVersions)(
 			 */
 			php.writeFile(
 				'/index.php',
-				`<?php 
+				`<?php
 				move_uploaded_file($_FILES["myFile"]["tmp_name"], '/tmp/moved.txt');
 				echo json_encode(file_exists('/tmp/moved.txt'));`
 			);
@@ -180,7 +180,7 @@ describe.each(SupportedPHPVersions)(
 		it('Should allow mixing data and files when `body` is a JavaScript object', async () => {
 			php.writeFile(
 				'/index.php',
-				`<?php 
+				`<?php
 				move_uploaded_file($_FILES["myFile"]["tmp_name"], '/tmp/moved.txt');
 				echo json_encode(array_merge(
 					$_POST,
@@ -203,7 +203,7 @@ describe.each(SupportedPHPVersions)(
 		it('Should handle an empty file object and post data', async () => {
 			await php.writeFile(
 				'/index.php',
-				`<?php 
+				`<?php
 				echo json_encode($_POST);`
 			);
 			const response = await handler.request({
@@ -234,6 +234,34 @@ describe.each(SupportedPHPVersions)(
 			});
 			expect((await response1).httpStatusCode).toEqual(200);
 			expect((await response2).httpStatusCode).toEqual(502);
+		});
+
+		it('should return 200 and pass query strings when a valid request is made to a PHP file', async () => {
+			php.writeFile('/test.php', `<?php echo $_GET['key'];`);
+			const response = await handler.request({
+				url: '/test.php?key=value',
+			});
+			expect(response.httpStatusCode).toEqual(200);
+			expect(response.text).toEqual('value');
+		});
+
+		it('should return 200 status and pass query strings when a valid request is made to a WordPress permalink', async () => {
+			php.writeFile('/index.php', `<?php echo $_GET['key'];`);
+			const response = await handler.request({
+				url: '/category/uncategorized/?key=value',
+			});
+			expect(response.httpStatusCode).toEqual(200);
+			expect(response.text).toEqual('value');
+		});
+
+		it('should return 200 and pass query strings when a valid request is made to a folder', async () => {
+			php.mkdirTree('/folder');
+			php.writeFile('/folder/index.php', `<?php echo $_GET['key'];`);
+			const response = await handler.request({
+				url: '/folder/?key=value',
+			});
+			expect(response.httpStatusCode).toEqual(200);
+			expect(response.text).toEqual('value');
 		});
 	}
 );
