f35a6ccef3ed023f8f1e3b655782edec1034fcb9
Michael Reichardt <30837295+reimic@users.noreply.github.com>
2024-03-02 10:46:30 +0000

    apply changes after review

--- packages/playground/blueprints/src/lib/steps/rmdir.spec.ts
+++ packages/playground/blueprints/src/lib/steps/rmdir.spec.ts
@@ -2,58 +2,55 @@ import { NodePHP } from '@php-wasm/node';
 import { RecommendedPHPVersion } from '@wp-playground/wordpress';
 import { rmdir } from './rmdir';
 
-const docroot = '/php';
 describe('Blueprint step rmdir()', () => {
 	let php: NodePHP;
 	beforeEach(async () => {
 		php = await NodePHP.load(RecommendedPHPVersion);
-		php.mkdir(docroot);
+		php.mkdir('/php');
 	});
 
 	it('should remove a directory', async () => {
-		php.mkdir(`/${docroot}/dir1`);
+		const directoryToRemove = '/php/dir';
+		php.mkdir(directoryToRemove);
 		await rmdir(php, {
-			path: `/${docroot}/dir1`,
+			path: directoryToRemove,
 		});
-		expect(php.fileExists(`/${docroot}/dir1`)).toBe(false);
+		expect(php.fileExists(directoryToRemove)).toBe(false);
 	});
 
 	it('should remove a directory with a subdirectory', async () => {
-		php.mkdir(`/${docroot}/dir1`);
-		php.mkdir(`/${docroot}/dir1/dir11`);
+		const directoryToRemove = '/php/dir';
+		php.mkdir('/php/dir/subDir');
 		await rmdir(php, {
-			path: `/${docroot}/dir1`,
+			path: directoryToRemove,
 		});
-		expect(php.fileExists(`/${docroot}/dir1/dir11`)).toBe(false);
-		expect(php.fileExists(`/${docroot}/dir1`)).toBe(false);
+		expect(php.fileExists(directoryToRemove)).toBe(false);
 	});
 
 	it('should remove a directory with a file', async () => {
-		php.mkdir(`/${docroot}/dir1`);
-		php.writeFile(
-			`/${docroot}/dir1/index.php`,
-			`<?php echo 'Hello World';`
-		);
+		const directoryToRemove = '/php/dir';
+		php.mkdir(directoryToRemove);
+		php.writeFile(`/php/dir/file.php`, `<?php echo 'Hello World';`);
 		await rmdir(php, {
-			path: `/${docroot}/dir1`,
+			path: directoryToRemove,
 		});
-		expect(php.fileExists(`/${docroot}/dir1/index.php`)).toBe(false);
-		expect(php.fileExists(`/${docroot}/dir1`)).toBe(false);
+		expect(php.fileExists(directoryToRemove)).toBe(false);
 	});
 
 	it('should fail when the directory does not exist', async () => {
 		await expect(
 			rmdir(php, {
-				path: `/${docroot}/dir1`,
+				path: '/php/dir',
 			})
 		).rejects.toThrow(/There is no such file or directory/);
 	});
 
 	it('should fail when the directory is a file', async () => {
-		php.writeFile(`/${docroot}/index.php`, `<?php echo 'Hello World';`);
+		php.mkdir('/php/dir');
+		php.writeFile(`/php/dir/index.php`, `<?php echo 'Hello World';`);
 		await expect(
 			rmdir(php, {
-				path: `/${docroot}/index.php`,
+				path: '/php/dir/index.php',
 			})
 		).rejects.toThrow(/Not a directory or a symbolic link to a directory./);
 	});
