009bc9813e3a3053440cab904f4c4cfbb429b40c
bgrgicak <berislav.grgicak@gmail.com>
2024-03-22 10:35:53 +0100

    Vif validation issues

--- packages/playground/website/public/logger.php
+++ packages/playground/website/public/logger.php
@@ -33,13 +33,13 @@ function response($ok, $error = null)
 function validate_message($message)
 {
     // Validate description. Description is required
-    preg_match('/(?<=What happened\?\n\n)(.*?)(?=\n\nLogs)/s', $message, $description);
+    preg_match('/(?<=What happened\?\n\n)(.+?)(?=\n\nLogs)/s', $message, $description);
     if (empty($description)) {
         return false;
     }
 
     // Validate logs if exists. Logs need to match the PHP error log format
-    preg_match('/(?<=Logs\n\n)(.*?)(?=\n\nUrl)/s', $message, $logs);
+    preg_match('/(?<=Logs\n\n)(.+?)(?=\n\nUrl)/s', $message, $logs);
     if (!empty($logs)) {
         $logs = $logs[0];
         if (preg_match('/\[\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2} UTC\](.*)/s', $logs) !== 1) {
@@ -48,7 +48,11 @@ function validate_message($message)
     }
 
     // Validate URL if exists
-    preg_match('/(?<=Url\n\n)(.*)/s', $message, $url);
+    preg_match(
+        '/(?<=Url\n\n)(.+)/s',
+        $message,
+        $url
+    );
     if (!empty($url)) {
         $url = $url[0];
         if (filter_var($url, FILTER_VALIDATE_URL) === false) {
@@ -66,12 +70,14 @@ if (empty($token)) {
 if (!isset($_POST['message'])) {
     response(false, 'No message provided');
 }
-$text = $_POST['message'];
+$message = urldecode($_POST['message']);
+$message = str_replace("\r\n", "\n", $message); // Replace \r\n with \n
+$message = str_replace("\r", "\n", $message); // Replace remaining \r with \n
 
-if (!validate_message($text)) {
+if (!validate_message($message)) {
     response(false, 'Invalid message');
 }
-$text = urlencode($text);
+$text = urlencode($message);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://slack.com/api/chat.postMessage?channel=$channel&text=$text");
