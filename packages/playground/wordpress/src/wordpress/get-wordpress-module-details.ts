feee461eab75a7d421ecb4dfd968da51e7027c3f
Adam Zielinski <adam@adamziel.com>
2024-03-29 11:55:40 +0100

    Refresh WordPress with the latest SQLite integration plugin (#1151)
    Brings the PHP 7.0 compat fix from
    https://github.com/WordPress/sqlite-database-integration/issues/81 into
    Playground by rebuilding the WordPress archives. Also, adds a "force"
    input to GitHub action to enable doing that directly from the UI even
    when the WP releases haven't changed recently.

--- packages/playground/wordpress/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress/src/wordpress/get-wordpress-module-details.ts
@@ -31,35 +31,35 @@ export function getWordPressModuleDetails(wpVersion: string = "6.4"): { size: nu
 		case 'beta':
 			/** @ts-ignore */
 			return {
-				size: 5127315,
+				size: 5127314,
 				url: url_beta,
 			};
 			
 		case '6.4':
 			/** @ts-ignore */
 			return {
-				size: 5015127,
+				size: 5015110,
 				url: url_6_4,
 			};
 			
 		case '6.3':
 			/** @ts-ignore */
 			return {
-				size: 3760149,
+				size: 3760135,
 				url: url_6_3,
 			};
 			
 		case '6.2':
 			/** @ts-ignore */
 			return {
-				size: 3654394,
+				size: 3654384,
 				url: url_6_2,
 			};
 			
 		case '6.1':
 			/** @ts-ignore */
 			return {
-				size: 3533948,
+				size: 3533940,
 				url: url_6_1,
 			};
 			
