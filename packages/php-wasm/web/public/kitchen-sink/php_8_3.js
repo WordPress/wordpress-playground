9f58f241f4301c6b54c11a253a5a6b48cdcaddcc
bgrgicak <berislav.grgicak@gmail.com>
2024-04-02 12:07:25 +0200

    Rebuild PHP

--- packages/php-wasm/web/public/kitchen-sink/php_8_3.js
+++ packages/php-wasm/web/public/kitchen-sink/php_8_3.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_3_0/php_8_3.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 10720585; 
+export const dependenciesTotalSize = 10723609; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
--- packages/php-wasm/web/public/light/php_7_2.js
+++ packages/php-wasm/web/public/light/php_7_2.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_2_34/php_7_2.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5768414; 
+export const dependenciesTotalSize = 5772269; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
