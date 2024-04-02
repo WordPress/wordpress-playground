10196dc120eb8c359b3d863ba1c6e0b6e018a90c
berislav grgičak <berislav.grgicak@gmail.com>
2024-04-02 12:55:16 +0200

    Add PHP iterator and yield support (#1181)
    Adds support for PHP iterators and yield and tests to ensure support
    works.
    
    ## What problem is it solving?
    
    Ensures users can use  PHP iterators and yield.
    
    ## How is the problem addressed?
    
    By adding missing functions to the Asyncify function list.
    
    ## Testing Instructions
    
    - ensure tests pass

--- packages/php-wasm/web/public/kitchen-sink/php_7_2.js
+++ packages/php-wasm/web/public/kitchen-sink/php_7_2.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_2_34/php_7_2.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 10798778; 
+export const dependenciesTotalSize = 10803235; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
--- packages/php-wasm/web/public/light/php_7_1.js
+++ packages/php-wasm/web/public/light/php_7_1.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_1_30/php_7_1.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5424795; 
+export const dependenciesTotalSize = 5429297; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
