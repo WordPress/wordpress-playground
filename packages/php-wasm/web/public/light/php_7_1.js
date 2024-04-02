297f08fb20122567f6805ecf01b20405f17dc707
bgrgicak <berislav.grgicak@gmail.com>
2024-04-02 11:29:18 +0200

    Rebuild PHP web

--- packages/php-wasm/web/public/light/php_7_1.js
+++ packages/php-wasm/web/public/light/php_7_1.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_1_30/php_7_1.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5424795; 
+export const dependenciesTotalSize = 5425397; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
--- packages/php-wasm/web/public/light/php_8_2.js
+++ packages/php-wasm/web/public/light/php_8_2.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_2_10/php_8_2.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 5492020; 
+export const dependenciesTotalSize = 5493202; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
