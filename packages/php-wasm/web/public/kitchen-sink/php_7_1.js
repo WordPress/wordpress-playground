305fa1a79b11b930cccb52b3e01b3b62be0cc324
bgrgicak <berislav.grgicak@gmail.com>
2024-04-03 10:09:14 +0200

    Reset PHP build to trunk

--- packages/php-wasm/web/public/kitchen-sink/php_7_1.js
+++ packages/php-wasm/web/public/kitchen-sink/php_7_1.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_1_30/php_7_1.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 9999437; 
+export const dependenciesTotalSize = 11078028; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
