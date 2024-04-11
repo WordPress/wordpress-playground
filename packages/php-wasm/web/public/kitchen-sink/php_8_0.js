db850a08541f107aa83ac2919a28f17bca1e57a9
Brandon Payton <brandon@happycode.net>
2024-04-11 06:20:18 -0600

    Update php-wasm web builds

--- packages/php-wasm/web/public/kitchen-sink/php_8_0.js
+++ packages/php-wasm/web/public/kitchen-sink/php_8_0.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_0_30/php_8_0.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 11611323; 
+export const dependenciesTotalSize = 11611332; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
