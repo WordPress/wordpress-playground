WordPress/wordpress-playground

commit fad3ccf0e67a7878bb04df15409da805b00827bb

Blueprints: Rename importFile to importWxr, switch to humanmade/WordPress importer (#1192)
The importFile step works by interacting with HTML form like a user
would. Problem is, the WXR importer requires some interactions with the
forms which we fake by adjusting the POST data before submitting the
form. This approach causes a number of issues listed below.

More context:
https://github.com/WordPress/wordpress-playground/issues/1183.

## Implementation

Refactors the importFile Blueprint step to:

* Be named importWxz. The old name continues to work, though.
* Use the humanmade/WordPress-Importer for importing content – it is
  more reliable than the official wordpress-importer and has a
  convenient PHP API.
* Drop WXZ support – it's a maintenance burden and doesn't add clear
  value.
* Remove dependency on DOMParser – this step should now work in `wp-now`

## Testing instructions

1. Go to
http://localhost:5400/website-server/?import-wxr=https://raw.githubusercontent.com/helgatheviking/Simple-User-Listing/3e38ea3eb3a057424fbae4305cba2fd9f73d896b/demo-content/demo-content.xml&php=8.0&wp=6.5&storage=none&php-extension-bundle=kitchen-sink
2. Confirm the "Simple User Listing Block" page was created
3. Confirm in wp-admin a bunch of user accounts were created
4. Confirm in wp-admin the WordPress Importer plugin was installed

Closes https://github.com/WordPress/wordpress-playground/issues/1183
Closes https://github.com/WordPress/wordpress-playground/issues/379
Closes https://github.com/WordPress/wordpress-playground/issues/1158
Closes https://github.com/WordPress/wordpress-playground/issues/1161

cc @dmsnell

packages/playground/blueprints/src/lib/steps/site-data.spec.ts
@@ -1,37 +1,0 @@
-import { NodePHP } from '@php-wasm/node';
-import {
-	RecommendedPHPVersion,
-	getWordPressModule,
-} from '@wp-playground/wordpress';
-import { setSiteOptions } from './site-data';
-import { unzip } from './unzip';
-
-describe('Blueprint step setSiteOptions()', () => {
-	let php: NodePHP;
-	beforeEach(async () => {
-		php = await NodePHP.load(RecommendedPHPVersion, {
-			requestHandler: {
-				documentRoot: '/wordpress',
-			},
-		});
-		await unzip(php, {
-			zipFile: await getWordPressModule(),
-			extractToPath: '/wordpress',
-		});
-	});
-
-	it('should set the site option', async () => {
-		await setSiteOptions(php, {
-			options: {
-				blogname: 'My test site!',
-			},
-		});
-		const response = await php.run({
-			code: `<?php
-                require '/wordpress/wp-load.php';
-                echo get_option('blogname');
-			`,
-		});
-		expect(response.text).toBe('My test site!');
-	});
-});

commit 581da69bb81ca1491b4196c4ec1b04263588ae01

Unit tests: Restore site-data.spec.ts (#1194)
The test was removed in #1192 due to intermittent failures like these:

```
FAIL  src/lib/steps/site-data.spec.ts > Blueprint step setSiteOptions() > should set the site option
```

![CleanShot 2024-04-04 at 13 09
25@2x](https://github.com/WordPress/wordpress-playground/assets/205419/fe68c5ff-80a7-4966-82a8-fbfcc7e9a101)

They seem related to #1189, but:

* I can't reproduce it locally
* The test actually passed in #1189 and only started failing in #1192
* All the other tests pass and the Playground website works so I wonder
what's going on there.
* The stack trace mentions network activity, but that call shouldn't
involve any. It's almost as if the trace was coming from
`php-networking.spec.ts`, but those tests actually pass.

Is the CI runner just running out of memory? But then it shouldn't be
able to allocate the memory segment in the first place. Weird!

CC @brandonpayton for insights

packages/playground/blueprints/src/lib/steps/site-data.spec.ts
@@ -1,0 +1,37 @@
+import { NodePHP } from '@php-wasm/node';
+import {
+	RecommendedPHPVersion,
+	getWordPressModule,
+} from '@wp-playground/wordpress';
+import { setSiteOptions } from './site-data';
+import { unzip } from './unzip';
+
+describe('Blueprint step setSiteOptions()', () => {
+	let php: NodePHP;
+	beforeEach(async () => {
+		php = await NodePHP.load(RecommendedPHPVersion, {
+			requestHandler: {
+				documentRoot: '/wordpress',
+			},
+		});
+		await unzip(php, {
+			zipFile: await getWordPressModule(),
+			extractToPath: '/wordpress',
+		});
+	});
+
+	it('should set the site option', async () => {
+		await setSiteOptions(php, {
+			options: {
+				blogname: 'My test site!',
+			},
+		});
+		const response = await php.run({
+			code: `<?php
+                require '/wordpress/wp-load.php';
+                echo get_option('blogname');
+			`,
+		});
+		expect(response.text).toBe('My test site!');
+	});
+});