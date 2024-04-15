ccaa86c70d14e898ea37e8ee2593ef04f6243b69
bgrgicak <berislav.grgicak@gmail.com>
2024-04-15 14:34:20 +0200

    Add context method

--- packages/php-wasm/logger/src/lib/logger.ts
+++ packages/php-wasm/logger/src/lib/logger.ts
@@ -220,6 +220,19 @@ export class Logger extends EventTarget {
 	public getLogs(): string[] {
 		return this.logs;
 	}
+
+	/**
+	 * Get context information for the error.
+	 */
+	public getContext(): any {
+		return {
+			PHP: '',
+			WordPress: '',
+			userAgent: navigator.userAgent,
+			'playground-worker-count': '',
+			memory: (window.performance as any)?.memory,
+		};
+	}
 }
 
 /**
