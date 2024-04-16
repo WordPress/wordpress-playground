77d9170aa392f629901eb565fc255b3445f5ed4c
Adam Zieliński <adam@adamziel.com>
2024-04-16 14:12:00 +0200

    Rebuild node.js versions of PHP

--- packages/php-wasm/node/public/php_7_4.js
+++ packages/php-wasm/node/public/php_7_4.js
@@ -1,6 +1,6 @@
 const dependencyFilename = __dirname + '/7_4_33/php_7_4.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 14331012; 
+export const dependenciesTotalSize = 14331074; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
