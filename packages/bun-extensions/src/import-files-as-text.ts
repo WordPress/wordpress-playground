16dbba839e32e33f9cb04ccd1bc36bf38aeea050
Adam Zieliński <adam@adamziel.com>
2024-04-23 11:17:28 +0200

    Document the Bun plugin

--- packages/bun-extensions/src/import-files-as-text.ts
+++ packages/bun-extensions/src/import-files-as-text.ts
@@ -1,28 +1,33 @@
-import { plugin } from 'bun';
+import { plugin, BunPlugin } from 'bun';
 
 /**
  * Adds support for `import contents from "file.php?raw"`.
- * 
+ *
  * It matches Vite's behavior, which is to read the file as text
  * and return its textual content. This allows us to run Bun and
  * Vite on the same codebase.
  */
-plugin({
+export const ImportFilesAsTextPlugin: BunPlugin = {
 	name: 'ImportFilesAsTextPlugin',
 	setup(build) {
-		build.onLoad({
-			// Looks for paths with a `raw` query parameter.
-			filter: /\?(.*&|)raw(&.*|)$/
-		}, async (args) => {
-			const path = args.path.split('?')[0];
-			// Use Bun.file to read the .php file as text
-			const text = await Bun.file(path).text();
+		build.onLoad(
+			{
+				// Looks for paths with a `raw` query parameter.
+				filter: /\?(.*&|)raw(&.*|)$/,
+			},
+			async (args) => {
+				const path = args.path.split('?')[0];
+				// Use Bun.file to read the .php file as text
+				const text = await Bun.file(path).text();
 
-			// Return the file content as a module exporting the text
-			return {
-				contents: `export default ${JSON.stringify(text)};`,
-				loader: 'js', // Treat the content as JavaScript
-			};
-		});
+				// Return the file content as a module exporting the text
+				return {
+					contents: `export default ${JSON.stringify(text)};`,
+					loader: 'js', // Treat the content as JavaScript
+				};
+			}
+		);
 	},
-});
+};
+
+plugin(ImportFilesAsTextPlugin);
