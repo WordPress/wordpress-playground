c4402dac7e2723346feea1219febfc087187537d
Adam Zieliński <adam@adamziel.com>
2024-04-23 10:53:41 +0200

    Document the used bun extension

--- packages/bun-extensions/src/import-files-as-text.ts
+++ packages/bun-extensions/src/import-files-as-text.ts
@@ -1,9 +1,19 @@
 import { plugin } from 'bun';
 
+/**
+ * Adds support for `import contents from "file.php?raw"`.
+ * 
+ * It matches Vite's behavior, which is to read the file as text
+ * and return its textual content. This allows us to run Bun and
+ * Vite on the same codebase.
+ */
 plugin({
-	name: 'PHPTextPlugin',
+	name: 'ImportFilesAsTextPlugin',
 	setup(build) {
-		build.onLoad({ filter: /\?(.*&|)raw(&.*|)$/ }, async (args) => {
+		build.onLoad({
+			// Looks for paths with a `raw` query parameter.
+			filter: /\?(.*&|)raw(&.*|)$/
+		}, async (args) => {
 			const path = args.path.split('?')[0];
 			// Use Bun.file to read the .php file as text
 			const text = await Bun.file(path).text();
