edb3ddf4da1f7fcabd1061d3456ae2ffd2294f46
Brandon Payton <brandon@happycode.net>
2024-04-25 19:57:18 -0400

    Make deploying much faster

--- packages/playground/website-deployment/apply-update.sh
+++ packages/playground/website-deployment/apply-update.sh
@@ -2,66 +2,51 @@
 
 set -euo pipefail
 
+
+# Explicitly use the site's declared PHP version.
+# Otherwise, we've seen this defaulting to PHP 7.4 which breaks our custom-redirects script.
 SITE_PHP="/usr/local/php${PHP_VERSION}/bin/php"
 SITE_API_BASE="$( "$SITE_PHP" -r 'require "/scripts/env.php"; echo SITE_API_BASE;')"
 
 echo Setting up staging directory
 cd ~
 rm -rf website-update
-mkdir website-update
 
+echo Moving updated Playground files to staging directory
+mv updated-playground-files website-update
+mkdir website-update/files-to-serve-via-php
+
+echo Copy supporting files for WP Cloud
 cp -r ~/website-deployment/__wp__ ~/website-update/
 cp ~/website-deployment/custom-redirects.php ~/website-update/
 cp ~/website-deployment/mime-types.php ~/website-update/
 
-echo Moving updated Playground files into staging directory
-mv updated-playground-files website-update/playground-files
-
-# Mirror directory structure from playground files
-echo Mirroring website directory structure for nginx
-cd ~/website-update/playground-files
-find . -type d -exec mkdir -p ../{} \;
-
-function should_serve_file_via_php() {
-    local FILE_PATH=$1
-    local FILE_NAME=$(basename "$FILE_PATH")
-
-    local PHP="
-    	require_once '/srv/htdocs/custom-redirects.php';
-	    if ( playground_file_needs_special_treatment('$FILE_NAME') ) {
-		// zero is the truthy exit code
-		exit(0);
-	    } else {
-		// non-zero is a falsy exit code
-		exit(1);
-	    }
-    ";
-
-    # Explicitly use the site's declared PHP version.
-    # Otherwise, we've seen this defaulting to PHP 7.4 which breaks our custom-redirects script.
-    "$SITE_PHP" -r "$PHP"
+function match_files_to_serve_via_php() {
+    "$SITE_PHP" -r '
+    require_once "/srv/htdocs/custom-redirects.php";
+    while ( $path = fgets( STDIN ) ) {
+        $path = trim( $path );
+        $filename = basename( $path );
+        if ( playground_file_needs_special_treatment($filename) ) {
+            echo "$path\n";
+        }
+    }
+    '
 }
 
-function link_files() {
-	while read PLAYGROUND_FILE_PATH; do
-	    WEB_PATH=$(echo $PLAYGROUND_FILE_PATH | sed 's#^playground-files/##')
-
-	    if [[ "$WEB_PATH" == "$PLAYGROUND_FILE_PATH" ]]; then
-		    echo "Error: Path prefix not removed from $PLAYGROUND_FILE_PATH" 1>&2
-		    return -1
-	    fi
-
-	    if ! should_serve_file_via_php "$WEB_PATH"; then
-		    ln -srf "$PLAYGROUND_FILE_PATH" "$WEB_PATH"
-	    else
-		echo "  php-served: $WEB_PATH"
-	    fi
-	done
+function set_aside_files_to_serve_via_php() {
+    while read FILE_TO_SERVE_VIA_PHP; do
+        echo "  php-served: $FILE_TO_SERVE_VIA_PHP"
+        TARGET_DIR="files-to-serve-via-php/$(dirname "$FILE_TO_SERVE_VIA_PHP")"
+        mkdir -p "$TARGET_DIR"
+        mv "$FILE_TO_SERVE_VIA_PHP" "$TARGET_DIR/"
+    done
 }
-
-cd ~/website-update
 echo Configure which files should be served by Nginx and which by PHP
-find playground-files -type f | link_files
+cd ~/website-update
+find -type f \
+    | match_files_to_serve_via_php \
+    | set_aside_files_to_serve_via_php
 
 echo Syncing staged files to production
 rsync -av --delete ~/website-update/* /srv/htdocs/ 2>&1 | tee ~/website-deployment/rsync-log
