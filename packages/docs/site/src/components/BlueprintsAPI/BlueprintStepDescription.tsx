d245b16dd4c937479cc3b2b3d76e7e947fce6364
Adam Zieliński <adam@adamziel.com>
2024-04-26 12:46:01 +0200

    Apply Markdown formatting to the entire step description, not to its pieces

--- packages/docs/site/src/components/BlueprintsAPI/BlueprintStepDescription.tsx
+++ packages/docs/site/src/components/BlueprintsAPI/BlueprintStepDescription.tsx
@@ -5,27 +5,14 @@ import ReactMarkdown from 'react-markdown';
 export function BlueprintStepDescription({ name }) {
 	const stepApi = getStepAPI(name);
 
-	return (
-		<p>
-			{stepApi.stepDetails.summary.map((part) => {
-				if (part.kind === 'code') {
-					return (
-						<>
-							{' '}
-							<code>{part.text.replace(/^`|`$/g, '')}</code>{' '}
-						</>
-					);
-				}
-				return (
-					<ReactMarkdown
-						components={{
-							p: 'span',
-						}}
-					>
-						{part.text}
-					</ReactMarkdown>
-				);
-			})}
-		</p>
-	);
+	const summary = stepApi.stepDetails.summary
+		.map((part) => {
+			if (part.kind === 'code') {
+				return '`' + part.text.replace(/^`|`$/g, '') + '`';
+			}
+			return part.text;
+		})
+		.join(' ');
+
+	return <ReactMarkdown>{summary}</ReactMarkdown>;
 }
