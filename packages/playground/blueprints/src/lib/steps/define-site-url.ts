72e1f699a08bd7556db9f8a1e0230c8942c33e0c
Ronny Shani <ronnyshani@gmail.com>
2024-05-01 01:31:14 +0200

    Add links to WP developer resource in relevant Blueprint steps

--- packages/playground/blueprints/src/lib/steps/define-site-url.ts
+++ packages/playground/blueprints/src/lib/steps/define-site-url.ts
@@ -13,10 +13,10 @@ export interface DefineSiteUrlStep {
 }
 
 /**
- * Sets WP_HOME and WP_SITEURL constants for the WordPress installation.
+ * Sets [`WP_HOME`](https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#blog-address-url) and [`WP_SITEURL`](https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#wp-siteurl) constants for the WordPress installation.
  *
- * Beware: Using this step makes no sense on playground.wordpress.net.
- * It is useful when you're building a custom Playground-based tool like wp-now,
+ * Using this step on playground.wordpress.net is moot.
+ * It is useful when building a custom Playground-based tool, like [`wp-now`](https://www.npmjs.com/package/@wp-now/wp-now),
  * or deploying Playground on a custom domain.
  *
  * @param playground The playground client.
