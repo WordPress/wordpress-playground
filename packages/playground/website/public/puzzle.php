54150717dc37cc0fdf57d10bf65c81974bc78c47
bgrgicak <berislav.grgicak@gmail.com>
2024-05-10 11:05:08 +0200

    Ensure post result

--- packages/playground/website/public/puzzle.php
+++ packages/playground/website/public/puzzle.php
@@ -2,8 +2,8 @@
 // error_reporting(0);
 header('Content-Type: application/json');
 
-$open_ai_api_key = "";
-$api_key = "";
+$open_ai_api_key = getenv('OPEN_AI_API_KEY');
+$api_key = getenv('PUZZLE_API_KEY');
 
 if (!isset($_POST["api_key"])) {
     response("error", "Please provide an API key.");
@@ -76,7 +76,7 @@ function siteName()
     }
 }
 
-function post()
+function generatePost()
 {
     global $open_ai_api_key;
     $curl = curl_init();
@@ -86,15 +86,15 @@ function post()
         CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_POSTFIELDS => json_encode([
             "model" => "gpt-3.5-turbo-instruct",
-            "prompt" => "Please write a short blog post about a random topic.
-            Make sure the post is complete and doesn't end abruptly.
+            "prompt" => "Please write a short fictional blog post about a great experience someone had while attending a WordCamp.
+            Make sure the post is complete and doesn't end abruptly. The post should be up to 300 words.
             The topic should be fun and not too serious. It should be something that people would enjoy reading.
             It should be safe for work and not contain any inappropriate content.
             Start with a title for the post and then write the content. Add two new lines after the title.
             Don't include any quotes or additional text in the output.
             Don't prefix the title with 'Title:', or content with the word 'Content:'.",
             "temperature" => 1,
-            "max_tokens" => 256,
+            "max_tokens" => 512,
             "top_p" => 1,
             "frequency_penalty" => 0,
             "presence_penalty" => 0,
@@ -113,10 +113,52 @@ function post()
     }
     try {
         $output = $response["choices"][0]["text"];
-        response("success", $output);
+
+        $titleRegex = '/^\n\n?(.+?)\n\n/s';
+
+        // Extract title and content
+        preg_match($titleRegex, $output, $matches);
+        $title = isset($matches[1]) ? $matches[1] : '';
+        $title = str_replace('Title: ', "", $title);
+        $title = str_replace('"', "", $title);
+        $title = str_replace("#", "", $title);
+        $title = trim($title);
+        $title = addslashes($title);
+
+        $slug = str_replace(' ', '-', strtolower($title));
+        $slug = preg_replace('/[^A-Za-z0-9\-]/', '', $slug);
+
+        $content = preg_replace($titleRegex, '', $output);
+        $content = trim($content);
+        $content = addslashes($content);
+
+        return ['title' => $title, 'content' => $content, 'slug' => $slug];
     } catch (Exception $e) {
-        response("error", "Invalid response from OpenAI");
+        return [];
     }
+}
+
+function isValidPost($post)
+{
+    return !empty($post['title']) && !empty($post['content']) && !empty($post['slug']);
+}
+
+function post()
+{
+    $max_tries = 2;
+    $tries = 0;
+    do {
+        $post = generatePost();
+    } while (
+        !isValidPost($post) &&
+        ++$tries < $max_tries
+    );
+
+    if (!isValidPost($post)) {
+        response("error", "Failed to generate a post.");
+    }
+
+    response("success", $post);
 }
 
 function readImage($image)
@@ -185,6 +227,9 @@ function readImage($image)
         $output = $response["choices"][0]["message"]["content"];
         $output = explode("\n", $output);
         $output = array_map("trim", $output);
+        $output = array_filter($output, function ($item) {
+            return !empty($item) && $item !== "NO";
+        });
         response("success", $output);
     } catch (Exception $e) {
         response("error", "Invalid response from OpenAI");
