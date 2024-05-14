edde403e8c07cb2a4cf0df5b18a44f6dc216b7e8
Adam Zieliński <adam@adamziel.com>
2024-05-14 10:34:17 +0200

    Add design notes to README and index.ts

--- packages/playground/common/src/index.ts
+++ packages/playground/common/src/index.ts
@@ -1,1 +1,12 @@
+/**
+ * Avoid adding new code here. @wp-playground/common should remain
+ * as lean as possible.
+ *
+ * This package exists to avoid circular dependencies. Let's not
+ * use it as a default place to add code that doesn't seem to fit
+ * anywhere else. If there's no good place for your code, perhaps
+ * it needs to be restructured? Or maybe there's a need for a new package?
+ * Let's always consider these questions before adding new code here.
+ */
+
 export const RecommendedPHPVersion = '8.0';
