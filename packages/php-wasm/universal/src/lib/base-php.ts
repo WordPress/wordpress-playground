WordPress/wordpress-playground

commit f316a763a180dae6bbd1d06d0759d575262f38f8

PHP: Move internal shared directories to /internal/shared (#1386)
The number of internal directories and files shared between all the
PHPProcessManager processes keeps growing so it makes sense to move them
all under a common root. This PR introduces an `/internal/shared`
directory that's a home to:

* `php.ini`
* `phpinfo.php`
* `preload/` directory with custom PHP files
* `ca-bundle.crt` for SSL connections
* `consts.json`
* `consts.php`
* Soon also db.php and sqlite-database-integration

 ## Testing instructions

Confirm all the tests passed

packages/php-wasm/universal/src/lib/base-php.ts
@@ -358,40 +358,42 @@ export abstract class BasePHP implements IsomorphicLocalPHP, Disposable {
 	}
 
 	#initWebRuntime() {
-		this.writeFile(
-			'/internal/auto_prepend_file.php',
-			`<?php
-			foreach (glob('/internal/preload/*.php') as $file) {
+		if (!this.fileExists('/internal/shared/auto_prepend_file.php')) {
+			this.writeFile(
+				'/internal/shared/auto_prepend_file.php',
+				`<?php
+			foreach (glob('/internal/shared/preload/*.php') as $file) {
 				require_once $file;
 			}
 		`
-		);
-		this.setPhpIniEntry(
-			'auto_prepend_file',
-			'/internal/auto_prepend_file.php'
-		);
-		/**
-		 * This creates a consts.php file in an in-memory
-		 * /internal/preload directory and sets the auto_prepend_file PHP option
-		 * to always load that file.
-		 * @see https://www.php.net/manual/en/ini.core.php#ini.auto-prepend-file
-		 *
-		 * Technically, this is a workaround. In the future, let's implement a
-		 * WASM SAPI method to pass consts directly.
-		 * @see https://github.com/WordPress/wordpress-playground/issues/750
-		 */
-		this.writeFile(
-			'/internal/preload/consts.php',
-			`<?php
-			if(file_exists('/internal/consts.json')) {
-				$consts = json_decode(file_get_contents('/internal/consts.json'), true);
+			);
+			this.setPhpIniEntry(
+				'auto_prepend_file',
+				'/internal/shared/auto_prepend_file.php'
+			);
+			/**
+			 * This creates a consts.php file in an in-memory
+			 * /internal/preload directory and sets the auto_prepend_file PHP option
+			 * to always load that file.
+			 * @see https://www.php.net/manual/en/ini.core.php#ini.auto-prepend-file
+			 *
+			 * Technically, this is a workaround. In the future, let's implement a
+			 * WASM SAPI method to pass consts directly.
+			 * @see https://github.com/WordPress/wordpress-playground/issues/750
+			 */
+			this.writeFile(
+				'/internal/shared/preload/consts.php',
+				`<?php
+			if(file_exists('/internal/shared/consts.json')) {
+				$consts = json_decode(file_get_contents('/internal/shared/consts.json'), true);
 				foreach ($consts as $const => $value) {
 					if (!defined($const) && is_scalar($value)) {
 						define($const, $value);
 					}
 				}
 			}`
-		);
+			);
+		}
 		if (this.#phpIniOverrides.length > 0) {
 			const overridesAsIni =
 				this.#phpIniOverrides
@@ -600,15 +602,16 @@ export abstract class BasePHP implements IsomorphicLocalPHP, Disposable {
 		let consts = {};
 		try {
 			consts = JSON.parse(
-				this.fileExists('/internal/consts.json')
-					? this.readFileAsText('/internal/consts.json') || '{}'
+				this.fileExists('/internal/shared/consts.json')
+					? this.readFileAsText('/internal/shared/consts.json') ||
+							'{}'
 					: '{}'
 			);
 		} catch (e) {
 			// ignore
 		}
 		this.writeFile(
-			'/internal/consts.json',
+			'/internal/shared/consts.json',
 			JSON.stringify({
 				...consts,
 				[key]: value,

packages/php-wasm/compile/php/phpwasm-emscripten-library.js
@@ -19,51 +19,54 @@ const LibraryExample = {
 			// The /internal directory is required by the C module. It's where the
 			// stdout, stderr, and headers information are written for the JavaScript
 			// code to read later on.
-			FS.mkdir("/internal");
+			FS.mkdir('/internal');
+			// The files from the shared directory are shared between all the
+			// PHP processes managed by PHPProcessManager.
+			FS.mkdir('/internal/shared');
 			// The files from the preload directory are preloaded using the
 			// auto_prepend_file php.ini directive.
-			FS.mkdir("/internal/preload");
+			FS.mkdir('/internal/shared/preload');
 
 			PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE
 				? require('events').EventEmitter
 				: class EventEmitter {
-					constructor() {
-						this.listeners = {};
-					}
-					emit(eventName, data) {
-						if (this.listeners[eventName]) {
-							this.listeners[eventName].forEach(
-								(callback) => {
-									callback(data);
-								}
-							);
-						}
-					}
-					once(eventName, callback) {
-						const self = this;
-						function removedCallback() {
-							callback(...arguments);
-							self.removeListener(eventName, removedCallback);
-						}
-						this.on(eventName, removedCallback);
-					}
-					removeAllListeners(eventName) {
-						if (eventName) {
-							delete this.listeners[eventName];
-						} else {
+						constructor() {
 							this.listeners = {};
 						}
-					}
-					removeListener(eventName, callback) {
-						if (this.listeners[eventName]) {
-							const idx =
-								this.listeners[eventName].indexOf(callback);
-							if (idx !== -1) {
-								this.listeners[eventName].splice(idx, 1);
+						emit(eventName, data) {
+							if (this.listeners[eventName]) {
+								this.listeners[eventName].forEach(
+									(callback) => {
+										callback(data);
+									}
+								);
 							}
 						}
-					}
-				};
+						once(eventName, callback) {
+							const self = this;
+							function removedCallback() {
+								callback(...arguments);
+								self.removeListener(eventName, removedCallback);
+							}
+							this.on(eventName, removedCallback);
+						}
+						removeAllListeners(eventName) {
+							if (eventName) {
+								delete this.listeners[eventName];
+							} else {
+								this.listeners = {};
+							}
+						}
+						removeListener(eventName, callback) {
+							if (this.listeners[eventName]) {
+								const idx =
+									this.listeners[eventName].indexOf(callback);
+								if (idx !== -1) {
+									this.listeners[eventName].splice(idx, 1);
+								}
+							}
+						}
+				  };
 			PHPWASM.child_proc_by_fd = {};
 			PHPWASM.child_proc_by_pid = {};
 			PHPWASM.input_devices = {};
@@ -188,11 +191,15 @@ const LibraryExample = {
 			};
 			return [promise, cancel];
 		},
-		noop: function () { },
+		noop: function () {},
 
 		spawnProcess: function (command, args, options) {
 			if (Module['spawnProcess']) {
-				const spawnedPromise = Module['spawnProcess'](command, args, options);
+				const spawnedPromise = Module['spawnProcess'](
+					command,
+					args,
+					options
+				);
 				return Promise.resolve(spawnedPromise).then(function (spawned) {
 					if (!spawned || !spawned.on) {
 						throw new Error(
@@ -213,8 +220,8 @@ const LibraryExample = {
 			}
 			const e = new Error(
 				'popen(), proc_open() etc. are unsupported in the browser. Call php.setSpawnHandler() ' +
-				'and provide a callback to handle spawning processes, or disable a popen(), proc_open() ' +
-				'and similar functions via php.ini.'
+					'and provide a callback to handle spawning processes, or disable a popen(), proc_open() ' +
+					'and similar functions via php.ini.'
 			);
 			e.code = 'SPAWN_UNSUPPORTED';
 			throw e;
@@ -264,7 +271,7 @@ const LibraryExample = {
 		const device = FS.createDevice(
 			'/dev',
 			filename,
-			function () { },
+			function () {},
 			function (byte) {
 				try {
 					dataBuffer.push(byte);
@@ -536,13 +543,11 @@ const LibraryExample = {
 			return -1;
 		}
 		if (PHPWASM.child_proc_by_pid[pid].exited) {
-			HEAPU32[exitCodePtr >> 2] =
-				PHPWASM.child_proc_by_pid[pid].exitCode;
+			HEAPU32[exitCodePtr >> 2] = PHPWASM.child_proc_by_pid[pid].exitCode;
 			return 1;
 		}
 		return 0;
 	},
-
 
 	js_waitpid: function (pid, exitCodePtr) {
 		if (!PHPWASM.child_proc_by_pid[pid]) {
@@ -770,30 +775,33 @@ const LibraryExample = {
 	js_fd_read: function (fd, iov, iovcnt, pnum) {
 		// Only run the read operation on a regular call,
 		// never when rewinding the stack.
-        if (Asyncify.state === Asyncify.State.Normal) {
-            var returnCode;
-            var stream;
+		if (Asyncify.state === Asyncify.State.Normal) {
+			var returnCode;
+			var stream;
 			let num = 0;
-            try {
-                stream = SYSCALLS.getStreamFromFD(fd);
-                const num = doReadv(stream, iov, iovcnt);
-                HEAPU32[pnum >> 2] = num;
+			try {
+				stream = SYSCALLS.getStreamFromFD(fd);
+				const num = doReadv(stream, iov, iovcnt);
+				HEAPU32[pnum >> 2] = num;
 				return 0;
 			} catch (e) {
 				// Rethrow any unexpected non-filesystem errors.
-				if (typeof FS == "undefined" || !(e.name === "ErrnoError")) {
+				if (typeof FS == 'undefined' || !(e.name === 'ErrnoError')) {
 					throw e;
 				}
-                // Only return synchronously if this isn't an asynchronous pipe.
+				// Only return synchronously if this isn't an asynchronous pipe.
 				// Error code 6 indicates EWOULDBLOCK – this is our signal to wait.
 				// We also need to distinguish between a process pipe and a file pipe, otherwise
 				// reading from an empty file would block until the timeout.
-				if (e.errno !== 6 || !(stream?.fd in PHPWASM.child_proc_by_fd)) {
+				if (
+					e.errno !== 6 ||
+					!(stream?.fd in PHPWASM.child_proc_by_fd)
+				) {
 					// On failure, yield 0 bytes read to indicate EOF.
 					HEAPU32[pnum >> 2] = 0;
-					return returnCode
+					return returnCode;
 				}
-            }
+			}
 		}
 
 		// At this point we know we have to poll.
@@ -831,12 +839,11 @@ const LibraryExample = {
 				}
 
 				const success = returnCode === 0;
-				const failure = (
+				const failure =
 					++retries > maxRetries ||
 					!(fd in PHPWASM.child_proc_by_fd) ||
 					PHPWASM.child_proc_by_fd[fd]?.exited ||
-					FS.isClosed(stream)
-				);
+					FS.isClosed(stream);
 
 				if (success) {
 					HEAPU32[pnum >> 2] = num;

packages/php-wasm/node/public/php_7_0.js
@@ -5562,7 +5562,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

packages/php-wasm/node/public/php_7_1.js
@@ -5562,7 +5562,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

packages/php-wasm/node/public/php_7_2.js
@@ -5562,7 +5562,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

packages/php-wasm/node/public/php_7_3.js
@@ -5562,7 +5562,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

packages/php-wasm/node/public/php_7_4.js
@@ -5561,7 +5561,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

packages/php-wasm/node/public/php_8_0.js
@@ -5561,7 +5561,7 @@ var allocateUTF8OnStack = stringToUTF8OnStack;
 
 var PHPWASM = {
  init: function() {
-  FS.mkdir("/internal");FS.mkdir("/internal/preload");
+  FS.mkdir("/internal");FS.mkdir("/internal/shared");FS.mkdir("/internal/shared/preload");
   PHPWASM.EventEmitter = ENVIRONMENT_IS_NODE ? require("events").EventEmitter : class EventEmitter {
    constructor() {
     this.listeners = {};

commit c53054278c893bcada6db0ecc311287c56b631a8

PHP: Always set the auto_prepend_file php.ini entry, even when the auto_prepend_file.php file exists
Filesystem files are shared between PHP instances within the same
PHPProcessManager, but php.ini entry are kept in WASM memory and must
be set on every PHP instance separately. Therefore, the
`auto_prepend_file` must always be set in `#initWebRuntime`

Fixes a regression introduced in #1386

To test, run `bun packages/playground/cli/src/cli.ts server --login` and
confirm it finishes without any errors.

packages/php-wasm/universal/src/lib/base-php.ts
@@ -358,6 +358,10 @@ export abstract class BasePHP implements IsomorphicLocalPHP, Disposable {
 	}
 
 	#initWebRuntime() {
+		this.setPhpIniEntry(
+			'auto_prepend_file',
+			'/internal/shared/auto_prepend_file.php'
+		);
 		if (!this.fileExists('/internal/shared/auto_prepend_file.php')) {
 			this.writeFile(
 				'/internal/shared/auto_prepend_file.php',
@@ -366,10 +370,6 @@ export abstract class BasePHP implements IsomorphicLocalPHP, Disposable {
 				require_once $file;
 			}
 		`
-			);
-			this.setPhpIniEntry(
-				'auto_prepend_file',
-				'/internal/shared/auto_prepend_file.php'
 			);
 			/**
 			 * This creates a consts.php file in an in-memory