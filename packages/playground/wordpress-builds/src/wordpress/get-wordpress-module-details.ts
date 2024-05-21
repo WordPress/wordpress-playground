afa535e1a78dbfe5889d28547ae20a364d5af9cf
Brandon Payton <brandon@happycode.net>
2024-05-20 20:55:10 -0400

    Add latest nightly build

--- packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
@@ -26,7 +26,7 @@ export function getWordPressModuleDetails(wpVersion: string = "6.5"): { size: nu
 		case 'nightly':
 			/** @ts-ignore */
 			return {
-				size: 4989289,
+				size: 4989363,
 				url: url_nightly,
 			};
 			
