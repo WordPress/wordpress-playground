42841b8c6a9e3a54f4b76c494fa714a55195759c
Adam Zieliński <adam@adamziel.com>
2024-05-22 22:05:28 -0400

    WordPress builde: Export getSqliteDatabaseModule similarly to how we have getWordPressModule

--- packages/playground/wordpress-builds/src/index.ts
+++ packages/playground/wordpress-builds/src/index.ts
@@ -1,6 +1,7 @@
 export { getWordPressModuleDetails } from './wordpress/get-wordpress-module-details';
 export { getWordPressModule } from './wordpress/get-wordpress-module';
-export * as sqliteDatabaseIntegration from './sqlite-database-integration/get-sqlite-database-plugin-details';
+export * as sqliteDatabaseIntegrationModuleDetails from './sqlite-database-integration/get-sqlite-database-plugin-details';
+export { getSqliteDatabaseModule } from './sqlite-database-integration/get-sqlite-database-module';
 import SupportedWordPressVersions from './wordpress/wp-versions.json';
 
 export { SupportedWordPressVersions };
