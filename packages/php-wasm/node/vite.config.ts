1c7da8bac230bb9928a29afa64b3750d8be35162
Adam Zieliński <adam@adamziel.com>
2024-05-29 09:51:39 +0200

    Update vite.config.ts

--- packages/php-wasm/node/vite.config.ts
+++ packages/php-wasm/node/vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig(function () {
 			cache: {
 				dir: '../../../node_modules/.vitest',
 			},
-			reporters: 'dot',
+			environment: 'jsdom',
 			include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		},
 
