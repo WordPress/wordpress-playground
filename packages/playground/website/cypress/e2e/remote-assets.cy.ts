fd6087dd7281e4bc6db060594f91ad3702ab33e6
Brandon Payton <brandon@happycode.net>
2024-06-06 19:38:34 -0400

    Run fewer remote asset tests due to instability

--- packages/playground/website/cypress/e2e/remote-assets.cy.ts
+++ packages/playground/website/cypress/e2e/remote-assets.cy.ts
@@ -1,65 +1,52 @@
-// We can't import the WordPress versions directly from the remote package because
-// of ESModules vs CommonJS incompatibilities. Let's just import the JSON file
-// directly.
-// @ts-ignore
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import * as SupportedWordPressVersions from '../../../wordpress-builds/src/wordpress/wp-versions.json';
-
-const wordPressVersions = Object.keys(SupportedWordPressVersions);
-const testedStorageOptions = ['none', 'browser'];
-
 describe('Remote Assets', () => {
-	wordPressVersions.forEach((wpVersion) => {
-		testedStorageOptions.forEach((storage) => {
-			it(`should load remote assets for WordPress-${wpVersion} and storage=${storage}`, () => {
-				const expectedRemoteAssetPath =
-					'wp-includes/blocks/navigation/style.min.css';
-				const blueprint =
-					'{"siteOptions":{"blogname":"remote asset test"}}';
+	const testedStorageOptions = ['none', 'browser'];
+	testedStorageOptions.forEach((storage) => {
+		it(`should load remote assets for storage=${storage}`, () => {
+			const expectedRemoteAssetPath =
+				'wp-includes/blocks/navigation/style.min.css';
+			const blueprint =
+				'{"siteOptions":{"blogname":"remote asset test"}}';
 
-				cy.visit(`/?wp=${wpVersion}&storage=${storage}#${blueprint}`);
+			cy.visit(`/?storage=${storage}#${blueprint}`);
+			runAssertions();
+
+			if (storage === 'browser') {
+				// Reload and re-assert to test when loading from browser storage
+				cy.reload();
 				runAssertions();
+			}
 
-				if (storage === 'browser') {
-					cy.reload();
-					runAssertions();
-				}
+			function runAssertions() {
+				// NOTE: This appears to be necessary for Cypress
+				// to wait until a WordPress page is actually loaded.
+				// Otherwise, the document.styleSheets assertions hang.
+				cy.wordPressDocument()
+					.its('body')
+					.should('contain', 'remote asset test');
 
-				function runAssertions() {
-					// NOTE: This appears to be necessary for Cypress to wait until a WordPress page
-					// is actually loaded. Otherwise, the document.styleSheets assertions hang.
-					cy.wordPressDocument()
-						.its('body')
-						.should('contain', 'remote asset test');
+				cy.window()
+					.then((win: any) => {
+						return win.playground
+							.readFileAsText(
+								'/wordpress/wordpress-remote-asset-paths'
+							)
+							.then((remoteAssetPaths: string) => {
+								return cy.wrap(remoteAssetPaths.split('\n'));
+							});
+					})
+					.should('include', expectedRemoteAssetPath);
 
-					cy.window()
-						.then((win: any) => {
-							return win.playground
-								.readFileAsText(
-									'/wordpress/wordpress-remote-asset-paths'
-								)
-								.then((remoteAssetPaths: string) => {
-									return cy.wrap(
-										remoteAssetPaths.split('\n')
-									);
-								});
-						})
-						.should('include', expectedRemoteAssetPath);
-
-					cy.wordPressDocument()
-						.its('styleSheets')
-						.then((styleSheets: StyleSheetList) =>
-							cy.wrap(
-								Array.from(styleSheets).find((sheet) =>
-									sheet.href?.includes(
-										expectedRemoteAssetPath
-									)
-								)
+				cy.wordPressDocument()
+					.its('styleSheets')
+					.then((styleSheets: StyleSheetList) =>
+						cy.wrap(
+							Array.from(styleSheets).find((sheet) =>
+								sheet.href?.includes(expectedRemoteAssetPath)
 							)
 						)
-						.should('exist');
-				}
-			});
+					)
+					.should('exist');
+			}
 		});
 	});
 });
