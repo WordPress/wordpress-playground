620720e14f5529ad6363b1a716fcbf4554362365
Brandon Payton <brandon@happycode.net>
2024-06-11 13:17:24 -0400

    Add unit tests

---
+++ packages/playground/wordpress/src/test/version-detect.spec.ts
@@ -0,0 +1,55 @@
+import {
+	SupportedWordPressVersions,
+	getSqliteDatabaseModule,
+	getWordPressModule,
+} from '@wp-playground/wordpress-builds';
+import { RecommendedPHPVersion } from '@wp-playground/common';
+import { loadNodeRuntime } from '@php-wasm/node';
+import { bootWordPress } from '../boot';
+import { getLoadedWordPressVersion } from '../version-detect';
+
+describe('Test WP version detection', async () => {
+	for (const expectedWordPressVersion of Object.keys(
+		SupportedWordPressVersions
+	)) {
+		it(`detects WP ${expectedWordPressVersion}`, async () => {
+			const handler = await bootWordPress({
+				createPhpRuntime: async () =>
+					await loadNodeRuntime(RecommendedPHPVersion),
+				siteUrl: 'http://playground-domain/',
+
+				wordPressZip: await getWordPressModule(
+					expectedWordPressVersion
+				),
+				sqliteIntegrationPluginZip: await getSqliteDatabaseModule(),
+			});
+			const php = await handler.getPrimaryPhp();
+
+			const loadedWordPressVersion = await getLoadedWordPressVersion(php);
+			expect(loadedWordPressVersion).to.equal(expectedWordPressVersion);
+		});
+	}
+
+	it('errors on failure to detect version', async () => {
+		const handler = await bootWordPress({
+			createPhpRuntime: async () =>
+				await loadNodeRuntime(RecommendedPHPVersion),
+			siteUrl: 'http://playground-domain/',
+
+			wordPressZip: await getWordPressModule(),
+			sqliteIntegrationPluginZip: await getSqliteDatabaseModule(),
+		});
+		const php = await handler.getPrimaryPhp();
+
+		php.writeFile(
+			`${handler.documentRoot}/wp-includes/version.php`,
+			'<?php $wp_version = "invalid-version";'
+		);
+
+		const detectionResult = await getLoadedWordPressVersion(php).then(
+			() => 'no-error',
+			() => 'error'
+		);
+		expect(detectionResult).to.equal('error');
+	});
+});
