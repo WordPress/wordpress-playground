c2fd2f9d491de23d47030a3a15c2734e48ccc68f
bgrgicak <berislav.grgicak@gmail.com>
2024-06-18 11:52:00 +0200

    Catch fetch error

--- packages/php-wasm/web-service-worker/src/fetch-caching.ts
+++ packages/php-wasm/web-service-worker/src/fetch-caching.ts
@@ -40,7 +40,12 @@ const isValidHostname = (url: URL) => {
 export const cachedFetch = async (request: Request): Promise<Response> => {
 	const url = new URL(request.url);
 	if (!isValidHostname(url)) {
-		return fetch(request);
+		try {
+			return await fetch(request);
+		} catch (e) {
+			logger.warn('Failed to fetch', request.url, e);
+			return new Response('Failed to fetch', { status: 500 });
+		}
 	}
 	const cacheKey = url.pathname;
 	const cache = await getCache(cacheKey);
