e5d7347e3f8223fe17518b1cc0a1821dde94b8cb
Adam Zieliński <adam@adamziel.com>
2024-07-08 14:01:31 +0200

    Build: Treat all dependencies of php-wasm/node as external (#1584)
    Without this PR, the released `@php-wasm/node` package bundles other
    `@php-wasm`
    packages, which caused multiple copies of, e.g. `@php-wasm/universal` to
    be loaded and resulted in variable identity issues such as
    #1579.
    
    This PR marks all the dependent packages as external and ensures the
    published package is lean and only contains its own code.
    
     ## Testing instructions
    
    Run `nx build php-wasm-node` and confirm the resulting
    `dist/packages/php-wasm/node/index.cjs` files does not contain any
    external modules## Motivation for the change, related issues

---
+++ packages/php-wasm/node/esbuild.js
@@ -0,0 +1,30 @@
+const esbuild = require('esbuild');
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+
+esbuild.build({
+	entryPoints: [
+		'packages/php-wasm/node/src/index.ts',
+		'packages/php-wasm/node/src/noop.ts',
+	],
+	plugins: [nodeExternalsPlugin()],
+	supported: {
+		'dynamic-import': false,
+	},
+	format: 'cjs',
+	outExtension: { '.js': '.cjs' },
+	outdir: 'dist/packages/php-wasm/node',
+	platform: 'node',
+	assetNames: '[name]',
+	chunkNames: '[name]',
+	logOverride: {
+		'commonjs-variable-in-esm': 'silent',
+	},
+	bundle: true,
+	tsconfig: 'packages/php-wasm/node/tsconfig.json',
+	external: ['@php-wasm/*', '@wp-playground/*'],
+	loader: {
+		'.php': 'text',
+		'.ini': 'file',
+		'.wasm': 'file',
+	},
+});
