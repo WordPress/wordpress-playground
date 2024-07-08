d3ad8b01f019515fc5eddaf62a77992356b6e9a7
Bero <berislav.grgicak@gmail.com>
2024-07-08 11:55:25 +0200

    Fix docs export issue

--- typedoc.js
+++ typedoc.js
@@ -10,6 +10,6 @@ module.exports = {
 		'./packages/playground/blueprints',
 		'./packages/playground/client',
 	],
-	intentionallyNotExported: ['WebClientMixin', 'PlaygroundWorkerEndpoint'],
+	intentionallyNotExported: [],
 	excludeExternals: true,
 };
