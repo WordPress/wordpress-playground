9190da46280fd4ff1a1636f77cac972bf24b61a6
Adam Zieliński <adam@adamziel.com>
2024-07-09 10:15:31 +0200

    Build: Use regular expressions to mark packages as external

--- packages/vite-extensions/vite-external-modules.ts
+++ packages/vite-extensions/vite-external-modules.ts
@@ -1,25 +1,3 @@
-import glob from 'glob';
-import fs from 'fs';
-
-const getPackageNames = (): string[] => {
-	const packageJsonPaths = glob
-		.sync('../php-wasm/*/package.json')
-		.concat(glob.sync('../playground/*/package.json'));
-	const packageNames: string[] = [];
-
-	packageJsonPaths.forEach((packageJsonPath) => {
-		const packageJson = JSON.parse(
-			fs.readFileSync(packageJsonPath, 'utf8')
-		);
-		packageNames.push(packageJson.name);
-	});
-
-	return packageNames;
-};
-
-// Usage
-const packageNames = getPackageNames();
-
 export const getExternalModules = () => {
 	return [
 		'yargs',
@@ -38,6 +16,7 @@ export const getExternalModules = () => {
 		'dns',
 		'ws',
 		/node_modules\//,
-		...packageNames,
+		/^@php-wasm\//,
+		/^@wp-playground\//,
 	];
 };
