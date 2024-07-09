70f590e98ce0844e3e3e214109d59327b7bebbf3
deployment_bot <deployment_bot@users.noreply.github.com>
2024-07-09 17:28:07 +0000

    Recompile WordPress major and beta versions

--- packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
@@ -31,7 +31,7 @@ export function getWordPressModuleDetails(wpVersion: string = "6.5"): { size: nu
 		case 'beta':
 			/** @ts-ignore */
 			return {
-				size: 4957498,
+				size: 4958521,
 				url: url_beta,
 			};
 			
