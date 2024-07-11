c09a2dc923a80db79940e1ce602f7581e3355229
Bero <berislav.grgicak@gmail.com>
2024-07-11 08:03:10 +0200

    Add comments

--- packages/playground/blueprints/src/lib/steps/set-site-language.ts
+++ packages/playground/blueprints/src/lib/steps/set-site-language.ts
@@ -1,11 +1,26 @@
 import { StepHandler } from '.';
 
+/**
+ * @inheritDoc setSiteLanguage
+ * @hasRunnableExample
+ * @example
+ *
+ * <code>
+ * {
+ * 		"step": "setSiteLanguage",
+ * 		"language": "en_US"
+ * }
+ * </code>
+ */
 export interface SetSiteLanguageStep {
 	step: 'setSiteLanguage';
 	/** The language to set, e.g. 'en_US' */
 	language: string;
 }
 
+/**
+ * Sets the site language and download translations.
+ */
 export const setSiteLanguage: StepHandler<SetSiteLanguageStep> = async (
 	playground,
 	{ language }
