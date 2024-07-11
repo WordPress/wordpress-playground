8b1357ec975f64013e4a35be656382d03f6d9bdf
Bero <berislav.grgicak@gmail.com>
2024-07-11 10:04:46 +0200

    Fetch all translations from downloads.wordpress.org

--- packages/playground/blueprints/src/lib/steps/set-site-language.ts
+++ packages/playground/blueprints/src/lib/steps/set-site-language.ts
@@ -1,4 +1,5 @@
 import { StepHandler } from '.';
+import { unzipFile } from '@wp-playground/common';
 
 /**
  * @inheritDoc setSiteLanguage
@@ -26,15 +27,112 @@ export const setSiteLanguage: StepHandler<SetSiteLanguageStep> = async (
 	{ language },
 	progress
 ) => {
+	progress?.tracker.setCaption(progress?.initialCaption || 'Translating');
+
+	await playground.defineConstant('WPLANG', language);
+
 	const docroot = await playground.documentRoot;
 
-	progress?.tracker.setCaption(progress?.initialCaption || 'Translating');
-	await playground.defineConstant('WPLANG', language);
-	await playground.run({
+	const wpVersion = (
+		await playground.run({
+			code: `<?php
+			require '${docroot}/wp-includes/version.php';
+			echo $wp_version;
+		`,
+		})
+	).text;
+
+	const translations = [
+		{
+			url: `https://downloads.wordpress.org/translation/core/${wpVersion}/${language}.zip`,
+			type: 'core',
+		},
+	];
+
+	const pluginListResponse = await playground.run({
 		code: `<?php
 		require_once('${docroot}/wp-load.php');
-		require_once('${docroot}/wp-admin/includes/admin.php');
-		require_once('${docroot}/wp-admin/includes/translation-install.php');
-		wp_download_language_pack('${language}');`,
+		require_once('${docroot}/wp-admin/includes/plugin.php');
+		echo json_encode(
+			array_values(
+				array_map(
+					function($plugin) {
+						return [
+							'slug'    => $plugin['TextDomain'],
+							'version' => $plugin['Version']
+						];
+					},
+					array_filter(
+						get_plugins(),
+						function($plugin) {
+							return !empty($plugin['TextDomain']);
+						}
+					)
+				)
+			)
+		);`,
 	});
+
+	const plugins = pluginListResponse.json;
+	for (const { slug, version } of plugins) {
+		translations.push({
+			url: `https://downloads.wordpress.org/translation/plugin/${slug}/${version}/${language}.zip`,
+			type: 'plugin',
+		});
+	}
+
+	const themeListResponse = await playground.run({
+		code: `<?php
+		require_once('${docroot}/wp-load.php');
+		require_once('${docroot}/wp-admin/includes/theme.php');
+		echo json_encode(
+			array_values(
+				array_map(
+					function($theme) {
+						return [
+							'slug'    => $theme->get('TextDomain'),
+							'version' => $theme->get('Version')
+						];
+					},
+					wp_get_themes()
+				)
+			)
+		);`,
+	});
+
+	const themes = themeListResponse.json;
+	for (const { slug, version } of themes) {
+		translations.push({
+			url: `https://downloads.wordpress.org/translation/theme/${slug}/${version}/${language}.zip`,
+			type: 'theme',
+		});
+	}
+
+	for (const { url, type } of translations) {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error(
+				`Failed to fetch translation for ${type} (${language})`
+			);
+		}
+
+		let destination = `${docroot}/wp-content/languages`;
+		if (type === 'plugin') {
+			destination += '/plugins';
+		} else if (type === 'theme') {
+			destination += '/themes';
+		}
+		if (!(await playground.isDir(destination))) {
+			await playground.mkdir(destination);
+		}
+
+		const zipPath = `${destination}/${language}-${type}.zip`;
+
+		await unzipFile(
+			playground,
+			new File([await response.blob()], zipPath),
+			destination
+		);
+	}
 };
