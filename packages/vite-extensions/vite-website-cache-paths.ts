194f7e97af4ad0678a449d5a3c54aa74363de1d6
Bero <berislav.grgicak@gmail.com>
2024-07-12 13:25:30 +0200

    Update patternsToNotCache list and document why files are excluded

--- packages/vite-extensions/vite-website-cache-paths.ts
+++ packages/vite-extensions/vite-website-cache-paths.ts
@@ -1,37 +1,49 @@
 import { readdirSync, statSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+/**
+ * We want to cache all files from the website root directory, and the assets directory, except for the files listed below.
+ */
 const patternsToNotCache = [
+	/**
+	 * Static files that are not needed for the website to function offline.
+	 * TODO: Check if these are needed in the production build.
+	 */
 	'/package.json',
 	'/README.md',
+	'/.DS_Store',
 	'/index.cjs',
 	'/index.d.ts',
-	'/builder/builder.html',
-	'/gutenberg.css',
-	'/gutenberg.html',
-	'/.htaccess',
-	'/ogimage.png',
-	'/previewer.css',
-	'/wordpress.html',
-	'/wordpress.svg',
-	'/.DS_Store',
-	'/.htaccess',
-	/^\/wp-[\w+((\.\d+)?)]+\/.*/, // WordPress static asset folders
-	/^\/demos\/.*/, // Demos
-	/\/.*\.php$/, // PHP files used by the website server
 	/\/lib\/.*/, // Remote lib files
-	/\/test-fixtures\/.*/,
-	/^\/assets\/.*\.svg$/,
-	/^\/assets\/builder.*/,
-	/^\/assets\/peer.*/,
-	/^\/assets\/php-blueprints.*/,
-	/^\/assets\/setup-playground-sync.*/,
-	/^\/assets\/sync.html-[\w]+\.js/,
-	/^\/assets\/terminal.*/,
-	/^\/assets\/time-traveling.*/,
-	/^\/assets\/wp-cli.html-[\w]+\.js/,
+	/\/test-fixtures\/.*/, // Test fixtures
+	/**
+	 * WordPress remote assets that are fetched on demand by Playground.
+	 */
+	/^\/wp-[\w+((\.\d+)?)]+\/.*/,
+	/**
+	 * Demos are not needed for the website to function offline.
+	 */
+	/^\/demos\/.*/,
+	/**
+	 * Files needed only by the Playground.WordPress.Net server.
+	 */
+	'/.htaccess',
+	/\/.*\.php$/, // PHP files used by the website server
+	/**
+	 * WordPress, PHP, and SQLite files that are loaded during boot.
+	 * By loading these files during boot, we can ensure that only the current PHP/WordPress/SQLite files are cached.
+	 * These files are large, so loading only the current files will reduce the bandwidth usage.
+	 */
+	/^\/assets\/php_.*\.wasm$/, // PHP WASM files
+	/^\/assets\/php_.*\.js$/, // PHP JS files
+	/^\/assets\/wp-.*\.zip$/, // WordPress zip files
+	/^\/assets\/sqlite-database-integration-[\w]+\.zip/, // SQLite plugin
 ];
 
+/**
+ * For Playground to work offline we need to cache all the files that are needed for the website to function.
+ * Playground uses this list at the end of the boot process to cache files.
+ */
 export const websiteCachePathsPlugin = () => {
 	function listFiles(dirPath: string, fileList: string[] = []) {
 		const files = readdirSync(dirPath);
@@ -75,7 +87,6 @@ export const websiteCachePathsPlugin = () => {
 						});
 					});
 			});
-
 			writeFileSync(outputManifestPath, JSON.stringify(files, null, 2));
 		},
 	};
