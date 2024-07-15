12651e86b0e96efaebbf5b3be0d5c394dcec26c2
Adam Zieliński <adam@adamziel.com>
2024-07-15 16:34:53 +0200

    Update packages/playground/components/vite.config.ts
    Co-authored-by: Bero <berislav.grgicak@gmail.com>

--- packages/playground/components/vite.config.ts
+++ packages/playground/components/vite.config.ts
@@ -23,15 +23,6 @@ export default defineConfig({
 		}),
 	],
 
-	// Uncomment this if you are using workers.
-	// worker: {
-	//  plugins: [
-	//    viteTsConfigPaths({
-	//      root: '../../../',
-	//    }),
-	//  ],
-	// },
-
 	// Configuration for building your library.
 	// See: https://vitejs.dev/guide/build.html#library-mode
 	build: {
