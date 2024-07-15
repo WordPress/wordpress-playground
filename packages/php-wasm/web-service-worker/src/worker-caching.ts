e7d7f813d20203264cd234a7a6429d11ab7b5a43
Bero <berislav.grgicak@gmail.com>
2024-07-15 12:49:09 +0200

    Fix small bugs

--- packages/php-wasm/web-service-worker/src/worker-caching.ts
+++ packages/php-wasm/web-service-worker/src/worker-caching.ts
@@ -31,7 +31,19 @@ export class WorkerCache {
 		) {
 			return false;
 		}
+
+		/**
+		 * Scoped URLs are requests made to the PHP Worker Thread.
+		 * These requests are not cached because they are not static assets.
+		 */
 		if (isURLScoped(url)) {
+			return false;
+		}
+
+		/**
+		 * Don't cache PHP files because they are dynamic.
+		 */
+		if (url.pathname.endsWith('.php')) {
 			return false;
 		}
 		return ['playground.wordpress.net', '127.0.0.1', 'localhost'].includes(
@@ -64,14 +76,22 @@ export class WorkerCache {
 	};
 
 	preCacheResources = async (): Promise<any> => {
+		console.log('Pre-caching resources for offline mode...');
 		if (!this.isValidHostname(new URL(location.href))) {
+			console.log(
+				'Skipping cache pre-caching because of the hostname.',
+				location.href
+			);
 			return;
 		}
 
 		const cache = await caches.open(this.cacheName);
 
 		// Get the cache manifest and add all the files to the cache
-		const manifestResponse = await fetch('/cache-files.json');
+		const manifestResponse = await fetch(
+			'/assets-required-for-offline-mode.json'
+		);
+		console.log('Manifest response:', manifestResponse);
 		const websiteUrls = await manifestResponse.json();
 		await cache.addAll([...websiteUrls, ...['/']]);
 		console.log('Playground is now ready to be used offline.');
