55d9830d027192a3487495fd637837a200c395cc
Brandon Payton <brandon@happycode.net>
2024-07-19 14:24:34 -0400

    See if we can avoid GH workflow auto-cancelation if we fork test execution into multiple processes

--- packages/php-wasm/node/vite.config.ts
+++ packages/php-wasm/node/vite.config.ts
@@ -47,6 +47,7 @@ export default defineConfig(function () {
 			},
 			environment: 'jsdom',
 			include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+			pool: 'forks',
 		},
 
 		define: {
