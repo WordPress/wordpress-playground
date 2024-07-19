826eead40a28cdb5fc4db549e6c24eaa3757cce3
Brandon Payton <brandon@happycode.net>
2024-07-19 11:31:33 -0400

    Revert "Temporarily reduce number of new php-request-handler tests"
    This reverts commit 4b09a5f562c8d48d2fe055d99b76106bfbbb8a0a.

--- packages/php-wasm/node/src/test/php-request-handler.spec.ts
+++ packages/php-wasm/node/src/test/php-request-handler.spec.ts
@@ -12,13 +12,11 @@ import {
 import { createSpawnHandler, joinPaths } from '@php-wasm/util';
 
 const configsForRequestTests = SupportedPHPVersions.map((phpVersion) => {
-	// TODO: Decide whether to re-enable or remove other options
-	//const documentRoots = ['/', '/wordpress'];
-	const documentRoots = ['/wordpress'];
+	const documentRoots = ['/', '/wordpress'];
 	return documentRoots.map((docRoot) => {
 		const absoluteUrls = [
 			undefined,
-			//'http://localhost:4321/nested/playground/',
+			'http://localhost:4321/nested/playground/',
 		];
 		return absoluteUrls.map((absoluteUrl) => ({
 			phpVersion,
