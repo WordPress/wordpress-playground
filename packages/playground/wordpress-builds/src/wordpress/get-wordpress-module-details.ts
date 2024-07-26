4ae9c3796b7a06cc725fe15160098c8af3eb067d
deployment_bot <deployment_bot@users.noreply.github.com>
2024-07-26 08:05:11 +0000

    Refresh WordPress Nightly

--- packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
@@ -24,7 +24,7 @@ export function getWordPressModuleDetails(wpVersion: string = "6.6"): { size: nu
 		case 'nightly':
 			/** @ts-ignore */
 			return {
-				size: 18396184,
+				size: 18396172,
 				url: url_nightly,
 			};
 			
