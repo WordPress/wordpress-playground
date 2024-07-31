f5e5c810fede5db85827b7e4059029b301963c73
Bero <berislav.grgicak@gmail.com>
2024-07-31 13:28:51 +0200

    Update packages/playground/blueprints/src/lib/steps/wp-cli.ts
    Co-authored-by: Adam Zieliński <adam@adamziel.com>

--- packages/playground/blueprints/src/lib/steps/wp-cli.ts
+++ packages/playground/blueprints/src/lib/steps/wp-cli.ts
@@ -32,7 +32,10 @@ export const wpCLI: StepHandler<WPCLIStep, Promise<PHPResponse>> = async (
 ) => {
 	if (!(await playground.fileExists(wpCliPath))) {
 		throw new Error(`wp-cli.phar not found at ${wpCliPath}.
-			You can enable wp-cli support by adding "wp-cli" to the list of extra libraries in your blueprint.
+			You can enable wp-cli support by adding "wp-cli" to the list of extra libraries in your blueprint as follows:
+			{
+				"extraLibraries": [ "wp-cli" ]
+			}
 
 			Read more about it in the documentation.
 			https://wordpress.github.io/wordpress-playground/blueprints-api/data-format#extra-libraries`);
