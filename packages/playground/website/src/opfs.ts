d84684a061d4927ebd60fe781a96df756572c201
Adam Zieliński <adam@adamziel.com>
2024-08-02 11:23:54 +0200

    Simplify removeDirHandleContents with the recursive option

--- packages/playground/website/src/opfs.ts
+++ packages/playground/website/src/opfs.ts
@@ -1,10 +1,9 @@
 export async function removeDirHandleContents(
 	parent: FileSystemDirectoryHandle
 ) {
-	for await (const [name, entry] of parent.entries()) {
-		if (entry.kind === 'directory') {
-			await removeDirHandleContents(entry as FileSystemDirectoryHandle);
-		}
-		await parent.removeEntry(name);
+	for await (const name of parent.keys()) {
+		await parent.removeEntry(name, {
+			recursive: true,
+		});
 	}
 }
