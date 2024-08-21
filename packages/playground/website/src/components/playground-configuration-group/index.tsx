WordPress/wordpress-playground

commit ccadc7d0daedda0b59b571e12aade32cf3abaeab

A boot() method to explicitly initialize the PHP worker (#1669)
## Motivation for the change, related issues

Refactors the `remote.html` and `worker-thread.ts` boot flows. 

* **Before this PR**, both files passed configuration using query
arguments and relied on implicit logic surrounding the endpoint class
* **After this PR**, both API Endpoint classes expose a `boot()` method
that is strongly typed, accepts the configuration values, and explicitly
orchestrates all the boot logic when it is called.

This work may eventually enable:

* A reusable, isomorphic Worker implementation that can be reused in the
browser and in a local CLI setup.
* Exposing `bootWordPress()` on a worker instance.
* Hot-swapping the PHP runtime and WordPress instance (e.g. by killing
the current worker and starting a new one).
* Spawning new workers, not just PHP class instances, via
PHPProcessManager.

Related to https://github.com/WordPress/wordpress-playground/issues/1398

## Follow-up work

* Explore spawning multiple workers for handling requests. This might
also require:
* Exposing the full boot protocol (`bootWordPress()`) on a worker
instance instead of the simplified `boot()` method.
   * Load balancer across all workers handling the same site instance.
* An OPFS Emscripten filesystem backend so that each worker can see the
changes made by other workers. This is a potential blocker as Emscripten
didn't seem to have a good way of doing that a few months ago.

## Testing Instructions (or ideally a Blueprint)

* Confirm the tests are all green
* Carefully review the changes, point out anything that seems off

cc @brandonpayton @bgrgicak

---------

Co-authored-by: Brandon Payton <brandon@happycode.net>

packages/playground/website/src/components/playground-configuration-group/index.tsx
@@ -16,6 +16,13 @@ import { OPFSButton } from './opfs-button';
 import { SyncLocalFilesButton } from './sync-local-files-button';
 import { logger } from '@php-wasm/logger';
 import { usePlaygroundContext } from '../../playground-context';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	PlaygroundDispatch,
+	PlaygroundReduxState,
+	setOpfsMountDescriptor,
+} from '../../lib/redux-store';
+import { MountDevice } from '@php-wasm/web';
 
 interface SiteSetupGroupProps {
 	initialConfiguration: PlaygroundConfiguration;
@@ -41,6 +48,10 @@ export default function PlaygroundConfigurationGroup({
 		resolve: any;
 		isResolved: boolean;
 	}>();
+	const dispatch: PlaygroundDispatch = useDispatch();
+	const mountDescriptor = useSelector(
+		(state: PlaygroundReduxState) => state.opfsMountDescriptor
+	);
 	const { playground } = usePlaygroundContext();
 	useEffect(() => {
 		if (!playgroundRef.current) {
@@ -66,7 +77,7 @@ export default function PlaygroundConfigurationGroup({
 	>({});
 
 	useEffect(() => {
-		playground?.getSupportedWordPressVersions().then(({ all, latest }) => {
+		playground?.getMinifiedWordPressVersions().then(({ all, latest }) => {
 			const formOptions: Record<string, string> = {};
 			for (const version of Object.keys(all)) {
 				if (version === 'beta') {
@@ -144,6 +155,12 @@ export default function PlaygroundConfigurationGroup({
 		dirHandle: FileSystemDirectoryHandle
 	) {
 		const playground = await playgroundRef.current!.promise;
+		const mountpoint = await playground.documentRoot;
+		const device: MountDevice = {
+			type: 'local-fs',
+			handle: dirHandle,
+		};
+		dispatch(setOpfsMountDescriptor({ device, mountpoint }));
 		if (idb) {
 			await saveDirectoryHandle(idb, dirHandle);
 		}
@@ -162,10 +179,14 @@ export default function PlaygroundConfigurationGroup({
 				}
 			}
 
-			await playground.bindOpfs(
+			if (await playground.hasOpfsMount(mountpoint)) {
+				await playground.unmountOpfs(mountpoint);
+			}
+
+			await playground.mountOpfs(
 				{
-					opfs: dirHandle,
-					mountpoint: '/wordpress',
+					device,
+					mountpoint,
 					initialSyncDirection: isPlaygroundDir
 						? 'opfs-to-memfs'
 						: 'memfs-to-opfs',
@@ -195,9 +216,6 @@ export default function PlaygroundConfigurationGroup({
 
 	async function handleSubmit(config: PlaygroundConfiguration) {
 		const hasPlayground = playgroundRef.current?.isResolved;
-		const playground = hasPlayground
-			? await playgroundRef.current!.promise
-			: undefined;
 		if (hasPlayground && config.resetSite && config.storage === 'browser') {
 			if (
 				!window.confirm(
@@ -208,7 +226,7 @@ export default function PlaygroundConfigurationGroup({
 			}
 		}
 
-		reloadWithNewConfiguration(config, playground);
+		reloadWithNewConfiguration(config, mountDescriptor?.device);
 	}
 	let WPLabel =
 		wpVersionChoices[currentConfiguration.wp] || currentConfiguration.wp;
@@ -270,7 +288,7 @@ export default function PlaygroundConfigurationGroup({
 										...initialConfiguration,
 										storage: 'none',
 									},
-									playground
+									mountDescriptor?.device
 								);
 							}}
 						>

packages/playground/website/src/lib/redux-store.ts
@@ -1,8 +1,6 @@
 import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {
-	directoryHandleResolve,
-	directoryHandleDone,
-} from './markdown-directory-handle';
+import { directoryHandleToOpfsPath } from '@wp-playground/storage';
+import type { MountDevice } from '@php-wasm/web';
 
 export type ActiveModal =
 	| 'error-report'
@@ -15,6 +13,10 @@ export type ActiveModal =
 interface AppState {
 	activeModal: string | null;
 	offline: boolean;
+	opfsMountDescriptor?: {
+		device: MountDevice;
+		mountpoint: string;
+	};
 }
 
 const query = new URL(document.location.href).searchParams;
@@ -28,37 +30,65 @@ const initialState: AppState = {
 	offline: !navigator.onLine,
 };
 
-if (initialState.activeModal !== 'mount-markdown-directory') {
-	directoryHandleResolve(null);
+if (query.get('storage') === 'browser') {
+	const siteSlug = query.get('site-slug') || 'wordpress';
+	const opfsRoot = await navigator.storage.getDirectory();
+	const opfsDir = await opfsRoot.getDirectoryHandle(
+		siteSlug === 'wordpress' ? siteSlug : 'site-' + siteSlug,
+		{
+			create: true,
+		}
+	);
+
+	initialState.opfsMountDescriptor = {
+		device: {
+			type: 'opfs',
+			path: await directoryHandleToOpfsPath(opfsDir),
+		},
+		mountpoint: '/wordpress',
+	};
 }
 
 // Create the slice
 const slice = createSlice({
 	name: 'app',
 	initialState,
+	selectors: {
+		getOpfsHandle: (state) => state.opfsMountDescriptor,
+	},
 	reducers: {
 		setActiveModal: (state, action: PayloadAction<string | null>) => {
-			if (
-				!directoryHandleDone &&
-				state.activeModal === 'mount-markdown-directory' &&
-				action.payload !== 'mount-markdown-directory'
-			) {
-				directoryHandleResolve(null);
-			}
 			state.activeModal = action.payload;
 		},
 		setOfflineStatus: (state, action: PayloadAction<boolean>) => {
 			state.offline = action.payload;
 		},
+		setOpfsMountDescriptor: (
+			state,
+			action: PayloadAction<AppState['opfsMountDescriptor']>
+		) => {
+			state.opfsMountDescriptor = action.payload;
+		},
 	},
 });
 
 // Export actions
-export const { setActiveModal } = slice.actions;
+export const { setActiveModal, setOpfsMountDescriptor } = slice.actions;
 
 // Configure store
 const store = configureStore({
 	reducer: slice.reducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				// Ignore these action types
+				ignoredActions: ['setOpfsMountDescriptor'],
+				// Ignore these field paths in all actions
+				ignoredActionPaths: ['payload.handle'],
+				// Ignore these paths in the state
+				ignoredPaths: ['opfsMountDescriptor.handle'],
+			},
+		}),
 });
 
 function setupOnlineOfflineListeners(dispatch: PlaygroundDispatch) {

packages/playground/website/src/lib/use-boot-playground.ts
@@ -3,25 +3,26 @@ import { Blueprint, startPlaygroundWeb } from '@wp-playground/client';
 import type { PlaygroundClient } from '@wp-playground/client';
 import { getRemoteUrl } from './config';
 import { logger } from '@php-wasm/logger';
-import { PlaygroundDispatch, setActiveModal } from './redux-store';
-import { useDispatch } from 'react-redux';
-import { directoryHandle } from './markdown-directory-handle';
-import { joinPaths } from '@php-wasm/util';
+import {
+	PlaygroundDispatch,
+	PlaygroundReduxState,
+	setActiveModal,
+} from './redux-store';
+import { useDispatch, useSelector } from 'react-redux';
+import { playgroundAvailableInOpfs } from '../components/playground-configuration-group/playground-available-in-opfs';
+import { directoryHandleFromMountDevice } from '@wp-playground/storage';
 
 interface UsePlaygroundOptions {
 	blueprint?: Blueprint;
-	storage?: 'browser' | 'device' | 'none';
-	siteSlug?: string;
 }
-export function useBootPlayground({
-	blueprint,
-	storage,
-	siteSlug,
-}: UsePlaygroundOptions) {
+export function useBootPlayground({ blueprint }: UsePlaygroundOptions) {
 	const iframeRef = useRef<HTMLIFrameElement>(null);
 	const iframe = iframeRef.current;
 	const started = useRef<string | undefined>(undefined);
 	const [url, setUrl] = useState<string>();
+	const mountDescriptor = useSelector(
+		(state: PlaygroundReduxState) => state.opfsMountDescriptor
+	);
 	const [playground, setPlayground] = useState<PlaygroundClient>();
 	const [awaitedIframe, setAwaitedIframe] = useState(false);
 	const dispatch: PlaygroundDispatch = useDispatch();
@@ -40,53 +41,53 @@ export function useBootPlayground({
 			return;
 		}
 
-		if (storage) {
-			remoteUrl.searchParams.set('storage', storage);
-		}
+		async function doRun() {
+			started.current = remoteUrl.toString();
 
-		started.current = remoteUrl.toString();
+			let isWordPressInstalled = false;
+			if (mountDescriptor) {
+				isWordPressInstalled = await playgroundAvailableInOpfs(
+					await directoryHandleFromMountDevice(mountDescriptor.device)
+				);
+			}
 
-		let playgroundTmp: PlaygroundClient | undefined = undefined;
-		startPlaygroundWeb({
-			iframe,
-			remoteUrl: remoteUrl.toString(),
-			blueprint,
-			siteSlug,
-			// Intercept the Playground client even if the
-			// Blueprint fails.
-			onClientConnected: (playground) => {
-				playgroundTmp = playground;
-				(window as any)['playground'] = playground;
-			},
-			async onBeforeBlueprint() {
-				const newDirectoryHandle = await directoryHandle;
-				if (!newDirectoryHandle) {
-					return;
-				}
-				await playgroundTmp!.bindOpfs({
-					opfs: newDirectoryHandle,
-					mountpoint: joinPaths(
-						await playgroundTmp!.documentRoot,
-						'wp-content',
-						'uploads',
-						'markdown'
-					),
-					initialSyncDirection: 'opfs-to-memfs',
+			let playgroundTmp: PlaygroundClient | undefined = undefined;
+			try {
+				await startPlaygroundWeb({
+					iframe: iframe!,
+					remoteUrl: getRemoteUrl().toString(),
+					blueprint,
+					// Intercept the Playground client even if the
+					// Blueprint fails.
+					onClientConnected: (playground) => {
+						playgroundTmp = playground;
+						(window as any)['playground'] = playground;
+					},
+					mounts: mountDescriptor
+						? [
+								{
+									...mountDescriptor,
+									initialSyncDirection: 'opfs-to-memfs',
+								},
+						  ]
+						: [],
+					shouldInstallWordPress: !isWordPressInstalled,
 				});
-			},
-		})
-			.catch((error) => {
+			} catch (error) {
 				logger.error(error);
 				dispatch(setActiveModal('start-error'));
-			})
-			.finally(async () => {
+			} finally {
 				if (playgroundTmp) {
-					playgroundTmp.onNavigation((url) => setUrl(url));
+					(playgroundTmp as PlaygroundClient).onNavigation(
+						(url: string) => setUrl(url)
+					);
 					setPlayground(() => playgroundTmp);
 				}
-			});
+			}
+		}
+		doRun();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [iframe, awaitedIframe, directoryHandle, siteSlug]);
+	}, [iframe, awaitedIframe, mountDescriptor]);
 
 	return { playground, url, iframeRef };
 }

packages/playground/website/src/components/mount-markdown-directory-modal/index.tsx
@@ -1,81 +1,0 @@
-import Modal from '../modal';
-import { Button } from '@wordpress/components';
-import { useDispatch } from 'react-redux';
-import { PlaygroundDispatch, setActiveModal } from '../../lib/redux-store';
-import { directoryHandleResolve } from '../../lib/markdown-directory-handle';
-import { logger } from '@php-wasm/logger';
-
-export const localStorageKey = 'playground-start-error-dont-show-again';
-
-export function MountMarkdownDirectoryModal() {
-	const dispatch: PlaygroundDispatch = useDispatch();
-
-	const handleDirectorySelection = (
-		newDirectoryHandle: FileSystemDirectoryHandle
-	) => {
-		directoryHandleResolve(newDirectoryHandle);
-	};
-
-	async function loadMarkdownDirectory(e: React.MouseEvent) {
-		e.preventDefault();
-		let dirHandle;
-		try {
-			// Request permission to access the directory.
-			// https://developer.mozilla.org/en-US/docs/Web/API/Window/showDirectoryPicker
-			dirHandle = await (window as any).showDirectoryPicker({
-				// By specifying an ID, the browser can remember different directories
-				// for different IDs.If the same ID is used for another picker, the
-				// picker opens in the same directory.
-				id: 'playground-directory',
-				mode: 'readwrite',
-			});
-		} catch (e) {
-			// No directory selected but log the error just in case.
-			logger.error(e);
-			return;
-		}
-		handleDirectorySelection(dirHandle);
-		handleClose();
-	}
-
-	function handleClose() {
-		dispatch(setActiveModal(null));
-	}
-
-	return (
-		<Modal isOpen={true} onRequestClose={handleClose}>
-			<header>
-				<h2>Markdown editor</h2>
-			</header>
-
-			<main>
-				{/* @TODO Don't do a wall of text. Do good UX instead. */}
-				<p>
-					This is an online markdown editor. Load your Markdown files
-					from the disk in one of the following ways:
-				</p>
-				<ul>
-					<li>Use the directory picker below</li>
-					<li>
-						Use the file picker below
-						{/* @TODO could make both the same */}
-					</li>
-					<li>
-						Drag & drop files from the disk
-						{/* @TODO If we do that, it won't work after closing this modal */}
-					</li>
-				</ul>
-				<p>
-					... or close this modal and paste the content of the file
-					into the post editor.
-				</p>
-				<footer>
-					<Button variant="primary" onClick={loadMarkdownDirectory}>
-						Load a Markdown directory
-					</Button>
-					<Button onClick={handleClose}>Cancel</Button>
-				</footer>
-			</main>
-		</Modal>
-	);
-}

packages/playground/website/src/components/site-manager/add-site-button/index.tsx
@@ -17,7 +17,7 @@ export function AddSiteButton({
 	onAddSite: (siteName: string) => void;
 	sites: Site[];
 }) {
-	const defaultName = 'My site';
+	const defaultName = 'My Site';
 	const [isModalOpen, setModalOpen] = useState(false);
 	const [siteName, setSiteName] = useState<string | undefined>(defaultName);
 	const addSiteButtonRef = useRef<HTMLFormElement>(null);

packages/playground/website/src/github/github-oauth-guard/index.tsx
@@ -112,7 +112,8 @@ function Authenticate({
 							checked={exported}
 							onChange={() => setExported(!exported)}
 						/>
-						I understand, and I have exported my Playground as a zip if needed.
+						I understand, and I have exported my Playground as a zip
+						if needed.
 					</label>
 				</>
 			) : null}

packages/php-wasm/web/src/lib/directory-handle-mount.ts
@@ -29,6 +29,16 @@ declare global {
 	}
 }
 
+export type MountDevice =
+	| {
+			type: 'opfs';
+			path: string;
+	  }
+	| {
+			type: 'local-fs';
+			handle: FileSystemDirectoryHandle;
+	  };
+
 export interface MountOptions {
 	initialSync: {
 		direction?: 'opfs-to-memfs' | 'memfs-to-opfs';

packages/php-wasm/web/src/lib/index.ts
@@ -4,12 +4,16 @@ export type { LoaderOptions as PHPWebLoaderOptions } from './load-runtime';
 
 export { loadWebRuntime } from './load-runtime';
 export { getPHPLoaderModule } from './get-php-loader-module';
-export { registerServiceWorker, setPhpApi } from './register-service-worker';
+export {
+	registerServiceWorker,
+	setPhpInstanceUsedByServiceWorker,
+} from './register-service-worker';
 export { setupPostMessageRelay } from './setup-post-message-relay';
 
 export { spawnPHPWorkerThread } from './worker-thread/spawn-php-worker-thread';
 export { createDirectoryHandleMountHandler } from './directory-handle-mount';
 export type {
+	MountDevice,
 	MountOptions,
 	SyncProgress,
 	SyncProgressCallback,

commit 5ade2fb68bc082a0e0edddaefd48d3d5503d3da1

Make the site switcher work again (#1698)
## Motivation for the change, related issues

The site switcher was broken by #1669. This PR is a fix for that.

Related to #1687

## Implementation details

The site that is booted depends on the redux store, so the current
loaded site now depends on redux state.

## Testing Instructions (or ideally a Blueprint)

- CI
- Use `npm run dev` and manually test switching between sites

packages/playground/website/src/components/playground-configuration-group/index.tsx
@@ -160,7 +160,6 @@ export default function PlaygroundConfigurationGroup({
 			type: 'local-fs',
 			handle: dirHandle,
 		};
-		dispatch(setOpfsMountDescriptor({ device, mountpoint }));
 		if (idb) {
 			await saveDirectoryHandle(idb, dirHandle);
 		}
@@ -200,7 +199,6 @@ export default function PlaygroundConfigurationGroup({
 				...currentConfiguration,
 				storage: 'device',
 			});
-			await playground.goTo('/');
 
 			// Read current querystring and replace storage=browser with
 			// storage=device.
@@ -208,7 +206,8 @@ export default function PlaygroundConfigurationGroup({
 			url.searchParams.set('storage', 'device');
 			window.history.pushState({}, '', url.toString());
 
-			alert('You are now using WordPress from your local directory.');
+			dispatch(setOpfsMountDescriptor({ device, mountpoint }));
+			alert('You are now loading WordPress from your local directory.');
 		} finally {
 			setMounting(false);
 		}

packages/playground/website/src/components/site-manager/index.tsx
@@ -1,5 +1,6 @@
 import { SiteManagerSidebar } from './site-manager-sidebar';
 import { __experimentalUseNavigator as useNavigator } from '@wordpress/components';
+import store, { selectSite } from '../../lib/redux-store';
 
 import css from './style.module.css';
 
@@ -26,7 +27,7 @@ export function SiteManager({
 		);
 	};
 
-	const onSiteClick = (siteSlug?: string) => {
+	const onSiteClick = async (siteSlug: string) => {
 		onSiteChange(siteSlug);
 		const url = new URL(window.location.href);
 		if (siteSlug) {
@@ -35,6 +36,8 @@ export function SiteManager({
 			url.searchParams.delete('site-slug');
 		}
 		window.history.pushState({}, '', url.toString());
+
+		await store.dispatch(selectSite(siteSlug));
 
 		/**
 		 * On mobile, the site editor and site preview are hidden.

packages/playground/website/src/components/site-manager/site-manager-sidebar/index.tsx
@@ -38,7 +38,7 @@ export function SiteManagerSidebar({
 }: {
 	className?: string;
 	siteSlug?: string;
-	onSiteClick: (siteSlug?: string) => void;
+	onSiteClick: (siteSlug: string) => void;
 }) {
 	const [sites, setSites] = useState<Site[]>([]);
 

packages/playground/website/src/lib/redux-store.ts
@@ -75,6 +75,27 @@ const slice = createSlice({
 // Export actions
 export const { setActiveModal, setOpfsMountDescriptor } = slice.actions;
 
+export function selectSite(siteSlug: string) {
+	return async (dispatch: typeof store.dispatch) => {
+		const opfsRoot = await navigator.storage.getDirectory();
+		const opfsDir = await opfsRoot.getDirectoryHandle(
+			siteSlug === 'wordpress' ? siteSlug : 'site-' + siteSlug,
+			{
+				create: true,
+			}
+		);
+		dispatch(
+			setOpfsMountDescriptor({
+				device: {
+					type: 'opfs',
+					path: await directoryHandleToOpfsPath(opfsDir),
+				},
+				mountpoint: '/wordpress',
+			})
+		);
+	};
+}
+
 // Configure store
 const store = configureStore({
 	reducer: slice.reducer,

packages/playground/website/src/lib/use-boot-playground.ts
@@ -29,9 +29,6 @@ export function useBootPlayground({ blueprint }: UsePlaygroundOptions) {
 
 	useEffect(() => {
 		const remoteUrl = getRemoteUrl();
-		if (started.current === remoteUrl.toString()) {
-			return;
-		}
 		if (!iframe) {
 			// Iframe ref is likely not set on the initial render.
 			// Re-render the current component to start the playground.