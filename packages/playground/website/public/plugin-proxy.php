7689465261ccca0ebac72061e87921447ef2f420
Peter Wilson <git-commit-by@peterwilson.cc>
2024-08-25 10:57:39 +1000

    Allow specifying of WordPress branch.

--- packages/playground/website/public/plugin-proxy.php
+++ packages/playground/website/public/plugin-proxy.php
@@ -334,6 +334,32 @@ try {
         }
 
         $downloader->streamFromGithubReleases($_GET['repo'], $_GET['name']);
+    } else if ( isset( $_GET['branch'] ) ) {
+        $branch = strtolower( $_GET['branch'] );
+        if ( $branch === 'trunk' ) {
+            $branch = 'master';
+            // If the brach is of the form x.x append '-branch' to it.
+        } elseif ( preg_match( '/^\d+\.\d+$/', $branch ) ) {
+            $branch .= '-branch';
+        } elseif ( ! preg_match( '/^\d+\.\d+-branch$/', $branch ) ) {
+            throw new ApiException( 'Invalid branch' );
+        }
+
+        $url = "https://codeload.github.com/WordPress/WordPress/zip/refs/heads/{$branch}";
+
+        streamHttpResponse(
+            $url,
+            'GET',
+            [],
+            file_get_contents('php://input'),
+            null,
+            [
+                'Content-Disposition: attachment; filename="wordpress.zip"',
+                "X-Proxy-Zip-Path: WordPress-{$branch}"
+            ]
+        );
+
+
     } else if (isset($_GET['url'])) {
         // Proxy the current request to $_GET['url'] and return the response,
         // but only if the URL is allowlisted.
