4567e997cbf7aa8cb648be0b39bf11ee2287e8bf
Adam Zieliński <adam@adamziel.com>
2024-08-27 14:12:11 +0200

    Add temporary site notice

--- packages/playground/website/src/components/site-manager/site-info-view/index.tsx
+++ packages/playground/website/src/components/site-manager/site-info-view/index.tsx
@@ -2,8 +2,12 @@ import classNames from 'classnames';
 import css from './style.module.css';
 import { SiteInfo } from '../../../lib/site-storage';
 import { getLogoDataURL, WordPressIcon } from '../icons';
+import { useState } from '@wordpress/element';
 import {
 	Button,
+	Notice,
+	Flex,
+	FlexItem,
 	DropdownMenu,
 	MenuGroup,
 	MenuItem,
@@ -30,141 +34,178 @@ export function SiteInfoView({
 		}
 	};
 
+	const [showNotice, setShowNotice] = useState(site.storage === 'temporary');
 	return (
-		<section className={classNames(className, css.siteManagerSiteInfo)}>
-			<header className={css.siteManagerSiteInfoHeader}>
-				<div className={css.siteManagerSiteInfoHeaderIcon}>
-					{site.logo ? (
-						<img
-							src={getLogoDataURL(site.logo)}
-							alt={site.name + ' logo'}
-						/>
-					) : (
-						<WordPressIcon
-							className={css.siteManagerSiteInfoHeaderIconDefault}
-						/>
-					)}
-				</div>
-				<div className={css.siteManagerSiteInfoHeaderDetails}>
-					<h1 className={css.siteManagerSiteInfoHeaderDetailsName}>
-						{site.name}
-					</h1>
-					<span
-						className={
-							css.siteManagerSiteInfoHeaderDetailsCreatedAt
-						}
-					>
-						{site.whenCreated
-							? `Created ${new Date(
-									site.whenCreated
-							  ).toLocaleString()}`
-							: '--'}
-					</span>
-				</div>
-				<div className={css.siteManagerSiteInfoHeaderActions}>
-					{/* TODO: Find how to specify classes on WP component Buttons. They don't seem to be applying. */}
-					<Button
-						className={css.sieManagerSiteInfoHeaderActionsWpAdmin}
-					>
-						WP Admin
-					</Button>
-					<Button className={css.sieManagerSiteInfoHeaderActionsOpen}>
-						Open site
-					</Button>
-					<DropdownMenu
-						icon={moreVertical}
-						label="Select a direction"
-						popoverProps={{
-							placement: 'bottom-end',
-						}}
-					>
-						{({ onClose }) => (
-							<>
-								{/* <MenuGroup>
-									<MenuItem icon={external} onClick={onClose}>
-										Open site
-									</MenuItem>
-									<MenuItem icon={external} onClick={onClose}>
-										WP Admin
-									</MenuItem>
-								</MenuGroup> */}
-								<MenuGroup>
-									{/* <MenuItem onClick={onClose}>
-										Duplicate
-									</MenuItem>
-									<MenuItem onClick={onClose}>Reset</MenuItem> */}
-									<MenuItem
-										onClick={() =>
-											removeSiteAndCloseMenu(onClose)
-										}
-										// Avoid deleting the default WordPress site
-										disabled={site.slug === 'wordpress'}
-									>
-										Delete
-									</MenuItem>
-								</MenuGroup>
-								{/* <MenuGroup>
-									<MenuItem onClick={onClose}>
-										Download as .zip
-									</MenuItem>
-									<MenuItem onClick={onClose}>
-										Restore from .zip
-									</MenuItem>
-								</MenuGroup>
-								<MenuGroup>
-									<MenuItem onClick={onClose}>
-										Share feedback
-									</MenuItem>
-								</MenuGroup> */}
-							</>
+		<section
+			className={classNames(className, css.siteManagerSiteInfoContainer)}
+		>
+			{showNotice ? (
+				<Notice
+					className={css.siteNotice}
+					status="info"
+					onRemove={() => setShowNotice(false)}
+				>
+					<Flex direction="row" gap={2}>
+						<FlexItem>
+							<b>This is a temporary site.</b> Your changes will
+							be lost on page refresh.
+						</FlexItem>
+						<FlexItem>
+							<Button
+								variant="primary"
+								onClick={() => {
+									alert('@TODO implement me');
+								}}
+							>
+								Save locally
+							</Button>
+						</FlexItem>
+					</Flex>
+				</Notice>
+			) : null}
+			<section className={css.siteManagerSiteInfoContents}>
+				<header className={css.siteManagerSiteInfoHeader}>
+					<div className={css.siteManagerSiteInfoHeaderIcon}>
+						{site.logo ? (
+							<img
+								src={getLogoDataURL(site.logo)}
+								alt={site.name + ' logo'}
+							/>
+						) : (
+							<WordPressIcon
+								className={
+									css.siteManagerSiteInfoHeaderIconDefault
+								}
+							/>
 						)}
-					</DropdownMenu>
-				</div>
-			</header>
-			<section className={css.siteManagerSiteInfoSection}>
-				<header className={css.siteManagerSiteInfoSectionTitle}>
-					Site details
+					</div>
+					<div className={css.siteManagerSiteInfoHeaderDetails}>
+						<h1
+							className={css.siteManagerSiteInfoHeaderDetailsName}
+						>
+							{site.name}
+						</h1>
+						<span
+							className={
+								css.siteManagerSiteInfoHeaderDetailsCreatedAt
+							}
+						>
+							{site.whenCreated
+								? `Created ${new Date(
+										site.whenCreated
+								  ).toLocaleString()}`
+								: '--'}
+						</span>
+					</div>
+					<div className={css.siteManagerSiteInfoHeaderActions}>
+						{/* TODO: Find how to specify classes on WP component Buttons. They don't seem to be applying. */}
+						<Button
+							className={
+								css.sieManagerSiteInfoHeaderActionsWpAdmin
+							}
+						>
+							WP Admin
+						</Button>
+						<Button
+							className={css.sieManagerSiteInfoHeaderActionsOpen}
+						>
+							Open site
+						</Button>
+						<DropdownMenu
+							icon={moreVertical}
+							label="Select a direction"
+							popoverProps={{
+								placement: 'bottom-end',
+							}}
+						>
+							{({ onClose }) => (
+								<>
+									{/* <MenuGroup>
+										<MenuItem icon={external} onClick={onClose}>
+											Open site
+										</MenuItem>
+										<MenuItem icon={external} onClick={onClose}>
+											WP Admin
+										</MenuItem>
+									</MenuGroup> */}
+									<MenuGroup>
+										{/* <MenuItem onClick={onClose}>
+											Duplicate
+										</MenuItem>
+										<MenuItem onClick={onClose}>Reset</MenuItem> */}
+										<MenuItem
+											onClick={() =>
+												removeSiteAndCloseMenu(onClose)
+											}
+											// Avoid deleting the default WordPress site
+											disabled={site.slug === 'wordpress'}
+										>
+											Delete
+										</MenuItem>
+									</MenuGroup>
+									{/* <MenuGroup>
+										<MenuItem onClick={onClose}>
+											Download as .zip
+										</MenuItem>
+										<MenuItem onClick={onClose}>
+											Restore from .zip
+										</MenuItem>
+									</MenuGroup>
+									<MenuGroup>
+										<MenuItem onClick={onClose}>
+											Share feedback
+										</MenuItem>
+									</MenuGroup> */}
+								</>
+							)}
+						</DropdownMenu>
+					</div>
 				</header>
-				<div className={css.siteManagerSiteInfoSectionRow}>
-					<div className={css.siteManagerSiteInfoSectionRowLabel}>
-						Name:
+				<section className={css.siteManagerSiteInfoSection}>
+					<header className={css.siteManagerSiteInfoSectionTitle}>
+						Site details
+					</header>
+					<div className={css.siteManagerSiteInfoSectionRow}>
+						<div className={css.siteManagerSiteInfoSectionRowLabel}>
+							Name:
+						</div>
+						<div className={css.siteManagerSiteInfoSectionRowValue}>
+							{site.name}
+						</div>
 					</div>
-					<div className={css.siteManagerSiteInfoSectionRowValue}>
-						{site.name}
+					<div className={css.siteManagerSiteInfoSectionRow}>
+						<div className={css.siteManagerSiteInfoSectionRowLabel}>
+							Storage:
+						</div>
+						<div className={css.siteManagerSiteInfoSectionRowValue}>
+							{site.storage}
+						</div>
 					</div>
-				</div>
-				<div className={css.siteManagerSiteInfoSectionRow}>
-					<div className={css.siteManagerSiteInfoSectionRowLabel}>
-						Storage:
+					<div className={css.siteManagerSiteInfoSectionRow}>
+						<div className={css.siteManagerSiteInfoSectionRowLabel}>
+							WordPress version:
+						</div>
+						<div className={css.siteManagerSiteInfoSectionRowValue}>
+							{site.wpVersion}
+						</div>
 					</div>
-					<div className={css.siteManagerSiteInfoSectionRowValue}>
-						{site.storage}
+					<div className={css.siteManagerSiteInfoSectionRow}>
+						<div className={css.siteManagerSiteInfoSectionRowLabel}>
+							PHP version:
+						</div>
+						<div className={css.siteManagerSiteInfoSectionRowValue}>
+							{site.phpVersion}
+						</div>
 					</div>
-				</div>
-				<div className={css.siteManagerSiteInfoSectionRow}>
-					<div className={css.siteManagerSiteInfoSectionRowLabel}>
-						WordPress version:
+					<div className={css.siteManagerSiteInfoSectionRow}>
+						<div className={css.siteManagerSiteInfoSectionRowLabel}>
+							Network access:
+						</div>
+						<div className={css.siteManagerSiteInfoSectionRowValue}>
+							TODO
+						</div>
 					</div>
-					<div className={css.siteManagerSiteInfoSectionRowValue}>
-						{site.wpVersion}
-					</div>
-				</div>
-				<div className={css.siteManagerSiteInfoSectionRow}>
-					<div className={css.siteManagerSiteInfoSectionRowLabel}>
-						PHP version:
-					</div>
-					<div className={css.siteManagerSiteInfoSectionRowValue}>
-						{site.phpVersion}
-					</div>
-				</div>
-				<div className={css.siteManagerSiteInfoSectionRow}>
-					<div className={css.siteManagerSiteInfoSectionRowLabel}>
-						Network access:
-					</div>
-					<div className={css.siteManagerSiteInfoSectionRowValue}>
-						TODO
-					</div>
-				</div>
+				</section>
 			</section>
 		</section>
 	);
