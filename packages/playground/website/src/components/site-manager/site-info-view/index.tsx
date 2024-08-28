cd459320c8d646d79c8ad0646216c34426a8961a
Adam Zieliński <adam@adamziel.com>
2024-08-28 13:10:59 +0200

    Design iteration

--- packages/playground/website/src/components/site-manager/site-info-view/index.tsx
+++ packages/playground/website/src/components/site-manager/site-info-view/index.tsx
@@ -25,7 +25,7 @@ function SiteInfoRow({
 	value: string | JSX.Element;
 }) {
 	return (
-		<Flex justify="flex-start" align="baseline">
+		<Flex justify="flex-start" align="baseline" expanded={true}>
 			<FlexItem className={css.infoRowLabel}>{label}</FlexItem>
 			<FlexItem className={css.infoRowValue}>{value}</FlexItem>
 		</Flex>
@@ -54,16 +54,14 @@ export function SiteInfoView({
 
 	const [showNotice, setShowNotice] = useState(site.storage === 'temporary');
 	return (
-		<section
-			className={classNames(className, css.siteManagerSiteInfoContainer)}
-		>
+		<section className={classNames(className, css.siteInfoView)}>
 			{showNotice ? (
 				<Notice
 					className={css.siteNotice}
 					status="info"
 					onRemove={() => setShowNotice(false)}
 				>
-					<Flex direction="row" gap={2}>
+					<Flex direction="row" gap={2} expanded={true}>
 						<FlexItem>
 							<b>This is a temporary site.</b> Your changes will
 							be lost on page refresh.
@@ -81,67 +79,77 @@ export function SiteInfoView({
 					</Flex>
 				</Notice>
 			) : null}
-			<section className={css.siteManagerSiteInfoContents}>
-				<header className={css.siteManagerSiteInfoHeader}>
-					<div className={css.siteManagerSiteInfoHeaderIcon}>
-						{site.logo ? (
-							<img
-								src={getLogoDataURL(site.logo)}
-								alt={site.name + ' logo'}
-							/>
-						) : (
-							<WordPressIcon
-								className={
-									css.siteManagerSiteInfoHeaderIconDefault
-								}
-							/>
-						)}
-					</div>
-					<div className={css.siteManagerSiteInfoHeaderDetails}>
-						<h1
-							className={css.siteManagerSiteInfoHeaderDetailsName}
-						>
-							{site.name}
-						</h1>
-						<span
-							className={
-								css.siteManagerSiteInfoHeaderDetailsCreatedAt
-							}
-						>
-							{site.whenCreated
-								? `Created ${new Date(
-										site.whenCreated
-								  ).toLocaleString()}`
-								: '--'}
-						</span>
-					</div>
-					<div className={css.siteManagerSiteInfoHeaderActions}>
-						<Button variant="tertiary">
-							WP Admin
-							<Icon
-								icon={external}
-								size={16}
-								style={{ marginLeft: '8px' }}
-							/>
-						</Button>
-						<Button variant="secondary">
-							Open site
-							<Icon
-								icon={external}
-								size={16}
-								style={{ marginLeft: '8px' }}
-							/>
-						</Button>
-						<DropdownMenu
-							icon={moreVertical}
-							label="Select a direction"
-							popoverProps={{
-								placement: 'bottom-end',
-							}}
-						>
-							{({ onClose }) => (
-								<>
-									{/* <MenuGroup>
+			<Flex
+				direction="column"
+				gap={1}
+				justify="flex-start"
+				expanded={true}
+			>
+				<FlexItem>
+					<Flex
+						direction="row"
+						gap={4}
+						justify="space-between"
+						align="center"
+						expanded={true}
+						className={css.padded}
+					>
+						<FlexItem>
+							<Flex direction="row" gap={4}>
+								<FlexItem className={css.siteInfoHeaderIcon}>
+									{site.logo ? (
+										<img
+											src={getLogoDataURL(site.logo)}
+											alt={site.name + ' logo'}
+										/>
+									) : (
+										<WordPressIcon
+											className={
+												css.siteInfoHeaderIconDefault
+											}
+										/>
+									)}
+								</FlexItem>
+								<Flex
+									direction="column"
+									gap={0.25}
+									expanded={true}
+								>
+									<h1
+										className={
+											css.siteInfoHeaderDetailsName
+										}
+									>
+										{site.name}
+									</h1>
+									<span
+										className={
+											css.siteInfoHeaderDetailsCreatedAt
+										}
+									>
+										{site.whenCreated
+											? `Created ${new Date(
+													site.whenCreated
+											  ).toLocaleString()}`
+											: 'Created recently'}
+									</span>
+								</Flex>
+							</Flex>
+						</FlexItem>
+						<FlexItem>
+							<Flex direction="row" gap={2} align="center">
+								<Button variant="tertiary">WP Admin</Button>
+								<Button variant="secondary">Open site</Button>
+								<DropdownMenu
+									icon={moreVertical}
+									label="Select a direction"
+									popoverProps={{
+										placement: 'bottom-end',
+									}}
+								>
+									{({ onClose }) => (
+										<>
+											{/* <MenuGroup>
 										<MenuItem icon={external} onClick={onClose}>
 											Open site
 										</MenuItem>
@@ -149,22 +157,27 @@ export function SiteInfoView({
 											WP Admin
 										</MenuItem>
 									</MenuGroup> */}
-									<MenuGroup>
-										{/* <MenuItem onClick={onClose}>
+											<MenuGroup>
+												{/* <MenuItem onClick={onClose}>
 											Duplicate
 										</MenuItem>
 										<MenuItem onClick={onClose}>Reset</MenuItem> */}
-										<MenuItem
-											onClick={() =>
-												removeSiteAndCloseMenu(onClose)
-											}
-											// Avoid deleting the default WordPress site
-											disabled={site.slug === 'wordpress'}
-										>
-											Delete
-										</MenuItem>
-									</MenuGroup>
-									{/* <MenuGroup>
+												<MenuItem
+													onClick={() =>
+														removeSiteAndCloseMenu(
+															onClose
+														)
+													}
+													// Avoid deleting the default WordPress site
+													disabled={
+														site.slug ===
+														'wordpress'
+													}
+												>
+													Delete
+												</MenuItem>
+											</MenuGroup>
+											{/* <MenuGroup>
 										<MenuItem onClick={onClose}>
 											Download as .zip
 										</MenuItem>
@@ -177,201 +190,208 @@ export function SiteInfoView({
 											Share feedback
 										</MenuItem>
 									</MenuGroup> */}
-								</>
-							)}
-						</DropdownMenu>
-					</div>
-				</header>
-				<TabPanel
-					className={css.maxWidth}
-					onSelect={function noRefCheck() {}}
-					tabs={[
-						{
-							name: 'settings',
-							title: 'Settings',
-						},
-						{
-							name: 'logs',
-							title: 'Logs',
-						},
-					]}
-				>
-					{(tab) => (
-						<div className={css.maxWidth}>
-							{tab.name === 'settings' && (
-								<section className={css.maxWidth}>
-									<Flex
-										direction="column"
-										gap={4}
-										className={css.infoTable}
-									>
-										<FlexItem>
-											<h3
-												className={
-													css.siteManagerSiteInfoSectionTitle
-												}
-											>
-												Site details
-											</h3>
-										</FlexItem>
-										<SiteInfoRow
-											label="Name:"
-											value={site.name}
-										/>
-										<SiteInfoRow
-											label="Theme:"
-											value="TBD"
-										/>
-										<SiteInfoRow
-											label="Storage:"
-											value={
-												{
-													temporary: 'None',
-													'local-fs':
-														'Local directory',
-													opfs: 'This browser',
-												}[site.storage]
-											}
-										/>
-										<SiteInfoRow
-											label="WordPress version:"
-											value={site.wpVersion}
-										/>
-										<SiteInfoRow
-											label="PHP version:"
-											value={site.phpVersion}
-										/>
-										<SiteInfoRow
-											label="Network access:"
-											value="TODO"
-										/>
-									</Flex>
-
-									<Flex direction="column" gap={4}>
-										<FlexItem>
-											<h3
-												className={
-													css.siteManagerSiteInfoSectionTitle
-												}
-											>
-												WP Admin
-											</h3>
-										</FlexItem>
-										<SiteInfoRow
-											label="Username:"
-											value={
-												<>
-													admin
-													<Button
-														variant="secondary"
-														size="small"
-														icon={
-															<Icon icon={copy} />
-														}
-														onClick={() => {
-															/* TODO: Implement copy */
-														}}
-														label="Copy password"
-													/>
-												</>
-											}
-										/>
-										<SiteInfoRow
-											label="Password:"
-											value={
-												<Flex gap={2}>
-													<FlexItem>
-														••••••••
-														<Button
-															variant="secondary"
-															size="small"
-															icon={
-																<Icon
-																	icon={copy}
-																/>
-															}
-															onClick={() => {
-																/* TODO: Implement copy */
-															}}
-															label="Copy password"
-														/>
-														<Button
-															variant="secondary"
-															size="small"
-															icon={
-																<Icon
-																	icon={seen}
-																/>
-															}
-															onClick={() => {
-																/* TODO: Implement reveal */
-															}}
-															label="Reveal password"
-														/>
-													</FlexItem>
-												</Flex>
-											}
-										/>
-										<SiteInfoRow
-											label="WP Admin URL:"
-											value={
-												<a
-													href={`${site.url}/wp-admin`}
-													target="_blank"
-													rel="noopener noreferrer"
-												>
-													{site.url}/wp-admin{' '}
-													<Icon
-														icon={external}
-														size={16}
-													/>
-												</a>
-											}
-										/>
-									</Flex>
-
-									<Flex direction="row" gap={2}>
-										<FlexItem>
-											<Button
-												variant="tertiary"
-												onClick={() => {
-													/* TODO: Implement edit site details */
-												}}
-											>
-												Edit site details
-											</Button>
-										</FlexItem>
-										<FlexItem>
-											<Button
-												variant="tertiary"
-												href="#" // TODO: Replace with actual Blueprint preview URL
-												target="_blank"
-												rel="noopener noreferrer"
-												icon={
-													<Icon
-														icon={external}
-														size={16}
-													/>
-												}
-												iconPosition="right"
-											>
-												Preview Blueprint
-											</Button>
-										</FlexItem>
-									</Flex>
-								</section>
-							)}
-							{tab.name === 'logs' && (
-								<section
-									className={css.siteManagerSiteInfoSection}
-								>
-									<SiteLogs />
-								</section>
-							)}
-						</div>
-					)}
-				</TabPanel>
-			</section>
+										</>
+									)}
+								</DropdownMenu>
+							</Flex>
+						</FlexItem>
+					</Flex>
+				</FlexItem>
+				<FlexItem>
+					<TabPanel
+						className={css.tabs}
+						onSelect={function noRefCheck() {}}
+						tabs={[
+							{
+								name: 'settings',
+								title: 'Settings',
+							},
+							{
+								name: 'logs',
+								title: 'Logs',
+							},
+						]}
+					>
+						{(tab) => (
+							<div
+								className={classNames(
+									css.tabContents,
+									css.padded
+								)}
+							>
+								{tab.name === 'settings' && (
+									<SiteSettingsTab site={site} />
+								)}
+								{tab.name === 'logs' && (
+									<section className={css.siteInfoSection}>
+										<SiteLogs />
+									</section>
+								)}
+							</div>
+						)}
+					</TabPanel>
+				</FlexItem>
+			</Flex>
 		</section>
+	);
+}
+
+function SiteSettingsTab({ site }: { site: SiteInfo }) {
+	return (
+		<Flex
+			gap={8}
+			direction="column"
+			className={css.maxWidth}
+			expanded={true}
+		>
+			<FlexItem>
+				<Flex
+					gap={4}
+					direction="column"
+					className={css.infoTable}
+					expanded={true}
+				>
+					<FlexItem>
+						<h3 className={css.sectionTitle}>Site details</h3>
+					</FlexItem>
+					<SiteInfoRow label="Site name" value={site.name} />
+					<SiteInfoRow
+						label="Storage"
+						value={
+							{
+								temporary: 'None',
+								'local-fs': 'Local directory',
+								opfs: 'This browser',
+							}[site.storage]
+						}
+					/>
+					<SiteInfoRow
+						label="WordPress version"
+						value={site.wpVersion}
+					/>
+					<SiteInfoRow label="PHP version" value={site.phpVersion} />
+					<SiteInfoRow label="Network access" value="TODO" />
+					<SiteInfoRow label="Theme:" value="TBD" />
+				</Flex>
+			</FlexItem>
+
+			<FlexItem>
+				<Flex direction="column" gap={2} expanded={true}>
+					<FlexItem>
+						<h3 className={css.sectionTitle}>WP Admin</h3>
+					</FlexItem>
+					<SiteInfoRow
+						label="Username"
+						value={
+							<Button
+								variant="link"
+								className={classNames(
+									css.grayLink,
+									css.buttonNoPadding
+								)}
+								icon={() => <Icon size={16} icon={copy} />}
+								iconPosition="right"
+								onClick={() => {
+									/* TODO: Implement copy */
+								}}
+								label="Copy password"
+							>
+								admin
+							</Button>
+						}
+					/>
+					<SiteInfoRow
+						label="Password"
+						value={
+							<Flex
+								gap={0}
+								expanded={true}
+								align="center"
+								justify="space-between"
+							>
+								<Button
+									variant="link"
+									className={classNames(
+										css.grayLink,
+										css.buttonNoPadding
+									)}
+									icon={() => <Icon size={16} icon={copy} />}
+									iconPosition="right"
+									onClick={() => {
+										/* TODO: Implement copy */
+									}}
+									label="Copy password"
+								>
+									••••••••
+								</Button>
+								<Button
+									variant="link"
+									className={classNames(
+										css.grayLink,
+										css.buttonNoPadding
+									)}
+									icon={() => <Icon size={18} icon={seen} />}
+									iconPosition="right"
+									onClick={() => {
+										/* TODO: Implement reveal */
+									}}
+									label="Reveal password"
+								/>
+							</Flex>
+						}
+					/>
+					<SiteInfoRow
+						label="Admin URL"
+						value={
+							<Button
+								variant="link"
+								href={`${site.url}/wp-admin`}
+								target="_blank"
+								rel="noopener noreferrer"
+								className={css.buttonNoPadding}
+								icon={() => <Icon size={16} icon={external} />}
+								iconPosition="right"
+								label="Go to Admin"
+							>
+								{site.url}/wp-admin{' '}
+							</Button>
+						}
+					/>
+				</Flex>
+			</FlexItem>
+			<FlexItem>
+				<Flex
+					direction="row"
+					gap={3}
+					expanded={true}
+					justify="flex-start"
+				>
+					<FlexItem>
+						<Button
+							variant="tertiary"
+							className={css.buttonNoPadding}
+							onClick={() => {
+								/* TODO: Implement edit site details */
+							}}
+						>
+							Edit settings
+						</Button>
+					</FlexItem>
+					<FlexItem>
+						<Button
+							variant="link"
+							className={css.buttonNoPadding}
+							href="#" // TODO: Replace with actual Blueprint preview URL
+							target="_blank"
+							rel="noopener noreferrer"
+							icon={() => <Icon icon={external} size={16} />}
+							iconPosition="right"
+						>
+							View Blueprint
+						</Button>
+					</FlexItem>
+				</Flex>
+			</FlexItem>
+		</Flex>
 	);
 }
