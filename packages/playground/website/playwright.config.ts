7ffd742e9474e7fce6d257faa649f02839271824
Bero <berislav.grgicak@gmail.com>
2024-09-16 13:36:26 +0200

    Disable webserver

--- packages/playground/website/playwright.config.ts
+++ packages/playground/website/playwright.config.ts
@@ -70,9 +70,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: 'npm run dev',
-		url: 'http://127.0.0.1:5400/website-server/',
-		reuseExistingServer: !process.env.CI,
-	},
+	// webServer: {
+	// 	command: 'npm run dev',
+	// 	url: 'http://127.0.0.1:5400/website-server/',
+	// 	reuseExistingServer: !process.env.CI,
+	// },
 });
