53b7035d594c5179f3acfae01322d0d3e5b36dcd
Bero <berislav.grgicak@gmail.com>
2024-09-17 08:05:38 +0200

    Fix base url

--- packages/playground/website/playwright.config.ts
+++ packages/playground/website/playwright.config.ts
@@ -7,6 +7,8 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+export const baseURL = process.env['BASE_URL'];
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -25,7 +27,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://127.0.0.1:5400/website-server/',
+		baseURL: baseURL ?? 'http://127.0.0.1:5400/website-server/',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
