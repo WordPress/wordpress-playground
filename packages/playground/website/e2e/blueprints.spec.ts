563756d517ec98adddaabf024348701cd66294e8
Bero <berislav.grgicak@gmail.com>
2024-09-17 10:37:50 +0200

    Rebuild CI commands

---
+++ packages/playground/website/e2e/blueprints.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from './wordpress-fixtures';
+import { Blueprint } from '@wp-playground/blueprints';
+import { encodeStringAsBase64 } from '../src/lib/base64';
+
+test('Base64-encoded Blueprints should work', async ({
+	page,
+	wordpressPage,
+}) => {
+	const blueprint: Blueprint = {
+		landingPage: '/',
+		steps: [{ step: 'enableMultisite' }],
+	};
+
+	const encodedBlueprint = encodeStringAsBase64(JSON.stringify(blueprint));
+	await page.goto(`/#${encodedBlueprint}`);
+
+	// Use wordpressPage to interact with the WordPress content
+	const bodyText = await wordpressPage.locator('body').textContent();
+	expect(bodyText).toContain('My Sites');
+});
