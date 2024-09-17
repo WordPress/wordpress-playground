847379fc115d569c1df16492cc287eb05b47602d
Bero <berislav.grgicak@gmail.com>
2024-09-17 09:37:07 +0200

    Don't start preview server

--- packages/playground/website/playwright.config.ts
+++ packages/playground/website/playwright.config.ts
@@ -70,11 +70,4 @@ export default defineConfig({
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	// 	command: 'npm run preview',
-	// 	url: 'http://127.0.0.1:5932/',
-	// 	reuseExistingServer: !process.env.CI,
-	// },
 });
