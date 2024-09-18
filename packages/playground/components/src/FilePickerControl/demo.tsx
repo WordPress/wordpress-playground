67c014489e7c6bd602484f18fb28bfd38980f5f1
Adam Zieliński <adam@adamziel.com>
2024-09-18 18:43:58 +0200

    Make repo and branch configurable

--- packages/playground/components/src/FilePickerControl/demo.tsx
+++ packages/playground/components/src/FilePickerControl/demo.tsx
@@ -17,10 +17,6 @@ import {
 } from '@wordpress/components';
 import css from './style.module.css';
 
-const repoUrl =
-	'http://127.0.0.1:5263/proxy.php/https://github.com/WordPress/wordpress-playground.git';
-const branch = 'refs/heads/trunk';
-
 // const mockFileStructure = [
 // 	{
 // 		name: '/',
@@ -53,6 +49,10 @@ const branch = 'refs/heads/trunk';
 // ];
 
 export default function GitBrowserDemo() {
+	const [repoUrl, setRepoUrl] = React.useState(
+		'http://127.0.0.1:5263/proxy.php/https://github.com/WordPress/wordpress-playground.git'
+	);
+	const [branch, setBranch] = React.useState('refs/heads/trunk');
 	const [customPath, setCustomPath] = React.useState('/wordpress/');
 	const [selectedPathType, setSelectedPathType] = React.useState<
 		'mu-plugin' | 'plugin' | 'theme' | 'custom'
@@ -80,92 +80,82 @@ export default function GitBrowserDemo() {
 		setCheckedOutFiles(checkedOutFiles);
 	}
 	return (
-		<div>
-			<style>
-				{`
-			.path-mapping-control-wrapper {
-				width: 300px;
-				max-width: 300px;
-				height: 500px;
-				max-height: 500px;
-				overflow-y: auto;
-				border: 1px solid #ddd;
-			}`}
-			</style>
-			<Flex
-				direction="row"
-				gap={4}
-				justify="flex-start"
-				align="flex-start"
-			>
-				<FlexItem>
-					<GitPathControl
-						value={selectedPath}
-						onChange={(selection) =>
-							setSelectedPath(selection.path)
-						}
-					/>
-				</FlexItem>
-				<FlexItem style={{ width: '300px' }}>
-					<h2>Path mapping</h2>
-					<SelectControl
-						label="Map as"
-						disabled={!selectedPath}
-						value={selectedPathType}
-						onChange={(value) => setSelectedPathType(value as any)}
-						options={[
-							{ label: 'Plugin', value: 'plugin' },
-							{ label: 'Theme', value: 'theme' },
-							{ label: 'mu-plugin', value: 'mu-plugin' },
-							{ label: 'Custom path', value: 'custom' },
-						]}
-					/>
-					{selectedPath && (
-						<>
-							{selectedPathType === 'custom' && (
-								<InputControl
-									disabled={!selectedPath}
-									label="Custom path"
-									value={customPath}
-									autoFocus
-									onChange={(value) =>
-										setCustomPath(value as string)
-									}
-								/>
-							)}
-							<h3>Mapped as:</h3>
-							{(function () {
-								const basePath = '/wordpress';
-								const lastSegment = selectedPath
-									.split('/')
-									.pop();
-								switch (selectedPathType) {
-									case 'plugin':
-										return `${basePath}/wp-content/plugins/${lastSegment}`;
-									case 'theme':
-										return `${basePath}/wp-content/themes/${lastSegment}`;
-									case 'mu-plugin':
-										return `${basePath}/wp-content/mu-plugins/${lastSegment}`;
-									case 'custom':
-										return customPath;
+		<Flex direction="row" gap={4} justify="flex-start" align="flex-start">
+			<FlexItem>
+				<InputControl
+					label="Repository URL"
+					value={repoUrl}
+					onChange={(value) => setRepoUrl(value as string)}
+				/>
+				<InputControl
+					label="Branch"
+					value={branch}
+					onChange={(value) => setBranch(value as string)}
+				/>
+				<GitPathControl
+					repoUrl={repoUrl}
+					branch={branch}
+					value={selectedPath}
+					onChange={(selection) => setSelectedPath(selection.path)}
+				/>
+			</FlexItem>
+			<FlexItem style={{ width: '300px' }}>
+				<h2>Path mapping</h2>
+				<SelectControl
+					label="Map as"
+					disabled={!selectedPath}
+					value={selectedPathType}
+					onChange={(value) => setSelectedPathType(value as any)}
+					options={[
+						{ label: 'Plugin', value: 'plugin' },
+						{ label: 'Theme', value: 'theme' },
+						{ label: 'mu-plugin', value: 'mu-plugin' },
+						{ label: 'Custom path', value: 'custom' },
+					]}
+				/>
+				{selectedPath && (
+					<>
+						{selectedPathType === 'custom' && (
+							<InputControl
+								disabled={!selectedPath}
+								label="Custom path"
+								value={customPath}
+								autoFocus
+								onChange={(value) =>
+									setCustomPath(value as string)
 								}
-							})()}
-						</>
-					)}
-				</FlexItem>
-				<FlexItem>
-					<h3>Selected path:</h3>
-					<pre>{JSON.stringify(selectedPath, null, 2)}</pre>
-					<h3>Repository files to checkout:</h3>
-					<pre>{JSON.stringify(filesToCheckout, null, 2)}</pre>
-					<button onClick={() => doSparseCheckout()}>
-						Sparse checkout mapped files
-					</button>
-					<h3>Checked out files:</h3>
-					<pre>{JSON.stringify(checkedOutFiles, null, 2)}</pre>
-				</FlexItem>
-			</Flex>
-		</div>
+							/>
+						)}
+						<h3>Mapped as:</h3>
+						{(function () {
+							const basePath = '/wordpress';
+							const lastSegment = selectedPath.split('/').pop();
+							switch (selectedPathType) {
+								case 'plugin':
+									return `${basePath}/wp-content/plugins/${lastSegment}`;
+								case 'theme':
+									return `${basePath}/wp-content/themes/${lastSegment}`;
+								case 'mu-plugin':
+									return `${basePath}/wp-content/mu-plugins/${lastSegment}`;
+								case 'custom':
+									return customPath;
+							}
+						})()}
+					</>
+				)}
+			</FlexItem>
+			<FlexItem>
+				<h3>Selected path:</h3>
+				<pre>{JSON.stringify(selectedPath, null, 2)}</pre>
+				<h3>Repository files to checkout:</h3>
+				<pre>{JSON.stringify(filesToCheckout, null, 2)}</pre>
+				<button onClick={() => doSparseCheckout()}>
+					Sparse checkout mapped files
+				</button>
+				<h3>Checked out files:</h3>
+				<pre>{JSON.stringify(checkedOutFiles, null, 2)}</pre>
+			</FlexItem>
+		</Flex>
 	);
 }
 
@@ -226,9 +216,13 @@ export function GitPathPicker({
 }
 
 export function GitPathControl({
+	repoUrl,
+	branch,
 	value = '',
 	onChange,
 }: {
+	repoUrl: string;
+	branch: string;
 	value?: string;
 	onChange: (selection: GitPathSelection) => void;
 }) {
