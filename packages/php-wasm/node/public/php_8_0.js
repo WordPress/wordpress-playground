3a16b4f710b0bc6a1fbf22135bf809f160b42810
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2024-09-19 00:31:38 +0700

    Compiled new wasm binaries

--- packages/php-wasm/node/public/php_8_0.js
+++ packages/php-wasm/node/public/php_8_0.js
@@ -1,6 +1,6 @@
 const dependencyFilename = __dirname + '/8_0_30/php_8_0.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 14710527; 
+export const dependenciesTotalSize = 14713713; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
