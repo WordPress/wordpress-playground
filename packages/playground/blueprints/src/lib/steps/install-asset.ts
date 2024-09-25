c0067010d98f3456f2a6016c53c7f0b9ffbfa48b
Dion Hulse <contact@dd32.id.au>
2024-09-25 12:57:51 +1000

    Add a targetSlug parameter to installAsset.

--- packages/playground/blueprints/src/lib/steps/install-asset.ts
+++ packages/playground/blueprints/src/lib/steps/install-asset.ts
@@ -17,6 +17,10 @@ export interface InstallAssetOptions {
 	 */
 	targetPath: string;
 	/**
+	 * Target slug to install the asset into.
+	 */
+	targetSlug?: string;
+	/**
 	 * What to do if the asset already exists.
 	 */
 	ifAlreadyInstalled?: 'overwrite' | 'skip' | 'error';
@@ -31,6 +35,7 @@ export async function installAsset(
 		targetPath,
 		zipFile,
 		ifAlreadyInstalled = 'overwrite',
+		targetSlug = ''
 	}: InstallAssetOptions
 ): Promise<{
 	assetFolderPath: string;
@@ -80,6 +85,11 @@ export async function installAsset(
 		} else {
 			tmpAssetPath = tmpUnzippedFilesPath;
 			assetFolderName = assetNameGuess;
+		}
+
+		// If a specific slug was requested be used, use that.
+		if ( targetSlug && targetSlug.length ) {
+			assetFolderName = targetSlug;
 		}
 
 		// Move asset folder to target path
