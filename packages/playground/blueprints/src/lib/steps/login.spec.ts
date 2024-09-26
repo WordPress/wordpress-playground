b23e5cb3a8a967baf2b3c6b14caa156155effda8
Brandon Payton <brandon@happycode.net>
2024-09-25 23:20:31 -0400

    Update login tests to account for expected redirects

--- packages/playground/blueprints/src/lib/steps/login.spec.ts
+++ packages/playground/blueprints/src/lib/steps/login.spec.ts
@@ -9,7 +9,7 @@ import { PHPRequestHandler } from '@php-wasm/universal';
 import { bootWordPress } from '@wp-playground/wordpress';
 import { loadNodeRuntime } from '@php-wasm/node';
 
-describe('Blueprint step installPlugin', () => {
+describe('Blueprint step login', () => {
 	let php: PHP;
 	let handler: PHPRequestHandler;
 	beforeEach(async () => {
@@ -24,11 +24,37 @@ describe('Blueprint step installPlugin', () => {
 		php = await handler.getPrimaryPhp();
 	});
 
-	it('should log the user in', async () => {
+	it('should redirect user to wp-login.php with correct redirect_to', async () => {
 		await login(php, {});
 		const response = await handler.request({
 			url: '/wp-admin/',
 		});
-		expect(response.text).toContain('Dashboard');
+		expect(response.httpStatusCode).toBe(302);
+		expect(response.headers['location']).toHaveLength(1);
+		const initialRedirectUrl = new URL(response.headers['location'][0]);
+		expect(initialRedirectUrl.pathname).toBe('/wp-login.php');
+		expect(initialRedirectUrl.searchParams.get('redirect_to')).toBe(
+			`${handler.absoluteUrl}/wp-admin/`
+		);
+
+		const loginResponse = await handler.request({
+			url: initialRedirectUrl.toString(),
+		});
+		expect(loginResponse.httpStatusCode).toBe(302);
+		expect(loginResponse.headers['location']).toHaveLength(1);
+		const postLoginRedirectUrl = new URL(
+			loginResponse.headers['location'][0]
+		);
+		expect(postLoginRedirectUrl.pathname).toBe(
+			`${handler.absoluteUrl}/wp-admin/`
+		);
+
+		const wpAdminResponse = await handler.request({
+			url: postLoginRedirectUrl.toString(),
+		});
+		expect(wpAdminResponse.text).toContain('Dashboard');
 	});
+
+	// TODO
+	it('should serve a logged out response when login disabled');
 });
