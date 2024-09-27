00d2d05af7a6c662854165dc0d67cfe6a6a3575c
Bero <berislav.grgicak@gmail.com>
2024-09-27 08:09:08 +0200

    Fix wp-login.php auto-login

--- packages/playground/wordpress/src/mu-plugins/auto_login.php
+++ packages/playground/wordpress/src/mu-plugins/auto_login.php
@@ -1,58 +1,85 @@
 <?php
 /**
- * Automatically logs the user in to aid the `login` Blueprint step and 
+ * Automatically logs the user in to aid the `login` Blueprint step and
  * the Playground runtimes.
  *
  * There are two ways to trigger the auto-login:
- * 
+ *
  * ## The `PLAYGROUND_AUTO_LOGIN_AS_USER` constant
  *
  * Used by the `login` Blueprint step does.
- * 
+ *
  * When the `PLAYGROUND_AUTO_LOGIN_AS_USER` constant is defined, this mu-plugin
  * will automatically log the user in on their first visit. The username is
  * the value of the constant.
- * 
+ *
  * On subsequent visits, the `playground_auto_login_already_happened` cookie will be
  * detected and the user will not be logged in. This means the "logout" feature
  * will work as expected.
- * 
+ *
  * ## The `playground_force_auto_login_as_user` GET parameter
- * 
+ *
  * Used by the "login" button in various Playground runtimes.
- * 
+ *
  * Only works if the `PLAYGROUND_FORCE_AUTO_LOGIN_ENABLED` constant is defined.
- * 
+ *
  * When the `playground_force_auto_login_as_user` GET parameter is present,
  * this mu-plugin will automatically log in any logged out visitor. This will
  * happen every time they visit, not just on their first visit.
- * 
- * 
+ *
+ *
  * ## Context
- * 
+ *
  * The `login` step used to make a HTTP request to the `/wp-login.php` endpoint,
  * but that approach had significant downsides:
- * 
+ *
  * * It only worked in web browsers
  * * It didn't support custom login mechanisms
  * * It required storing plaintext passwords in the Blueprint files
  */
 
 /**
+ * Returns the username to auto-login as, if any.
+ *
+ * @return string|false
+ */
+function get_playground_auto_login_as_user() {
+	/**
+	 * Allow users to auto-login as a specific user on their first visit.
+	 *
+	 * Prevent the auto-login if it already happened by checking for the
+	 * `playground_auto_login_already_happened` cookie.
+	 * This is used to allow the user to logout.
+	 */
+	if ( defined('PLAYGROUND_AUTO_LOGIN_AS_USER') && !isset($_COOKIE['playground_auto_login_already_happened']) ) {
+		return PLAYGROUND_AUTO_LOGIN_AS_USER;
+	}
+
+	/**
+	 * Allow users to auto-login as a specific user by passing the
+	 * `playground_force_auto_login_as_user` GET parameter.
+	 */
+	if ( defined('PLAYGROUND_FORCE_AUTO_LOGIN_ENABLED') && isset($_GET['playground_force_auto_login_as_user']) ) {
+		return esc_attr($_GET['playground_force_auto_login_as_user']);
+	}
+
+	return false;
+}
+
+/**
  * Logs the user in on their first visit if the Playground runtime told us to.
  */
 function playground_auto_login() {
-	if ( defined('PLAYGROUND_AUTO_LOGIN_AS_USER') && !isset($_COOKIE['playground_auto_login_already_happened']) ) {
-		$user = get_user_by('login', PLAYGROUND_AUTO_LOGIN_AS_USER);
-	} else if ( defined('PLAYGROUND_FORCE_AUTO_LOGIN_ENABLED') && isset($_GET['playground_force_auto_login_as_user']) ) {
-		$user = get_user_by('login', $_GET['playground_force_auto_login_as_user'] ?? 'admin');
-	} else {
+	$user_name = get_playground_auto_login_as_user();
+	if ( false === $user_name ) {
 		return;
 	}
 
 	if ( is_user_logged_in() ) {
 		return;
 	}
+
+	$user = get_user_by('login', $user_name);
 
 	if (!$user) {
 		return;
@@ -76,7 +103,13 @@ add_action('wp', 'playground_auto_login', 1);
  * called, so we need to catch the request and redirect to the admin page.
  **/
 add_action('init', function() {
-	if (!str_ends_with($_SERVER['REQUEST_URI'], '/wp-login.php')) {
+	/**
+	 * Check if the request is for the login page.
+	 */
+	if (false !== stripos( wp_login_url(), $_SERVER['SCRIPT_NAME'] )) {
+		return;
+	}
+	if (false === get_playground_auto_login_as_user()) {
 		return;
 	}
 	playground_auto_login();
@@ -91,7 +124,7 @@ add_action('init', function() {
  * via autologin.
  */
 add_filter('admin_email_check_interval', function($interval) {
-	if(isset($_COOKIE['playground_auto_login_already_happened'])) {
+	if(false === get_playground_auto_login_as_user()) {
 		return 0;
 	}
 
