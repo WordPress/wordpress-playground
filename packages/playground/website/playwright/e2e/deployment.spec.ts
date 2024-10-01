a5aa7ae04308dc5061a14ce9699b0c04cd5cf73b
Adam Zieliński <adam@adamziel.com>
2024-10-01 13:07:51 +0200

    Use a python server instead of node server. Running a node HTTP server from the same file as Playwright tests had indeterministic side effects

--- packages/playground/website/playwright/e2e/deployment.spec.ts
+++ packages/playground/website/playwright/e2e/deployment.spec.ts
@@ -1,97 +1,40 @@
 import { test, expect } from '../playground-fixtures.ts';
-import serveHandler from 'serve-handler';
-import * as http from 'http';
 
-const host = 'localhost';
-const port = 7999;
-const url = `http://${host}:${port}`;
+const url = `http://localhost:7994`;
 const maxDiffPixels = 2000;
 
-type Server = {
-	kill: () => Promise<void>;
-};
-
-const startServer = async (page): Promise<Server> => {
-	const oldVersionDirectory = 'dist/packages/playground/wasm-wordpress-net-old';
-	const newVersionDirectory = 'dist/packages/playground/wasm-wordpress-net-new';
-	let httpCacheEnabled = true;
-	let currentDirectory = oldVersionDirectory;
-	const server = http.createServer((req, res) => {
-		if (req.url === '/switch-to-new-version') {
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html><body>Switched to the new version</body></html>');
-			currentDirectory = newVersionDirectory;
-		} else if (req.url === '/switch-to-old-version') {
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html><body>Switched to the old version</body></html>');
-			currentDirectory = oldVersionDirectory;
-		} else if (req.url === '/disable-http-cache') {
-			httpCacheEnabled = false;
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html><body>HTTP cache disabled</body></html>');
-		} else if (req.url === '/enable-http-cache') {
-			httpCacheEnabled = true;
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html><body>HTTP cache enabled</body></html>');
+// Remember to run the server before running the tests:
+// python server.py 7994 dist/packages/playground/wasm-wordpress-net-old dist/packages/playground/wasm-wordpress-net-new
+for (const cachingEnabled of [true, false]) {
+	test(`When a new website version is deployed, it should be loaded upon a regular page refresh (with HTTP caching ${
+		cachingEnabled ? 'enabled' : 'disabled'
+	})`, async ({ website, page }) => {
+		if (cachingEnabled) {
+			await page.goto(`${url}/enable-http-cache`);
+			await expect(page.locator('body').textContent()).resolves.toBe(
+				'HTTP cache enabled'
+			);
 		} else {
-			return serveHandler(req, res, {
-				public: currentDirectory,
-				headers: [
-					{
-						'Cache-Control': httpCacheEnabled
-							? 'max-age=3600'
-							: 'no-cache, no-store, must-revalidate',
-					},
-				],
-			});
+			await page.goto(`${url}/disable-http-cache`);
+			await expect(page.locator('body').textContent()).resolves.toBe(
+				'HTTP cache disabled'
+			);
 		}
-	});
-	type ListeningServer = ReturnType<typeof server.listen>;
-	const listeningServer = await new Promise<ListeningServer>((resolve) => {
-		const result = server.listen({ host, port }, () => resolve(result));
-	});
+		await page.goto(url);
+		await website.waitForNestedIframes();
 
-	await page.waitForTimeout(1000);
-	console.log('Started server', currentDirectory);
-	return {
-		kill: () => {
-			return new Promise<void>((resolve) => {
-				listeningServer.close(() => {
-					resolve();
-					console.log('Killed server', currentDirectory);
-				});
-				listeningServer.closeAllConnections();
-			});
-		},
-	};
-};
+		await expect(page).toHaveScreenshot('website-old-chromium-darwin.png', {
+			maxDiffPixels,
+		});
 
-let server: Server | null = null;
-test.beforeAll(async ({ page }) => {
-	server = await startServer(page);
-});
-test.afterAll(async () => {
-	if (server) {
-		await server.kill();
-	}
-});
-
-test('When a new website version is deployed, it should be loaded upon a regular page refresh', async ({
-	website,
-	page,
-}) => {
-	await page.goto(url);
-	await website.waitForNestedIframes();
-	await expect(page).toHaveScreenshot('website-old-chromium-darwin.png', {
-		maxDiffPixels,
+		await page.goto(`${url}/switch-to-new-version`);
+		await page.goto(url);
+		await website.waitForNestedIframes();
+		await expect(page).toHaveScreenshot('website-new-chromium-darwin.png', {
+			maxDiffPixels,
+		});
 	});
-
-	await page.goto(`${url}/switch-to-new-version`);
-	await website.waitForNestedIframes();
-	await expect(page).toHaveScreenshot('website-new-chromium-darwin.png', {
-		maxDiffPixels,
-	});
-});
+}
 
 // test(
 // 	'When a new website version is deployed while the old version is still opened in two browser tabs, ' +
