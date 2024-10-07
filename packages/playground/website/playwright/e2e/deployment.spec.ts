18af253ba8086a27c7f9ba4d1e836e2a9bbc633b
Adam Zieliński <adam@adamziel.com>
2024-10-07 18:00:27 +0200

    Resolve the deployment e2e tests conflicts

--- packages/playground/website/playwright/e2e/deployment.spec.ts
+++ packages/playground/website/playwright/e2e/deployment.spec.ts
@@ -57,52 +57,6 @@ for (const cachingEnabled of [true, false]) {
 	});
 }
 
-test('Creating a vanilla new site should work', async ({
-	website,
-	page,
-	wordpress,
-}) => {
-	server!.setHttpCacheEnabled(true);
-	server!.switchToNewVersion();
-
-	await page.goto(`${url}/?wp=6.5`);
-	await website.waitForNestedIframes();
-
-	// The non-reloaded tab should still work. The remote.html iframes
-	// that are already loaded should continue to work, and the newly
-	// loaded remote.html iframes should pull in the latest Playground version.
-	await website.ensureSiteManagerIsOpen();
-	await website.openNewSiteModal();
-
-	await website.page
-		.locator('select[name="phpVersion"]')
-		.selectOption({ label: 'PHP 7.4' });
-
-	await website.page
-		.locator('select[name="wpVersion"]')
-		.selectOption({ label: 'WordPress 6.5' });
-
-	await website.page
-		.locator('select[name="language"]')
-		.selectOption({ label: 'Polish' });
-
-	await website.clickCreateInNewSiteModal();
-	await website.waitForNestedIframes();
-
-	// @TODO locate by role or title or so
-	// Confirm the page was not reloaded in the meantime, as in – we should still
-	// have two sites in the sidebar.
-	expect(
-		website.page.locator(
-			'button[title="This is a temporary Playground. Your changes will be lost on page refresh."]'
-		)
-	).toHaveCount(2);
-
-	// Confirm we're looking at the Polish site.
-	await website.ensureSiteManagerIsClosed();
-	expect(wordpress.locator('body')).toContainText('Edytuj witrynę');
-});
-
 test(
 	'When a new website version is deployed while the old version is still loaded, ' +
 		'creating a new site should still work.',
@@ -121,87 +75,14 @@ test(
 		// that are already loaded should continue to work, and the newly
 		// loaded remote.html iframes should pull in the latest Playground version.
 		await website.ensureSiteManagerIsOpen();
-		await website.openNewSiteModal();
+
+		await website.page.getByLabel('PHP version').selectOption('7.4');
+		await website.page.getByLabel('WordPress version').selectOption('6.5');
+		await website.page.getByLabel('Language').selectOption('pl_PL');
 
 		await website.page
-			.locator('select[name="phpVersion"]')
-			.selectOption({ label: 'PHP 7.4' });
-
-		await website.page
-			.locator('select[name="wpVersion"]')
-			.selectOption({ label: 'WordPress 6.5' });
-
-		await website.page
-			.locator('select[name="language"]')
-			.selectOption({ label: 'Polish' });
-
-		await website.clickCreateInNewSiteModal();
-		await website.waitForNestedIframes();
-
-		// @TODO locate by role or title or so
-		// Confirm the page was not reloaded in the meantime, as in – we should still
-		// have two sites in the sidebar.
-		expect(
-			website.page.locator(
-				'button[title="This is a temporary Playground. Your changes will be lost on page refresh."]'
-			)
-		).toHaveCount(2);
-
-		// Confirm we're looking at the Polish site.
-		await website.ensureSiteManagerIsClosed();
-		expect(wordpress.locator('body')).toContainText('Edytuj witrynę');
-	}
-);
-
-test(
-	'When a fresh tab is upgraded to a newly deployed Playground version while another tab is' +
-		'still running the old version, both tabs should continue to work.',
-	async ({ website, wordpress, page, browser, browserName }) => {
-		server!.switchToMidVersion();
-
-		test.skip(
-			browserName === 'webkit',
-			`Playwright creates separate ephemeral browser contexts for each Safari page, ` +
-				`which means they don't actually share the service worker and the first tab won't` +
-				`be refreshed when the second tab updates its service worker registration.`
-		);
-		await page.goto(`${url}/?php=7.4&wp=6.5&lang=pl_PL`);
-		await website.waitForNestedIframes();
-		expect(wordpress.locator('body')).toContainText('Edit site');
-
-		server!.switchToNewVersion();
-
-		const page2 = await browser.newPage();
-		// The "mid" version only ships PHP 7.4 and WP 6.5. By
-		// going to a newer version, we can confirm we're using
-		// the new version.
-		await page2.goto(`${url}/?php=8.2&wp=6.6`);
-		await website.waitForNestedIframes(page2);
-		// If the WordPress loaded, we know the new version is being used.
-		expect(website.wordpress(page2).locator('body')).toContainText(
-			'Edit site'
-		);
-
-		// Now let's create another site in the first tab and confirm
-		// everything still works as expected.
-		await website.ensureSiteManagerIsOpen();
-		await website.openNewSiteModal();
-
-		// Intentionally choose PHP and WordPress versions that are unavailable
-		// in the "mid" version.
-		await website.page
-			.locator('select[name="phpVersion"]')
-			.selectOption({ label: 'PHP 7.3' });
-
-		await website.page
-			.locator('select[name="wpVersion"]')
-			.selectOption({ label: 'WordPress 6.6' });
-
-		await website.page
-			.locator('select[name="language"]')
-			.selectOption({ label: 'Polish' });
-
-		await website.clickCreateInNewSiteModal();
+			.getByText('Apply Settings & Reset Playground')
+			.click();
 		await website.waitForNestedIframes();
 
 		// @TODO locate by role or title or so
