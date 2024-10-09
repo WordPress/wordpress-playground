b1b73f929b1d05d38bec80ad8bafecb617c96c1a
Adam Zieliński <adam@adamziel.com>
2024-10-09 21:28:03 +0200

    PHP.wasm: Load correct php.wasm paths in the built Node.js packages
     Gets `@php-wasm/node` and all the other CLI packages to load the .wasm
     binaries from the correct path after #1867 introduced JSPI and #1873 made
     it pass all the tests in Node.js
    
     ## Technical details
    
    In the repo, php.js files are stored in php_wasm/node/jspi or php_wasm/node/asyncify.
     They start with a line like this:
    
     const dependencyFilename = __dirname + '/8_0_30/php_8_0.wasm';
    
     After the build, the contents are concatenated into a single file, which
     breaks the dependencyFilename variable. This plugin corrects that by
     replacing __dirname with the correct value such as 'jspi' or 'asyncify'.
    
     The implementation is naive and assumes the substrings __dirname is only used
     as a variable, are not a part of any other name, and is not seen in any string
     literals.
    
     ## Test plan
    
    Build the production packages with `nx build php-wasm-node` and run the
    PHP CLI package to confirm it doesn't crash. I couldn't get npm link to
    work so I created a `dist/packages/php-wasm/node_modules/@php-wasm`
    directory and linked all the built packages there with `ln -s`.

--- packages/php-wasm/node/src/lib/get-php-loader-module.ts
+++ packages/php-wasm/node/src/lib/get-php-loader-module.ts
@@ -9,61 +9,61 @@ export async function getPHPLoaderModule(
 		switch (version) {
 			case '8.3':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_8_3.js`);
+				return await import(`../../jspi/php_8_3.js`);
 			case '8.2':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_8_2.js`);
+				return await import(`../../jspi/php_8_2.js`);
 			case '8.1':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_8_1.js`);
+				return await import(`../../jspi/php_8_1.js`);
 			case '8.0':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_8_0.js`);
+				return await import(`../../jspi/php_8_0.js`);
 			case '7.4':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_7_4.js`);
+				return await import(`../../jspi/php_7_4.js`);
 			case '7.3':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_7_3.js`);
+				return await import(`../../jspi/php_7_3.js`);
 			case '7.2':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_7_2.js`);
+				return await import(`../../jspi/php_7_2.js`);
 			case '7.1':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_7_1.js`);
+				return await import(`../../jspi/php_7_1.js`);
 			case '7.0':
 				// @ts-ignore
-				return await import(`../../public/jspi/php_7_0.js`);
+				return await import(`../../jspi/php_7_0.js`);
 		}
 	} else {
 		switch (version) {
 			case '8.3':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_8_3.js`);
+				return await import(`../../asyncify/php_8_3.js`);
 			case '8.2':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_8_2.js`);
+				return await import(`../../asyncify/php_8_2.js`);
 			case '8.1':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_8_1.js`);
+				return await import(`../../asyncify/php_8_1.js`);
 			case '8.0':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_8_0.js`);
+				return await import(`../../asyncify/php_8_0.js`);
 			case '7.4':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_7_4.js`);
+				return await import(`../../asyncify/php_7_4.js`);
 			case '7.3':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_7_3.js`);
+				return await import(`../../asyncify/php_7_3.js`);
 			case '7.2':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_7_2.js`);
+				return await import(`../../asyncify/php_7_2.js`);
 			case '7.1':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_7_1.js`);
+				return await import(`../../asyncify/php_7_1.js`);
 			case '7.0':
 				// @ts-ignore
-				return await import(`../../public/asyncify/php_7_0.js`);
+				return await import(`../../asyncify/php_7_0.js`);
 		}
 	}
 	throw new Error(`Unsupported PHP version ${version}`);
