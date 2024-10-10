d443ebe1ce37be9459e5f087ce86f557b7326e7e
Brandon Payton <brandon@happycode.net>
2024-10-10 00:45:59 -0400

    Add note to deployment logic

--- packages/playground/website-deployment/apply-update.sh
+++ packages/playground/website-deployment/apply-update.sh
@@ -105,6 +105,8 @@ curl -sS -X POST -H "Auth: $ATOMIC_SITE_API_KEY" "$SITE_API_BASE/edge-cache/$ATO
 
 # @TODO: Enable this once we're deploying the CORS proxy
 # echo Applying latest CORS proxy rate-limiting schema
+# NOTE: This will reset rate-limiting token buckets, but that should be tolerable
+# as long as we're generally discouraging abuse of the proxy.
 # cat ~/website-deployment/cors-proxy-rate-limiting-table.sql | mysql --database="$DB_NAME"
 
 echo Done!
