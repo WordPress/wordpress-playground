5d7d4018b90f3a0c2cb263847b623eb9dd0943c1
Brandon Payton <brandon@happycode.net>
2024-10-14 15:31:19 +0100

    Persist fill rate and fix syntax bug

--- packages/playground/website-deployment/cors-proxy-rate-limiting-table.sql
+++ packages/playground/website-deployment/cors-proxy-rate-limiting-table.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS cors_proxy_token_bucket;
+DROP TABLE IF EXISTS cors_proxy_rate_limiting;
 CREATE TABLE cors_proxy_rate_limiting (
 	remote_addr INET6 PRIMARY KEY,
 	capacity SMALLINT UNSIGNED NOT NULL,
+	fill_rate_per_minute SMALLINT UNSIGNED NOT NULL,
 	tokens SMALLINT UNSIGNED NOT NULL,
-	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	CHECK (capacity > 0 AND tokens < capacity)
 );
