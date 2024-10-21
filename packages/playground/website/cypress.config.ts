89220d82c3d10f910b40588239bc682dbfa0d8f3
mho22 <yannick@capsules.codes>
2024-10-21 17:19:20 +0200

    Express Dependency Update patch

--- packages/playground/website/cypress.config.ts
+++ packages/playground/website/cypress.config.ts
@@ -1,8 +1,10 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 export default defineConfig({
-	e2e: nxE2EPreset(__dirname, {
+	e2e: nxE2EPreset(path.dirname(fileURLToPath(import.meta.url)), {
 		cypressDir: 'cypress',
 	}),
 	// Playground may be slow on GitHub CI
