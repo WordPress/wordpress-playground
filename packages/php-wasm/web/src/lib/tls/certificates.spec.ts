66e4fdc32ef1b92480d811d42c93ba3517122a50
Adam Zieliński <adam@adamziel.com>
2024-10-22 14:51:46 +0200

    Remove the temporary test files

--- packages/php-wasm/web/src/lib/tls/certificates.spec.ts
+++ packages/php-wasm/web/src/lib/tls/certificates.spec.ts
@@ -1,3 +1,77 @@
+describe('generateCertificate', () => {
+	it('should generate the expected certificates', async () => {
+		// TODO
+		expect(true).toBe(true);
+	});
+});
+
+/*
+Use this to generate the certificates:
+
+const CAroot = await generateCertificate({
+	subject: {
+		countryName: 'US',
+		organizationName: 'Playground CA',
+		commonName: 'playground-CA.com-bun',
+	},
+	basicConstraints: {
+		ca: true,
+	},
+});
+
+const SiteCert = await generateCertificate(
+	{
+		subject: {
+			commonName: 'playground-site',
+			organizationName: 'Playground Site',
+			countryName: 'US',
+		},
+		issuer: {
+			countryName: 'US',
+			organizationName: 'Playground CA',
+			commonName: 'playground-CA.com-bun',
+		},
+	},
+	CAroot.keyPair
+);
+
+function uint8ArrayToHexString(arr: Uint8Array) {
+	let hexString = '';
+	for (let i = 0; i < arr.length; i++) {
+		const hex = arr[i].toString(16).padStart(2, '0');
+		hexString += hex;
+	}
+	return hexString;
+}
+
+import { writeFileSync } from 'fs';
+
+const caCertPEM = certificateToPEM(CAroot.certificate);
+const siteCertPEM = certificateToPEM(SiteCert.certificate);
+
+writeFileSync('ca_cert.crt', caCertPEM);
+writeFileSync(
+	'ca_key.pem',
+	await crypto.subtle.exportKey('pkcs8', CAroot.keyPair.privateKey)
+);
+writeFileSync('ca_cert_tbs.bin', CAroot.tbsCertificate);
+const sha256 = new Uint8Array(
+	await crypto.subtle.digest('SHA-256', CAroot.tbsCertificate)
+);
+writeFileSync('ca_cert_tbs.bin.sha256', new Uint8Array(sha256));
+console.log('ca_cert_tbs.bin.sha256', uint8ArrayToHexString(sha256));
+
+writeFileSync('ca_cert.bin', CAroot.certificate);
+writeFileSync('site_cert.crt', siteCertPEM);
+writeFileSync(
+	'site_key.pem',
+	await crypto.subtle.exportKey('pkcs8', SiteCert.keyPair.privateKey)
+);
+*/
+
+/*
+Use these to test the generated certificates:
+
 # === BEST COMMAND TO DUMP THE CERT INFO ===
 openssl x509 -in ca_cert.crt -text -noout
 openssl x509 -in site_cert.crt -text -noout
@@ -16,14 +90,19 @@ openssl asn1parse -in ca_cert.crt -strparse 4 -out ca_cert_tbs.bin.actual -noout
 # Dump the contents in a human readable format
 openssl asn1parse -in ca_cert_tbs.bin.actual -inform der -i
 # Compare the actual TBS data with the expected TBS data – there should be no differences
-ksdiff <(openssl asn1parse -in ca_cert_tbs.bin.actual -inform der -i) <(openssl asn1parse -in ca_cert_tbs.bin -inform der -i)
+ksdiff \
+    <(openssl asn1parse -in ca_cert_tbs.bin.actual -inform der -i) \
+    <(openssl asn1parse -in ca_cert_tbs.bin -inform der -i)
 # Compute the SHA256 hash of the TBS data – there should be no differences
 openssl dgst -sha256 -binary ca_cert_tbs.bin.actual | tee ca_cert_tbs.sha256.actual | hexdump
 cat ca_cert_tbs.bin.sha256 | hexdump
 
 # === Compare the signatures ===
 # Extract the raw signatures from created certificate
-openssl x509 -in ca_cert.crt -text -noout -certopt ca_default -certopt no_validity -certopt no_serial -certopt no_subject -certopt no_extensions -certopt no_signame | grep -v 'Signature' | tr -d '[:space:]:' | tee ca_cert_sig.actual.hex
+openssl x509\
+     -in ca_cert.crt -text -noout -certopt ca_default -certopt no_validity \
+     -certopt no_serial -certopt no_subject -certopt no_extensions \
+     -certopt no_signame | grep -v 'Signature' | tr -d '[:space:]:' | tee ca_cert_sig.actual.hex
 # Compute the hash signature
 cat ca_cert_tbs.sha256.actual | openssl dgst -sha256 -sign ca_key.pem -binary | hexdump
 # Compute the TBS certificate signature
@@ -59,36 +138,4 @@ cat decrypted.txt
 # Perform signature integrity check
 openssl dgst -sha256 -sign site_key.pem -out test.sig test.txt
 openssl dgst -sha256 -verify site_cert.pub.pem -signature test.sig test.txt
-
-# === Outdated, consider deleting ===
-
-# === DEBUG SITE CERT ===
-# Extract the certificate's TBS (To Be Signed) portion:
-openssl asn1parse -in site_cert.crt -strparse 4 -out site_cert_tbs.bin.actual -noout
-# Dump the contents in a human readable format
-openssl asn1parse -in site_cert_tbs.bin.actual -inform der -i
-# Calculate the SHA256 hash of the TBS data
-openssl dgst -sha256 -binary ca_cert_tbs.bin > ca_cert_tbs.sha256.actual 
-openssl dgst -sha256 -hex site_cert_tbs.sha256 | awk '{print $2}'
-
-# Extract the public key from the CA certificate:
-openssl x509 -in ca_cert.pem -pubkey -noout > ca_pubkey.pem
-
-# Dump more info about the site certificate
-openssl x509 -in site_cert.crt -text -noout -certopt ca_default -certopt no_validity -certopt no_serial -certopt no_subject -certopt no_extensions -certopt no_signame 
-
-openssl x509 -in site_cert.crt -outform DER | openssl asn1parse -inform DER -strparse 4 -out site_cert_sig.actual.bin
-
-
-# Compute the signature from the TBS data
-openssl asn1parse -in ca_cert.crt -strparse 4 -out ca_cert_tbs.bin -noout
-openssl dgst -sha256 -binary ca_cert_tbs.bin > ca_cert_tbs.sha256
-openssl pkeyutl -sign -in ca_cert_tbs.sha256 -inkey ca_key.pem -out ca_cert_sig.bin
-hexdump ca_cert_sig.bin
-
-openssl asn1parse -in site_cert.crt -strparse 4 -out site_cert_tbs.bin -noout
-openssl dgst -sha256 -binary site_cert_tbs.bin > site_cert_tbs.sha256
-openssl pkeyutl -sign -in site_cert_tbs.sha256 -inkey ca_key.pem -out site_cert_sig.bin
-
-# Verify the signature
-openssl dgst -sha256 -verify ca_pubkey.pem -signature site_cert_sig.bin site_cert_tbs.sha256
+*/
