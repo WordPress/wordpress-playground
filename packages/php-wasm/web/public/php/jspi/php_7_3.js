56cd3f6bfd105edc5557e9f1469549da7e9587c3
Adam Zieliński <adam@adamziel.com>
2024-10-24 10:57:59 +0200

    Rebuild all PHP web versions with CURL

--- packages/php-wasm/web/public/php/jspi/php_7_3.js
+++ packages/php-wasm/web/public/php/jspi/php_7_3.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './7_3_33/php_7_3.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 12076266; 
+export const dependenciesTotalSize = 12375802; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
--- packages/php-wasm/web/public/php/jspi/php_8_1.js
+++ packages/php-wasm/web/public/php/jspi/php_8_1.js
@@ -1,6 +1,6 @@
 import dependencyFilename from './8_1_23/php_8_1.wasm'; 
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 12530583; 
+export const dependenciesTotalSize = 12530586; 
 export function init(RuntimeName, PHPLoader) {
     /**
      * Overrides Emscripten's default ExitStatus object which gets
