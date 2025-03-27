f4e6f1884e4abec21097cd9ed6b085ab5fba2e48
Brandon Payton <brandon@happycode.net>
2025-03-27 17:43:04 -0400

    Hack together a boot method for secondary workers

--- packages/playground/cli/src/worker-thread.ts
+++ packages/playground/cli/src/worker-thread.ts
@@ -1,9 +1,12 @@
 import {
 	PHP,
+	PHPRequestHandler,
 	PHPWorker,
 	SupportedPHPVersion,
 	consumeAPI,
 	exposeAPI,
+	proxyFileSystem,
+	rotatePHPRuntime,
 } from '@php-wasm/universal';
 import {
 	createNodeFsMountHandler,
@@ -13,17 +16,23 @@ import {
 import { EmscriptenDownloadMonitor } from '@php-wasm/progress';
 import { zipDirectory } from '@wp-playground/common';
 import { parentPort } from 'worker_threads';
-import { bootWordPress } from '@wp-playground/wordpress';
+import {
+	bootWordPress,
+	getFileNotFoundActionForWordPress,
+	preloadPhpInfoRoute,
+	wordPressRewriteRules,
+} from '@wp-playground/wordpress';
 import { logger } from '@php-wasm/logger';
 import { rootCertificates } from 'tls';
 import nodeEndpoint from 'comlink/dist/esm/node-adapter';
+import { joinPaths } from '@php-wasm/util';
 
 export interface Mount {
 	hostPath: string;
 	vfsPath: string;
 }
 
-export type WorkerBootOptions = {
+export type PrimaryWorkerBootOptions = {
 	wpVersion?: string;
 	phpVersion?: SupportedPHPVersion;
 	absoluteUrl: string;
@@ -32,6 +41,14 @@ export type WorkerBootOptions = {
 	shouldInstallWordPress: boolean;
 	wordPressZip?: ArrayBuffer;
 	sqliteIntegrationPluginZip?: ArrayBuffer;
+	runtimeIdBase: number;
+};
+
+export type SecondaryWorkerBootOptions = {
+	phpVersion?: SupportedPHPVersion;
+	absoluteUrl: string;
+	mountsBeforeWpInstall: Array<Mount>;
+	mountsAfterWpInstall: Array<Mount>;
 	runtimeIdBase: number;
 };
 
@@ -69,7 +86,7 @@ export class PlaygroundCliWorker extends PHPWorker {
 		wordPressZip,
 		sqliteIntegrationPluginZip,
 		runtimeIdBase,
-	}: WorkerBootOptions) {
+	}: PrimaryWorkerBootOptions) {
 		if (this.booted) {
 			throw new Error('Playground already booted');
 		}
@@ -137,6 +154,108 @@ export class PlaygroundCliWorker extends PHPWorker {
 		} catch (e) {
 			setAPIError(e as Error);
 			throw e;
+		}
+	}
+
+	async bootSecondaryWorker({
+		absoluteUrl,
+		mountsBeforeWpInstall,
+		mountsAfterWpInstall,
+		phpVersion = '8.0',
+		runtimeIdBase,
+	}: SecondaryWorkerBootOptions) {
+		if (this.booted) {
+			throw new Error('Playground already booted');
+		}
+		this.booted = true;
+
+		const fileLockManager = consumeAPI<FileLockManager>(parentPort!);
+		await fileLockManager.isConnected();
+
+		try {
+			// TODO: Create request handler from scratch
+			// TODO: Likely remove this log message
+			logger.log(`Booting secondary worker...`);
+
+			const requestHandler: PHPRequestHandler = new PHPRequestHandler({
+				phpFactory: async ({ isPrimary }) =>
+					createPhp(requestHandler, isPrimary),
+				// TODO: Consider adding explicit documentRoot param for booting worker
+				documentRoot: '/wordpress',
+				absoluteUrl,
+				rewriteRules: wordPressRewriteRules,
+				getFileNotFoundAction: getFileNotFoundActionForWordPress,
+				cookieStore: false,
+			});
+
+			this.__internal_setRequestHandler(requestHandler);
+			// TODO: Likely remove this log message
+			logger.log(`Booted secondary worker!`);
+
+			const php = await requestHandler.getPrimaryPhp();
+			mountResources(php, mountsBeforeWpInstall);
+			mountResources(php, mountsAfterWpInstall);
+
+			setApiReady();
+		} catch (e) {
+			setAPIError(e as Error);
+			throw e;
+		}
+
+		async function createPhpRuntime() {
+			return await loadNodeRuntime(phpVersion, {
+				emscriptenOptions: {
+					fileLockManager,
+					runtimeIdBase,
+				},
+			});
+		}
+
+		// TODO: This is copied from wordpress/boot. Consider what actually makes sense to avoid unnecessarily duplicated logic.
+		async function createPhp(
+			requestHandler: PHPRequestHandler,
+			isPrimary: boolean
+		) {
+			const php = new PHP(await createPhpRuntime());
+			php.setSapiName('cli');
+			if (requestHandler) {
+				php.requestHandler = requestHandler;
+			}
+			/**
+			 * Set up mu-plugins in /internal/shared/mu-plugins
+			 * using auto_prepend_file to provide platform-level
+			 * customization without altering the installed WordPress
+			 * site.
+			 *
+			 * We only do that in the primary PHP instance –
+			 * the filesystem there is the source of truth
+			 * for all other PHP instances.
+			 */
+			if (isPrimary) {
+				await preloadPhpInfoRoute(
+					php,
+					joinPaths(new URL(absoluteUrl).pathname, 'phpinfo.php')
+				);
+			} else {
+				// Proxy the filesystem for all secondary PHP instances to
+				// the primary one.
+				proxyFileSystem(await requestHandler.getPrimaryPhp(), php, [
+					'/tmp',
+					requestHandler.documentRoot,
+					'/internal/shared',
+				]);
+			}
+
+			// Rotate the PHP runtime periodically to avoid memory leak-related crashes.
+			// @see https://github.com/WordPress/wordpress-playground/pull/990 for more context
+			rotatePHPRuntime({
+				php,
+				cwd: requestHandler.documentRoot,
+				recreateRuntime: createPhpRuntime,
+				maxRequests: 400,
+			});
+
+			return php;
 		}
 	}
 }
