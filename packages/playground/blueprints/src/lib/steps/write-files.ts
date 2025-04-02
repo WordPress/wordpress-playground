bb3fd5f31112a647953daff22973324090323d6f
karthick-murugan <karthick.murugan02@gmail.com>
2025-04-02 21:44:47 +0530

    Reverting typescript file changes

--- packages/playground/blueprints/src/lib/steps/write-files.ts
+++ packages/playground/blueprints/src/lib/steps/write-files.ts
@@ -28,19 +28,13 @@ export interface WriteFilesStep<DirectoryResource> {
 	step: 'writeFiles';
 	/** The path of the file to write to */
 	writeToPath: string;
-	/** The 'filesTree' defines the directory structure, supporting 'literal:directory' or 'git:directory' types. The 'name' represents the root directory, while 'files' is an object where keys are file paths, and values contain either file content as a string or nested objects for subdirectories. */
+	/** The data to write */
 	filesTree: DirectoryResource;
 }
 
 /**
  * Writes multiple files to a specified directory in the Playground
  * filesystem.
- * ```
- * my-plugin/
- * ├── index.php
- * └── public/
- *     └── style.css
- * ```
  */
 export const writeFiles: StepHandler<WriteFilesStep<Directory>> = async (
 	playground,
