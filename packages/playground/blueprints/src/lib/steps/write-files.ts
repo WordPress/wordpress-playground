5ac9bd03f5ba7dcb1364ae6dc38ea6605adc0cb5
karthick-murugan <karthick.murugan02@gmail.com>
2025-04-04 10:54:12 +0530

    Fixing linting issues

--- packages/playground/blueprints/src/lib/steps/write-files.ts
+++ packages/playground/blueprints/src/lib/steps/write-files.ts
@@ -28,7 +28,12 @@ export interface WriteFilesStep<DirectoryResource> {
 	step: 'writeFiles';
 	/** The path of the file to write to */
 	writeToPath: string;
-	/** The 'filesTree' defines the directory structure, supporting 'literal:directory' or 'git:directory' types. The 'name' represents the root directory, while 'files' is an object where keys are file paths, and values contain either file content as a string or nested objects for subdirectories. */
+	/**
+	 * The 'filesTree' defines the directory structure, supporting 'literal:directory' or
+	 * 'git:directory' types. The 'name' represents the root directory, while 'files' is an object
+	 * where keys are file paths, and values contain either file content as a string or nested objects
+	 * for subdirectories.
+	 */
 	filesTree: DirectoryResource;
 }
 
