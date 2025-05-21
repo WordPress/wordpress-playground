04c8320b490d9ecbe8b6f13524360fef201aa4ca
mho22 <yannick@capsules.codes>
2025-05-21 17:47:08 +0200

    wip

--- packages/php-wasm/node/src/test/php-request-handler.spec.ts
+++ packages/php-wasm/node/src/test/php-request-handler.spec.ts
@@ -1,4 +1,3 @@
-import { RecommendedPHPVersion } from '@wp-playground/common';
 // eslint-disable-next-line @nx/enforce-module-boundaries -- ignore test-related interdependencies so we can test.
 import { getFileNotFoundActionForWordPress } from '@wp-playground/wordpress';
 import { loadNodeRuntime } from '..';
@@ -9,7 +8,7 @@ import {
 	PHPResponse,
 	SupportedPHPVersions,
 } from '@php-wasm/universal';
-import { createSpawnHandler, joinPaths } from '@php-wasm/util';
+import { joinPaths } from '@php-wasm/util';
 
 interface ConfigForRequestTests {
 	phpVersion: (typeof SupportedPHPVersions)[number];
@@ -640,128 +639,3 @@ describe.each(configsForRequestTests)(
 		});
 	}
 );
-
-describe.each(SupportedPHPVersions)(
-	'[PHP %s] PHPRequestHandler – PHP_SELF',
-	(phpVersion) => {
-		let handler: PHPRequestHandler;
-		beforeEach(async () => {
-			handler = new PHPRequestHandler({
-				phpFactory: async () =>
-					new PHP(await loadNodeRuntime(phpVersion)),
-				documentRoot: '/var/www',
-				maxPhpInstances: 1,
-			});
-			const php = await handler.getPrimaryPhp();
-			php.mkdirTree('/var/www');
-		});
-
-		it.each([
-			['/index.php', '/index.php'],
-			['/index.php?foo=bar', '/index.php'],
-			['/index.php?foo=bar&baz=qux', '/index.php'],
-			['/', '/index.php'],
-		])(
-			'Should assign the correct PHP_SELF for %s',
-			async (url: string, expected: string) => {
-				const php = await handler.getPrimaryPhp();
-				php.writeFile(
-					'/var/www/index.php',
-					`<?php echo $_SERVER['PHP_SELF'];`
-				);
-				const response = await handler.request({
-					url,
-				});
-				expect(response.text).toEqual(expected);
-			}
-		);
-
-		it('should assign the correct PHP_SELF (file in subdirectory, query string present)', async () => {
-			const php = await handler.getPrimaryPhp();
-			php.mkdirTree('/var/www/subdir');
-			php.writeFile(
-				'/var/www/subdir/index.php',
-				`<?php echo $_SERVER['PHP_SELF'];`
-			);
-			const response = await handler.request({
-				url: '/subdir/?foo=bar',
-			});
-			expect(response.text).toEqual('/subdir/index.php');
-		});
-	}
-);
-
-describe('PHPRequestHandler – Loopback call', () => {
-	let handler: PHPRequestHandler;
-
-	it('Spawn: exec() can spawn another PHP before the previous run() concludes', async () => {
-		async function createPHP() {
-			const php = new PHP(await loadNodeRuntime(RecommendedPHPVersion));
-			php.setSpawnHandler(
-				createSpawnHandler(async function (args, processApi, options) {
-					if (args[0] !== 'php') {
-						throw new Error(
-							`Unexpected command: ${args.join(' ')}`
-						);
-					}
-					const { php, reap } =
-						await handler.processManager.acquirePHPInstance();
-					const result = await php.run({
-						scriptPath: args[1],
-						env: options.env,
-					});
-					processApi.stdout(result.bytes);
-					processApi.stderr(result.errors);
-					processApi.exit(result.exitCode);
-					reap();
-				})
-			);
-			php.writeFile(
-				'/first.php',
-				`<?php echo 'Starting: '; echo exec("php /second.php", $output, $return_var); echo ' Done';`
-			);
-			php.writeFile('/second.php', `<?php echo 'Ran second.php!'; `);
-			return php;
-		}
-		handler = new PHPRequestHandler({
-			documentRoot: '/',
-			phpFactory: createPHP,
-			maxPhpInstances: 2,
-		});
-		const response = await handler.request({
-			url: '/first.php',
-		});
-		expect(response.text).toEqual('Starting: Ran second.php! Done');
-	});
-
-	it('Loopback: handler.request() can be called before the previous call concludes', async () => {
-		async function createPHP() {
-			const php = new PHP(await loadNodeRuntime(RecommendedPHPVersion));
-			php.setSpawnHandler(
-				createSpawnHandler(async function (args, processApi) {
-					const result = await handler.request({
-						url: '/second.php',
-					});
-					processApi.stdout(result.bytes);
-					processApi.stderr(result.errors);
-					processApi.exit(result.exitCode);
-				})
-			);
-			php.writeFile(
-				'/first.php',
-				`<?php echo 'Starting: '; echo exec("php /second.php", $output, $return_var); echo ' Done';`
-			);
-			php.writeFile('/second.php', `<?php echo 'Ran second.php!'; `);
-			return php;
-		}
-		handler = new PHPRequestHandler({
-			documentRoot: '/',
-			phpFactory: createPHP,
-			maxPhpInstances: 2,
-		});
-		const response = await handler.request({
-			url: '/first.php',
-		});
-		expect(response.text).toEqual('Starting: Ran second.php! Done');
-	});
-});
