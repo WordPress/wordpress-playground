d42c770e03de8872c2d4a28c64d5a9dc3b025562
Bero <berislav.grgicak@gmail.com>
2025-06-02 11:27:49 +0200

    Escape Windows paths

--- packages/playground/cli/src/mount.ts
+++ packages/playground/cli/src/mount.ts
@@ -23,8 +23,8 @@ export function parseMountWithDelimiterArguments(mounts: string[]): Mount[] {
 		if (mountParts.length !== 2) {
 			throw new Error(`Invalid mount format: ${mount}.
 				Expected format: /host/path:/vfs/path.
-				If your path contains a colon, e.g. C:\myplugin, use the --mount-dir option instead.
-				Example: --mount-dir C:\my-plugin /wordpress/wp-content/plugins/my-plugin`);
+				If your path contains a colon, e.g. C:\\myplugin, use the --mount-dir option instead.
+				Example: --mount-dir C:\\my-plugin /wordpress/wp-content/plugins/my-plugin`);
 		}
 		const [hostPath, vfsPath] = mountParts;
 		if (!existsSync(hostPath)) {
