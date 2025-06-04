a6f86fe3a42ec70f59cb733262805ac03d46a595
Brandon Payton <brandon@happycode.net>
2025-06-04 12:09:11 -0400

    Resolve relative to import.meta.url

--- packages/php-wasm/node/src/lib/data/with-icu-data.ts
+++ packages/php-wasm/node/src/lib/data/with-icu-data.ts
@@ -3,12 +3,11 @@ import { FSHelpers } from '@php-wasm/universal';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { basename } from 'path';
-// @ts-ignore -- this is a valid import for a URL
-import icuDataUrl from './shared/icudt74l.dat?url';
 
 export async function withICUData(
 	options: EmscriptenOptions
 ): Promise<EmscriptenOptions> {
+	const icuDataUrl = new URL('./shared/icudt74l.dat', import.meta.url);
 	const filePath = fileURLToPath(icuDataUrl);
 	const fileName = basename(filePath);
 	const ICUData = fs.readFileSync(filePath);
