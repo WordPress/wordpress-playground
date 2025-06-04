1271db74b1fb85fbb54620115d0d305e4d6244bf
Adam Zieliński <adam@adamziel.com>
2025-06-05 00:24:46 +0200

    PHP: Stream stdout and stderr
    Implements a php.runStream() method that returns a StreamedPHPResponse instance.
    It exposes stdout and stderr as ReadableStreams, allowing the caller to interact
    with partial output data. Before this PR, we only had php.run() that buffered 
    stdout and stderr data and returned it all at once after the PHP code was fully
    executed.
    
    ## Implementation
    
    We register three FS devices at:
    
    * /internal/stdout
    * /internal/stderr
    * /internal/headers
    
    They are private to every PHP instance and are never shared with other runtimes.
    
    Then, in JavaScript, whenever a chunk of data is written to either of these devices, we propagate
    it to consumer via a callback, e.g. `PHPWASM.onStdout(chunk)`.
    
    Users of the `PHP` class never have to interact with these devices or callbacks directly.
    The PHP class creates the relevant ReadableStreams and pushes the data through them
    – see php.#executeWithErrorHandling() for details.
    
    #### Why not use Emscripten's stdout and stderr?
    
    Emscripten's native stdout and stderr devices stop processing data when they encounter
    the first null byte. However, null bytes are common when dealing with binary data.
    
    ### Backwards Compatibility
    
    php.run() continues to work. It creates a streamed response under the hood and buffers
    the streamed output before returning a buffered `PHPResponse` object.
    
    ## Remaining work
    
    * Add streaming-specific tests
    
    ## Follow-up work
    
    * Stream the response bytes in the web/service worker

--- packages/php-wasm/universal/src/lib/php-response.ts
+++ packages/php-wasm/universal/src/lib/php-response.ts
@@ -47,6 +47,165 @@ const responseTexts: Record<number, string> = {
 	200: 'OK',
 };
 
+export class StreamedPHPResponse {
+	/**
+	 * Response headers.
+	 */
+	private readonly headersStream: ReadableStream<Uint8Array>;
+
+	/**
+	 * Response body. Contains the output from `echo`,
+	 * `print`, inline HTML etc.
+	 */
+	readonly stdout: ReadableStream<Uint8Array>;
+
+	/**
+	 * Stderr contents, if any.
+	 */
+	readonly stderr: ReadableStream<Uint8Array>;
+
+	/**
+	 * The exit code of the script. `0` is a success, anything
+	 * else is an error.
+	 */
+	readonly exitCode: Promise<number>;
+
+	private parsedHeaders: Promise<{
+		headers: Record<string, string[]>;
+		httpStatusCode: number;
+	}> | null = null;
+
+	private cachedStdoutText: Promise<string> | null = null;
+	private cachedStderrText: Promise<string> | null = null;
+
+	constructor(
+		headers: ReadableStream<Uint8Array>,
+		stdout: ReadableStream<Uint8Array>,
+		stderr: ReadableStream<Uint8Array>,
+		exitCode: Promise<number>
+	) {
+		this.headersStream = headers;
+		this.stdout = stdout;
+		this.stderr = stderr;
+		this.exitCode = exitCode;
+	}
+
+	/**
+	 * Returns a promise that resolves to true if the response was successful (status code 200-399),
+	 * false otherwise.
+	 */
+	async ok(): Promise<boolean> {
+		try {
+			const statusCode = await this.httpStatusCode;
+			return statusCode >= 200 && statusCode < 400;
+		} catch (e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Returns a promise that resolves when the response has finished processing.
+	 */
+	get finished(): Promise<void> {
+		return Promise.allSettled([this.exitCode.finally(() => {})]).then(
+			() => {}
+		);
+	}
+
+	get headers(): Promise<Record<string, string[]>> {
+		return this.getParsedHeaders().then((headers) => headers.headers);
+	}
+
+	get httpStatusCode(): Promise<number> {
+		return this.getParsedHeaders().then(
+			(headers) => headers.httpStatusCode,
+			() =>
+				this.exitCode.then(
+					(exitCode) => (exitCode === 0 ? 200 : 500),
+					() => 500
+				)
+		);
+	}
+
+	get stdoutText(): Promise<string> {
+		if (!this.cachedStdoutText) {
+			this.cachedStdoutText = streamToText(this.stdout);
+		}
+		return this.cachedStdoutText;
+	}
+
+	get stderrText(): Promise<string> {
+		if (!this.cachedStderrText) {
+			this.cachedStderrText = streamToText(this.stderr);
+		}
+		return this.cachedStderrText;
+	}
+
+	private async getParsedHeaders() {
+		if (!this.parsedHeaders) {
+			this.parsedHeaders = parseHeadersStream(this.headersStream);
+		}
+		return await this.parsedHeaders;
+	}
+}
+
+async function parseHeadersStream(
+	headersStream: ReadableStream<Uint8Array>
+): Promise<{
+	headers: Record<string, string[]>;
+	httpStatusCode: number;
+}> {
+	const headersText = await streamToText(headersStream);
+	let headersData;
+	try {
+		headersData = JSON.parse(headersText);
+	} catch (e) {
+		console.log('error parsing headers', e);
+		return { headers: {}, httpStatusCode: 200 };
+	}
+	const headers: PHPResponse['headers'] = {};
+	for (const line of headersData.headers) {
+		// Skip invalid response headers and the last "__terminator__" line.
+		// @TODO: Should we log a warning on an invalid header line?
+		//        What's the typical browser behavior when encountering such a line?
+		if (!line.includes(': ')) {
+			continue;
+		}
+		const colonIndex = line.indexOf(': ');
+		const headerName = line.substring(0, colonIndex).toLowerCase();
+		const headerValue = line.substring(colonIndex + 2);
+		if (!(headerName in headers)) {
+			headers[headerName] = [] as string[];
+		}
+		headers[headerName].push(headerValue);
+	}
+	return {
+		headers,
+		httpStatusCode: headersData.status,
+	};
+}
+
+function streamToText(stream: ReadableStream<Uint8Array>): Promise<string> {
+	return new Promise(async (resolve, reject) => {
+		const reader = stream.pipeThrough(new TextDecoderStream()).getReader();
+		const text: string[] = [];
+		try {
+			while (true) {
+				const { done, value } = await reader.read();
+				if (done) {
+					resolve(text.join(''));
+					return;
+				}
+				if (value) {
+					text.push(value);
+				}
+			}
+		} catch (e) {
+			reject(e);
+		}
+	});
+}
+
 /**
  * PHP response. Body is an `ArrayBuffer` because it can
  * contain binary data.
@@ -101,6 +260,19 @@ export class PHPResponse implements PHPResponseData {
 			data.bytes,
 			data.errors,
 			data.exitCode
+		);
+	}
+
+	static async fromStreamedResponse(
+		streamedResponse: StreamedPHPResponse
+	): Promise<PHPResponse> {
+		await streamedResponse.finished;
+		return new PHPResponse(
+			await streamedResponse.httpStatusCode,
+			await streamedResponse.headers,
+			new TextEncoder().encode(await streamedResponse.stdoutText),
+			await streamedResponse.stderrText,
+			await streamedResponse.exitCode
 		);
 	}
 
