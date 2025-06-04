f45eb72045b364523e88ea8f5c5171ee35338412
Adam Zieliński <adam@adamziel.com>
2025-06-05 00:24:46 +0200

    PHP: Stream stdout and stderr
    Implements a php.runStream() method that returns a StreamedPHPResponse instance.
    It exposes stdout and stderr as ReadableStreams, allowing the caller to interact
    with partial output data. Before this PR, we only had php.run() that buffered 
    stdout and stderr data and returned it all at once after the PHP code was fully
    executed.
    
    ## Implementation
    
    We register three FS devices at:
    
    * /internal/stdout
    * /internal/stderr
    * /internal/headers
    
    They are private to every PHP instance and are never shared with other runtimes.
    
    Then, in JavaScript, whenever a chunk of data is written to either of these devices, we propagate
    it to consumer via a callback, e.g. `PHPWASM.onStdout(chunk)`.
    
    Users of the `PHP` class never have to interact with these devices or callbacks directly.
    The PHP class creates the relevant ReadableStreams and pushes the data through them
    – see php.#executeWithErrorHandling() for details.
    
    #### Why not use Emscripten's stdout and stderr?
    
    Emscripten's native stdout and stderr devices stop processing data when they encounter
    the first null byte. However, null bytes are common when dealing with binary data.
    
    ### Backwards Compatibility
    
    php.run() continues to work. It creates a streamed response under the hood and buffers
    the streamed output before returning a buffered `PHPResponse` object.
    
    ## Remaining work
    
    * Add streaming-specific tests
    
    ## Follow-up work
    
    * Stream the response bytes in the web/service worker
    
    Remove old PHP CLI bindings

---
+++ packages/php-wasm/universal/src/lib/is-exit-code.ts
@@ -0,0 +1,28 @@
+/**
+ * Check if the Emscripten-thrown error is an exit code 0 error.
+ *
+ * @param e The error to check
+ * @returns True if the error is an exit code 0 error
+ */
+export function isExitCodeZero(e: any): e is { exitCode: number } {
+	if (!(e instanceof Error)) {
+		return false;
+	}
+	return (
+		('exitCode' in e && e?.exitCode === 0) ||
+		(e?.name === 'ExitStatus' && 'status' in e && e.status === 0)
+	);
+}
+
+/**
+ * Check if the Emscripten-thrown error is an exit code 0 error.
+ *
+ * @param e The error to check
+ * @returns True if the error is an exit code 0 error
+ */
+export function isExitCode(e: any): e is { exitCode: number } {
+	if (!(e instanceof Error)) {
+		return false;
+	}
+	return 'exitCode' in e || (e?.name === 'ExitStatus' && 'status' in e);
+}
