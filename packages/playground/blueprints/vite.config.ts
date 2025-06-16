c81455170c273892af14b8ec62a09bc39e9363ab
Adam Zieliński <adam@adamziel.com>
2025-06-16 16:25:27 +0200

    Increase the default test timeout for blueprints package to 10s

--- packages/playground/blueprints/vite.config.ts
+++ packages/playground/blueprints/vite.config.ts
@@ -79,6 +79,7 @@ export default defineConfig({
 		cache: {
 			dir: '../../../node_modules/.vitest',
 		},
+		testTimeout: 10000,
 		environment: 'node',
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		reporters: ['default'],
