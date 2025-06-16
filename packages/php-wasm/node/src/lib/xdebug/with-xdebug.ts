460c878e9bd89ac4a5ab66bf73ca50e2e31219ea
mho22 <yannick@capsules.codes>
2025-06-16 11:27:31 +0200

    Prevent Xdebug to be loaded in Asyncify

--- packages/php-wasm/node/src/lib/xdebug/with-xdebug.ts
+++ packages/php-wasm/node/src/lib/xdebug/with-xdebug.ts
@@ -6,57 +6,71 @@ import type {
 import { LatestSupportedPHPVersion } from '@php-wasm/universal';
 import { fullyQualifiedPHPVersionDirectory } from './supported-php-versions';
 import fs from 'fs';
+import { jspi } from 'wasm-feature-detect';
 
 export async function withXdebug(
 	version: SupportedPHPVersion = LatestSupportedPHPVersion,
 	options: EmscriptenOptions
 ): Promise<EmscriptenOptions> {
-	const fileName = 'xdebug.so';
-	const directoryName = fullyQualifiedPHPVersionDirectory(version);
-	const filePath = `${__dirname}/jspi/${directoryName}/extensions/${fileName}`;
-	const extension = fs.readFileSync(filePath);
+	if (await jspi()) {
+		const fileName = 'xdebug.so';
+		const directoryName = fullyQualifiedPHPVersionDirectory(version);
+		const filePath = `${__dirname}/jspi/${directoryName}/extensions/${fileName}`;
+		const extension = fs.readFileSync(filePath);
 
-	return {
-		ENV: {
-			...options.ENV,
-			PHP_INI_SCAN_DIR: '/internal/shared/extensions',
-		},
-		onRuntimeInitialized: (phpRuntime: PHPRuntime) => {
-			if (options.onRuntimeInitialized) {
-				options.onRuntimeInitialized(phpRuntime);
-			}
-			/* The extension file previously read
-			 * is written inside the /extensions directory
-			 */
-			phpRuntime.FS.mkdirTree('/internal/shared/extensions');
-			phpRuntime.FS.writeFile(
-				`/internal/shared/extensions/${fileName}`,
-				new Uint8Array(extension)
-			);
-			/* The extension has its share of ini entries
-			 * to write in a separate ini file
-			 */
-			phpRuntime.FS.writeFile(
-				'/internal/shared/extensions/xdebug.ini',
-				[
-					'zend_extension=/internal/shared/extensions/xdebug.so',
-					'html_errors=Off',
-					'xdebug.mode=debug',
-					'xdebug.start_with_request=yes',
-					'xdebug.log=/xdebug.log',
-				].join('\n')
-			);
-			/* The extension needs to mount the current
-			 * working directory in order to sync with
-			 * the debugger
-			 */
-			phpRuntime.FS.mkdirTree(process.cwd());
-			phpRuntime.FS.mount(
-				phpRuntime.FS.filesystems['NODEFS'],
-				{ root: process.cwd() },
-				process.cwd()
-			);
-			phpRuntime.FS.chdir(process.cwd());
-		},
-	};
+		return {
+			ENV: {
+				...options.ENV,
+				PHP_INI_SCAN_DIR: '/internal/shared/extensions',
+			},
+			onRuntimeInitialized: (phpRuntime: PHPRuntime) => {
+				if (options.onRuntimeInitialized) {
+					options.onRuntimeInitialized(phpRuntime);
+				}
+				/* The extension file previously read
+				 * is written inside the /extensions directory
+				 */
+				phpRuntime.FS.mkdirTree('/internal/shared/extensions');
+				phpRuntime.FS.writeFile(
+					`/internal/shared/extensions/${fileName}`,
+					new Uint8Array(extension)
+				);
+				/* The extension has its share of ini entries
+				 * to write in a separate ini file
+				 */
+				phpRuntime.FS.writeFile(
+					'/internal/shared/extensions/xdebug.ini',
+					[
+						'zend_extension=/internal/shared/extensions/xdebug.so',
+						'html_errors=Off',
+						'xdebug.mode=debug',
+						'xdebug.start_with_request=yes',
+						'xdebug.log=/xdebug.log',
+					].join('\n')
+				);
+				/* The extension needs to mount the current
+				 * working directory in order to sync with
+				 * the debugger
+				 */
+				phpRuntime.FS.mkdirTree(process.cwd());
+				phpRuntime.FS.mount(
+					phpRuntime.FS.filesystems['NODEFS'],
+					{ root: process.cwd() },
+					process.cwd()
+				);
+				phpRuntime.FS.chdir(process.cwd());
+			},
+		};
+	} else {
+		return {
+			ENV: {
+				...options.ENV,
+			},
+			onRuntimeInitialized: (phpRuntime: PHPRuntime) => {
+				if (options.onRuntimeInitialized) {
+					options.onRuntimeInitialized(phpRuntime);
+				}
+			},
+		};
+	}
 }
