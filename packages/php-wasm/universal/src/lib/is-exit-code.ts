ad8cb0a4ad5961ccfa4a0e6789f15470e1d24892
Brandon Payton <brandon@happycode.net>
2025-06-16 10:59:51 -0400

    Correct inline docs for isExitCode return type

--- packages/php-wasm/universal/src/lib/is-exit-code.ts
+++ packages/php-wasm/universal/src/lib/is-exit-code.ts
@@ -2,7 +2,7 @@
  * Check if the Emscripten-thrown error is an exit code 0 error.
  *
  * @param e The error to check
- * @returns True if the error is an exit code 0 error
+ * @returns True if the error appears to represent an exit code or status
  */
 export function isExitCode(e: any): e is { exitCode: number } {
 	if (!(e instanceof Error)) {
