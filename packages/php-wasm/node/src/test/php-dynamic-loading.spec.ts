fefc94cd4b2c7e9993395fdba125cb7d3abbe0f2
mho22 <yannick@capsules.codes>
2025-06-17 18:09:26 +0200

    Add last test on DBGP communication

--- packages/php-wasm/node/src/test/php-dynamic-loading.spec.ts
+++ packages/php-wasm/node/src/test/php-dynamic-loading.spec.ts
@@ -2,6 +2,7 @@ import { loadNodeRuntime } from '..';
 import { PHP, SupportedPHPVersions } from '@php-wasm/universal';
 import path from 'path';
 import fs from 'fs';
+import { createServer } from 'net';
 
 vi.mock('path', async (originalPath) => ({
 	...(await originalPath()),
@@ -20,12 +21,12 @@ describe.each(SupportedPHPVersions)('PHP %s', (phpVersion) => {
 		it('does not load dynamically by default', async () => {
 			php = new PHP(await loadNodeRuntime(phpVersion as any));
 
-			const result = await php.run({
+			const result = await php.runStream({
 				code: `<?php
                     var_dump(extension_loaded('xdebug'));`,
 			});
 
-			expect(result.text).toEqual('bool(false)\n');
+			expect(await result.stdoutText).toEqual('bool(false)\n');
 		});
 
 		it('supports dynamic loading', async () => {
@@ -36,12 +37,12 @@ describe.each(SupportedPHPVersions)('PHP %s', (phpVersion) => {
 				.concat('\nhtml_errors=off');
 			php.writeFile(iniPath, entries);
 
-			const result = await php.run({
+			const result = await php.runStream({
 				code: `<?php
                     var_dump(extension_loaded('xdebug'));`,
 			});
 
-			expect(result.text).toEqual('bool(true)\n');
+			expect(await result.stdoutText).toEqual('bool(true)\n');
 		});
 
 		it('has its own ini file and entries', async () => {
@@ -63,6 +64,56 @@ describe.each(SupportedPHPVersions)('PHP %s', (phpVersion) => {
 			expect(php.listFiles(process.cwd())).toEqual(
 				fs.readdirSync(process.cwd())
 			);
+		});
+
+		it('communicates with default DBGP port', async () => {
+			const queries = [
+				'feature_set -i 1 -n resolved_breakpoints -v 1',
+				'feature_set -i 2 -n notify_ok -v 1',
+				'feature_set -i 3 -n extended_properties -v 1',
+				'feature_set -i 4 -n breakpoint_include_return_value -v 1',
+				'feature_set -i 5 -n max_children -v 100',
+				'run -i 6',
+				'stop -i 7',
+			];
+
+			let responses = '';
+			let i = 0;
+			let stopped = false;
+
+			const server = createServer();
+
+			server.on('connection', function handleConnection(tcpSource) {
+				tcpSource.on('data', (data) => {
+					if (queries[i]) {
+						responses += new TextDecoder().decode(data);
+						const payload = `${Buffer.byteLength(queries[i])}\x00${
+							queries[i]
+						}\x00`;
+						tcpSource.write(new TextEncoder().encode(payload));
+						i++;
+					} else {
+						stopped = true;
+						server.close();
+					}
+				});
+			});
+
+			server.listen(9003);
+
+			const result = await php.runStream({
+				code: `<?php
+					echo "Hello Xdebug World";`,
+			});
+
+			await vi.waitUntil(() => stopped, { timeout: 5000 });
+
+			expect(responses).toContain(
+				'<init xmlns="urn:debugger_protocol_v1"'
+			);
+			expect(responses).toContain('success="1"></response>');
+
+			expect(await result.stdoutText).toEqual('Hello Xdebug World');
 		});
 	});
 });
