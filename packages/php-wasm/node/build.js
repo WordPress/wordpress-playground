255a96e862729e131ce00e3d50d9b2ad21a3d7e9
Brandon Payton <brandon@happycode.net>
2025-06-18 22:54:04 -0400

    Update php-wasm-node build to be able to deal with native Node modules

--- packages/php-wasm/node/build.js
+++ packages/php-wasm/node/build.js
@@ -1,6 +1,7 @@
 import esbuild from 'esbuild';
 import fs from 'fs';
 import path from 'path';
+import { createRequire } from 'module';
 
 try {
 	fs.mkdirSync('dist/packages/php-wasm/node', { recursive: true });
@@ -72,6 +73,54 @@ const dirnamePlugin = {
 	},
 };
 
+// Create a require function that can be used in the nativeNodeModulesPlugin.
+const require = createRequire(import.meta.url);
+
+/**
+ * This is a plugin for handling native Node.js modules.
+ *
+ * Taken from:
+ * https://github.com/evanw/esbuild/issues/1051#issuecomment-806325487
+ */
+const nativeNodeModulesPlugin = {
+	name: 'native-node-modules',
+	setup(build) {
+		// If a ".node" file is imported within a module in the "file" namespace, resolve
+		// it to an absolute path and put it into the "node-file" virtual namespace.
+		build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => ({
+			path: require.resolve(args.path, { paths: [args.resolveDir] }),
+			namespace: 'node-file',
+		}));
+
+		// Files in the "node-file" virtual namespace call "require()" on the
+		// path from esbuild of the ".node" file in the output directory.
+		build.onLoad({ filter: /.*/, namespace: 'node-file' }, (args) => ({
+			contents: `
+				import path from ${JSON.stringify(args.path)}
+				try { module.exports = require(path) }
+				catch {}
+			`,
+		}));
+
+		// If a ".node" file is imported within a module in the "node-file" namespace, put
+		// it in the "file" namespace where esbuild's default loading behavior will handle
+		// it. It is already an absolute path since we resolved it to one above.
+		build.onResolve(
+			{ filter: /\.node$/, namespace: 'node-file' },
+			(args) => ({
+				path: args.path,
+				namespace: 'file',
+			})
+		);
+
+		// Tell esbuild's default loading behavior to use the "file" loader for
+		// these ".node" files.
+		let opts = build.initialOptions;
+		opts.loader = opts.loader || {};
+		opts.loader['.node'] = 'file';
+	},
+};
+
 async function build() {
 	await esbuild.build({
 		entryPoints: [
@@ -98,7 +147,7 @@ async function build() {
 			'.ini': 'file',
 			'.wasm': 'file',
 		},
-		plugins: [dirnamePlugin],
+		plugins: [dirnamePlugin, nativeNodeModulesPlugin],
 	});
 
 	await esbuild.build({
@@ -134,7 +183,7 @@ const __dirname = import.meta.dirname;
 			'.ini': 'file',
 			'.wasm': 'file',
 		},
-		plugins: [dirnamePlugin],
+		plugins: [dirnamePlugin, nativeNodeModulesPlugin],
 	});
 
 	fs.copyFileSync(
