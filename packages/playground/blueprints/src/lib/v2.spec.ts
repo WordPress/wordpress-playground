fc838c9615d8aa8bc86032731fddd3824cfe7834
Adam Zieliński <adam@adamziel.com>
2025-06-19 14:17:24 +0200

    Use sandboxed spawn handler in v2.spec.ts

--- packages/playground/blueprints/src/lib/v2.spec.ts
+++ packages/playground/blueprints/src/lib/v2.spec.ts
@@ -1,21 +1,26 @@
 import { loadNodeRuntime } from '@php-wasm/node';
 import {
-	PHPProcessManager,
 	sandboxedSpawnHandlerFactory,
 	type PHPRequestHandler,
 } from '@php-wasm/universal';
-import { RecommendedPHPVersion } from '@wp-playground/common';
 import { bootRequestHandler } from '@wp-playground/wordpress';
 import { rootCertificates } from 'node:tls';
 import { runBlueprintV2 } from './v2';
-import { createSpawnHandler } from '@php-wasm/util';
+import { RecommendedPHPVersion } from '@wp-playground/common';
 
 describe('V2 runner', () => {
 	let handler: PHPRequestHandler;
 
 	beforeEach(async () => {
 		handler = await bootRequestHandler({
-			createPhpRuntime: async () => await loadNodeRuntime('8.3'),
+			createPhpRuntime: async () =>
+				await loadNodeRuntime(RecommendedPHPVersion, {
+					emscriptenOptions: {
+						ENV: {
+							DOCROOT: '/wordpress',
+						},
+					},
+				}),
 			sapiName: 'cli',
 			siteUrl: 'http://playground-domain/',
 			phpIniEntries: {
@@ -24,8 +29,12 @@ describe('V2 runner', () => {
 			createFiles: {
 				'/internal/shared/ca-bundle.crt': rootCertificates.join('\n'),
 			},
-			spawnHandler: spawnHandlerFactory,
-			// spawnHandler: sandboxedSpawnHandlerFactory,
+			spawnHandler: (processManager) =>
+				sandboxedSpawnHandlerFactory(processManager, {
+					onError: (error) => {
+						console.log('Error in childPHP:', error);
+					},
+				}),
 		});
 	});
 
@@ -34,104 +43,14 @@ describe('V2 runner', () => {
 		const result = await runBlueprintV2({
 			php: instance.php as any,
 			blueprint: '{"version":2}',
-			cliArgs: ['--site-url=http://playground-domain/'],
+			cliArgs: [
+				'--site-url=http://playground-domain/',
+				'--db-engine=sqlite',
+			],
 		});
 		await result.finished;
-		expect(await result.stdoutText).toBe('');
-		expect(await result.stderrText).toBe('');
+		expect(await result.exitCode).toBe(0);
 		const instance2 = await handler.processManager.acquirePHPInstance();
 		expect(instance2.php.listFiles('/wordpress')).toContain('wp-content');
 	}, 60000);
 });
-
-export function spawnHandlerFactory(processManager: PHPProcessManager) {
-	return createSpawnHandler(async function (args, processApi, options) {
-		processApi.notifySpawn();
-		if (args[0] === 'exec') {
-			args.shift();
-		}
-
-		if (args[0].endsWith('.php') || args[0].endsWith('.phar')) {
-			args.unshift('php');
-		}
-
-		// Mock programs required by wp-cli:
-		if (
-			args[0] === '/usr/bin/env' &&
-			args[1] === 'stty' &&
-			args[2] === 'size'
-		) {
-			// These numbers are hardcoded because this
-			// spawnHandler is transmitted as a string to
-			// the PHP backend and has no access to local
-			// scope. It would be nice to find a way to
-			// transfer / proxy a live object instead.
-			// @TODO: Do not hardcode this
-			processApi.stdout(`18 140`);
-			processApi.exit(0);
-		} else if (args[0] === 'tput' && args[1] === 'cols') {
-			processApi.stdout(`140`);
-			processApi.exit(0);
-		} else if (args[0] === 'less') {
-			processApi.on('stdin', (data: Uint8Array) => {
-				processApi.stdout(data);
-			});
-			processApi.flushStdin();
-			processApi.exit(0);
-		} else if (args[0] === 'php') {
-			const { php, reap } = await processManager.acquirePHPInstance({
-				considerPrimary: false,
-			});
-
-			php.chdir(options.cwd as string);
-			try {
-				// Figure out more about setting env, putenv(), etc.
-				const result = await php.cli(args, {
-					env: {
-						...options.env,
-						DOCROOT: '/wordpress',
-						SCRIPT_PATH: args[1],
-						// Set SHELL_PIPE to 0 to ensure WP-CLI formats
-						// the output as ASCII tables.
-						// @see https://github.com/wp-cli/wp-cli/issues/1102
-						SHELL_PIPE: '0',
-					},
-				});
-
-				result.stdout.pipeTo(
-					new WritableStream({
-						write(chunk) {
-							processApi.stdout(chunk);
-						},
-					})
-				);
-				result.stderr.pipeTo(
-					new WritableStream({
-						write(chunk) {
-							processApi.stderr(chunk);
-						},
-					})
-				);
-				await result.exitCode.then(
-					(exitCode) => {
-						processApi.exit(exitCode);
-					},
-					(error) => {
-						console.error('Error in childPHP:', error);
-						processApi.exit(1);
-					}
-				);
-			} catch (e) {
-				// logger.error('Error in childPHP:', e);
-				if (e instanceof Error) {
-					processApi.stderr(e.message);
-				}
-				processApi.exit(1);
-			} finally {
-				reap();
-			}
-		} else {
-			processApi.exit(1);
-		}
-	});
-}
