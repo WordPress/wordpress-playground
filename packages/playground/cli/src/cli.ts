ac86f08ba5823c0f33e7c87bd1a99067f82577b6
Adam Zieliński <adam@adamziel.com>
2025-06-21 20:04:42 +0200

    Lint

--- packages/playground/cli/src/cli.ts
+++ packages/playground/cli/src/cli.ts
@@ -1,7 +1,7 @@
 import { parseOptionsAndRunCLI } from './run-cli';
 
 // Do not await this as top-level await is not supported in all environments.
-parseOptionsAndRunCLI().catch((e) => {
+parseOptionsAndRunCLI().catch(() => {
 	// process.exit(1); is here and not in parseOptionsAndRunCLI()
 	// so that we can unit test the failure modes with try/catch.
 	process.exit(1);
