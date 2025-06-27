8914548ff089e7c1f0e9b74c6695e8abcb518973
Adam Zieliński <adam@adamziel.com>
2025-06-27 16:23:33 +0200

    Keep the server alive

--- packages/playground/cli/src/cli.ts
+++ packages/playground/cli/src/cli.ts
@@ -2,7 +2,9 @@ import { parseOptionsAndRunCLI } from './run-cli';
 
 // Do not await this as top-level await is not supported in all environments.
 parseOptionsAndRunCLI().then(
-	() => process.exit(0),
+	() => {
+		// Do nothing, just keep the server alive.
+	},
 	() => {
 		// process.exit(1); is here and not in parseOptionsAndRunCLI()
 		// so that we can unit test the failure modes with try/catch.
