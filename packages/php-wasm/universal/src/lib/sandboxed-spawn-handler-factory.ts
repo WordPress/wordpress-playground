635e63537d71964d8ba51653f1e310c546da5c35
Adam Zieliński <adam@adamziel.com>
2025-07-01 17:44:50 +0200

    Document sandboxedSpawnHandlerFactory()

--- packages/php-wasm/universal/src/lib/sandboxed-spawn-handler-factory.ts
+++ packages/php-wasm/universal/src/lib/sandboxed-spawn-handler-factory.ts
@@ -1,6 +1,16 @@
 import { createSpawnHandler } from '@php-wasm/util';
 import type { PHPProcessManager } from './php-process-manager';
 
+/**
+ * An isomorphic proc_open() handler that implements typical shell in TypeScript
+ * without relying on a server runtime. It can be used in the browser and Node.js
+ * alike whenever you need to spawn a PHP subprocess, query the terminal size, etc.
+ * It is open for future expansion if more shell or busybox calls are needed, but
+ * advanced shell features such as piping, stream redirection etc. are outside of
+ * the scope of this minimal handler. If they become vital at any point, let's
+ * explore bringing in an actual shell implementation or at least a proper command
+ * parser.
+ */
 export function sandboxedSpawnHandlerFactory(
 	processManager: PHPProcessManager
 ) {
