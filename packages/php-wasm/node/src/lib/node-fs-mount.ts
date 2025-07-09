6619235c68340c09c0a4f6eec09b350a00e09e34
Bero <berislav.grgicak@gmail.com>
2025-07-09 09:59:01 +0200

    When mounting a file create a empty file node instead of a directory node

--- packages/php-wasm/node/src/lib/node-fs-mount.ts
+++ packages/php-wasm/node/src/lib/node-fs-mount.ts
@@ -1,28 +1,42 @@
 import type { MountHandler } from '@php-wasm/universal';
+import { statSync } from 'fs';
+import { dirname } from 'path';
 
 export function createNodeFsMountHandler(localPath: string): MountHandler {
 	return async function (php, FS, vfsMountPoint) {
 		/**
-		 * DON'T MERGE THIS.
-		 * This is a temporary workaround to demonstrate how mounting requires the mount point to be a directory.
-		 *
-		 * Emscripten requires the mount point to be a directory.
-		 * By creating a directory we can even mount a file over it as long as the paths match.
-		 * PHP-WASM source: https://github.com/WordPress/wordpress-playground/blob/trunk/packages/php-wasm/node/asyncify/php_8_0.js#L2679
-		 *
 		 * When Emscripten attempt to mount a local path into VFS, it looks up the path
 		 * and adds the local path as a mount to the VFS Node.
 		 * PHP-WASM source: https://github.com/WordPress/wordpress-playground/blob/trunk/packages/php-wasm/node/asyncify/php_8_0.js#L2700
 		 *
-		 * For mounting to work, the Node must exist in VFS and be a directory.
+		 * For mounting to work, the Node must exist in VFS.
 		 * If the Node doesn't exist, the mount fails with error 44 (MEMFS.doesNotExistError).
 		 * PHP-WASM source: https://github.com/WordPress/wordpress-playground/blob/trunk/packages/php-wasm/node/asyncify/php_8_0.js#L1201
+		 *
+		 * Emscripten requires the mount point to be a directory.
+		 * To work around this, the PHP-wasm compile removes the directory check.
+		 * PHP-WASM source: https://github.com/WordPress/wordpress-playground/blob/5821cee231f452d050fd337b99ad0b26ebda487e/packages/php-wasm/compile/php/Dockerfile#L2148
 		 */
+		let lookup;
 		try {
-			FS.lookupPath(vfsMountPoint);
+			lookup = FS.lookupPath(vfsMountPoint);
 		} catch (e) {
 			// FS.lookupPath will throw an error if the path doesn't exist.
-			FS.mkdirTree(vfsMountPoint);
+			if (statSync(localPath).isFile()) {
+				FS.writeFile(vfsMountPoint, '');
+			} else if (statSync(localPath).isDirectory()) {
+				FS.mkdirTree(dirname(vfsMountPoint));
+				FS.mkdirTree(vfsMountPoint);
+			} else {
+				throw new Error(
+					'Unsupported file type. PHP-wasm supports only symlinks that link to files, directories, or symlinks.'
+				);
+			}
+			lookup = FS.lookupPath(vfsMountPoint);
+		}
+		if (!lookup.node) {
+			// TODO: Improve error once I understand the limitations.
+			throw new Error('Unable to access the mount point in VFS.');
 		}
 		FS.mount(FS.filesystems['NODEFS'], { root: localPath }, vfsMountPoint);
 		return () => {
