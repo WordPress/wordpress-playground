9cae57756aca5f241d57253ed7412f3a166af8ae
mho22 <yannick@capsules.codes>
2025-07-17 13:22:42 +0200

    Add xdebug option in fopen tests

--- packages/php-wasm/node/src/test/php-fopen.spec.ts
+++ packages/php-wasm/node/src/test/php-fopen.spec.ts
@@ -11,6 +11,7 @@ import { phpVars } from '@php-wasm/util';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import InitialDockerfile from '../../../compile/php/Dockerfile?raw';
 import { loadNodeRuntime } from '../lib';
+import type { PHPLoaderOptions } from '../lib';
 import { jspi } from 'wasm-feature-detect';
 
 const runtimeMode = (await jspi()) ? 'jspi' : 'asyncify';
@@ -84,260 +85,274 @@ const httpsServer = https.createServer(
 				fclose($fp);`,
 		};
 
-		describe.each(phpVersions)(`PHP %s – ${runtimeMode}`, (phpVersion) => {
-			let php: PHP;
-			beforeEach(async () => {
-				php = new PHP(await loadNodeRuntime(phpVersion as any));
-				await setPhpIniEntries(php, { allow_url_fopen: 1 });
-			});
+		const phpLoaderOptions: PHPLoaderOptions[] = [{}, { withXdebug: true }];
 
-			afterEach(async () => {
-				php.exit();
-			});
-
-			describe.each(Object.keys(topOfTheStack))(
-				'%s',
-				(networkCallKey) => {
-					const networkCall = topOfTheStack[networkCallKey];
-					test('Direct call', () => assertNoCrash(networkCall));
-					describe('Function calls', () => {
-						test('Simple call', () =>
-							assertNoCrash(`${networkCall};`));
-						test('Simple call', () =>
-							assertNoCrash(
-								`function top() { ${networkCall} } top();`
-							));
-						test('Via call_user_func', () =>
-							assertNoCrash(
-								`function top() { ${networkCall} } call_user_func('top'); `
-							));
-						test('Via call_user_func_array', () =>
-							assertNoCrash(
-								`function top() { ${networkCall} } call_user_func_array('top', array());`
-							));
+		phpLoaderOptions.forEach((options) => {
+			describe.each(phpVersions)(
+				`PHP %s – ${runtimeMode}`,
+				(phpVersion) => {
+					let php: PHP;
+					beforeEach(async () => {
+						php = new PHP(
+							await loadNodeRuntime(phpVersion as any, options)
+						);
+						await setPhpIniEntries(php, { allow_url_fopen: 1 });
 					});
 
-					describe('Array functions', () => {
-						test('array_filter', () =>
-							assertNoCrash(`
-							function top() { ${networkCall} }
-							array_filter(array('top'), 'top');
-						`));
+					afterEach(async () => {
+						php.exit();
+					});
 
-						test('array_map', () =>
-							assertNoCrash(`
+					describe.each(Object.keys(topOfTheStack))(
+						'%s',
+						(networkCallKey) => {
+							const networkCall = topOfTheStack[networkCallKey];
+							test('Direct call', () =>
+								assertNoCrash(networkCall));
+							describe('Function calls', () => {
+								test('Simple call', () =>
+									assertNoCrash(`${networkCall};`));
+								test('Simple call', () =>
+									assertNoCrash(
+										`function top() { ${networkCall} } top();`
+									));
+								test('Via call_user_func', () =>
+									assertNoCrash(
+										`function top() { ${networkCall} } call_user_func('top'); `
+									));
+								test('Via call_user_func_array', () =>
+									assertNoCrash(
+										`function top() { ${networkCall} } call_user_func_array('top', array());`
+									));
+							});
+
+							describe('Array functions', () => {
+								test('array_filter', () =>
+									assertNoCrash(`
 								function top() { ${networkCall} }
-								array_map(array('top'), 'top');
+								array_filter(array('top'), 'top');
 							`));
 
-						// Network calls in sort() would be silly so let's skip those for now.
-					});
+								test('array_map', () =>
+									assertNoCrash(`
+									function top() { ${networkCall} }
+									array_map(array('top'), 'top');
+								`));
 
-					describe('Class method calls', () => {
-						test('Regular method', () =>
-							assertNoCrash(`
-						class Top {
-							function my_method() { ${networkCall} }
-						}
-						$x = new Top();
-						$x->my_method();
-					`));
-						test('Via ReflectionMethod->invoke()', () =>
-							assertNoCrash(`
-						class Top {
-							function my_method() { ${networkCall} }
-						}
-						$reflectionMethod = new ReflectionMethod('Top', 'my_method');
-						$reflectionMethod->invoke(new Top());
-					`));
-						test('Via ReflectionMethod->invokeArgs()', () =>
-							assertNoCrash(`
-						class Top {
-							function my_method() { ${networkCall} }
-						}
-						$reflectionMethod = new ReflectionMethod('Top', 'my_method');
-						$reflectionMethod->invokeArgs(new Top(), array());
-					`));
-						test('Via call_user_func', () =>
-							assertNoCrash(`
-						class Top {
-							function my_method() { ${networkCall} }
-						}
-						call_user_func([new Top(), 'my_method']);
-						`));
-						test('Via call_user_func_array', () =>
-							assertNoCrash(`
-						class Top {
-							function my_method() { ${networkCall} }
-						}
-						call_user_func_array([new Top(), 'my_method'], []);
-						`));
-						test('Constructor', () =>
-							assertNoCrash(`
-						class Top {
-							function __construct() { ${networkCall} }
-						}
-						new Top();
-					`));
-						test('Destructor', () =>
-							assertNoCrash(`
-						class Top {
-							function __destruct() { ${networkCall} }
-						}
-						$x = new Top();
-						unset($x);
-					`));
-						test('__call', () =>
-							assertNoCrash(`
-						class Top {
-							function __call($method, $args) { ${networkCall} }
-						}
-						$x = new Top();
-						$x->test();
-					`));
-						test('__get', () =>
-							assertNoCrash(`
-						class Top {
-							function __get($prop) { ${networkCall} }
-						}
-						$x = new Top();
-						$x->test;
-					`));
-						test('__set', () =>
-							assertNoCrash(`
-						class Top {
-							function __set($prop, $value) { ${networkCall} }
-						}
-						$x = new Top();
-						$x->test = 1;
-					`));
-						test('__isset', () =>
-							assertNoCrash(`
-						class Top {
-							function __isset($prop) { ${networkCall} }
-						}
-						$x = new Top();
-						isset($x->test);
-					`));
-						test('ArrayAccess', () => {
-							assertNoCrash(`
-							class Top implements ArrayAccess {
-								function offsetExists($offset) { ${networkCall} }
-								function offsetGet($offset) { ${networkCall} }
-								function offsetSet($offset, $value) { ${networkCall} }
-								function offsetUnset($offset) { ${networkCall} }
+								// Network calls in sort() would be silly so let's skip those for now.
+							});
+
+							describe('Class method calls', () => {
+								test('Regular method', () =>
+									assertNoCrash(`
+							class Top {
+								function my_method() { ${networkCall} }
 							}
 							$x = new Top();
-							isset($x['test']);
-							$a = $x['test'];
-							$x['test'] = 123;
-							unset($x['test']);
-						`);
-						});
-						test('Iterator', () =>
-							assertNoCrash(`
-						$data = new class() implements IteratorAggregate {
-							public function getIterator(): Traversable {
+							$x->my_method();
+						`));
+								test('Via ReflectionMethod->invoke()', () =>
+									assertNoCrash(`
+							class Top {
+								function my_method() { ${networkCall} }
+							}
+							$reflectionMethod = new ReflectionMethod('Top', 'my_method');
+							$reflectionMethod->invoke(new Top());
+						`));
+								test('Via ReflectionMethod->invokeArgs()', () =>
+									assertNoCrash(`
+							class Top {
+								function my_method() { ${networkCall} }
+							}
+							$reflectionMethod = new ReflectionMethod('Top', 'my_method');
+							$reflectionMethod->invokeArgs(new Top(), array());
+						`));
+								test('Via call_user_func', () =>
+									assertNoCrash(`
+							class Top {
+								function my_method() { ${networkCall} }
+							}
+							call_user_func([new Top(), 'my_method']);
+							`));
+								test('Via call_user_func_array', () =>
+									assertNoCrash(`
+							class Top {
+								function my_method() { ${networkCall} }
+							}
+							call_user_func_array([new Top(), 'my_method'], []);
+							`));
+								test('Constructor', () =>
+									assertNoCrash(`
+							class Top {
+								function __construct() { ${networkCall} }
+							}
+							new Top();
+						`));
+								test('Destructor', () =>
+									assertNoCrash(`
+							class Top {
+								function __destruct() { ${networkCall} }
+							}
+							$x = new Top();
+							unset($x);
+						`));
+								test('__call', () =>
+									assertNoCrash(`
+							class Top {
+								function __call($method, $args) { ${networkCall} }
+							}
+							$x = new Top();
+							$x->test();
+						`));
+								test('__get', () =>
+									assertNoCrash(`
+							class Top {
+								function __get($prop) { ${networkCall} }
+							}
+							$x = new Top();
+							$x->test;
+						`));
+								test('__set', () =>
+									assertNoCrash(`
+							class Top {
+								function __set($prop, $value) { ${networkCall} }
+							}
+							$x = new Top();
+							$x->test = 1;
+						`));
+								test('__isset', () =>
+									assertNoCrash(`
+							class Top {
+								function __isset($prop) { ${networkCall} }
+							}
+							$x = new Top();
+							isset($x->test);
+						`));
+								test('ArrayAccess', () => {
+									assertNoCrash(`
+								class Top implements ArrayAccess {
+									function offsetExists($offset) { ${networkCall} }
+									function offsetGet($offset) { ${networkCall} }
+									function offsetSet($offset, $value) { ${networkCall} }
+									function offsetUnset($offset) { ${networkCall} }
+								}
+								$x = new Top();
+								isset($x['test']);
+								$a = $x['test'];
+								$x['test'] = 123;
+								unset($x['test']);
+							`);
+								});
+								test('Iterator', () =>
+									assertNoCrash(`
+							$data = new class() implements IteratorAggregate {
+								public function getIterator(): Traversable {
+									${networkCall};
+									return new ArrayIterator( [] );
+								}
+							};
+							echo json_encode( [
+								...$data
+							] );
+						`));
+
+								test('Countable', () =>
+									assertNoCrash(`
+							$data = new class() implements Countable {
+								public function count() {
+									${networkCall}
+									return 0;
+								}
+							};
+							count($data);
+						`));
+
+								test('yield', () =>
+									assertNoCrash(`
+							function countTo2() {
 								${networkCall};
-								return new ArrayIterator( [] );
+								yield '1';
+								${networkCall};
+								yield '2';
 							}
-						};
-						echo json_encode( [
-							...$data
-						] );
-					`));
-
-						test('Countable', () =>
-							assertNoCrash(`
-						$data = new class() implements Countable {
-							public function count() {
-								${networkCall}
-								return 0;
+							foreach(countTo2() as $number) {
+								echo $number;
 							}
-						};
-						count($data);
-					`));
+						`));
+							});
 
-						test('yield', () =>
-							assertNoCrash(`
-						function countTo2() {
-							${networkCall};
-							yield '1';
-							${networkCall};
-							yield '2';
+							describe('exif extension support', () => {
+								it('exif_read_data', async () => {
+									assertNoCrash(
+										`var_dump(exif_read_data('${httpUrl}/image.jpg'));`
+									);
+								});
+								it('exif_imagetype', async () => {
+									assertNoCrash(
+										`var_dump(exif_imagetype('${httpUrl}/image.jpg'));`
+									);
+								});
+								it('exif_thumbnail', async () => {
+									assertNoCrash(
+										`var_dump(exif_thumbnail('${httpUrl}/image.jpg'));`
+									);
+								});
+							});
 						}
-						foreach(countTo2() as $number) {
-							echo $number;
+					);
+
+					async function assertNoCrash(code: string) {
+						try {
+							const result = await php.run({
+								code: `<?php ${code}`,
+							});
+							expect(result).toBeTruthy();
+							expect(result.text).toBe('');
+							expect(result.errors).toBeFalsy();
+						} catch (e) {
+							if (
+								'FIX_DOCKERFILE' in process.env &&
+								process.env['FIX_DOCKERFILE'] === 'true' &&
+								runtimeMode == 'asyncify' &&
+								'functionsMaybeMissingFromAsyncify' in php
+							) {
+								const missingCandidates = (
+									php.functionsMaybeMissingFromAsyncify as string[]
+								)
+									.map((candidate) =>
+										candidate.replace('byn$fpcast-emu$', '')
+									)
+									.filter(
+										(candidate) =>
+											!Dockerfile.includes(
+												`"${candidate}"`
+											)
+									);
+								if (missingCandidates.length) {
+									addAsyncifyFunctionsToDockerfile(
+										missingCandidates
+									);
+									throw new Error(
+										`Asyncify crash! The following missing functions were just auto-added to the ASYNCIFY_ONLY list in the Dockerfile: \n ` +
+											missingCandidates.join(', ') +
+											`\nYou now need to rebuild PHP and re-run this test: \n` +
+											`  npm run recompile:php:node:asyncify:8.0\n` +
+											`  node --stack-trace-limit=100 ./node_modules/.bin/nx test php-wasm-node --test-name-pattern='asyncify'\n`
+									);
+								}
+
+								const err = new Error(
+									`Asyncify crash! No C functions present in the stack trace were missing ` +
+										`from the Dockerfile. This could mean the stack trace is too short – try increasing the stack trace limit ` +
+										`with --stack-trace-limit=100. If you already did that, fixing this problem will likely take more digging.`
+								);
+								err.cause = e;
+								throw err;
+							}
 						}
-					`));
-					});
-
-					describe('exif extension support', () => {
-						it('exif_read_data', async () => {
-							assertNoCrash(
-								`var_dump(exif_read_data('${httpUrl}/image.jpg'));`
-							);
-						});
-						it('exif_imagetype', async () => {
-							assertNoCrash(
-								`var_dump(exif_imagetype('${httpUrl}/image.jpg'));`
-							);
-						});
-						it('exif_thumbnail', async () => {
-							assertNoCrash(
-								`var_dump(exif_thumbnail('${httpUrl}/image.jpg'));`
-							);
-						});
-					});
-				}
-			);
-
-			async function assertNoCrash(code: string) {
-				try {
-					const result = await php.run({
-						code: `<?php ${code}`,
-					});
-					expect(result).toBeTruthy();
-					expect(result.text).toBe('');
-					expect(result.errors).toBeFalsy();
-				} catch (e) {
-					if (
-						'FIX_DOCKERFILE' in process.env &&
-						process.env['FIX_DOCKERFILE'] === 'true' &&
-						runtimeMode == 'asyncify' &&
-						'functionsMaybeMissingFromAsyncify' in php
-					) {
-						const missingCandidates = (
-							php.functionsMaybeMissingFromAsyncify as string[]
-						)
-							.map((candidate) =>
-								candidate.replace('byn$fpcast-emu$', '')
-							)
-							.filter(
-								(candidate) =>
-									!Dockerfile.includes(`"${candidate}"`)
-							);
-						if (missingCandidates.length) {
-							addAsyncifyFunctionsToDockerfile(missingCandidates);
-							throw new Error(
-								`Asyncify crash! The following missing functions were just auto-added to the ASYNCIFY_ONLY list in the Dockerfile: \n ` +
-									missingCandidates.join(', ') +
-									`\nYou now need to rebuild PHP and re-run this test: \n` +
-									`  npm run recompile:php:node:asyncify:8.0\n` +
-									`  node --stack-trace-limit=100 ./node_modules/.bin/nx test php-wasm-node --test-name-pattern='asyncify'\n`
-							);
-						}
-
-						const err = new Error(
-							`Asyncify crash! No C functions present in the stack trace were missing ` +
-								`from the Dockerfile. This could mean the stack trace is too short – try increasing the stack trace limit ` +
-								`with --stack-trace-limit=100. If you already did that, fixing this problem will likely take more digging.`
-						);
-						err.cause = e;
-						throw err;
 					}
 				}
-			}
+			);
 		});
 	});
 });
