b6c0dd054b09193d88127e57edfb7b8d4b58775e
Adam Zieliński <adam@adamziel.com>
2025-07-22 15:46:17 +0200

    [Playground CLI] Improve error reporting
    Improves Playground CLI error reporting in two ways:
    
    * Stop printing the stack trace if the `--debug` flag isn't set. The
      typical errors are similar to "unknown option --blueprin" and
      displaying the stack trace makes the user experience worse.
    * Rejects unrecognized options. Before this PR, a typo in one of the
      flags will go undetected until the user realizes the CLI doesn't do
      what they want it to. With this PR, the typo is immediately apparent
      as the process will short-circuit and report a failure. We still
      support both kebab-cased and camelCased CLI flags
    
     ## Testing instructions
    
    Confirm this command prints a short error message:
    
    ```
    node --experimental-strip-types --experimental-transform-types --disable-warning=ExperimentalWarning --import ./packages/meta/src/node-es-module-loader/register.mts ./packages/playground/cli/src/cli.ts server --php=8.4 --blueprin=./file.json
    ```
    
    Add `--debug` and confirm you can see the entire stacktrace

--- packages/playground/cli/src/cli.ts
+++ packages/playground/cli/src/cli.ts
@@ -1,8 +1,4 @@
 import { parseOptionsAndRunCLI } from './run-cli';
 
 // Do not await this as top-level await is not supported in all environments.
-parseOptionsAndRunCLI().catch((e: any) => {
-	// eslint-disable-next-line no-console
-	console.error(e);
-	process.exit(1);
-});
+parseOptionsAndRunCLI();
