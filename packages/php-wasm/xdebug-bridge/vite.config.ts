587fee5380fc2d5a5cba8e0347bff4d1590d0bb3
mho22 <yannick@capsules.codes>
2025-07-23 13:38:08 +0200

    Correct error related to tests

--- packages/php-wasm/xdebug-bridge/vite.config.ts
+++ packages/php-wasm/xdebug-bridge/vite.config.ts
@@ -56,7 +56,6 @@ export default defineConfig({
 		},
 		environment: 'node',
 		globals: true,
-		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		reporters: ['default'],
 	},
 });
