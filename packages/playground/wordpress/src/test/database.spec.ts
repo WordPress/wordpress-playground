27d16524aee4f72e4bd2bfc65ed982f4aef95e2c
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-07-29 10:22:00 +0200

    Check if the integration plugin is installed inside the folder

--- packages/playground/wordpress/src/test/database.spec.ts
+++ packages/playground/wordpress/src/test/database.spec.ts
@@ -2,9 +2,37 @@ import { loadNodeRuntime } from '@php-wasm/node';
 import { RecommendedPHPVersion } from '@wp-playground/common';
 import { getWordPressModule } from '@wp-playground/wordpress-builds';
 import { bootWordPress, isWordPressInstalled } from '../boot';
+import { join } from 'path';
+import { tmpdir } from 'os';
+import { mkdirSync, rmdirSync } from 'fs';
+import { createNodeFsMountHandler } from '@php-wasm/node';
 
 describe('Test database', () => {
-	it(`should not start WordPress without a working driver module`, async () => {
+	let tempDir: string;
+
+	beforeAll(() => {
+		tempDir = join(tmpdir(), 'database-test');
+
+		try {
+			mkdirSync(
+				join(
+					tempDir,
+					'wp-content',
+					'mu-plugins',
+					'sqlite-database-integration'
+				),
+				{ recursive: true }
+			);
+		} catch {
+			// Ignore error if directory already exists
+		}
+	});
+
+	afterAll(() => {
+		rmdirSync(tempDir, { recursive: true });
+	});
+
+	it('should not start WordPress without a working driver module', async () => {
 		await expect(async () => {
 			await bootWordPress({
 				createPhpRuntime: async () =>
@@ -13,22 +41,39 @@ describe('Test database', () => {
 				wordPressZip: await getWordPressModule(),
 				sqliteIntegrationPluginZip: undefined,
 			});
-		}).rejects.toThrow();
+		}).rejects.toThrow('SQLite integration plugin is not installed.');
 	});
 
-	it(`should not install WordPress with data but without a working driver module`, async () => {
-		const handler = await bootWordPress({
-			createPhpRuntime: async () =>
-				await loadNodeRuntime(RecommendedPHPVersion),
-			siteUrl: 'http://playground-domain/',
-			wordPressZip: await getWordPressModule(),
-			sqliteIntegrationPluginZip: undefined,
-			dataSqlPath: '/wordpress/wp-content/database/.ht.sqlite',
-		});
+	it('should not install WordPress with data but without a working driver module', async () => {
+		await expect(async () => {
+			await bootWordPress({
+				createPhpRuntime: async () =>
+					await loadNodeRuntime(RecommendedPHPVersion),
+				siteUrl: 'http://playground-domain/',
+				wordPressZip: await getWordPressModule(),
+				sqliteIntegrationPluginZip: undefined,
+				dataSqlPath: '/wordpress/wp-content/database/.ht.sqlite',
+			});
+		}).rejects.toThrow('SQLite integration plugin is not installed.');
+	});
 
-		const php = await handler.getPrimaryPhp();
-		const isInstalled = await isWordPressInstalled(php);
-
-		expect(isInstalled).toBe(false);
+	it('should fail if the SQLite integration plugin is specified but not installed', async () => {
+		await expect(async () => {
+			await bootWordPress({
+				createPhpRuntime: async () =>
+					await loadNodeRuntime(RecommendedPHPVersion),
+				siteUrl: 'http://playground-domain/',
+				wordPressZip: await getWordPressModule(),
+				sqliteIntegrationPluginZip: undefined,
+				hooks: {
+					beforeWordPressFiles: async (php) => {
+						await php.mount(
+							'/wordpress',
+							createNodeFsMountHandler(tempDir)
+						);
+					},
+				},
+			});
+		}).rejects.toThrow('WordPress installation has failed.');
 	});
 });
