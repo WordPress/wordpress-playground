dc26d16cf5ece7e954037d42be4d3213a44457b0
mho22 <yannick@capsules.codes>
2025-08-04 16:33:45 +0200

    Compile php-wasm-node asyncify

--- packages/php-wasm/node/asyncify/php_7_3.js
+++ packages/php-wasm/node/asyncify/php_7_3.js
@@ -8,7 +8,7 @@ import path from 'path';
 
 const dependencyFilename = path.join(__dirname, '7_3_33', 'php_7_3.wasm');
 export { dependencyFilename };
-export const dependenciesTotalSize = 29695998;
+export const dependenciesTotalSize = 29698903;
 const phpVersionString = '7.3.33';
 export function init(RuntimeName, PHPLoader) {
 	// The rest of the code comes from the built php.js file and esm-suffix.js
