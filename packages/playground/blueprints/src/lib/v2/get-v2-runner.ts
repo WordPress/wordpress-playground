12b2cf7142e1f51e5230c29aa2101b871b125fae
Adam Zieliński <adam@adamziel.com>
2025-08-08 15:10:24 +0200

    restore ts-ignore

--- packages/playground/blueprints/src/lib/v2/get-v2-runner.ts
+++ packages/playground/blueprints/src/lib/v2/get-v2-runner.ts
@@ -6,10 +6,8 @@ export async function getV2Runner(): Promise<File> {
 	 * not ideal, but there's no other standard solution for shipping static files with isomorphic
 	 * npm CJS+ESM packages so this will have to do until a better solution emerges.
 	 */
-	// @ts-ignore
-	const blueprintsPharBytes = (
-		await import('../../../blueprints.phar?base64')
-	).default;
+	const blueprintsPharBytes = // @ts-ignore
+		(await import('../../../blueprints.phar?base64')).default;
 
 	return new File([blueprintsPharBytes], `blueprints.phar`, {
 		type: 'application/zip',
