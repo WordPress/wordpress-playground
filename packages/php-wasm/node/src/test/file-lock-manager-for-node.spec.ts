f9e8de243d8176025e67814f5aa2b1119c7630fe
Brandon Payton <brandon@happycode.net>
2025-08-13 21:38:09 -0400

    Re-enable and fix native file locking tests

--- packages/php-wasm/node/src/test/file-lock-manager-for-node.spec.ts
+++ packages/php-wasm/node/src/test/file-lock-manager-for-node.spec.ts
@@ -17,6 +17,7 @@ import {
 import type { SupportedPHPVersion } from '@php-wasm/universal';
 import { joinPaths } from '@php-wasm/util';
 import { jspi } from 'wasm-feature-detect';
+import { flockSync as nativeFlockSync } from 'fs-ext';
 
 const TEST_FILE1 = new URL('test1.txt', import.meta.url).pathname;
 const TEST_FILE2 = new URL('test2.txt', import.meta.url).pathname;
@@ -25,7 +26,7 @@ describe('FileLockManagerForNode', () => {
 	let lockManager: FileLockManagerForNode;
 
 	beforeEach(() => {
-		lockManager = new FileLockManagerForNode();
+		lockManager = new FileLockManagerForNode(nativeFlockSync);
 		writeFileSync(TEST_FILE1, `test file 1 for ${import.meta.url}`);
 		writeFileSync(TEST_FILE2, `test file 2 for ${import.meta.url}`);
 	});
