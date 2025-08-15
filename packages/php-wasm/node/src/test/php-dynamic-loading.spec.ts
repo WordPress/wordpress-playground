631e18609ab179d98b816267b727524f020947c0
mho22 <yannick@capsules.codes>
2025-08-15 12:32:51 +0200

    Isolated Intl dynamic testing for jspi versions only

--- packages/php-wasm/node/src/test/php-dynamic-loading.spec.ts
+++ packages/php-wasm/node/src/test/php-dynamic-loading.spec.ts
@@ -2,10 +2,11 @@ import { loadNodeRuntime } from '..';
 import { PHP, SupportedPHPVersions } from '@php-wasm/universal';
 import fs from 'fs';
 import { createServer } from 'net';
+import { jspi } from 'wasm-feature-detect';
 
 const phpVersions =
 	'PHP' in process.env ? [process.env['PHP']!] : SupportedPHPVersions;
-describe.each(phpVersions)('PHP %s', (phpVersion) => {
+describe.each(phpVersions)('PHP %s', async (phpVersion) => {
 	describe('XDebug', () => {
 		let php: PHP;
 		beforeEach(async () => {
@@ -116,87 +117,93 @@ describe.each(phpVersions)('PHP %s', (phpVersion) => {
 		});
 	});
 
-	describe('Intl', () => {
-		let php: PHP;
-		beforeEach(async () => {
-			php = new PHP(
-				await loadNodeRuntime(phpVersion as any, { withIntl: true })
-			);
-		});
-
-		afterEach(async () => {
-			php.exit();
-		});
-
-		it('does not load dynamically by default', async () => {
-			php = new PHP(await loadNodeRuntime(phpVersion as any));
-
-			const result = await php.runStream({
-				code: `<?php
-					var_dump(extension_loaded('intl'));
-					var_dump(class_exists('Collator'));`,
+	if (await jspi()) {
+		describe('Intl', () => {
+			let php: PHP;
+			beforeEach(async () => {
+				php = new PHP(
+					await loadNodeRuntime(phpVersion as any, { withIntl: true })
+				);
 			});
 
-			expect(await result.stdoutText).toEqual(
-				'bool(false)\nbool(false)\n'
-			);
-		});
-
-		it('supports dynamic loading', async () => {
-			const result = await php.runStream({
-				code: `<?php
-					var_dump(extension_loaded('intl'));
-					var_dump(class_exists('Collator'));`,
+			afterEach(async () => {
+				php.exit();
 			});
 
-			expect(await result.stdoutText).toEqual('bool(true)\nbool(true)\n');
-		});
+			it('does not load dynamically by default', async () => {
+				php = new PHP(await loadNodeRuntime(phpVersion as any));
 
-		it('has its own ini file and entries', async () => {
-			const entries = php.readFileAsText(
-				'/internal/shared/extensions/intl.ini'
-			);
+				const result = await php.runStream({
+					code: `<?php
+						var_dump(extension_loaded('intl'));
+						var_dump(class_exists('Collator'));`,
+				});
 
-			const expected = [
-				'extension=/internal/shared/extensions/intl.so',
-			].join('\n');
-
-			expect(entries).toEqual(expected);
-		});
-
-		it('loads the icu data file', async () => {
-			expect(php.listFiles('/internal/shared')).toContain('icudt74l.dat');
-		});
-
-		it('uses intl functions', async () => {
-			const response = await php.runStream({
-				code: `<?php
-						$formatter = numfmt_create('en-US', NumberFormatter::CURRENCY);
-						echo numfmt_format($formatter, 100.00);
-						$formatter = numfmt_create('fr-FR', NumberFormatter::CURRENCY);
-						echo numfmt_format($formatter, 100.00);
-					?>`,
+				expect(await result.stdoutText).toEqual(
+					'bool(false)\nbool(false)\n'
+				);
 			});
-			expect(await response.stdoutText).toEqual('$100.00100,00\xA0€');
-		});
 
-		it('uses intl classes', async () => {
-			const response = await php.runStream({
-				code: `<?php
-						$data = array(
-							'F' => 'Foo',
-							'Br' => 'Bar',
-							'Bz' => 'Bz',
-						);
+			it('supports dynamic loading', async () => {
+				const result = await php.runStream({
+					code: `<?php
+						var_dump(extension_loaded('intl'));
+						var_dump(class_exists('Collator'));`,
+				});
 
-						$collator = new Collator('en_US');
-						$collator->asort($data, Collator::SORT_STRING);
-						var_dump($data);
-					?>`,
+				expect(await result.stdoutText).toEqual(
+					'bool(true)\nbool(true)\n'
+				);
 			});
-			expect(await response.stdoutText).toEqual(
-				'array(3) {\n  ["Br"]=>\n  string(3) "Bar"\n  ["Bz"]=>\n  string(2) "Bz"\n  ["F"]=>\n  string(3) "Foo"\n}\n'
-			);
+
+			it('has its own ini file and entries', async () => {
+				const entries = php.readFileAsText(
+					'/internal/shared/extensions/intl.ini'
+				);
+
+				const expected = [
+					'extension=/internal/shared/extensions/intl.so',
+				].join('\n');
+
+				expect(entries).toEqual(expected);
+			});
+
+			it('loads the icu data file', async () => {
+				expect(php.listFiles('/internal/shared')).toContain(
+					'icudt74l.dat'
+				);
+			});
+
+			it('uses intl functions', async () => {
+				const response = await php.runStream({
+					code: `<?php
+							$formatter = numfmt_create('en-US', NumberFormatter::CURRENCY);
+							echo numfmt_format($formatter, 100.00);
+							$formatter = numfmt_create('fr-FR', NumberFormatter::CURRENCY);
+							echo numfmt_format($formatter, 100.00);
+						?>`,
+				});
+				expect(await response.stdoutText).toEqual('$100.00100,00\xA0€');
+			});
+
+			it('uses intl classes', async () => {
+				const response = await php.runStream({
+					code: `<?php
+							$data = array(
+								'F' => 'Foo',
+								'Br' => 'Bar',
+								'Bz' => 'Bz',
+							);
+
+							$collator = new Collator('en_US');
+							$collator->asort($data, Collator::SORT_STRING);
+							var_dump($data);
+						?>`,
+				});
+				expect(await response.stdoutText).toEqual(
+					'array(3) {\n  ["Br"]=>\n  string(3) "Bar"\n  ["Bz"]=>\n  string(2) "Bz"\n  ["F"]=>\n  string(3) "Foo"\n}\n'
+				);
+			});
 		});
-	});
+	}
 });
