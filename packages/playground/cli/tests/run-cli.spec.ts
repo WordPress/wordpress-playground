1bec0197ddbdc8889ba4772d71e2e6efd37ea6af
Brandon Payton <brandon@happycode.net>
2025-08-15 22:40:19 -0400

    Add broken WIP run-cli tests for Blueprints v2

--- packages/playground/cli/tests/run-cli.spec.ts
+++ packages/playground/cli/tests/run-cli.spec.ts
@@ -18,292 +18,329 @@ import {
 import { createHash } from 'node:crypto';
 import { MinifiedWordPressVersionsList } from '@wp-playground/wordpress-builds';
 
-describe('run-cli', () => {
-	let cliServer: RunCLIServer;
+const blueprintVersions = [
+	{
+		version: 'v1',
+		suiteCliArgs: {},
+	},
+	{
+		version: 'v2',
+		suiteCliArgs: { 'experimental-blueprints-v2-runner': true },
+	},
+];
 
-	afterEach(async () => {
-		if (cliServer) {
-			await cliServer.server.close();
-		}
-	});
+describe.each(blueprintVersions)(
+	'run-cli with Blueprints $version',
+	({ suiteCliArgs }) => {
+		let cliServer: RunCLIServer;
 
-	test('should set PHP version', async () => {
-		cliServer = await runCLI({
-			command: 'server',
-			php: '8.0',
+		afterEach(async () => {
+			if (cliServer) {
+				await cliServer.server.close();
+			}
 		});
-		await cliServer.playground.writeFile(
-			'/wordpress/version.php',
-			'<?php echo phpversion(); ?>'
-		);
-		const response = await cliServer.playground.request({
-			url: '/version.php',
-			method: 'GET',
-		});
-		expect(response.httpStatusCode).toBe(200);
-		expect(response.text).toContain('8.0');
-	});
 
-	test('should set WordPress version', async () => {
-		const oldestSupportedVersion =
-			MinifiedWordPressVersionsList[
-				MinifiedWordPressVersionsList.length - 1
-			];
-		cliServer = await runCLI({
-			command: 'server',
-			wp: oldestSupportedVersion,
+		test('should set PHP version', async () => {
+			cliServer = await runCLI({
+				...suiteCliArgs,
+				command: 'server',
+				php: '8.0',
+			});
+			await cliServer.playground.writeFile(
+				'/wordpress/version.php',
+				'<?php echo phpversion(); ?>'
+			);
+			const response = await cliServer.playground.request({
+				url: '/version.php',
+				method: 'GET',
+			});
+			expect(response.httpStatusCode).toBe(200);
+			expect(response.text).toContain('8.0');
 		});
-		await cliServer.playground.writeFile(
-			'/wordpress/version.php',
-			`<?php
+
+		test('should set WordPress version', async () => {
+			const oldestSupportedVersion =
+				MinifiedWordPressVersionsList[
+					MinifiedWordPressVersionsList.length - 1
+				];
+			cliServer = await runCLI({
+				...suiteCliArgs,
+				command: 'server',
+				wp: oldestSupportedVersion,
+			});
+			await cliServer.playground.writeFile(
+				'/wordpress/version.php',
+				`<?php
             require_once '/wordpress/wp-load.php';
             echo get_bloginfo("version");
             ?>`
-		);
-		const response = await cliServer.playground.request({
-			url: '/version.php',
-			method: 'GET',
+			);
+			const response = await cliServer.playground.request({
+				url: '/version.php',
+				method: 'GET',
+			});
+			expect(response.httpStatusCode).toBe(200);
+			expect(response.text).toContain(oldestSupportedVersion);
 		});
-		expect(response.httpStatusCode).toBe(200);
-		expect(response.text).toContain(oldestSupportedVersion);
-	});
 
-	test('should run blueprint', async () => {
-		cliServer = await runCLI({
-			command: 'server',
-			blueprint: {
-				steps: [
-					{
-						step: 'setSiteOptions',
-						options: {
-							blogname: 'My Blog Name',
-						},
-					},
-				],
-			},
-		});
-		const response = await cliServer.playground.request({
-			url: '/',
-			method: 'GET',
-		});
-		expect(response.httpStatusCode).toBe(200);
-		expect(response.text).toContain('<title>My Blog Name</title>');
-	});
-
-	test('should be able to follow external symlinks in primary and secondary PHP instances', async () => {
-		// TODO: Make sure test always uses a single worker.
-		// TODO: Is there a way to confirm we are testing use of a non-primary PHP instance?
-		const tmpDir = await mkdtemp(path.join(tmpdir(), 'playground-test-'));
-		writeFileSync(
-			path.join(tmpDir, 'sleep.php'),
-			'<?php sleep(1); echo "Slept"; '
-		);
-		const symlinkPath = path.join(
-			import.meta.dirname,
-			'mount-examples',
-			'symlinking',
-			'symlinked-script'
-		);
-
-		mkdirSync(path.dirname(symlinkPath), { recursive: true });
-
-		try {
-			if (existsSync(symlinkPath)) {
-				unlinkSync(symlinkPath);
-			}
-			// TODO: Confirm that symlink target is outside of current working dir tree.
-			symlinkSync(tmpDir, symlinkPath);
+		test('should run blueprint', async () => {
 			cliServer = await runCLI({
-				debug: true,
+				...suiteCliArgs,
 				command: 'server',
-				followSymlinks: true,
-				'mount-before-install': [
-					{
-						hostPath: symlinkPath,
-						vfsPath: '/wordpress/wp-content/test-script',
-					},
-				],
+				blueprint: {
+					steps: [
+						{
+							step: 'setSiteOptions',
+							options: {
+								blogname: 'My Blog Name',
+							},
+						},
+					],
+				},
 			});
-			expect(cliServer.workerThreadCount).toBe(1);
-			// Make multiple simultaneous requests to force the use of a secondary PHP instance.
-			// TODO: Find way to confirm this.
-			const responses = await Promise.all([
-				cliServer.playground.request({
-					url: '/wp-content/test-script/sleep.php',
-					method: 'GET',
-				}),
-				cliServer.playground.request({
-					url: '/wp-content/test-script/sleep.php',
-					method: 'GET',
-				}),
-				// Test a third request to hopefully test more than one secondary instance.
-				cliServer.playground.request({
-					url: '/wp-content/test-script/sleep.php',
-					method: 'GET',
-				}),
-			]);
-			responses.forEach((response) => {
-				expect(response.httpStatusCode).toBe(200);
-				expect(response.text).toContain('Slept');
+			const response = await cliServer.playground.request({
+				url: '/',
+				method: 'GET',
 			});
-		} finally {
-			if (existsSync(symlinkPath)) {
-				unlinkSync(symlinkPath);
-			}
-		}
-	});
+			expect(response.httpStatusCode).toBe(200);
+			expect(response.text).toContain('<title>My Blog Name</title>');
+		});
 
-	// @TODO: Also test with Blueprints v2.
-	describe('auto-mount', () => {
-		const getDirectoryChecksum = async (dir: string) => {
-			const hash = createHash('sha256');
-			for (const file of readdirSync(dir)) {
-				hash.update(file);
+		test('should be able to follow external symlinks in primary and secondary PHP instances', async () => {
+			// TODO: Make sure test always uses a single worker.
+			// TODO: Is there a way to confirm we are testing use of a non-primary PHP instance?
+			const tmpDir = await mkdtemp(
+				path.join(tmpdir(), 'playground-test-')
+			);
+			writeFileSync(
+				path.join(tmpDir, 'sleep.php'),
+				'<?php sleep(1); echo "Slept"; '
+			);
+			const symlinkPath = path.join(
+				import.meta.dirname,
+				'mount-examples',
+				'symlinking',
+				'symlinked-script'
+			);
+
+			mkdirSync(path.dirname(symlinkPath), { recursive: true });
+
+			try {
+				if (existsSync(symlinkPath)) {
+					unlinkSync(symlinkPath);
+				}
+				// TODO: Confirm that symlink target is outside of current working dir tree.
+				symlinkSync(tmpDir, symlinkPath);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					debug: true,
+					command: 'server',
+					followSymlinks: true,
+					'mount-before-install': [
+						{
+							hostPath: symlinkPath,
+							vfsPath: '/wordpress/wp-content/test-script',
+						},
+					],
+				});
+				expect(cliServer.workerThreadCount).toBe(1);
+				// Make multiple simultaneous requests to force the use of a secondary PHP instance.
+				// TODO: Find way to confirm this.
+				const responses = await Promise.all([
+					cliServer.playground.request({
+						url: '/wp-content/test-script/sleep.php',
+						method: 'GET',
+					}),
+					cliServer.playground.request({
+						url: '/wp-content/test-script/sleep.php',
+						method: 'GET',
+					}),
+					// Test a third request to hopefully test more than one secondary instance.
+					cliServer.playground.request({
+						url: '/wp-content/test-script/sleep.php',
+						method: 'GET',
+					}),
+				]);
+				responses.forEach((response) => {
+					expect(response.httpStatusCode).toBe(200);
+					expect(response.text).toContain('Slept');
+				});
+			} finally {
+				if (existsSync(symlinkPath)) {
+					unlinkSync(symlinkPath);
+				}
 			}
-			return hash.digest('hex');
-		};
-		const getActiveTheme = async () => {
-			const response = await cliServer.playground.run({
-				code: `<?php
+		});
+
+		// @TODO: Also test with Blueprints v2.
+		describe('auto-mount', () => {
+			const getDirectoryChecksum = async (dir: string) => {
+				const hash = createHash('sha256');
+				for (const file of readdirSync(dir)) {
+					hash.update(file);
+				}
+				return hash.digest('hex');
+			};
+			const getActiveTheme = async () => {
+				const response = await cliServer.playground.run({
+					code: `<?php
 					require_once '/wordpress/wp-load.php';
 					$theme = wp_get_theme();
 					echo $theme->get('Name');
 				?>`,
+				});
+				return response.text;
+			};
+			afterEach(() => {
+				if ((process.cwd as unknown as MockInstance).mockRestore) {
+					(process.cwd as unknown as MockInstance).mockRestore();
+				}
 			});
-			return response.text;
-		};
-		afterEach(() => {
-			if ((process.cwd as unknown as MockInstance).mockRestore) {
-				(process.cwd as unknown as MockInstance).mockRestore();
-			}
-		});
 
-		test(`should run a plugin project using --auto-mount`, async () => {
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(import.meta.dirname, 'mount-examples', 'plugin')
-			);
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-			const phpResponse = await cliServer.playground.run({
-				code: `<?php
+			test.only(`should run a plugin project using --auto-mount`, async () => {
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(import.meta.dirname, 'mount-examples', 'plugin')
+				);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+				const phpResponse = await cliServer.playground.run({
+					code: `<?php
 					require_once '/wordpress/wp-load.php';
 					require_once '/wordpress/wp-admin/includes/plugin.php';
 					echo is_plugin_active('plugin/sample-plugin.php') ? '1' : '0';
 				?>`,
-			});
-			expect(phpResponse.text).toBe('1');
+				});
+				expect(phpResponse.text).toBe('1');
 
-			const response = await cliServer.playground.request({
-				url: '/',
-				method: 'GET',
+				const response = await cliServer.playground.request({
+					url: '/',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+				expect(response.text).toContain(
+					'<title>My WordPress Website</title>'
+				);
 			});
-			expect(response.httpStatusCode).toBe(200);
-			expect(response.text).toContain(
-				'<title>My WordPress Website</title>'
-			);
+			test(`should run a theme project using --auto-mount`, async () => {
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(import.meta.dirname, 'mount-examples', 'theme')
+				);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+
+				expect(await getActiveTheme()).toBe('Yolo Theme');
+
+				const response = await cliServer.playground.request({
+					url: '/',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+				expect(response.text).toContain(
+					'<title>My WordPress Website</title>'
+				);
+			});
+
+			test(`should run a wp-content project using --auto-mount`, async () => {
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(
+						import.meta.dirname,
+						'mount-examples',
+						'wp-content'
+					)
+				);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+				const response = await cliServer.playground.request({
+					url: '/wp-login.php',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+			});
+
+			test('should run a static html project using --auto-mount', async () => {
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(
+						import.meta.dirname,
+						'mount-examples',
+						'static-html'
+					)
+				);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+				const response = await cliServer.playground.request({
+					url: '/',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+				expect(response.text).toContain('<title>Static HTML</title>');
+			});
+
+			test('should run a php project using --auto-mount', async () => {
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(import.meta.dirname, 'mount-examples', 'php')
+				);
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+				const response = await cliServer.playground.request({
+					url: '/',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+				expect(response.text).toContain('Hello world');
+			});
+
+			test('should run a wordpress project using --auto-mount', async () => {
+				const tmpDir = await mkdtemp(
+					path.join(tmpdir(), 'playground-test-')
+				);
+				vi.spyOn(process, 'cwd').mockReturnValue(
+					path.join(tmpDir, 'wordpress')
+				);
+
+				const zip = await fetch('https://wordpress.org/latest.zip');
+				const zipPath = path.join(tmpDir, 'wp.zip');
+				await writeFile(
+					zipPath,
+					new Uint8Array(await zip.arrayBuffer())
+				);
+				await promisify(exec)(`unzip "${zipPath}" -d "${tmpDir}"`);
+
+				const checksum = await getDirectoryChecksum(tmpDir);
+
+				cliServer = await runCLI({
+					...suiteCliArgs,
+					command: 'server',
+					autoMount: '',
+				});
+				const response = await cliServer.playground.request({
+					url: '/',
+					method: 'GET',
+				});
+				expect(response.httpStatusCode).toBe(200);
+				expect(response.text).toContain(
+					'<title>My WordPress Website</title>'
+				);
+
+				/**
+				 * Playground should not modify the mounted directory.
+				 */
+				expect(await getDirectoryChecksum(tmpDir)).toBe(checksum);
+			});
 		});
-		test(`should run a theme project using --auto-mount`, async () => {
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(import.meta.dirname, 'mount-examples', 'theme')
-			);
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-
-			expect(await getActiveTheme()).toBe('Yolo Theme');
-
-			const response = await cliServer.playground.request({
-				url: '/',
-				method: 'GET',
-			});
-			expect(response.httpStatusCode).toBe(200);
-			expect(response.text).toContain(
-				'<title>My WordPress Website</title>'
-			);
-		});
-
-		test(`should run a wp-content project using --auto-mount`, async () => {
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(import.meta.dirname, 'mount-examples', 'wp-content')
-			);
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-			const response = await cliServer.playground.request({
-				url: '/wp-login.php',
-				method: 'GET',
-			});
-			expect(response.httpStatusCode).toBe(200);
-		});
-
-		test('should run a static html project using --auto-mount', async () => {
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(import.meta.dirname, 'mount-examples', 'static-html')
-			);
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-			const response = await cliServer.playground.request({
-				url: '/',
-				method: 'GET',
-			});
-			expect(response.httpStatusCode).toBe(200);
-			expect(response.text).toContain('<title>Static HTML</title>');
-		});
-
-		test('should run a php project using --auto-mount', async () => {
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(import.meta.dirname, 'mount-examples', 'php')
-			);
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-			const response = await cliServer.playground.request({
-				url: '/',
-				method: 'GET',
-			});
-			expect(response.httpStatusCode).toBe(200);
-			expect(response.text).toContain('Hello world');
-		});
-
-		test('should run a wordpress project using --auto-mount', async () => {
-			const tmpDir = await mkdtemp(
-				path.join(tmpdir(), 'playground-test-')
-			);
-			vi.spyOn(process, 'cwd').mockReturnValue(
-				path.join(tmpDir, 'wordpress')
-			);
-
-			const zip = await fetch('https://wordpress.org/latest.zip');
-			const zipPath = path.join(tmpDir, 'wp.zip');
-			await writeFile(zipPath, new Uint8Array(await zip.arrayBuffer()));
-			await promisify(exec)(`unzip "${zipPath}" -d "${tmpDir}"`);
-
-			const checksum = await getDirectoryChecksum(tmpDir);
-
-			cliServer = await runCLI({
-				command: 'server',
-				autoMount: '',
-			});
-			const response = await cliServer.playground.request({
-				url: '/',
-				method: 'GET',
-			});
-			expect(response.httpStatusCode).toBe(200);
-			expect(response.text).toContain(
-				'<title>My WordPress Website</title>'
-			);
-
-			/**
-			 * Playground should not modify the mounted directory.
-			 */
-			expect(await getDirectoryChecksum(tmpDir)).toBe(checksum);
-		});
-	});
-});
+	}
+);
