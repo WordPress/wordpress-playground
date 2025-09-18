f057db1c4fa34d227ec6d34209c9874d7771b990
Adam Zieliński <adam@adamziel.com>
2025-09-18 23:08:50 +0200

    Restore files from trunk

--- packages/playground/blueprints/bin/generate-schema.js
+++ packages/playground/blueprints/bin/generate-schema.js
@@ -1,8 +1,8 @@
+import tsj from 'ts-json-schema-generator';
+import fs from 'fs';
 import Ajv from 'ajv';
 import ajvStandaloneCode from 'ajv/dist/standalone/index.js';
-import fs from 'fs';
 import prettier from 'prettier';
-import tsj from 'ts-json-schema-generator';
 
 /** @type {import('ts-json-schema-generator/dist/src/Config').Config} */
 const config = {
