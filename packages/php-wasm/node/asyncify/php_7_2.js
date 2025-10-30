2d214859e29af6f5280d4aa223b8384aeab62af1
Adam Zieliński <adam@adamziel.com>
2025-10-30 20:30:14 +0100

    Rebuild imagemagick with stable ReadImage function name – it used to be randomly suffixed

--- packages/php-wasm/node/asyncify/php_7_2.js
+++ packages/php-wasm/node/asyncify/php_7_2.js
@@ -8,7 +8,7 @@ import path from 'path';
 
 const dependencyFilename = path.join(__dirname, '7_2_34', 'php_7_2.wasm');
 export { dependencyFilename };
-export const dependenciesTotalSize = 27793495;
+export const dependenciesTotalSize = 27792017;
 const phpVersionString = '7.2.34';
 export function init(RuntimeName, PHPLoader) {
 	// The rest of the code comes from the built php.js file and esm-suffix.js
