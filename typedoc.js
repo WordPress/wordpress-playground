af028fff6a344bf4ed8665c17e25ab82778555de
Adam Zieliński <adam@adamziel.com>
2025-10-30 01:42:23 +0100

    Restore typedoc.js

---
+++ typedoc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	$schema: 'https://typedoc.org/schema.json',
+	entryPointStrategy: 'packages',
+	entryPoints: [
+		'./packages/php-wasm/web',
+		'./packages/php-wasm/node',
+		'./packages/php-wasm/progress',
+		'./packages/php-wasm/universal',
+		'./packages/php-wasm/util',
+		'./packages/playground/blueprints',
+		'./packages/playground/client',
+	],
+	intentionallyNotExported: ['WebClientMixin', 'PlaygroundWorkerEndpoint'],
+	excludeExternals: true,
+};
