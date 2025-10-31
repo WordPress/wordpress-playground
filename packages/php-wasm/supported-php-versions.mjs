2be6992e878c5d90a4f504c7159f4cc996ed085f
Adam Zieliński <adam@adamziel.com>
2025-10-31 01:00:50 +0100

    rebuild asyncify

--- packages/php-wasm/supported-php-versions.mjs
+++ packages/php-wasm/supported-php-versions.mjs
@@ -6,7 +6,7 @@
  * @property {string} lastRelease
  */
 
-export const lastRefreshed = '2025-10-29T22:33:25.532Z';
+export const lastRefreshed = '2025-10-30T23:23:11.157Z';
 
 /**
  * @type {PhpVersion[]}
