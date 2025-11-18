682202279e3fb0fc9d9c28179d8e3547c1177aac
Adam Zieliński <adam@adamziel.com>
2025-11-18 11:37:42 +0100

    Adjust unit test expression

--- packages/playground/cli/tests/run-cli.spec.ts
+++ packages/playground/cli/tests/run-cli.spec.ts
@@ -556,7 +556,7 @@ describe.each(blueprintVersions)(
 							'Starting a PHP server...',
 							'Starting up workers',
 							expect.stringMatching(
-								/^Resolved WordPress release URL: https:\/\/downloads\.w(ordpress)?\.org\/release\/wordpress-\d+\.\d+\.(?:RC|beta)]\d+\.zip$/
+								/^Resolved WordPress release URL: https:\/\/downloads\.w(ordpress)?\.org\/release\/wordpress-\d+\.\d+(?:\.\d+|-RC\d+|-beta\d+)?\.zip$/
 							),
 							'Fetching SQLite integration plugin...',
 							'Booting WordPress...',
