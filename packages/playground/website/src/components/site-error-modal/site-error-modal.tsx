084ecd51f424536144d42700a259d3541f2c3461
Adam Zieliński <adam@adamziel.com>
2025-11-19 11:35:08 +0100

    Remove unused React import

--- packages/playground/website/src/components/site-error-modal/site-error-modal.tsx
+++ packages/playground/website/src/components/site-error-modal/site-error-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 import { Button, TextareaControl } from '@wordpress/components';
 import { logger } from '@php-wasm/logger';
