ad77f6ec22a6b6d6ecf5d9a11d72c6f409bfa261
shimotmk <shimotmk1104@gmail.com>
2025-11-23 16:45:35 +0900

    update-website

--- packages/playground/website-deployment/apply-update.sh
+++ packages/playground/website-deployment/apply-update.sh
@@ -94,7 +94,9 @@ find -type f |
     set_aside_static_files_to_serve_via_php
 
 echo Syncing staged files to production
-rsync -av --delete --no-perms --omit-dir-times ~/website-update/ /srv/htdocs/
+# rsync -av --delete --no-perms --omit-dir-times ~/website-update/ /srv/htdocs/
+DEST_DIR="/home/shimotmk/public_html/playground.shimomuratomoki.com"
+rsync -av --delete --no-perms --omit-dir-times ~/website-update/ "${DEST_DIR}/"
 
 echo Purging edge cache
 curl -sS -X POST -H "Auth: $ATOMIC_SITE_API_KEY" "$SITE_API_BASE/edge-cache/$ATOMIC_SITE_ID/purge" \
