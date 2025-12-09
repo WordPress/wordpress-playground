120ae0791777176db7a3ceb3ca6c864c6c6bcd9e
= <yannick@capsules.codes>
2025-12-09 15:48:13 +0100

    Lint PHP.wasm Web vite config file

--- packages/php-wasm/web/vite.config.ts
+++ packages/php-wasm/web/vite.config.ts
@@ -14,91 +14,89 @@ import viteGlobalExtensions from '../../vite-extensions/vite-global-extensions';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { getExternalModules } from '../../vite-extensions/vite-external-modules';
 
-export default defineConfig(({}) => {
-	return {
-		cacheDir: '../../../node_modules/.vite/php-wasm',
+export default defineConfig({
+	cacheDir: '../../../node_modules/.vite/php-wasm',
 
-		plugins: [
-			viteTsConfigPaths({
-				root: '../../../',
-			}),
-			dts({
-				entryRoot: 'src',
-				tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
-				pathsToAliases: false,
-			}),
-			viteIgnoreImports({
-				extensions: ['wasm', 'so', 'dat'],
-			}),
-			vitePreserveLoadersImports([
-				{
-					regex: /php_\d_\d\.js$/,
-					/*
-					 * ../ lifts the file up to the dist entryRoot
-					 * web/src/lib/get-php-loader-module.ts > web/src/get-php-loader-module.ts
-					 *
-					 * slice(-3) strips the `public` directory from the path
-					 * web/public/php/jspi/php_8_4.js > ./web/php/jspi/php_8_4.js
-					 */
-					transform: (specifier) =>
-						`../${specifier.split('/').slice(-3).join('/')}`,
-				},
-				{
-					regex: /intl\.so$/,
-					/*
-					 * ../../../ lifts the file up to the dist entryRoot
-					 * web/src/lib/extensions/intl/get-intl-loader-module.ts > web/src/get-intl-loader-module.ts
-					 *
-					 * slice(-6) strips the `public` directory from the path
-					 * web/public/php/jspi/extensions/8_4/intl/intl.so > ./web/php/jspi/extensions/8_4/intl/intl.so
-					 */
-					transform: (specifier) =>
-						`../../../${specifier.split('/').slice(-6).join('/')}`,
-				},
-				{
-					regex: /icu\.dat$/,
-					/*
-					 * ../../../ lifts the file up to the dist entryRoot
-					 * web/src/lib/extensions/intl/with-intl.ts > web/src/with-intl.ts
-					 *
-					 * slice(-2) strips the `public` directory from the path
-					 * web/public/shared/icu.dat > ./web/shared/icu.dat
-					 */
-					transform: (specifier) =>
-						`../../../${specifier.split('/').slice(-2).join('/')}`,
-				},
-			]),
-			...viteGlobalExtensions,
-		],
-
-		// Configuration for building your library.
-		// See: https://vitejs.dev/guide/build.html#library-mode
-		build: {
-			lib: {
-				// Could also be a dictionary or array of multiple entry points.
-				entry: 'src/index.ts',
-				name: 'php-wasm-web',
-				fileName: 'index',
-				formats: ['es', 'cjs'],
+	plugins: [
+		viteTsConfigPaths({
+			root: '../../../',
+		}),
+		dts({
+			entryRoot: 'src',
+			tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+			pathsToAliases: false,
+		}),
+		viteIgnoreImports({
+			extensions: ['wasm', 'so', 'dat'],
+		}),
+		vitePreserveLoadersImports([
+			{
+				regex: /php_\d_\d\.js$/,
+				/*
+				 * ../ lifts the import located file up to the dist entryRoot
+				 * web/src/lib/get-php-loader-module.ts > web/src/get-php-loader-module.ts
+				 *
+				 * slice(-3) strips the `public` directory from the path
+				 * web/public/php/jspi/php_8_4.js > ./web/php/jspi/php_8_4.js
+				 */
+				transform: (specifier) =>
+					`../${specifier.split('/').slice(-3).join('/')}`,
 			},
-			sourcemap: true,
-			rollupOptions: {
-				// Don't bundle the PHP loaders in the final build. See
-				// the preserve-php-loaders-imports plugin above.
-				external: [
-					/php_\d_\d.js$/,
-					/icu.dat$/,
-					/intl.so$/,
-					...getExternalModules(),
-				],
+			{
+				regex: /intl\.so$/,
+				/*
+				 * ../../../ lifts the import located file to the dist entryRoot
+				 * web/src/lib/extensions/intl/get-intl-loader-module.ts > web/src/get-intl-loader-module.ts
+				 *
+				 * slice(-6) strips the `public` directory from the path
+				 * web/public/php/jspi/extensions/8_4/intl/intl.so > ./web/php/jspi/extensions/8_4/intl/intl.so
+				 */
+				transform: (specifier) =>
+					`../../../${specifier.split('/').slice(-6).join('/')}`,
 			},
-		},
+			{
+				regex: /icu\.dat$/,
+				/*
+				 * ../../../ lifts the import located file to the dist entryRoot
+				 * web/src/lib/extensions/intl/with-intl.ts > web/src/with-intl.ts
+				 *
+				 * slice(-2) strips the `public` directory from the path
+				 * web/public/shared/icu.dat > ./web/shared/icu.dat
+				 */
+				transform: (specifier) =>
+					`../../../${specifier.split('/').slice(-2).join('/')}`,
+			},
+		]),
+		...viteGlobalExtensions,
+	],
 
-		// TODO : move Vitest tests to Playwright tests inside test directory
-		test: {
-			globals: true,
-			environment: 'node',
-			reporters: ['default'],
+	// Configuration for building your library.
+	// See: https://vitejs.dev/guide/build.html#library-mode
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points.
+			entry: 'src/index.ts',
+			name: 'php-wasm-web',
+			fileName: 'index',
+			formats: ['es', 'cjs'],
 		},
-	};
+		sourcemap: true,
+		rollupOptions: {
+			// Don't bundle the PHP loaders in the final build. See
+			// the preserve-php-loaders-imports plugin above.
+			external: [
+				/php_\d_\d.js$/,
+				/icu.dat$/,
+				/intl.so$/,
+				...getExternalModules(),
+			],
+		},
+	},
+
+	// TODO : move Vitest tests to Playwright tests inside test directory
+	test: {
+		globals: true,
+		environment: 'node',
+		reporters: ['default'],
+	},
 });
