7c225026ee3b522b47da16baf0b8d450991986e8
Brandon Payton <brandon@happycode.net>
2025-12-10 12:27:31 -0500

    Update fs-ext-extra-prebuilt

--- packages/php-wasm/node/src/lib/file-locking/file-lock-manager-for-windows.ts
+++ packages/php-wasm/node/src/lib/file-locking/file-lock-manager-for-windows.ts
@@ -39,8 +39,9 @@ export class FileLockManagerForWindows implements FileLockManager {
 				lengthLow,
 				lengthHigh
 			);
+			const success = result === 0;
 
-			if (result) {
+			if (success) {
 				this.wholeFileLockMap.get(path)?.get(op.pid)?.delete(op.fd);
 				if (this.wholeFileLockMap.get(path)?.get(op.pid)?.size === 0) {
 					this.wholeFileLockMap.get(path)?.delete(op.pid);
@@ -51,131 +52,134 @@ export class FileLockManagerForWindows implements FileLockManager {
 			}
 
 			// TODO: Else if unlock failed in Windows, probably log an error.
-			return result;
-		} else {
-			const preexistingLock = this.wholeFileLockMap
-				.get(path)
-				?.get(op.pid)
-				?.get(op.fd);
-			if (op.type === preexistingLock?.type) {
-				// There is nothing to do.
-				return true;
-			}
+			return success;
+		}
 
-			let flags = 0;
-			if (!op.waitForLock) {
-				flags |= constants.LOCKFILE_FAIL_IMMEDIATELY;
-			}
+		const preexistingLock = this.wholeFileLockMap
+			.get(path)
+			?.get(op.pid)
+			?.get(op.fd);
+		if (op.type === preexistingLock?.type) {
+			// There is nothing to do.
+			return true;
+		}
 
-			let lockResult;
-			if (op.type === 'shared') {
-				/**
-				 * Since we are requesting a shared lock, we can obtain it first
-				 * even if we already hold the exclusive lock.
-				 *
-				 * "Shared locks can overlap a locked region provided locks held
-				 * on that region are shared locks. A shared lock can overlap an
-				 * exclusive lock if both locks were created using the same file handle."
-				 * @see https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-lockfileex
-				 */
-				lockResult = lockFileExSync(
+		let flags = 0;
+		if (!op.waitForLock) {
+			flags |= constants.LOCKFILE_FAIL_IMMEDIATELY;
+		}
+
+		let success;
+		if (op.type === 'shared') {
+			// TODO: Do we need to do anything to check for ERROR_IO_PENDING errors?
+			/**
+			 * Since we are requesting a shared lock, we can obtain it first
+			 * even if we already hold the exclusive lock.
+			 *
+			 * "Shared locks can overlap a locked region provided locks held
+			 * on that region are shared locks. A shared lock can overlap an
+			 * exclusive lock if both locks were created using the same file handle."
+			 * @see https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-lockfileex
+			 */
+			const lockResult = lockFileExSync(
+				op.fd,
+				flags,
+				offsetLow,
+				offsetHigh,
+				lengthLow,
+				lengthHigh
+			);
+			success = lockResult === 0;
+
+			if (lockResult && preexistingLock?.type === 'exclusive') {
+				const exclusiveUnlockResult = unlockFileExSync(
 					op.fd,
-					flags,
 					offsetLow,
 					offsetHigh,
 					lengthLow,
 					lengthHigh
 				);
 
-				if (lockResult && preexistingLock?.type === 'exclusive') {
-					const exclusiveUnlockResult = unlockFileExSync(
-						op.fd,
-						offsetLow,
-						offsetHigh,
-						lengthLow,
-						lengthHigh
-					);
-
-					if (!exclusiveUnlockResult) {
-						// This should never happen. Log and throw an error.
-						const message =
-							'Failed to unlock preexisting exclusive lock after failing to obtain shared lock';
-						logger.error(message);
-						throw new Error(message);
-					}
+				if (exclusiveUnlockResult === 0) {
+					// This should never happen. Log and throw an error.
+					const message =
+						'Failed to unlock preexisting exclusive lock after failing to obtain shared lock';
+					logger.error(message);
+					throw new Error(message);
 				}
 			}
+		} else if (op.type === 'exclusive') {
+			flags |= constants.LOCKFILE_EXCLUSIVE_LOCK;
 
-			if (op.type === 'exclusive') {
-				flags |= constants.LOCKFILE_EXCLUSIVE_LOCK;
-
-				let sharedUnlockResult;
-				if (preexistingLock?.type === 'shared') {
-					sharedUnlockResult = unlockFileExSync(
-						op.fd,
-						offsetLow,
-						offsetHigh,
-						lengthLow,
-						lengthHigh
-					);
-					// TODO: Log if there's an error
-				}
-
-				lockResult = lockFileExSync(
+			let sharedUnlockResult;
+			if (preexistingLock?.type === 'shared') {
+				sharedUnlockResult = unlockFileExSync(
 					op.fd,
-					flags,
 					offsetLow,
 					offsetHigh,
 					lengthLow,
 					lengthHigh
 				);
 				// TODO: Log if there's an error
-
-				if (!lockResult && sharedUnlockResult) {
-					/*
-					 * We failed to obtain the exclusive lock but already
-					 * dropped the shared lock because preexisting shared locks
-					 * will block the exclusive lock.
-					 *
-					 * "If an exclusive lock is requested for a range of a file that
-					 * already has a shared or exclusive lock, the function returns
-					 * the error ERROR_IO_PENDING."
-					 * @see https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-lockfileex
-					 */
-					const sharedReLockResult = lockFileExSync(
-						op.fd,
-						// Wait to restore the shared lock.
-						0,
-						offsetLow,
-						offsetHigh,
-						lengthLow,
-						lengthHigh
-					);
-
-					if (!sharedReLockResult) {
-						// This should never happen. Log and throw an error.
-						const message =
-							'Failed to re-lock preexisting shared lock after failing to obtain exclusive lock';
-						logger.error(message);
-						throw new Error(message);
-					}
-				}
 			}
 
-			if (lockResult) {
-				if (!this.wholeFileLockMap.has(path)) {
-					this.wholeFileLockMap.set(path, new Map());
-				}
-				const pidMap = this.wholeFileLockMap.get(path)!;
-				if (!pidMap.has(op.pid)) {
-					pidMap.set(op.pid, new Map());
-				}
-				const pathMap = pidMap.get(op.pid)!;
-				pathMap.set(op.fd, op);
-			}
+			const lockResult = lockFileExSync(
+				op.fd,
+				flags,
+				offsetLow,
+				offsetHigh,
+				lengthLow,
+				lengthHigh
+			);
+			success = lockResult === 0;
+			// TODO: Log if there's an error
 
-			return !!lockResult;
+			if (lockResult !== 0 && sharedUnlockResult === 0) {
+				/*
+				 * We failed to obtain the exclusive lock but already
+				 * dropped the shared lock because preexisting shared locks
+				 * will block the exclusive lock.
+				 *
+				 * "If an exclusive lock is requested for a range of a file that
+				 * already has a shared or exclusive lock, the function returns
+				 * the error ERROR_IO_PENDING."
+				 * @see https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-lockfileex
+				 */
+				const sharedReLockResult = lockFileExSync(
+					op.fd,
+					// Wait to restore the shared lock.
+					0,
+					offsetLow,
+					offsetHigh,
+					lengthLow,
+					lengthHigh
+				);
+
+				if (sharedReLockResult !== 0) {
+					// This should never happen. Log and throw an error.
+					const message =
+						'Failed to re-lock preexisting shared lock after failing to obtain exclusive lock';
+					logger.error(message);
+					throw new Error(message);
+				}
+			}
+		} else {
+			throw new Error(`Unexpected wholeFileLock() op: '${op.type}'`);
 		}
+
+		if (success) {
+			if (!this.wholeFileLockMap.has(path)) {
+				this.wholeFileLockMap.set(path, new Map());
+			}
+			const pidMap = this.wholeFileLockMap.get(path)!;
+			if (!pidMap.has(op.pid)) {
+				pidMap.set(op.pid, new Map());
+			}
+			const pathMap = pidMap.get(op.pid)!;
+			pathMap.set(op.fd, op);
+		}
+
+		return success;
 	}
 
 	lockFileByteRange(
