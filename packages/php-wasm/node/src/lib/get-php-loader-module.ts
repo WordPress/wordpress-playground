e985e2117260416caeeda299e0048b1bc5928a49
Bero <berislav.grgicak@gmail.com>
2026-01-08 10:52:54 +0100

    Use require in getPHPLoaderModule when used in CommonJS

--- packages/php-wasm/node/src/lib/get-php-loader-module.ts
+++ packages/php-wasm/node/src/lib/get-php-loader-module.ts
@@ -10,12 +10,53 @@ import type { PHPLoaderModule, SupportedPHPVersion } from '@php-wasm/universal';
  * - @php-wasm/node-8-3
  * - etc.
  *
+ * In CommonJS environments (like Jest), we use require() to avoid dynamic import issues.
+ * In ESM environments, we use dynamic import().
+ *
  * @param version The PHP version to load.
  * @returns The PHP loader module.
  */
 export async function getPHPLoaderModule(
 	version: SupportedPHPVersion = LatestSupportedPHPVersion
 ): Promise<PHPLoaderModule> {
+	// CommonJS: require() avoids dynamic import() issues without --experimental-vm-modules
+	if (
+		typeof module !== 'undefined' &&
+		typeof module.exports !== 'undefined'
+	) {
+		switch (version) {
+			case '8.5':
+				// @ts-ignore
+				return require('@php-wasm/node-8-5').getPHPLoaderModule();
+			case '8.4':
+				// @ts-ignore
+				return require('@php-wasm/node-8-4').getPHPLoaderModule();
+			case '8.3':
+				// @ts-ignore
+				return require('@php-wasm/node-8-3').getPHPLoaderModule();
+			case '8.2':
+				// @ts-ignore
+				return require('@php-wasm/node-8-2').getPHPLoaderModule();
+			case '8.1':
+				// @ts-ignore
+				return require('@php-wasm/node-8-1').getPHPLoaderModule();
+			case '8.0':
+				// @ts-ignore
+				return require('@php-wasm/node-8-0').getPHPLoaderModule();
+			case '7.4':
+				// @ts-ignore
+				return require('@php-wasm/node-7-4').getPHPLoaderModule();
+			case '7.3':
+				// @ts-ignore
+				return require('@php-wasm/node-7-3').getPHPLoaderModule();
+			case '7.2':
+				// @ts-ignore
+				return require('@php-wasm/node-7-2').getPHPLoaderModule();
+		}
+		throw new Error(`Unsupported PHP version ${version}`);
+	}
+
+	// In ESM, use dynamic import
 	switch (version) {
 		case '8.5':
 			// @ts-ignore
