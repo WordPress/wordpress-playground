01b99688f3fe201aa8a0e5e230ffe057ec9fcd71
Adam Zieliński <adam@adamziel.com>
2026-02-11 11:24:32 +0100

    Remove useless comments in breakpoints.ts

--- packages/playground/website/src/lib/constants/breakpoints.ts
+++ packages/playground/website/src/lib/constants/breakpoints.ts
@@ -3,10 +3,7 @@
  * Use these inline, e.g. `window.innerWidth < BREAKPOINTS.tablet`.
  */
 export const BREAKPOINTS = {
-	/** Phones: < 600px */
 	mobile: 600,
-	/** Tablets: >= 600px and < 875px */
 	tablet: 875,
-	/** Desktops: >= 1024px */
 	desktop: 1024,
 } as const;
