b020e48ddac5e45fefd5d9f0c1040109e1ece642
mho22 <yannick@capsules.codes>
2026-02-25 12:39:20 +0100

    Recompile PHP.wasm Node JSPI

--- packages/php-wasm/node-builds/8-4/jspi/php_8_4.js
+++ packages/php-wasm/node-builds/8-4/jspi/php_8_4.js
@@ -15,7 +15,7 @@ const currentDirPath =
 		: path.dirname(fileURLToPath(import.meta.url));
 const dependencyFilename = path.join(currentDirPath, '8_4_18', 'php_8_4.wasm');
 export { dependencyFilename }; 
-export const dependenciesTotalSize = 24730207; 
+export const dependenciesTotalSize = 24730206; 
 const phpVersionString = '8.4.18';
 export function init(RuntimeName, PHPLoader) {
     // The rest of the code comes from the built php.js file and esm-suffix.js
