ca371ce60869a5c53711d6d133c4d8d478265450
Jan Jakeš <jan@jakes.pro>
2026-02-27 11:27:57 +0100

    Always load JSPI binary in web-builds, drop Asyncify fallback
    The JSPI polyfill handles non-JSPI browsers via sync XHR, so
    Asyncify-compiled binaries are no longer needed as a fallback.
    Remove the jspi() feature detection and always import from the
    jspi/ directory.

--- packages/php-wasm/web-builds/8-2/src/index.ts
+++ packages/php-wasm/web-builds/8-2/src/index.ts
@@ -1,25 +1,11 @@
 import type { PHPLoaderModule } from '@php-wasm/universal';
-import { jspi } from 'wasm-feature-detect';
 
 export async function getPHPLoaderModule(): Promise<PHPLoaderModule> {
-	if (await jspi()) {
-		// @ts-ignore
-		return await import('../jspi/php_8_2.js');
-	} else {
-		// @ts-ignore
-		return await import('../asyncify/php_8_2.js');
-	}
+	// @ts-ignore
+	return await import('../jspi/php_8_2.js');
 }
 
 export async function getIntlExtensionPath(): Promise<string> {
-	if (await jspi()) {
-		// @ts-ignore
-		return (await import('../jspi/extensions/intl/intl.so?url')).default;
-	} else {
-		// @ts-ignore
-		return (await import('../asyncify/extensions/intl/intl.so?url'))
-			.default;
-	}
+	// @ts-ignore
+	return (await import('../jspi/extensions/intl/intl.so?url')).default;
 }
-
-export { jspi };
