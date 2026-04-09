f9ae9ff1b9ce1d57eabb7e22ca15180aa7fece54
mho22 <yannick@capsules.codes>
2026-04-09 15:20:26 +0200

    Remove php-wasm/node-polyfills from php-wasm packages

--- packages/php-wasm/stream-compression/src/index.ts
+++ packages/php-wasm/stream-compression/src/index.ts
@@ -1,5 +1,3 @@
-import '@php-wasm/node-polyfills';
-
 export { collectBytes } from './utils/collect-bytes';
 export { collectFile } from './utils/collect-file';
 export { iteratorToStream } from './utils/iterator-to-stream';
