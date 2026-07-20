699d8e107688f43edf90fe27554d41b12da77e3a
Adam Zieliński <adam@adamziel.com>
2026-07-20 20:29:56 +0200

    [Website] Log OPFS write worker failures

--- packages/playground/website/src/lib/state/opfs/opfs-site-storage-worker-for-safari.ts
+++ packages/playground/website/src/lib/state/opfs/opfs-site-storage-worker-for-safari.ts
@@ -6,34 +6,44 @@
  *
  * This worker exists so non-worker threads can trigger writing to OPFS files.
  */
+import { logger } from '@php-wasm/logger';
+
 onmessage = async function (event: MessageEvent) {
 	const filePath: string = event.data.path;
 	const content: string = event.data.content;
 	const responsePort = event.ports[0];
 
-	const pathParts = filePath.split('/').filter((p) => p.length > 0);
-
-	const fileName = pathParts.pop();
-	if (fileName === undefined) {
-		throw new Error(`Invalid path: '${filePath}'`);
-	}
-
-	let parentDirHandle = await navigator.storage.getDirectory();
-	for (const part of pathParts) {
-		parentDirHandle = await parentDirHandle.getDirectoryHandle(part);
-	}
-
-	const fileHandle = await parentDirHandle.getFileHandle(fileName, {
-		create: true,
-	});
-
-	const syncAccessHandle = await fileHandle.createSyncAccessHandle();
 	try {
-		const encodedContent = new TextEncoder().encode(content);
-		syncAccessHandle.truncate(0);
-		syncAccessHandle.write(encodedContent);
-		responsePort.postMessage('done');
-	} finally {
-		syncAccessHandle.close();
+		const pathParts = filePath.split('/').filter((p) => p.length > 0);
+
+		const fileName = pathParts.pop();
+		if (fileName === undefined) {
+			throw new Error(`Invalid path: '${filePath}'`);
+		}
+
+		let parentDirHandle = await navigator.storage.getDirectory();
+		for (const part of pathParts) {
+			parentDirHandle = await parentDirHandle.getDirectoryHandle(part);
+		}
+
+		const fileHandle = await parentDirHandle.getFileHandle(fileName, {
+			create: true,
+		});
+
+		const syncAccessHandle = await fileHandle.createSyncAccessHandle();
+		try {
+			const encodedContent = new TextEncoder().encode(content);
+			syncAccessHandle.truncate(0);
+			syncAccessHandle.write(encodedContent);
+			responsePort.postMessage('done');
+		} finally {
+			syncAccessHandle.close();
+		}
+	} catch (error) {
+		logger.error('Error in OPFS write worker.', {
+			path: filePath,
+			error,
+		});
+		throw error;
 	}
 };
