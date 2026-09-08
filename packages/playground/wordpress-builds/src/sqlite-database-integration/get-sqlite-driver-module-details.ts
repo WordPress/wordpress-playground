314b8a160adf68623106dc78a781d9b1f169395b
deployment_bot <deployment_bot@users.noreply.github.com>
2026-09-08 09:15:27 +0000

    Refresh SQLite integration plugin

--- packages/playground/wordpress-builds/src/sqlite-database-integration/get-sqlite-driver-module-details.ts
+++ packages/playground/wordpress-builds/src/sqlite-database-integration/get-sqlite-driver-module-details.ts
@@ -25,7 +25,7 @@ export function getSqliteDriverModuleDetails(
 		case 'trunk':
 			/** @ts-ignore */
 			return {
-				size: 230104,
+				size: 230740,
 				url: url_trunk,
 			};
 		case 'v2.1.16':
