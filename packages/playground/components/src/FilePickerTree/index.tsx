41a4f5dc57a2490605dcb0556954ae7718cff0cc
Alex Kirk <akirk@users.noreply.github.com>
2026-09-21 15:55:49 +0200

    Expand newly mounted Git directories

--- packages/playground/components/src/FilePickerTree/index.tsx
+++ packages/playground/components/src/FilePickerTree/index.tsx
@@ -550,7 +550,7 @@ export const FilePickerTree = forwardRef<
 			refresh: async (path: string) => await refreshChildren(path),
 			revealPath: async (path: string) => {
 				await refreshChildren(dirname(path));
-				await expandToPath(dirname(path));
+				await expandToPath(path);
 				selectPath(path, false);
 				setFocusedPath(path);
 				setTimeout(() => focusDomNode(path), 0);
